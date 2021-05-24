424c9c56db9d6dd166fce452d7118ecb2916939f
Neven <piaf1986@gmail.com>
2021-05-24 18:13:48 +0200

    Fixes too late loading of react-native-crypto lib

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
  * @format
  */
 
+require('node-libs-react-native/globals');
+require('react-native-crypto');
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-require('node-libs-react-native/globals');
 
 AppRegistry.registerComponent(appName, () => App);
