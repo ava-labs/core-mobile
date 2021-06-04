763014a821b18c03bc907dd4a811fca092cd2d57
Neven <piaf1986@gmail.com>
2021-06-04 12:15:58 +0200

    Add Login screen
    Extracted logic from AppViewModel to MainViewViewModel.
    AppViewModel now holds wallet instance and is shared via App.tsx
    to containing children views and their view models.

--- App.tsx
+++ App.tsx
@@ -6,38 +6,19 @@
  */
 
 import React, {Component} from "react"
-import {
-  Alert,
-  Appearance,
-  Button,
-  SafeAreaView,
-  SectionList,
-  StatusBar,
-  StyleSheet,
-  Text,
-} from "react-native"
-import Header from "./src/mainView/Header"
-import AppViewModel from "./src/AppViewModel"
-import Clock from "./src/mainView/Clock"
-import {Colors} from "react-native/Libraries/NewAppScreen"
-import SendAvaxModal from "./src/mainView/SendAvaxModal"
+import {Appearance, SafeAreaView, StatusBar, StyleSheet,} from "react-native"
+import AppViewModel, {SelectedView} from "./src/AppViewModel"
 import CommonViewModel from "./src/CommonViewModel"
+import Login from "./src/login/Login"
+import MainView from "./src/mainView/MainView"
+import {MnemonicWallet} from "./wallet_sdk"
 
 type AppProps = {}
 type AppState = {
-  avaxPrice: number
   backgroundStyle: any
-  mnemonic: string
-  walletCAddress: string
-  walletEvmAddress: string
   isDarkMode: boolean
-  externalAddressX: string
-  externalAddressP: string
-  addressC: string
-  availableX: string
-  availableP: string
-  availableC: string
-  sendAvaxVisible: boolean
+  selectedView: SelectedView
+  wallet: MnemonicWallet | null
 }
 
 class App extends Component<AppProps, AppState> {
@@ -47,29 +28,17 @@ class App extends Component<AppProps, AppState> {
   constructor(props: AppProps | Readonly<AppProps>) {
     super(props)
     this.state = {
-      avaxPrice: 0,
       backgroundStyle: {},
-      mnemonic: "",
-      walletCAddress: "",
-      walletEvmAddress: "",
       isDarkMode: false,
-      externalAddressX: "",
-      externalAddressP: "",
-      addressC: "",
-      availableX: "",
-      availableP: "",
-      availableC: "",
-      sendAvaxVisible: false,
+      selectedView: SelectedView.Login,
+      wallet: null
     }
   }
 
   componentWillUnmount() {
-    console.log("componentWillUnmount")
   }
 
   componentDidMount() {
-    console.log("componentDidMount")
-
     this.commonViewModel.isDarkMode.subscribe(value => {
       this.setState({isDarkMode: value})
     })
@@ -78,167 +47,55 @@ class App extends Component<AppProps, AppState> {
     })
 
     this.viewModel.onComponentMount()
-    this.viewModel.avaxPrice.subscribe(value => {
-      this.setState({avaxPrice: value})
+
+    this.viewModel.selectedView.subscribe(value => {
+      this.setState({selectedView: value})
     })
-    this.setState({mnemonic: this.viewModel.mnemonic})
-    this.viewModel.walletCAddress.subscribe(value => {
-      this.setState({walletCAddress: value})
-    })
-    this.viewModel.walletEvmAddrBech.subscribe(value => {
-      this.setState({walletEvmAddress: value})
-    })
-    this.viewModel.externalAddressesX.subscribe(value => {
-      if (value.length != 0) {
-        this.setState({externalAddressX: value[0]})
-      }
-    })
-    this.viewModel.externalAddressesP.subscribe(value => {
-      if (value.length != 0) {
-        this.setState({externalAddressP: value[0]})
-      }
-    })
-    this.viewModel.addressC.subscribe(value => {
-      this.setState({addressC: value})
-    })
-    this.viewModel.availableX.subscribe(value => {
-      this.setState({availableX: value})
-    })
-    this.viewModel.availableP.subscribe(value => {
-      this.setState({availableP: value})
-    })
-    this.viewModel.availableC.subscribe(value => {
-      this.setState({availableC: value})
+
+    this.viewModel.wallet.subscribe(value => {
+      this.setState({wallet: value})
     })
   }
 
-  private onResetHdIndices(): void {
-    this.viewModel.onResetHdIndices()
-      .subscribe({
-        next: value => console.log(value),
-        error: err => console.error(err),
-        complete: () => {
-        },
-      })
+  onEnterWallet(mnemonic: string): void {
+    this.viewModel.onEnterWallet(mnemonic)
   }
 
-  private onSend(addressX: string, amount: string): void {
-    this.viewModel.onSendAvaxX(addressX, amount)
-      .subscribe({
-        next: txHash => {
-          Alert.alert("Success", "Created transaction: " + txHash)
-        },
-        error: err => Alert.alert("Error", err.message),
-        complete: () => {
-        },
-      })
+  getSelectedView(): Element {
+    switch (this.state.selectedView) {
+      case SelectedView.Login:
+        return <Login onEnterWallet={mnemonic => this.onEnterWallet(mnemonic)}/>
+      case SelectedView.Main:
+        if (this.state.wallet === null) throw Error("Wallet not defined")
+        return <MainView wallet={this.state.wallet} onLogout={() => this.viewModel.onLogout()}/>
+      default:
+        throw Error("invalid state")
+    }
   }
 
   render() {
-    console.log("render")
-    const sectionListData = [
-      {
-        title: "Avax Price",
-        data: ["$" + this.state.avaxPrice],
-      },
-      {
-        title: "Mnemonic",
-        data: [this.state.mnemonic],
-      },
-      {
-        title: "External addresses X",
-        data: [this.state.externalAddressX],
-      },
-      {
-        title: "External addresses P",
-        data: [this.state.externalAddressP],
-      },
-      {
-        title: "External addresses C",
-        data: [this.state.addressC],
-      },
-      {
-        title: "Available (X)",
-        data: [this.state.availableX],
-      },
-      {
-        title: "Available (P)",
-        data: [this.state.availableP],
-      },
-      {
-        title: "Available (C)",
-        data: [this.state.availableC],
-      },
-    ]
     return (
       <SafeAreaView style={this.state.backgroundStyle}>
         <StatusBar
           barStyle={this.state.isDarkMode ? "light-content" : "dark-content"}
         />
-        <Clock/>
-        <Header/>
-        <SectionList
-          sections={sectionListData}
-          renderItem={({item}) => (
-            <Text
-              style={[
-                styles.item,
-                {color: this.state.isDarkMode ? Colors.light : Colors.dark},
-              ]}>
-              {item}
-            </Text>
-          )}
-          renderSectionHeader={({section}) => (
-            <Text style={styles.sectionHeader}>{section.title}</Text>
-          )}
-          keyExtractor={(item, index) => index.toString()}
-        />
-        <Button
-          title={"Reset Hd indices"}
-          onPress={() => this.onResetHdIndices()}
-        />
-        <SendAvaxModal
-          visible={this.state.sendAvaxVisible}
-          onClose={() => {
-            this.setState({
-              sendAvaxVisible: false,
-            })
-          }}
-          onSend={(addressX, amount) => {
-            this.onSend(addressX, amount)
-          }}
-        />
-        <Button
-          title={"Send AVAX X"}
-          onPress={() => {
-            this.setState({
-              sendAvaxVisible: true,
-            })
-          }}
-        />
+        {this.getSelectedView()}
       </SafeAreaView>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22,
+  text: {
+    fontSize: 26,
+    fontWeight: "700",
+    textAlign: "center",
+    marginTop: 26
   },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: "bold",
-    backgroundColor: "rgba(247,247,247,1.0)",
-  },
-  item: {
+  input: {
     padding: 10,
-    fontSize: 18,
-    height: 44,
+    margin: 12,
+    borderWidth: 1,
   },
 })
 export default App
---
+++ src/mainView/MainView.tsx
@@ -0,0 +1,229 @@
+import React, {Component} from "react"
+import {Alert, Appearance, Button, SectionList, StyleSheet, Text, View} from "react-native"
+import {Colors} from "react-native/Libraries/NewAppScreen"
+import CommonViewModel from "../CommonViewModel"
+import Clock from "./Clock";
+import Header from "./Header";
+import MainViewViewModel from "./MainViewViewModel";
+import SendAvaxModal from "./SendAvaxModal";
+import {MnemonicWallet} from "../../wallet_sdk";
+
+type MainViewProps = {
+  wallet: MnemonicWallet,
+  onLogout: () => void,
+}
+type MainViewState = {
+  isDarkMode: boolean
+  backgroundStyle: any
+  avaxPrice: number
+  externalAddressX: string
+  externalAddressP: string
+  addressC: string
+  availableX: string
+  availableP: string
+  availableC: string
+  sendAvaxVisible: boolean
+  walletCAddress: string
+  walletEvmAddress: string
+}
+
+class MainView extends Component<MainViewProps, MainViewState> {
+  viewModel!: MainViewViewModel
+  commonViewModel: CommonViewModel = new CommonViewModel(Appearance.getColorScheme() as string)
+
+  constructor(props: MainViewProps | Readonly<MainViewProps>) {
+    super(props)
+    this.state = {
+      isDarkMode: false,
+      backgroundStyle: {},
+      avaxPrice: 0,
+      externalAddressX: "",
+      externalAddressP: "",
+      addressC: "",
+      availableX: "",
+      availableP: "",
+      availableC: "",
+      sendAvaxVisible: false,
+      walletCAddress: "",
+      walletEvmAddress: "",
+    }
+    this.viewModel = new MainViewViewModel(props.wallet)
+  }
+
+  componentDidMount(): void {
+    this.viewModel.onComponentMount()
+
+    this.commonViewModel.isDarkMode.subscribe(value => {
+      this.setState({isDarkMode: value})
+    })
+    this.commonViewModel.backgroundStyle.subscribe(value => {
+      this.setState({backgroundStyle: value})
+    })
+    this.viewModel.avaxPrice.subscribe(value => {
+      this.setState({avaxPrice: value})
+    })
+    this.viewModel.walletCAddress.subscribe(value => {
+      this.setState({walletCAddress: value})
+    })
+    this.viewModel.walletEvmAddrBech.subscribe(value => {
+      this.setState({walletEvmAddress: value})
+    })
+    this.viewModel.externalAddressesX.subscribe(value => {
+      if (value.length != 0) {
+        this.setState({externalAddressX: value[0]})
+      }
+    })
+    this.viewModel.externalAddressesP.subscribe(value => {
+      if (value.length != 0) {
+        this.setState({externalAddressP: value[0]})
+      }
+    })
+    this.viewModel.addressC.subscribe(value => {
+      this.setState({addressC: value})
+    })
+    this.viewModel.availableX.subscribe(value => {
+      this.setState({availableX: value})
+    })
+    this.viewModel.availableP.subscribe(value => {
+      this.setState({availableP: value})
+    })
+    this.viewModel.availableC.subscribe(value => {
+      this.setState({availableC: value})
+    })
+  }
+
+  componentWillUnmount(): void {
+  }
+
+  private onResetHdIndices(): void {
+    this.viewModel.onResetHdIndices()
+      .subscribe({
+        next: value => console.log(value),
+        error: err => console.error(err),
+        complete: () => {
+        },
+      })
+  }
+
+  private onSend(addressX: string, amount: string): void {
+    this.viewModel.onSendAvaxX(addressX, amount)
+      .subscribe({
+        next: txHash => {
+          Alert.alert("Success", "Created transaction: " + txHash)
+        },
+        error: err => Alert.alert("Error", err.message),
+        complete: () => {
+        },
+      })
+  }
+
+  private onLogout(): void {
+    this.props.onLogout()
+  }
+
+  render(): Element {
+    const sectionListData = [
+      {
+        title: "Avax Price",
+        data: ["$" + this.state.avaxPrice],
+      },
+      {
+        title: "External addresses X",
+        data: [this.state.externalAddressX],
+      },
+      {
+        title: "External addresses P",
+        data: [this.state.externalAddressP],
+      },
+      {
+        title: "External addresses C",
+        data: [this.state.addressC],
+      },
+      {
+        title: "Available (X)",
+        data: [this.state.availableX],
+      },
+      {
+        title: "Available (P)",
+        data: [this.state.availableP],
+      },
+      {
+        title: "Available (C)",
+        data: [this.state.availableC],
+      },
+    ]
+
+    return (
+      <View>
+        <Clock/>
+        <Header/>
+        <SectionList
+          sections={sectionListData}
+          renderItem={({item}) => (
+            <Text
+              style={[
+                styles.item,
+                {color: this.state.isDarkMode ? Colors.light : Colors.dark},
+              ]}>
+              {item}
+            </Text>
+          )}
+          renderSectionHeader={({section}) => (
+            <Text style={styles.sectionHeader}>{section.title}</Text>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
+        <Button
+          title={"Reset Hd indices"}
+          onPress={() => this.onResetHdIndices()}
+        />
+        <SendAvaxModal
+          visible={this.state.sendAvaxVisible}
+          onClose={() => {
+            this.setState({
+              sendAvaxVisible: false,
+            })
+          }}
+          onSend={(addressX, amount) => {
+            this.onSend(addressX, amount)
+          }}
+        />
+        <Button
+          title={"Send AVAX X"}
+          onPress={() => {
+            this.setState({
+              sendAvaxVisible: true,
+            })
+          }}
+        />
+        <Button
+          title={"LogOut"}
+          onPress={() => this.onLogout()}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: "bold",
+    backgroundColor: "rgba(247,247,247,1.0)",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+export default MainView
