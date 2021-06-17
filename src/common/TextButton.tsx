c96fc218907d9d265a7673081291295e5f6748bf
Neven <piaf1986@gmail.com>
2021-06-17 12:57:36 +0200

    Redesign MainView

---
+++ src/common/TextButton.tsx
@@ -0,0 +1,47 @@
+import React, {Component} from "react"
+import {Appearance, Text} from "react-native"
+import CommonViewModel from "../CommonViewModel"
+import {COLORS, COLORS_NIGHT} from "./Constants"
+
+type Props = {
+  text: string,
+}
+type State = {
+  isDarkMode: boolean,
+}
+
+class TextButton extends Component<Props, State> {
+  commonViewModel: CommonViewModel = new CommonViewModel(Appearance.getColorScheme() as string)
+
+  constructor(props: Props | Readonly<Props>) {
+    super(props)
+    this.state = {
+      isDarkMode: false,
+    }
+  }
+
+  componentDidMount(): void {
+    this.commonViewModel.isDarkMode.subscribe(value => this.setState({isDarkMode: value}))
+  }
+
+  render(): Element {
+    let THEME = this.state.isDarkMode ? COLORS_NIGHT : COLORS
+    return (
+      <Text
+        style={[
+          {
+            color: THEME.onPrimary,
+            fontSize: 14,
+            fontWeight: "700",
+            textTransform: "uppercase",
+            fontFamily: "Rubik-Regular",
+            textAlign: "center",
+          },
+        ]}>
+        {this.props.text}
+      </Text>
+    )
+  }
+}
+
+export default TextButton
