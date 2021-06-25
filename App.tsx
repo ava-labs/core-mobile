c0cf55f0ace15cec7c2b7062fcaaf11ccb63a948
Neven <piaf1986@gmail.com>
2021-06-25 16:41:23 +0200

    Add screen to recall mnemonic after creation
    * After successfully entered mnemonic it is stored to
    keystore with biometric data
    * Upon entering wallet if there is keystore data biometric
    data will be requested to load mnemonic and enter wallet

--- App.tsx
+++ App.tsx
@@ -10,11 +10,10 @@ import {Appearance, SafeAreaView, StatusBar,} from 'react-native'
 import AppViewModel, {SelectedView} from './src/AppViewModel'
 import CommonViewModel from './src/CommonViewModel'
 import Login from './src/login/Login'
-import MainView from './src/mainView/MainView'
 import Onboard from './src/onboarding/Onboard'
 import CreateWallet from './src/onboarding/CreateWallet'
-import * as Keychain from 'react-native-keychain'
-import {ACCESS_CONTROL, ACCESSIBLE, AUTHENTICATION_TYPE, Options, SECURITY_RULES} from 'react-native-keychain'
+import CheckMnemonic from "./src/onboarding/CheckMnemonic"
+import MainView from "./src/mainView/MainView"
 
 type AppProps = {}
 type AppState = {
@@ -32,7 +31,7 @@ class App extends Component<AppProps, AppState> {
     this.state = {
       backgroundStyle: {},
       isDarkMode: false,
-      selectedView: SelectedView.Login,
+      selectedView: SelectedView.Onboard,
     }
   }
 
@@ -44,42 +43,35 @@ class App extends Component<AppProps, AppState> {
     this.commonViewModel.isDarkMode.subscribe(value => this.setState({isDarkMode: value}))
     this.commonViewModel.backgroundStyle.subscribe(value => this.setState({backgroundStyle: value}))
     this.viewModel.selectedView.subscribe(value => this.setState({selectedView: value}))
-
-    const options:Options = {
-      accessControl: ACCESS_CONTROL.BIOMETRY_CURRENT_SET,
-      accessible: ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
-      authenticationPrompt: {
-        title: "Prompt",
-        description: "desc prompt",
-        subtitle: "subtitle",
-        cancel: "cancel"
-      },
-      authenticationType: AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
-      rules: SECURITY_RULES.AUTOMATIC_UPGRADE
-    }
-    Keychain.setGenericPassword("username", "pass", options).then(value => {
-      console.warn(value)
-      Keychain.getGenericPassword(options).then(value => console.warn(value)).catch(reason => console.error(reason.message))
-    }).catch(reason => console.error(reason.message))
   }
 
-  onEnterWallet(mnemonic: string): void {
+  private onEnterWallet = (mnemonic: string): void => {
     this.viewModel.onEnterWallet(mnemonic)
+  }
+
+  private onSavedMnemonic = (mnemonic: string): void => {
+    this.viewModel.onSavedMnemonic(mnemonic)
   }
 
   getSelectedView(): Element {
     switch (this.state.selectedView) {
       case SelectedView.CreateWallet:
         return <CreateWallet
-          onSavedMyPhrase={mnemonic => this.onEnterWallet(mnemonic)}
+          onSavedMyPhrase={this.onSavedMnemonic}
           onClose={() => this.viewModel.setSelectedView(SelectedView.Onboard)}/>
+      case SelectedView.CheckMnemonic:
+        if (this.viewModel.wallet === null) throw Error("Wallet not defined")
+        return <CheckMnemonic
+          onSuccess={() => this.viewModel.setSelectedView(SelectedView.Main)}
+          onClose={() => this.viewModel.setSelectedView(SelectedView.Onboard)}
+          mnemonic={this.viewModel.wallet.mnemonic}/>
       case SelectedView.Onboard:
         return <Onboard
           onAlreadyHaveWallet={() => this.viewModel.setSelectedView(SelectedView.Login)}
           onCreateWallet={() => this.viewModel.setSelectedView(SelectedView.CreateWallet)}/>
       case SelectedView.Login:
         return <Login
-          onEnterWallet={mnemonic => this.onEnterWallet(mnemonic)}
+          onEnterWallet={this.onEnterWallet}
           onClose={() => this.viewModel.setSelectedView(SelectedView.Onboard)}/>
       case SelectedView.Main:
         if (this.viewModel.wallet === null) throw Error("Wallet not defined")
