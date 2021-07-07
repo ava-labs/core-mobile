dd1d5d96dd5fd87a3843dca1d85463da9fc8af08
Neven <piaf1986@gmail.com>
2021-07-07 16:20:07 +0200

    Hide buttons on Onboard screen if has stored wallet

--- src/onboarding/Onboard.tsx
+++ src/onboarding/Onboard.tsx
@@ -4,10 +4,7 @@ import CommonViewModel from '../CommonViewModel'
 import TextTitle from "../common/TextTitle"
 import ButtonAva from "../common/ButtonAva"
 import TextLabel from "../common/TextLabel"
-import BiometricsSDK from "../BiometricsSDK"
-import {UserCredentials} from "react-native-keychain"
-import {asyncScheduler, timer} from "rxjs"
-import {concatMap} from "rxjs/operators"
+import OnboardViewModel, {MnemonicLoaded, NothingToLoad, WalletLoadingResults} from "./OnboardViewModel"
 
 type Props = {
   onCreateWallet: () => void,
@@ -16,17 +13,20 @@ type Props = {
 }
 type State = {
   isDarkMode: boolean,
-  backgroundStyle: any
+  backgroundStyle: any,
+  showButtons: boolean
 }
 
 class Onboard extends Component<Props, State> {
   commonViewModel: CommonViewModel = new CommonViewModel(Appearance.getColorScheme())
+  viewModel: OnboardViewModel = new OnboardViewModel()
   pkg = require('../../package.json')
 
   constructor(props: Props | Readonly<Props>) {
     super(props)
     this.state = {
       isDarkMode: false,
+      showButtons: false,
       backgroundStyle: {},
     }
   }
@@ -34,23 +34,20 @@ class Onboard extends Component<Props, State> {
   componentDidMount(): void {
     this.commonViewModel.isDarkMode.subscribe(value => this.setState({isDarkMode: value}))
     this.commonViewModel.backgroundStyle.subscribe(value => this.setState({backgroundStyle: value}))
+    this.viewModel.showButtons.subscribe(value => this.setState({showButtons: value}))
 
-    this.promptForWalletLoadingIfExists()
-  }
-
-  private promptForWalletLoadingIfExists() {
-    timer(100, asyncScheduler).pipe(
-      concatMap(value => BiometricsSDK.loadMnemonic(BiometricsSDK.loadOptions))
-    ).subscribe({
-      next: value => {
-        if (value !== false) {
-          const mnemonic = (value as UserCredentials).password
-          this.props.onEnterWallet(mnemonic)
+    this.viewModel.promptForWalletLoadingIfExists().subscribe({
+      next: (value: WalletLoadingResults) => {
+        if (value instanceof MnemonicLoaded) {
+          this.props.onEnterWallet(value.mnemonic)
+        } else if (value instanceof NothingToLoad) {
+          //do nothing
         }
       },
       error: err => console.log(err.message)
     })
   }
+
 
   componentWillUnmount(): void {
   }
@@ -73,8 +70,9 @@ class Onboard extends Component<Props, State> {
             style={styles.logo}/>
           <TextTitle text={"Avalanche Wallet"} textAlign={"center"} bold={true}/>
         </View>
-        <ButtonAva text={"Create wallet"} onPress={() => this.onCreateWallet()}/>
-        <ButtonAva text={"I already have wallet"} onPress={() => this.onAlreadyHaveWallet()}/>
+        {this.state.showButtons && <ButtonAva text={"Create wallet"} onPress={() => this.onCreateWallet()}/>}
+        {this.state.showButtons &&
+        <ButtonAva text={"I already have wallet"} onPress={() => this.onAlreadyHaveWallet()}/>}
         <TextLabel text={"v" + this.pkg.version}/>
       </View>
     )
