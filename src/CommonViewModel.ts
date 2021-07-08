807af7c82e1b2ccc830360dd47bdf03d677fae47
Neven <piaf1986@gmail.com>
2021-07-08 11:47:37 +0200

    Add bottom nav bar

--- src/CommonViewModel.ts
+++ src/CommonViewModel.ts
@@ -1,16 +1,25 @@
-import {BehaviorSubject, Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {ColorSchemeName} from 'react-native';
+import {BehaviorSubject, Observable} from 'rxjs'
+import {map} from 'rxjs/operators'
+import {ColorSchemeName} from 'react-native'
 import {COLORS, COLORS_NIGHT} from "./common/Constants"
 
 export default class {
   constructor(colorScheme: ColorSchemeName) {
-    this.isDarkMode.next(colorScheme === 'dark');
-    this.iconSufix.next(colorScheme);
+    this.isDarkMode.next(colorScheme === 'dark')
+    this.iconSufix.next(colorScheme)
   }
 
-  isDarkMode: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  iconSufix: BehaviorSubject<ColorSchemeName> = new BehaviorSubject<ColorSchemeName>("light");
+  isDarkMode: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
+  iconSufix: BehaviorSubject<ColorSchemeName> = new BehaviorSubject<ColorSchemeName>("light")
+  appBackgroundStyle: Observable<object> = this.isDarkMode.pipe(
+    map(isDarkMode => {
+      const THEME = isDarkMode ? COLORS_NIGHT : COLORS
+      return {
+        backgroundColor: THEME.bg,
+        flex: 1,
+      }
+    }),
+  )
   backgroundStyle: Observable<object> = this.isDarkMode.pipe(
     map(isDarkMode => {
       const THEME = isDarkMode ? COLORS_NIGHT : COLORS
@@ -20,7 +29,7 @@ export default class {
         paddingBottom: 16,
         paddingStart: 16,
         paddingEnd: 16,
-      };
+      }
     }),
-  );
+  )
 }
--- src/mainView/MainView.tsx
+++ src/mainView/MainView.tsx
@@ -1,18 +1,15 @@
 import React, {Component} from "react"
-import {Alert, Appearance, Modal, ScrollView, StyleSheet, View} from "react-native"
+import {Alert, Appearance, Modal, StyleSheet, View} from "react-native"
 import CommonViewModel from "../CommonViewModel"
-import Header from "./Header"
 import MainViewViewModel from "./MainViewViewModel"
-import SendAvaxX from "../sendAvax/SendAvaxX"
-import SendAvaxC from "../sendAvax/SendAvaxC"
-import SendCrossChain from "../sendAvax/SendCrossChain";
-import Loader from "../common/Loader"
-import Validate from "../earn/Validate"
 import {MnemonicWallet} from "@avalabs/avalanche-wallet-sdk"
-import ButtonAva from "../common/ButtonAva"
-import TabbedAddressCards from "./TabbedAddressCards"
-import Balances from "./Balances"
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from "@react-navigation/native"
+import Portfolio from "../portfolio/Portfolio"
+import SendView from "../sendAvax/SendView"
+import EarnView from "../earn/EarnView"
 import Transactions from "../transactions/Transactions"
+import Loader from "../common/Loader"
 
 type Props = {
   wallet: MnemonicWallet,
@@ -21,20 +18,10 @@ type Props = {
 }
 type State = {
   isDarkMode: boolean
-  backgroundStyle: any
-  loaderVisible: boolean
-  avaxPrice: number
-  addressX: string
-  addressP: string
-  addressC: string
-  sendXVisible: boolean
-  sendCVisible: boolean
-  crossChainVisible: boolean
-  validateVisible: boolean
-  transactionsVisible: boolean
-  walletCAddress: string
-  walletEvmAddress: string
+  walletReady: boolean
 }
+
+const Tab = createBottomTabNavigator()
 
 class MainView extends Component<Props, State> {
   viewModel!: MainViewViewModel
@@ -44,34 +31,13 @@ class MainView extends Component<Props, State> {
     super(props)
     this.state = {
       isDarkMode: false,
-      backgroundStyle: {},
-      loaderVisible: true,
-      avaxPrice: 0,
-      addressX: "",
-      addressP: "",
-      addressC: "",
-      sendXVisible: false,
-      sendCVisible: false,
-      crossChainVisible: false,
-      validateVisible: false,
-      transactionsVisible: false,
-      walletCAddress: "",
-      walletEvmAddress: "",
+      walletReady: false,
     }
     this.viewModel = new MainViewViewModel(props.wallet)
   }
 
   componentDidMount(): void {
-    this.viewModel.onComponentMount()
-
     this.commonViewModel.isDarkMode.subscribe(value => this.setState({isDarkMode: value}))
-    this.commonViewModel.backgroundStyle.subscribe(value => this.setState({backgroundStyle: value}))
-    this.viewModel.avaxPrice.subscribe(value => this.setState({avaxPrice: value}))
-    this.viewModel.walletCAddress.subscribe(value => this.setState({walletCAddress: value}))
-    this.viewModel.walletEvmAddrBech.subscribe(value => this.setState({walletEvmAddress: value}))
-    this.viewModel.addressX.subscribe(value => this.setState({addressX: value}))
-    this.viewModel.addressP.subscribe(value => this.setState({addressP: value}))
-    this.viewModel.addressC.subscribe(value => this.setState({addressC: value}))
 
     this.viewModel.onResetHdIndices()
       .subscribe({
@@ -79,12 +45,11 @@ class MainView extends Component<Props, State> {
           this.onExit()
           Alert.alert("Error", err.message)
         },
-        complete: () => this.setState({loaderVisible: false}),
+        complete: () => this.setState({walletReady: true}),
       })
   }
 
   componentWillUnmount(): void {
-    this.viewModel.onComponentUnMount()
   }
 
   private onExit = (): void => {
@@ -95,118 +60,46 @@ class MainView extends Component<Props, State> {
     this.props.onSwitchWallet()
   }
 
-  render(): Element {
+  private _Portfolio = () => <Portfolio wallet={this.viewModel.wallet.value} onSwitchWallet={this.onSwitchWallet}
+                                        onExit={this.onExit}/>
+  private _Send = () => <SendView wallet={this.viewModel.wallet.value}/>
+  private _Earn = () => <EarnView wallet={this.viewModel.wallet.value}/>
+  private _Transactions = () => <Transactions wallet={this.viewModel.wallet.value}/>
+  private _Nav = () => (
+    <NavigationContainer>
+      <Tab.Navigator sceneContainerStyle={styles.navContainer}>
+        <Tab.Screen name="Portfolio" component={this._Portfolio}/>
+        <Tab.Screen name="Send" component={this._Send}/>
+        <Tab.Screen name="Earn" component={this._Earn}/>
+        <Tab.Screen name="Transactions" component={this._Transactions}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 
+  render(): Element {
     return (
-      <ScrollView>
+      <View style={styles.container}>
         <Modal
           animationType="fade"
           transparent={true}
-          visible={this.state.loaderVisible}>
+          visible={!this.state.walletReady}>
           <Loader message={"Loading wallet"}/>
         </Modal>
 
-        <Header showExit onExit={this.onExit} showSwitchWallet onSwitchWallet={this.onSwitchWallet}/>
-        <Balances wallet={this.props.wallet}/>
-        <TabbedAddressCards addressP={this.state.addressP} addressX={this.state.addressX} addressC={this.state.addressC}/>
-
-        <View style={styles.container}>
-          <ButtonAva
-            text={"Send AVAX X"}
-            onPress={() => this.setState({sendXVisible: true})}/>
-          <ButtonAva
-            text={"Send AVAX C"}
-            onPress={() => this.setState({sendCVisible: true})}/>
-          <ButtonAva
-            text={"Cross chain"}
-            onPress={() => this.setState({crossChainVisible: true})}/>
-          <ButtonAva
-            text={"Validate"}
-            onPress={() => this.setState({validateVisible: true})}/>
-          <ButtonAva
-            text={"Transactions"}
-            onPress={() => this.setState({transactionsVisible: true})}/>
-        </View>
-
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={this.state.sendXVisible}
-          onRequestClose={() => this.setState({sendXVisible: false})}>
-          <SendAvaxX
-            wallet={this.viewModel.wallet.value}
-            onClose={() => {
-              this.setState({
-                sendXVisible: false,
-              })
-            }}
-          />
-        </Modal>
-
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={this.state.sendCVisible}
-          onRequestClose={() => this.setState({sendCVisible: false})}>
-          <SendAvaxC
-            wallet={this.viewModel.wallet.value}
-            onClose={() => {
-              this.setState({
-                sendCVisible: false,
-              })
-            }}/>
-        </Modal>
-
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={this.state.crossChainVisible}
-          onRequestClose={() => this.setState({crossChainVisible: false})}>
-          <SendCrossChain
-            wallet={this.viewModel.wallet.value}
-            onClose={() => {
-              this.setState({
-                crossChainVisible: false,
-              })
-            }}/>
-        </Modal>
-
-        <Modal
-          animationType="slide"
-          transparent={true}
-          onRequestClose={() => this.setState({validateVisible:false})}
-          visible={this.state.validateVisible}>
-          <Validate
-            wallet={this.viewModel.wallet.value}
-            onClose={() => this.setState({validateVisible: false})}/>
-        </Modal>
-
-        <Modal
-          animationType="slide"
-          transparent={true}
-          onRequestClose={() => this.setState({transactionsVisible:false})}
-          visible={this.state.transactionsVisible}>
-          <Transactions
-            wallet={this.viewModel.wallet.value}
-            onClose={() => this.setState({transactionsVisible: false})}/>
-        </Modal>
-      </ScrollView>
+        {this.state.walletReady && this._Nav()}
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    alignItems: "center"
+    height: "100%"
   },
-  horizontalLayout: {
-    flexDirection: 'row',
-    padding: 8,
-  },
-  column: {
-    flex: 1,
+  navContainer: {
+    backgroundColor: "transparent",
+    paddingStart: 16,
+    paddingEnd: 16,
   },
 })
 
