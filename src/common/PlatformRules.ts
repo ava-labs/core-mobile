6a57e643737063f88985656c0a8a2c366124b493
Neven <piaf1986@gmail.com>
2021-07-28 00:04:38 +0200

    Implement react-navigation for native look and feel

--- src/common/PlatformRules.ts
+++ src/common/PlatformRules.ts
@@ -1,13 +1,5 @@
-import {Platform} from "react-native"
-
 export class PlatformRules {
   static delayedPress = (onPress: () => void): void => {
-    if (Platform.OS === "android") {
-      setTimeout(() => {
-        onPress()
-      }, 200)
-    } else {
-      onPress()
-    }
+    onPress()
   }
 }
