5a706a51f49fdc81aab6a496471f19eea898314a
Neven <piaf1986@gmail.com>
2021-07-28 16:08:12 +0200

    Restyle Onboard screen

--- src/onboarding/Onboard.tsx
+++ src/onboarding/Onboard.tsx
@@ -11,13 +11,11 @@ import OnboardViewModel, {
 } from "./OnboardViewModel"
 import {Subscription} from "rxjs"
 import CommonViewModel from "../CommonViewModel"
-import ImgButtonAva from "../common/ImgButtonAva"
+import ButtonAvaTextual from "../common/ButtonAvaTextual"
 
 type Props = {
   onCreateWallet: () => void,
-  onLoginWithMnemonic: () => void,
-  onLoginWithPrivateKey: () => void,
-  onLoginWithKeystoreFile: () => void,
+  onAlreadyHaveWallet: () => void,
   onEnterWallet: (mnemonic: string) => void,
   onEnterSingletonWallet: (privateKey: string) => void,
 }
@@ -53,35 +51,27 @@ export default function Onboard(props: Props | Readonly<Props>) {
     props.onCreateWallet()
   }
 
-  const onLoginWithMnemonic = (): void => {
-    props.onLoginWithMnemonic()
-  }
-
-  const onLoginWithPrivateKey = (): void => {
-    props.onLoginWithPrivateKey()
-  }
-
-  const onLoginWithKeystoreFile = (): void => {
-    props.onLoginWithKeystoreFile()
+  const onAlreadyHaveWallet = (): void => {
+    props.onAlreadyHaveWallet()
   }
 
   const logo = commonViewModel.isDarkMode ? require("../assets/ava_logo_dark.png") : require("../assets/ava_logo_light.png")
-  const loginRecoveryIcon = commonViewModel.isDarkMode ? require("../assets/icons/login_recovery_dark.png") : require("../assets/icons/login_recovery_dark.png") //fixme:  replace with light when its designed
-  const loginPrivateKey = commonViewModel.isDarkMode ? require("../assets/icons/private_key_dark.png") : require("../assets/icons/private_key_dark.png") //fixme:  replace with light when its designed
-  const loginKeystoreFile = commonViewModel.isDarkMode ? require("../assets/icons/keystore_dark.png") : require("../assets/icons/keystore_dark.png") //fixme:  replace with light when its designed
+  // const loginRecoveryIcon = commonViewModel.isDarkMode ? require("../assets/icons/login_recovery_dark.png") : require("../assets/icons/login_recovery_dark.png") //fixme:  replace with light when its designed
+  // const loginPrivateKey = commonViewModel.isDarkMode ? require("../assets/icons/private_key_dark.png") : require("../assets/icons/private_key_dark.png") //fixme:  replace with light when its designed
+  // const loginKeystoreFile = commonViewModel.isDarkMode ? require("../assets/icons/keystore_dark.png") : require("../assets/icons/keystore_dark.png") //fixme:  replace with light when its designed
 
-  const buttonWithText = (icon: any, text: string, onPress: () => void) => {
-    return (
-      <View style={styles.buttonWithText}>
-        <View>
-          <ImgButtonAva src={icon} onPress={onPress} width={68} height={68}/>
-        </View>
-        <View style={[{width: 68}]}>
-          <TextLabel text={text} multiline/>
-        </View>
-      </View>
-    )
-  }
+  // const buttonWithText = (icon: any, text: string, onPress: () => void) => {
+  //   return (
+  //     <View style={styles.buttonWithText}>
+  //       <View>
+  //         <ImgButtonAva src={icon} onPress={onPress} width={68} height={68}/>
+  //       </View>
+  //       <View style={[{width: 68}]}>
+  //         <TextLabel text={text} multiline/>
+  //       </View>
+  //     </View>
+  //   )
+  // }
 
   return (
     <View style={styles.verticalLayout}>
@@ -90,15 +80,19 @@ export default function Onboard(props: Props | Readonly<Props>) {
           accessibilityRole="image"
           source={logo}
           style={styles.logo}/>
-        <TextTitle text={"Avalanche Wallet"} textAlign={"center"} bold={true}/>
+        <View style={[{height: 18}]}/>
+        <TextTitle text={"Wallet"} textAlign={"center"} bold={true} size={36}/>
+        <View style={[{height: 8}]}/>
+        <TextTitle text={"Your simple and secure crypto wallet"} textAlign={"center"} size={16}/>
       </View>
 
-      {showButtons && <View style={styles.roundButtons}>
-        {buttonWithText(loginRecoveryIcon, "Recovery Phrase", onLoginWithMnemonic)}
-        {buttonWithText(loginPrivateKey, "Private Key", onLoginWithPrivateKey)}
-        {buttonWithText(loginKeystoreFile, "Keystore File", onLoginWithKeystoreFile)}
-      </View>}
+      {/*{showButtons && <View style={styles.roundButtons}>*/}
+      {/*  {buttonWithText(loginRecoveryIcon, "Recovery Phrase", onLoginWithMnemonic)}*/}
+      {/*  {buttonWithText(loginPrivateKey, "Private Key", onLoginWithPrivateKey)}*/}
+      {/*  {buttonWithText(loginKeystoreFile, "Keystore File", onLoginWithKeystoreFile)}*/}
+      {/*</View>}*/}
 
+      {showButtons && <ButtonAvaTextual text={"I already have a wallet"} onPress={() => onAlreadyHaveWallet()}/>}
       {showButtons && <ButtonAva text={"Create new wallet"} onPress={() => onCreateWallet()}/>}
       <TextLabel text={"v" + pkg.version + " Fuji network"}/>
     </View>
