aba7096ac1a9afe2fabf983310ccf91380654e58
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2021-09-01 23:54:02 -0600

    scroll header animation tune up.

--- app/screens/portfolio/PortfolioHeader.tsx
+++ app/screens/portfolio/PortfolioHeader.tsx
@@ -1,5 +1,5 @@
-import React, {useContext, useState} from 'react';
-import {Animated, StyleSheet, Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {Animated, Platform, StyleSheet, Text, View} from 'react-native';
 import {usePortfolio} from 'screens/portfolio/PortfolioHook';
 import {MnemonicWallet} from '@avalabs/avalanche-wallet-sdk';
 import PortfolioActionButton from './components/PortfolioActionButton';
@@ -11,87 +11,121 @@ interface PortfolioHeaderProps {
   scrollY: Animated.AnimatedInterpolation;
 }
 
-export const PORTFOLIO_HEADER_HEIGHT = 320;
-const scrollStart = PORTFOLIO_HEADER_HEIGHT * 0.5;
-const scrollEnd = PORTFOLIO_HEADER_HEIGHT * 0.8;
+export const HEADER_MAX_HEIGHT = 260;
+export const HEADER_MIN_HEIGHT = Platform.OS === 'ios' ? 60 : 73;
+export const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
 function PortfolioHeader({wallet, scrollY}: PortfolioHeaderProps) {
   const context = useContext(ApplicationContext);
   const [avaxPrice, walletEvmAddrBech] = usePortfolio(wallet);
-  const [tokenValueOriginalY, setTokenValueOriginalY] = useState(0);
 
-  const opacityAnimation = {
-    opacity: scrollY.interpolate({
-      inputRange: [0, scrollStart, scrollEnd],
-      outputRange: [1, 0, 0],
-      extrapolate: 'clamp',
-    }),
-  };
+  const headerTranslate = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE],
+    outputRange: [0, -HEADER_SCROLL_DISTANCE],
+    extrapolate: 'clamp',
+  });
 
-  const positionAnimation = {
-    transform: [
-      {
-        translateY: scrollY.interpolate({
-          inputRange: [0, scrollStart - 50, scrollEnd],
-          outputRange: [0, tokenValueOriginalY, tokenValueOriginalY + 100],
-          extrapolate: 'clamp',
-        }),
-      },
+  const imageOpacity = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE / 3, HEADER_SCROLL_DISTANCE],
+    outputRange: [1, 0, 0],
+    extrapolate: 'clamp',
+  });
+  const imageTranslate = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE],
+    outputRange: [0, -300],
+    extrapolate: 'clamp',
+  });
+
+  const titleTranslate = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+    outputRange: [
+      0,
+      -(HEADER_SCROLL_DISTANCE / 4),
+      -(HEADER_SCROLL_DISTANCE / 2),
     ],
-  };
+    extrapolate: 'clamp',
+  });
 
   return (
-    <Animated.View style={[styles.container]}>
-      <Animated.View>
-        <AvaListItem.Account
-          accountName={'My Awesome Wallet'}
-          accountAddress={walletEvmAddrBech ?? ''}
-        />
-      </Animated.View>
+    <>
       <Animated.View
-        style={[positionAnimation, styles.center]}
-        onLayout={({nativeEvent}) => {
-          const y = nativeEvent.layout.y;
-          setTokenValueOriginalY(y);
-        }}>
-        <Text style={[styles.amount, {color: context.theme.buttonIcon}]}>
-          {`$${avaxPrice} USD`}
-        </Text>
+        pointerEvents="none"
+        style={[
+          styles.header,
+          {
+            // while we wait for the proper background from UX
+            backgroundColor: context.isDarkMode
+              ? '#000'
+              : context.theme.tcwbBg2,
+            transform: [{translateY: headerTranslate}],
+          },
+        ]}
+      />
+
+      <Animated.View style={[styles.bar]} pointerEvents="box-none">
+        <Animated.View style={{opacity: imageOpacity}}>
+          <AvaListItem.Account
+            accountName={'My Awesome Wallet'}
+            accountAddress={walletEvmAddrBech ?? ''}
+          />
+        </Animated.View>
+        <Animated.View
+          style={{
+            alignItems: 'center',
+            transform: [{translateY: titleTranslate}],
+          }}>
+          <Text style={[styles.text, {color: context.theme.buttonIcon}]}>
+            {`$${avaxPrice} USD`}
+          </Text>
+        </Animated.View>
+        <Animated.View
+          style={{
+            opacity: imageOpacity,
+            transform: [{translateY: imageTranslate}],
+          }}>
+          <Animated.View style={[styles.actionsContainer]}>
+            <PortfolioActionButton.Send />
+            <View style={{paddingHorizontal: 24}}>
+              <PortfolioActionButton.Receive />
+            </View>
+            <PortfolioActionButton.Buy />
+          </Animated.View>
+        </Animated.View>
       </Animated.View>
-      <Animated.View style={[opacityAnimation, styles.actionsContainer]}>
-        <PortfolioActionButton.Send />
-        <View style={{paddingHorizontal: 24}}>
-          <PortfolioActionButton.Receive />
-        </View>
-        <PortfolioActionButton.Buy />
-      </Animated.View>
-    </Animated.View>
+    </>
   );
 }
 
+// @ts-ignore
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-    height: PORTFOLIO_HEADER_HEIGHT,
-  },
-  center: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignContent: 'center',
-    padding: 20,
-  },
-  amount: {
-    fontWeight: 'bold',
-    fontSize: 36,
-    lineHeight: 44,
-  },
   actionsContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: 60,
-    paddingBottom: 46,
+  },
+  text: {
+    fontSize: 30,
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    overflow: 'hidden',
+    height: HEADER_MAX_HEIGHT,
+  },
+  bar: {
+    backgroundColor: 'transparent',
+    marginTop: Platform.OS === 'ios' ? 0 : 0,
+    height: HEADER_MAX_HEIGHT,
+    justifyContent: 'space-between',
+    position: 'absolute',
+    width: '100%',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
+    paddingBottom: 8,
   },
 });
 
--- app/screens/portfolio/PortfolioView.tsx
+++ app/screens/portfolio/PortfolioView.tsx
@@ -4,13 +4,13 @@ import {
   FlatList,
   ListRenderItemInfo,
   SafeAreaView,
-  StatusBar,
   StyleSheet,
 } from 'react-native';
 import {MnemonicWallet} from '@avalabs/avalanche-wallet-sdk';
 import AvaListItem from 'screens/portfolio/AvaListItem';
 import PortfolioHeader, {
-  PORTFOLIO_HEADER_HEIGHT,
+  HEADER_MAX_HEIGHT,
+  HEADER_MIN_HEIGHT,
 } from 'screens/portfolio/PortfolioHeader';
 import {ApplicationContext} from 'contexts/ApplicationContext';
 import SearchHeader from 'screens/portfolio/components/SearchHeader';
@@ -26,22 +26,11 @@ const data: JSON[] = require('assets/coins.json');
 const PortfolioView: FC<Props> = ({wallet, onExit, onSwitchWallet}) => {
   const [searchText, setSearchText] = useState('');
   const scrollY = useRef(new Animated.Value(0)).current;
-  const context = useContext(ApplicationContext);
-  const isDarkMode = context.isDarkMode;
-  const headerHeight = PORTFOLIO_HEADER_HEIGHT;
-  const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
   const listRef = useRef<FlatList>(null);
+  const context = useContext(ApplicationContext);
+  const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
   const keyExtractor = (item: any, index: number) => item?.id ?? index;
-
-  /**
-   * Handles animating the header's absolute positioning
-   */
-  const translateY = scrollY.interpolate({
-    inputRange: [0, headerHeight],
-    outputRange: [0, -(headerHeight - 50)],
-    extrapolate: 'clamp',
-  });
 
   const renderItem = (item: ListRenderItemInfo<any>) => {
     const json = item.item;
@@ -54,41 +43,38 @@ const PortfolioView: FC<Props> = ({wallet, onExit, onSwitchWallet}) => {
     );
   };
 
-  const backgroundColor = isDarkMode ? '#000' : '#F8F8FB';
-
   return (
     <SafeAreaView style={styles.flex}>
-      <StatusBar animated={true} backgroundColor={backgroundColor} />
-      <Animated.View
-        style={[
-          styles.searchHeader,
-          {
-            transform: [{translateY: translateY}],
-            backgroundColor: backgroundColor,
-          },
-        ]}>
-        <PortfolioHeader wallet={wallet} scrollY={scrollY} />
-        <SearchHeader
-          searchText={searchText}
-          onSearchTextChanged={setSearchText}
-          listRef={listRef}
-        />
-      </Animated.View>
       <AnimatedFlatList
         ref={listRef}
-        style={{
-          backgroundColor: isDarkMode ? '#1A1A1C' : '#FFF',
-        }}
+        style={[
+          {
+            flex: 1,
+            backgroundColor: context.theme.cardBg,
+            marginTop: HEADER_MIN_HEIGHT,
+          },
+        ]}
         data={data}
-        contentContainerStyle={{paddingTop: headerHeight + headerHeight * 0.2}}
         renderItem={renderItem}
         keyExtractor={keyExtractor}
-        nestedScrollEnabled
+        scrollEventThrottle={16}
+        stickyHeaderIndices={[0]}
         onScroll={Animated.event(
           [{nativeEvent: {contentOffset: {y: scrollY}}}],
           {useNativeDriver: true},
         )}
+        contentContainerStyle={{
+          paddingTop: HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT,
+        }}
+        ListHeaderComponent={
+          <SearchHeader
+            searchText={searchText}
+            onSearchTextChanged={setSearchText}
+            listRef={listRef}
+          />
+        }
       />
+      <PortfolioHeader wallet={wallet} scrollY={scrollY} />
     </SafeAreaView>
   );
 };
@@ -97,31 +83,6 @@ const styles = StyleSheet.create({
   flex: {
     flex: 1,
     backgroundColor: '#F8F8FB',
-  },
-  container: {
-    flexDirection: 'row',
-  },
-  searchHeader: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    width: '100%',
-    zIndex: 1,
-  },
-  progressContainer: {flex: 0.1, backgroundColor: '#63a4ff'},
-  text: {
-    fontSize: 30,
-  },
-  separator: {
-    paddingVertical: 16,
-    paddingHorizontal: 10,
-  },
-  touchableTitle: {
-    textAlign: 'center',
-    color: '#000',
-  },
-  touchableTitleActive: {
-    color: '#fff',
   },
 });
 
