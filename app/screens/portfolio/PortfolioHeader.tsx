8375ff5dd99f70e9bdcd8a474e1ba74a078e9161
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2021-09-08 16:29:25 -0600

    removed all animations and updated account and token list items.

--- app/screens/portfolio/PortfolioHeader.tsx
+++ app/screens/portfolio/PortfolioHeader.tsx
@@ -1,107 +1,57 @@
 import React, {useContext} from 'react';
-import {Animated, Platform, StyleSheet, Text, View} from 'react-native';
-import {usePortfolio} from 'screens/portfolio/PortfolioHook';
-import {MnemonicWallet} from '@avalabs/avalanche-wallet-sdk';
-import PortfolioActionButton from './components/PortfolioActionButton';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 import AvaListItem from 'screens/portfolio/AvaListItem';
 import {ApplicationContext} from 'contexts/ApplicationContext';
+import {useWalletStateContext} from '@avalabs/wallet-react-components';
 
-interface PortfolioHeaderProps {
-  wallet: MnemonicWallet;
-  scrollY: Animated.AnimatedInterpolation;
-}
-
-export const HEADER_MAX_HEIGHT = 180;
+export const HEADER_MAX_HEIGHT = 150;
 export const HEADER_MIN_HEIGHT = Platform.OS === 'ios' ? 60 : 73;
 export const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
-function PortfolioHeader({wallet, scrollY}: PortfolioHeaderProps) {
+function PortfolioHeader() {
   const context = useContext(ApplicationContext);
-  const [avaxPrice, walletEvmAddrBech] = usePortfolio(wallet);
-
-  const headerTranslate = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE],
-    outputRange: [0, -HEADER_SCROLL_DISTANCE],
-    extrapolate: 'clamp',
-  });
-
-  const imageOpacity = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE / 3, HEADER_SCROLL_DISTANCE],
-    outputRange: [1, 0, 0],
-    extrapolate: 'clamp',
-  });
-  const imageTranslate = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE],
-    outputRange: [0, -300],
-    extrapolate: 'clamp',
-  });
-
-  const titleTranslate = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
-    outputRange: [
-      0,
-      -(HEADER_SCROLL_DISTANCE / 4),
-      -(HEADER_SCROLL_DISTANCE / 2),
-    ],
-    extrapolate: 'clamp',
-  });
+  const walletStateContext = useWalletStateContext();
 
   return (
     <>
-      <Animated.View
+      <View
         pointerEvents="none"
         style={[
           styles.header,
           {
             // while we wait for the proper background from UX
-            backgroundColor: context.isDarkMode
-              ? '#000'
-              : context.theme.tcwbBg2,
-            transform: [{translateY: headerTranslate}],
+            backgroundColor: context.theme.bgOnBgApp,
           },
         ]}
       />
 
-      <Animated.View style={[styles.bar]} pointerEvents="box-none">
-        <Animated.View style={{opacity: imageOpacity}}>
+      <View style={[styles.bar]} pointerEvents="box-none">
+        <View>
           <AvaListItem.Account
             accountName={'My Awesome Wallet'}
-            accountAddress={walletEvmAddrBech ?? ''}
+            accountAddress={walletStateContext?.addresses?.addrC ?? ''}
           />
-        </Animated.View>
-        <Animated.View
+        </View>
+        <View
           style={{
+            alignItems: 'flex-end',
             justifyContent: 'center',
             flexDirection: 'row',
-            transform: [{translateY: titleTranslate}],
           }}>
-          <Text style={[styles.text, {color: context.theme.buttonIcon}]}>
-            {`$${avaxPrice}`}
+          <Text style={[styles.text, {color: context.theme.txtOnBgApp}]}>
+            {walletStateContext?.avaxPrice ?? 0}
           </Text>
           <Text
             style={{
               fontSize: 16,
-              lineHeight: 32,
-              color: '#F8F8FB',
+              color: context.theme.txtOnBgApp,
               paddingLeft: 4,
+              lineHeight: 28,
             }}>
             USD
           </Text>
-        </Animated.View>
-        <Animated.View
-          style={{
-            opacity: imageOpacity,
-            transform: [{translateY: imageTranslate}],
-          }}>
-          {/*<Animated.View style={[styles.actionsContainer]}>*/}
-          {/*  <PortfolioActionButton.Send />*/}
-          {/*  <View style={{paddingHorizontal: 24}}>*/}
-          {/*    <PortfolioActionButton.Receive />*/}
-          {/*  </View>*/}
-          {/*  <PortfolioActionButton.Buy />*/}
-          {/*</Animated.View>*/}
-        </Animated.View>
-      </Animated.View>
+        </View>
+      </View>
     </>
   );
 }
@@ -116,14 +66,8 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 36,
     fontWeight: 'bold',
-    lineHeight: 36,
-    textAlignVertical: 'bottom',
   },
   header: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
     overflow: 'hidden',
     height: HEADER_MAX_HEIGHT,
   },
