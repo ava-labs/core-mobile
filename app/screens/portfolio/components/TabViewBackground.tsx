e681663d306625b6bbbefa6f3a814f59c9054c51
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2021-09-14 23:17:41 -0600

    introducing TabViewAva component to make life easier when composing views that depends on tabs

---
+++ app/screens/portfolio/components/TabViewBackground.tsx
@@ -0,0 +1,30 @@
+import React, {useContext, useMemo} from 'react';
+import {BottomSheetBackgroundProps} from '@gorhom/bottom-sheet';
+import {ApplicationContext} from 'contexts/ApplicationContext';
+import {View} from 'react-native';
+
+const TabViewBackground = ({style}: BottomSheetBackgroundProps) => {
+  const theme = useContext(ApplicationContext).theme;
+
+  // styles
+  const containerStyle = useMemo(
+    () => [
+      style,
+      {
+        backgroundColor: theme.bgOnBgApp,
+      },
+    ],
+    [style],
+  );
+
+  return (
+    <View
+      style={[
+        containerStyle,
+        {borderTopLeftRadius: 12, borderTopRightRadius: 12},
+      ]}
+    />
+  );
+};
+
+export default TabViewBackground;
