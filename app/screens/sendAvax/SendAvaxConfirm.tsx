77b30c4c7306243dcbdcbda91c77f027e3947221
Neven <piaf1986@gmail.com>
2021-09-17 10:55:07 +0200

    Restyle confirm transaction screen

--- app/screens/sendAvax/SendAvaxConfirm.tsx
+++ app/screens/sendAvax/SendAvaxConfirm.tsx
@@ -1,10 +1,11 @@
 import React, {useContext, useState} from 'react';
-import {SafeAreaView} from 'react-native';
-import Header from 'screens/mainView/Header';
+import {Image, View} from 'react-native';
 import {ApplicationContext} from 'contexts/ApplicationContext';
 import Divider from 'components/Divider';
-import SendConfirmItem from 'screens/sendAvax/SendConfirmItem';
 import ButtonAva from 'components/ButtonAva';
+import TextTitle from 'components/TextTitle';
+import OvalTagBg from 'components/OvalTagBg';
+import AvaText from 'components/AvaText';
 
 type SendAvaxXProps = {
   tokenImageUrl: string;
@@ -23,18 +24,42 @@ export default function SendAvaxConfirm(
   const [backgroundStyle] = useState(context.backgroundStyle);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <Header showBack onBack={props.onClose} />
-      <Divider size={12} />
-      <SendConfirmItem.Amount
-        image={props.tokenImageUrl}
-        tokenAmount={props.tokenAmount}
-        fiatAmount={props.fiatAmount}
+    <View
+      style={[
+        backgroundStyle,
+        {
+          alignItems: 'center',
+          backgroundColor: context.theme.bgOnBgApp,
+          paddingLeft: 0,
+          paddingRight: 0,
+        },
+      ]}>
+      <Divider size={40} />
+      <Image
+        style={{width: 40, height: 40}}
+        width={40}
+        height={40}
+        source={{uri: props.tokenImageUrl}}
       />
-      <SendConfirmItem.Address address={props.destinationAddress} />
-      {props.memo && <SendConfirmItem.Memo memo={props.memo} />}
+      <Divider size={16} />
+      <TextTitle text={props.tokenAmount} size={24} bold />
+      <Divider size={8} />
+      <TextTitle text={props.fiatAmount} size={14} />
 
-      <ButtonAva text={'Confirm'} onPress={props.onConfirm} />
-    </SafeAreaView>
+      <Divider size={32} />
+      <OvalTagBg color={context.theme.bgApp}>
+        <AvaText.Tag>Send to</AvaText.Tag>
+      </OvalTagBg>
+      <Divider size={32} />
+
+      <View style={{paddingLeft: 24, paddingRight: 24}}>
+        <AvaText.Heading2>{props.destinationAddress}</AvaText.Heading2>
+      </View>
+
+      <View style={{flex: 1}} />
+      <View style={{width: '100%'}}>
+        <ButtonAva text={'Send'} onPress={props.onConfirm} />
+      </View>
+    </View>
   );
 }
