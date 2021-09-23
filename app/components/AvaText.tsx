912381534c8f015dc80f3e500fa6e0fc59087616
Neven <piaf1986@gmail.com>
2021-09-23 19:53:06 +0200

    Add Heading1 and Heading3 to AvaText

--- app/components/AvaText.tsx
+++ app/components/AvaText.tsx
@@ -6,10 +6,28 @@ interface TextProps {
   textStyle?: StyleProp<TextStyle>;
 }
 
+const TextHeading1: FC<TextProps> = ({textStyle, children}) => {
+  const theme = useContext(ApplicationContext).theme;
+  return (
+    <Text style={[styles.heading1, {color: theme.txtListItem}, textStyle]}>
+      {children}
+    </Text>
+  );
+};
+
 const TextHeading2: FC<TextProps> = ({textStyle, children}) => {
   const theme = useContext(ApplicationContext).theme;
   return (
     <Text style={[styles.heading2, {color: theme.txtListItem}, textStyle]}>
+      {children}
+    </Text>
+  );
+};
+
+const TextHeading3: FC<TextProps> = ({textStyle, children}) => {
+  const theme = useContext(ApplicationContext).theme;
+  return (
+    <Text style={[styles.heading3, {color: theme.txtListItem}, textStyle]}>
       {children}
     </Text>
   );
@@ -43,17 +61,29 @@ const TextTag: FC<TextProps> = ({textStyle, children}) => {
 };
 
 const AvaText = {
+  Heading1: TextHeading1,
   Heading2: TextHeading2,
+  Heading3: TextHeading3,
   Body1: TextBody1,
   Body2: TextBody2,
   Tag: TextTag,
 };
 
 const styles = StyleSheet.create({
+  heading1: {
+    fontWeight: '700',
+    fontSize: 24,
+    lineHeight: 29,
+  },
   heading2: {
     fontWeight: '700',
     fontSize: 18,
     lineHeight: 22,
+  },
+  heading3: {
+    fontWeight: '600',
+    fontSize: 16,
+    lineHeight: 24,
   },
   body1: {
     fontWeight: '400',
