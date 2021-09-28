5c6ed0437172bd968d9abe0bc482076a4be1ebf9
Neven <piaf1986@gmail.com>
2021-09-28 20:03:25 +0200

    Add PrimaryLarge and SecondaryLarge buttons to AvaButton

--- app/components/AvaButton.tsx
+++ app/components/AvaButton.tsx
@@ -1,20 +1,22 @@
 import React, {FC, useContext} from 'react';
-import {StyleSheet, TouchableNativeFeedback, View} from 'react-native';
+import {StyleSheet, Text, TouchableNativeFeedback, View} from 'react-native';
 import {ApplicationContext} from 'contexts/ApplicationContext';
 
 interface BaseProps {
   onPress: () => void;
+  disabled?: boolean;
   style?: any;
 }
 
-const AvaButtonBase: FC<BaseProps> = ({onPress, style, children}) => {
+const AvaButtonBase: FC<BaseProps> = ({onPress, style, disabled, children}) => {
   const theme = useContext(ApplicationContext).theme;
   return (
     <TouchableNativeFeedback
       useForeground={true}
       onPress={onPress}
-      background={TouchableNativeFeedback.Ripple(theme.buttonRipple, true)}>
-      <View style={[styles.button, style]}>{children}</View>
+      disabled={disabled}
+      background={TouchableNativeFeedback.Ripple(theme.buttonRipple, false)}>
+      <View style={style}>{children}</View>
     </TouchableNativeFeedback>
   );
 };
@@ -27,23 +29,103 @@ const AvaButtonIcon: FC<BaseProps> = ({onPress, children}) => {
   );
 };
 
+const BtnPrimaryLarge: FC<BaseProps> = ({onPress, disabled, children}) => {
+  const theme = useContext(ApplicationContext).theme;
+  return (
+    <AvaButtonBase
+      onPress={onPress}
+      style={[
+        styles.btnPrimaryLarge,
+        {
+          backgroundColor: disabled ? theme.colorDisabled : theme.colorPrimary1,
+        },
+      ]}>
+      <Text
+        style={[
+          styles.btnPrimaryLargeText,
+          {
+            color: disabled ? theme.colorText2 : theme.colorText3,
+          },
+        ]}>
+        {children}
+      </Text>
+    </AvaButtonBase>
+  );
+};
+
+const BtnSecondaryLarge: FC<BaseProps> = ({onPress, disabled, children}) => {
+  const theme = useContext(ApplicationContext).theme;
+  return (
+    <AvaButtonBase
+      onPress={onPress}
+      disabled={disabled}
+      style={[
+        styles.btnSecondaryLarge,
+        {
+          borderColor: disabled ? theme.colorDisabled : theme.colorPrimary1,
+          backgroundColor: theme.transparent,
+        },
+      ]}>
+      <Text
+        style={[
+          styles.btnSecondaryLargeText,
+          {
+            color: disabled ? theme.colorDisabled : theme.colorPrimary1,
+          },
+        ]}>
+        {children}
+      </Text>
+    </AvaButtonBase>
+  );
+};
+
 const AvaButton = {
+  PrimaryLarge: BtnPrimaryLarge,
+  SecondaryLarge: BtnSecondaryLarge,
   Base: AvaButtonBase,
   Icon: AvaButtonIcon,
 };
 
 const styles = StyleSheet.create({
-  button: {
-    padding: 4,
-  },
   buttonIcon: {
     height: 48,
     justifyContent: 'center',
-    paddingHorizontal: 24,
-    paddingVertical: 10,
+    paddingHorizontal: 28,
+    paddingVertical: 14,
     marginHorizontal: 16,
     marginVertical: 8,
     borderRadius: 8,
+  },
+  btnPrimaryLarge: {
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+    borderRadius: 8,
+    width: 'auto',
+    height: 48,
+  },
+  btnSecondaryLarge: {
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+    borderRadius: 8,
+    borderWidth: 2,
+    width: 'auto',
+    height: 48,
+  },
+  btnPrimaryLargeText: {
+    fontSize: 18,
+    lineHeight: 24,
+    fontWeight: '700',
+    fontFamily: 'Inter-Regular',
+    textAlign: 'center',
+  },
+  btnSecondaryLargeText: {
+    fontSize: 18,
+    lineHeight: 22,
+    fontWeight: '700',
+    fontFamily: 'Inter-Regular',
+    textAlign: 'center',
   },
 });
 
