9c206937aac1c4431e99e26bbb1f7bc210b91d06
Neven <piaf1986@gmail.com>
2021-09-29 14:29:46 +0200

    Update HdWalletLogin.tsx to match design
    * Added PrimaryMedium and TextLarge buttons to AvaButton
    * Added Body3, ButtonLarge, ButtonMedium and Button Small to AvaText

--- app/components/AvaText.tsx
+++ app/components/AvaText.tsx
@@ -6,6 +6,15 @@ interface TextProps {
   textStyle?: StyleProp<TextStyle>;
 }
 
+const LargeTitleBold: FC<TextProps> = ({textStyle, children}) => {
+  const theme = useContext(ApplicationContext).theme;
+  return (
+    <Text style={[styles.largeTitleBold, {color: theme.colorText1}, textStyle]}>
+      {children}
+    </Text>
+  );
+};
+
 const TextHeading1: FC<TextProps> = ({textStyle, children}) => {
   const theme = useContext(ApplicationContext).theme;
   return (
@@ -51,6 +60,10 @@ const TextBody2: FC<TextProps> = ({textStyle, children}) => {
   );
 };
 
+const TextBody3: FC<TextProps> = ({textStyle, children}) => {
+  return <Text style={[styles.body3, textStyle]}>{children}</Text>;
+};
+
 const TextTag: FC<TextProps> = ({textStyle, children}) => {
   const theme = useContext(ApplicationContext).theme;
   return (
@@ -60,16 +73,38 @@ const TextTag: FC<TextProps> = ({textStyle, children}) => {
   );
 };
 
+const TextButtonLarge: FC<TextProps> = ({textStyle, children}) => {
+  return <Text style={[styles.textButtonLarge, textStyle]}>{children}</Text>;
+};
+
+const TextButtonMedium: FC<TextProps> = ({textStyle, children}) => {
+  return <Text style={[styles.textButtonMedium, textStyle]}>{children}</Text>;
+};
+
+const TextButtonSmall: FC<TextProps> = ({textStyle, children}) => {
+  return <Text style={[styles.textButtonSmall, textStyle]}>{children}</Text>;
+};
+
 const AvaText = {
+  LargeTitleBold: LargeTitleBold,
   Heading1: TextHeading1,
   Heading2: TextHeading2,
   Heading3: TextHeading3,
   Body1: TextBody1,
   Body2: TextBody2,
+  Body3: TextBody3,
   Tag: TextTag,
+  ButtonLarge: TextButtonLarge,
+  ButtonMedium: TextButtonMedium,
+  ButtonSmall: TextButtonSmall,
 };
 
 const styles = StyleSheet.create({
+  largeTitleBold: {
+    fontWeight: '700',
+    fontSize: 36,
+    lineHeight: 44,
+  },
   heading1: {
     fontWeight: '700',
     fontSize: 24,
@@ -95,10 +130,33 @@ const styles = StyleSheet.create({
     fontSize: 14,
     lineHeight: 17,
   },
+  body3: {
+    fontWeight: '400',
+    fontSize: 12,
+    lineHeight: 15,
+  },
   textTag: {
     fontWeight: 'bold',
     fontSize: 16,
     lineHeight: 24,
+  },
+  textButtonLarge: {
+    fontFamily: 'Inter-Regular',
+    fontSize: 18,
+    lineHeight: 22,
+    fontWeight: '700',
+  },
+  textButtonMedium: {
+    fontFamily: 'Inter-Regular',
+    fontSize: 14,
+    lineHeight: 24,
+    fontWeight: '700',
+  },
+  textButtonSmall: {
+    fontFamily: 'Inter-Regular',
+    fontSize: 12,
+    lineHeight: 16,
+    fontWeight: '700',
   },
 });
 
