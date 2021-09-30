c46a1a2c90479e4f2a6488e510562bc074b195cc
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2021-09-29 22:18:57 -0600

    drawer items

---
+++ app/screens/drawer/components/AdvancedItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Alert} from 'react-native';
+import AvaListItem from 'components/AvaListItem';
+import CarrotSVG from 'components/svg/CarrotSVG';
+
+export default function AdvancedItem() {
+  return (
+    <>
+      <AvaListItem.Custom
+        title={'Advanced'}
+        leftComponent={null}
+        rightComponent={<CarrotSVG />}
+        onPress={() => {
+          Alert.alert('naviagate to advanced');
+        }}
+      />
+    </>
+  );
+}
