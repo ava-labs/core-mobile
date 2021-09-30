0dac2b8fe17929722d14f8339e4509c4b17068e7
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2021-09-30 11:30:37 -0600

    experimenting with container pattern to see if it helps with all the flickering.

--- app/screens/portfolio/PortfolioHeader.tsx
+++ app/screens/portfolio/PortfolioHeader.tsx
@@ -1,57 +1,83 @@
-import React, {useContext} from 'react';
+import React, {FC, memo, useContext} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import AvaListItem from 'components/AvaListItem';
-import {ApplicationContext} from 'contexts/ApplicationContext';
+import {
+  ApplicationContext,
+  ApplicationContextState,
+} from 'contexts/ApplicationContext';
 import {usePortfolio} from 'screens/portfolio/usePortfolio';
-import {useNavigation} from '@react-navigation/native';
+import {
+  DrawerActions,
+  NavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
 import AppNavigation from 'navigation/AppNavigation';
-import { DrawerActions } from '@react-navigation/native';
 
-
-function PortfolioHeader() {
+// experimenting with container pattern and stable props to try to reduce re-renders
+function PortfolioHeaderContainer() {
   const context = useContext(ApplicationContext);
   const navigation = useNavigation();
   const {addressC, balanceTotalInUSD} = usePortfolio();
 
   return (
-    <View pointerEvents="box-none">
-      <View>
-        <AvaListItem.Account
-          accountName={'Account 1'}
-          accountAddress={addressC ?? ''}
-          onRightComponentPress={() => {
-            navigation.navigate(AppNavigation.Wallet.SearchScreen);
-          }}
-          onLeftComponentPress={() => {
-            navigation.dispatch(DrawerActions.openDrawer());
-          }}
-          onAccountPressed={() => {
-            navigation.navigate(AppNavigation.Modal.AccountBottomSheet);
-          }}
-        />
-      </View>
-      <View
-        style={{
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-          flexDirection: 'row',
-        }}>
-        <Text style={[styles.text, {color: context.theme.txtOnBgApp}]}>
-          {balanceTotalInUSD}
-        </Text>
-        <Text
-          style={{
-            fontSize: 16,
-            color: context.theme.txtOnBgApp,
-            paddingLeft: 4,
-            lineHeight: 28,
-          }}>
-          USD
-        </Text>
-      </View>
-    </View>
+    <PortfolioHeader
+      appContext={context}
+      navigation={navigation}
+      addressC={addressC}
+      balanceTotalUSD={balanceTotalInUSD}
+    />
   );
 }
+
+interface PortfolioHeaderProps {
+  appContext: ApplicationContextState;
+  navigation: NavigationProp<ReactNavigation.RootParamList>;
+  addressC: string;
+  balanceTotalUSD: string;
+}
+
+const PortfolioHeader: FC<PortfolioHeaderProps> = memo(
+  ({navigation, addressC, appContext, balanceTotalUSD = 0}) => {
+    return (
+      <View pointerEvents="box-none">
+        <View>
+          <AvaListItem.Account
+            accountName={'Account 1'}
+            accountAddress={addressC ?? ''}
+            onRightComponentPress={() => {
+              navigation.navigate(AppNavigation.Wallet.SearchScreen);
+            }}
+            onLeftComponentPress={() => {
+              navigation.dispatch(DrawerActions.openDrawer());
+            }}
+            onAccountPressed={() => {
+              navigation.navigate(AppNavigation.Modal.AccountBottomSheet);
+            }}
+          />
+        </View>
+        <View
+          style={{
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+            flexDirection: 'row',
+          }}>
+          <Text style={[styles.text, {color: appContext?.theme.txtOnBgApp}]}>
+            {balanceTotalUSD}
+          </Text>
+          <Text
+            style={{
+              fontSize: 16,
+              color: appContext?.theme.txtOnBgApp,
+              paddingLeft: 4,
+              lineHeight: 28,
+            }}>
+            USD
+          </Text>
+        </View>
+      </View>
+    );
+  },
+);
 
 const styles = StyleSheet.create({
   actionsContainer: {
@@ -68,4 +94,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default PortfolioHeader;
+export default PortfolioHeaderContainer;
