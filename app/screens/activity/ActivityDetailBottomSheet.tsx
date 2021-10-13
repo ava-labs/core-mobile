49c242d54fdd78def160e46fd742f11baf7cae6a
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2021-10-13 00:26:10 -0600

    fix bottomsheet background

--- app/screens/activity/ActivityDetailBottomSheet.tsx
+++ app/screens/activity/ActivityDetailBottomSheet.tsx
@@ -1,11 +1,20 @@
-import React, {useCallback, useEffect, useMemo, useRef} from 'react';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+} from 'react';
 import BottomSheet from '@gorhom/bottom-sheet';
-import {InteractionManager, Pressable, StyleSheet} from 'react-native';
+import {InteractionManager, Pressable, StyleSheet, View} from 'react-native';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import ActivityDetailView from 'screens/activity/ActivityDetailView';
+import {ApplicationContext} from 'contexts/ApplicationContext';
+import TabViewBackground from 'screens/portfolio/components/TabViewBackground';
 
 function ActivityDetailBottomSheet() {
   const {goBack} = useNavigation();
+  const theme = useContext(ApplicationContext).theme;
   const route = useRoute();
   const bottomSheetModalRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ['0%', '50%'], []);
@@ -42,6 +51,7 @@ function ActivityDetailBottomSheet() {
         ref={bottomSheetModalRef}
         index={0}
         snapPoints={snapPoints}
+        backgroundComponent={TabViewBackground}
         onChange={handleChange}>
         <ActivityDetailView txItem={txItem} />
       </BottomSheet>
