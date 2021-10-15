6c572c5ff203e6d0f14619f62dd544b38875d0b8
Neven <piaf1986@gmail.com>
2021-10-15 16:42:32 +0200

    Remove unnecessary view

--- app/screens/portfolio/SendReceiveBottomSheet.tsx
+++ app/screens/portfolio/SendReceiveBottomSheet.tsx
@@ -1,24 +1,14 @@
-import React, {
-  FC,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-  useRef,
-} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {FC, useCallback, useEffect, useMemo, useRef} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import BottomSheet, {BottomSheetBackdrop} from '@gorhom/bottom-sheet';
 import TabViewBackground from './components/TabViewBackground';
 import SendTokenStackScreen from 'navigation/SendTokenStackScreen';
-import {ApplicationContext} from 'contexts/ApplicationContext';
 import {Space} from 'components/Space';
 
 const SendReceiveBottomSheet: FC = () => {
   const bottomSheetRef = useRef<BottomSheet>(null);
   const {goBack, canGoBack} = useNavigation();
   const snapPoints = useMemo(() => ['0%', '86%'], []);
-  const theme = useContext(ApplicationContext).theme;
 
   useEffect(() => {
     // intentionally setting delay so animation is visible.
@@ -44,26 +34,17 @@ const SendReceiveBottomSheet: FC = () => {
 
   // renders
   return (
-    <View style={[styles.container, {backgroundColor: theme.transparent}]}>
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={-1}
-        snapPoints={snapPoints}
-        handleComponent={MyHandle}
-        onChange={handleChange}
-        backdropComponent={BottomSheetBackdrop}
-        backgroundComponent={TabViewBackground}>
-        <SendTokenStackScreen onClose={handleClose} />
-      </BottomSheet>
-    </View>
+    <BottomSheet
+      ref={bottomSheetRef}
+      index={-1}
+      snapPoints={snapPoints}
+      handleComponent={MyHandle}
+      onChange={handleChange}
+      backdropComponent={BottomSheetBackdrop}
+      backgroundComponent={TabViewBackground}>
+      <SendTokenStackScreen onClose={handleClose} />
+    </BottomSheet>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
 
 export default SendReceiveBottomSheet;
