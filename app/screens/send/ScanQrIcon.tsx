07758c7e51d33a707bcec2d86b7d5cad0d3dbc83
Neven <piaf1986@gmail.com>
2021-10-21 14:32:25 +0200

    Fix ScanQrIcon offset on iOS

---
+++ app/screens/send/ScanQrIcon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Platform, View} from 'react-native';
+import AvaButton from 'components/AvaButton';
+import QRCode from 'components/svg/QRCode';
+
+export const ScanQrIcon = ({onScanBarcode}: {onScanBarcode: () => void}) => {
+  return (
+    <View
+      style={[
+        {
+          position: 'absolute',
+          right: 0,
+          marginRight: 20,
+          top: 0,
+          marginTop: Platform.OS === 'ios' ? 34 : 40,
+        },
+      ]}>
+      <AvaButton.Icon onPress={onScanBarcode}>
+        <QRCode />
+      </AvaButton.Icon>
+    </View>
+  );
+};
