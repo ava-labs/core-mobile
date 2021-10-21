8575d2e1fc50dbd16d34ecda723ae580b8316b85
Neven <piaf1986@gmail.com>
2021-10-21 15:01:19 +0200

    Fix hidden inputs on send screens

--- app/screens/sendANT/SendANT.tsx
+++ app/screens/sendANT/SendANT.tsx
@@ -11,6 +11,7 @@ import AvaText from 'components/AvaText';
 import FlexSpacer from 'components/FlexSpacer';
 import {SendANTContext} from 'contexts/SendANTContext';
 import {ScanQrIcon} from 'screens/send/ScanQrIcon';
+import {BottomSheetScrollView} from '@gorhom/bottom-sheet';
 
 export default function SendANT(): JSX.Element {
   const context = useContext(ApplicationContext);
@@ -34,93 +35,95 @@ export default function SendANT(): JSX.Element {
   const {navigate} = useNavigation();
 
   return (
-    <View
-      style={[
-        backgroundStyle,
-        {
-          backgroundColor: undefined,
-          paddingStart: 0,
-          paddingEnd: 0,
-          paddingBottom: 0,
-        },
-      ]}>
-      <View style={[{paddingStart: 4, paddingEnd: 4, marginTop: 20}]}>
-        <InputText
-          value={sendAmountString}
-          label="Amount"
-          placeholder="Enter the amount"
-          helperText="$0"
-          errorText={
-            errorMsg?.startsWith('Amount')
-              ? errorMsg
-              : errorMsg?.indexOf('balance') !== -1
-              ? errorMsg
-              : undefined
-          }
-          keyboardType="numeric"
-          onChangeText={text => {
-            clearErrorMsg();
-            setSendAmountString(text);
-          }}
-        />
-        <View style={styles.transactionFee}>
-          <AvaText.Body3
-            textStyle={{
-              textAlign: 'right',
-              color: context.theme.txtListItemSubscript,
-            }}>
-            {'Transaction fee: ' + sendFeeString}
-          </AvaText.Body3>
-        </View>
-      </View>
-
-      <View style={styles.horizontalLayout}>
-        <View style={[{flex: 1, paddingStart: 4, paddingEnd: 4}]}>
+    <BottomSheetScrollView contentContainerStyle={{flexGrow: 1}}>
+      <View
+        style={[
+          backgroundStyle,
+          {
+            backgroundColor: undefined,
+            paddingStart: 0,
+            paddingEnd: 0,
+            paddingBottom: 0,
+          },
+        ]}>
+        <View style={[{paddingStart: 4, paddingEnd: 4, marginTop: 20}]}>
           <InputText
-            label={'Address'}
-            placeholder="Enter the address"
-            multiline={true}
+            value={sendAmountString}
+            label="Amount"
+            placeholder="Enter the amount"
+            helperText="$0"
             errorText={
-              errorMsg?.indexOf('Address') !== -1 ? errorMsg : undefined
+              errorMsg?.startsWith('Amount')
+                ? errorMsg
+                : errorMsg?.indexOf('balance') !== -1
+                ? errorMsg
+                : undefined
             }
+            keyboardType="numeric"
             onChangeText={text => {
-              setAddress(text);
               clearErrorMsg();
+              setSendAmountString(text);
             }}
-            value={destinationAddress}
           />
-          {destinationAddress.length === 0 && (
-            <ScanQrIcon onScanBarcode={onScanBarcode} />
-          )}
+          <View style={styles.transactionFee}>
+            <AvaText.Body3
+              textStyle={{
+                textAlign: 'right',
+                color: context.theme.txtListItemSubscript,
+              }}>
+              {'Transaction fee: ' + sendFeeString}
+            </AvaText.Body3>
+          </View>
         </View>
+
+        <View style={styles.horizontalLayout}>
+          <View style={[{flex: 1, paddingStart: 4, paddingEnd: 4}]}>
+            <InputText
+              label={'Address'}
+              placeholder="Enter the address"
+              multiline={true}
+              errorText={
+                errorMsg?.indexOf('Address') !== -1 ? errorMsg : undefined
+              }
+              onChangeText={text => {
+                setAddress(text);
+                clearErrorMsg();
+              }}
+              value={destinationAddress}
+            />
+            {destinationAddress.length === 0 && (
+              <ScanQrIcon onScanBarcode={onScanBarcode} />
+            )}
+          </View>
+        </View>
+
+        <FlexSpacer />
+
+        <AvaButton.PrimaryLarge
+          disabled={!canSubmit}
+          style={{margin: 16}}
+          onPress={() =>
+            navigate(AppNavigation.SendToken.ConfirmTransactionScreen)
+          }>
+          Next
+        </AvaButton.PrimaryLarge>
+
+        <Modal animationType="fade" transparent={true} visible={loaderVisible}>
+          <Loader message={loaderMsg} />
+        </Modal>
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          onRequestClose={() => setCameraVisible(false)}
+          visible={cameraVisible}>
+          <QrScannerAva
+            onSuccess={data => onBarcodeScanned(data)}
+            onCancel={() => setCameraVisible(false)}
+          />
+        </Modal>
       </View>
-
-      <FlexSpacer />
-
-      <AvaButton.PrimaryLarge
-        disabled={!canSubmit}
-        style={{margin: 16}}
-        onPress={() =>
-          navigate(AppNavigation.SendToken.ConfirmTransactionScreen)
-        }>
-        Next
-      </AvaButton.PrimaryLarge>
-
-      <Modal animationType="fade" transparent={true} visible={loaderVisible}>
-        <Loader message={loaderMsg} />
-      </Modal>
-
-      <Modal
-        animationType="slide"
-        transparent={true}
-        onRequestClose={() => setCameraVisible(false)}
-        visible={cameraVisible}>
-        <QrScannerAva
-          onSuccess={data => onBarcodeScanned(data)}
-          onCancel={() => setCameraVisible(false)}
-        />
-      </Modal>
-    </View>
+    </BottomSheetScrollView>
   );
 }
 
