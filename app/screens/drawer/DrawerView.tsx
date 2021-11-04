e8382bba246e2258929dc1fd154d340279734ce9
Neven <piaf1986@gmail.com>
2021-11-04 23:09:55 +0100

    Match DrawerView.tsx with design

--- app/screens/drawer/DrawerView.tsx
+++ app/screens/drawer/DrawerView.tsx
@@ -1,9 +1,8 @@
-import React, {FC, useEffect, useRef, useState} from 'react';
+import React, {FC, useState} from 'react';
 import {Alert, Modal, Pressable, StyleSheet, View} from 'react-native';
 import {useApplicationContext} from 'contexts/ApplicationContext';
 import AvaLogoSVG from 'components/svg/AvaLogoSVG';
 import AvaText from 'components/AvaText';
-import BottomSheet from '@gorhom/bottom-sheet';
 import {DrawerContentComponentProps} from '@react-navigation/drawer';
 import CurrencyItem from 'screens/drawer/components/CurrencyItem';
 import SecurityItem from 'screens/drawer/components/SecurityItem';
@@ -20,14 +19,8 @@ import {useWalletSetup} from 'hooks/useWalletSetup';
 
 const DrawerView: FC<DrawerContentComponentProps> = ({navigation}) => {
   const context = useApplicationContext();
-  const bottomSheetRef = useRef<BottomSheet>(null);
   const {destroyWallet} = useWalletSetup();
-  const [openNetworkSwitcher, setOpenNetworkSwitcher] = useState(false);
   const [logoutWarningVisible, setLogoutWarningVisible] = useState(false);
-
-  useEffect(() => {
-    bottomSheetRef?.current?.snapTo(openNetworkSwitcher ? 0 : 1);
-  }, [openNetworkSwitcher]);
 
   function toggleDarkLightMode() {
     Alert.alert('Toggle dark/ligt mode');
@@ -57,23 +50,23 @@ const DrawerView: FC<DrawerContentComponentProps> = ({navigation}) => {
     </View>
   );
 
-  const networkSwitcher = (
-    <NetworkSelector
-      isExpanded={openNetworkSwitcher}
-      toggleOpenClose={() => setOpenNetworkSwitcher(!openNetworkSwitcher)}
-      closeDrawer={handleCloseDrawer}
-    />
-  );
+  const networkSwitcher = <NetworkSelector closeDrawer={handleCloseDrawer} />;
 
   const renderContent = () => (
-    <>
-      <ScrollView style={{backgroundColor: context.theme.bgOnBgApp, flex: 1}}>
+    <View
+      style={{
+        borderTopLeftRadius: 16,
+        borderTopRightRadius: 16,
+        flex: 1,
+        backgroundColor: context.theme.colorBg2,
+      }}>
+      <ScrollView>
         <CurrencyItem navigation={navigation} />
         <SecurityItem navigation={navigation} />
         <LegalItem navigation={navigation} />
         {/*<AdvancedItem />*/}
       </ScrollView>
-      <Separator />
+      <Separator style={{marginHorizontal: 16}} />
       <VersionItem />
 
       <AvaButton.TextLarge
@@ -121,71 +114,25 @@ const DrawerView: FC<DrawerContentComponentProps> = ({navigation}) => {
           </AvaButton.TextLarge>
         </View>
       </Modal>
-    </>
-  );
-
-  /**
-   * Controls the background for where the handle is.
-   * Here we use it to match our content background color.
-   */
-  const bottomSheetBackgroundComponent = () => (
-    <View
-      style={[
-        styles.contentContainer,
-        {backgroundColor: context.theme.bgOnBgApp},
-      ]}
-    />
-  );
-
-  /**
-   * Allows customization of the handle. In this case we don't want to show one.
-   */
-  const bottomSheetHandleComponent = () => (
-    <View style={styles.closeLineContainer} />
+    </View>
   );
 
   return (
     <View style={[styles.container, {backgroundColor: context.theme.colorBg1}]}>
       {header}
       {networkSwitcher}
-      <BottomSheet
-        ref={bottomSheetRef}
-        snapPoints={['65%', '83%']}
-        index={1}
-        backgroundComponent={bottomSheetBackgroundComponent}
-        handleComponent={bottomSheetHandleComponent}
-        enableContentPanningGesture={false}
-        enableHandlePanningGesture={false}>
-        {renderContent()}
-      </BottomSheet>
+      {renderContent()}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  contentContainer: {
-    ...StyleSheet.absoluteFillObject,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-  },
-  closeLineContainer: {
-    alignSelf: 'center',
-    height: 20,
-  },
-  closeLine: {
-    width: 40,
-    height: 6,
-    borderRadius: 3,
-    marginTop: 9,
-  },
   container: {
-    paddingHorizontal: 16,
-    marginVertical: 4,
     flex: 1,
   },
   headerContainer: {
+    paddingHorizontal: 16,
     flexDirection: 'row',
-    alignItems: 'center',
     justifyContent: 'flex-start',
     paddingTop: 24,
   },
