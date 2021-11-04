550d60b5e51ef9430b924991330a15fe4a308c34
Neven <piaf1986@gmail.com>
2021-11-04 19:52:03 +0100

    Match keyboard appearance with theme (iOS only)

--- app/components/InputText.tsx
+++ app/components/InputText.tsx
@@ -1,5 +1,5 @@
 import React, {RefObject, useEffect, useRef, useState} from 'react';
-import {InteractionManager, TextInput, View} from 'react-native';
+import {Appearance, InteractionManager, TextInput, View} from 'react-native';
 import {useApplicationContext} from 'contexts/ApplicationContext';
 import TextLabel from 'components/TextLabel';
 import ImgButtonAva from 'components/ImgButtonAva';
@@ -163,6 +163,7 @@ export default function InputText(props: Props | Readonly<Props>) {
           },
         ]}>
         <TextInput
+          keyboardAppearance={Appearance.getColorScheme() || 'default'}
           ref={textInputRef}
           autoCapitalize="none"
           placeholder={props.placeholder}
