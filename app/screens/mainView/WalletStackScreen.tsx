18557b3e572a9107107f1be4cf061a69255992c7
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2021-11-05 03:27:22 -0600

    PM2-1199- login with mnemonic from security prompt

--- app/screens/mainView/WalletStackScreen.tsx
+++ app/screens/mainView/WalletStackScreen.tsx
@@ -33,6 +33,7 @@ import moment from 'moment';
 import ReceiveOnlyBottomSheet from 'screens/portfolio/receive/ReceiveOnlyBottomSheet';
 import AvaText from 'components/AvaText';
 import {MainHeaderOptions} from 'navigation/NavUtils';
+import AppViewModel, {SelectedView} from 'AppViewModel';
 
 type Props = {
   onExit: () => void;
@@ -282,7 +283,10 @@ function WalletStackScreen(props: Props | Readonly<Props>) {
         <Modal visible={showSecurityModal} animationType={'slide'} animated>
           <PinOrBiometryLogin
             onSignInWithRecoveryPhrase={() => {
-              // ignored
+              AppViewModel.immediateLogout().then(() => {
+                AppViewModel.setSelectedView(SelectedView.LoginWithMnemonic);
+                setShowSecurityModal(false);
+              });
             }}
             onEnterWallet={() => {
               setShowSecurityModal(false);
