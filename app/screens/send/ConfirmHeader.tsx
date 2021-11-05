1e16483572c9b68e44d0550d26ee142afe53e1c8
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2021-11-05 11:20:36 -0600

    implemetation of walletContext.clearMnemonic() on logout, removal of modal for loading state on portfolio, small cleanups.

--- app/screens/send/ConfirmHeader.tsx
+++ app/screens/send/ConfirmHeader.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 import AvaButton from 'components/AvaButton';
 import CarrotSVG from 'components/svg/CarrotSVG';
