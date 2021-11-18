2808a2f7e9db4866b0fe800ad71a20861abc9f53
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2021-11-18 15:30:34 -0700

    common Switch component
    PM2-1510

--- app/components/Snackbar.ts
+++ app/components/Snackbar.ts
@@ -3,7 +3,7 @@ const LENGTH_LONG = 5000;
 
 export function ShowSnackBar(text: string, long = false) {
   // provided by `global`. See ContextApp.tsx and index.d.ts at the root of the project.
-  toast.show(text, {
+  global?.toast?.show(text, {
     type: 'normal',
     placement: 'bottom',
     duration: long ? LENGTH_LONG : LENGTH_SHORT,
