5b9f511213d1af92232d6f7ee81111e8c4c9e23e
Neven <piaf1986@gmail.com>
2021-11-29 23:48:14 +0100

    Navigation refactoring, solves behaviour of android's back button

---
+++ app/navigation/wallet/SecurityPrivacyStackScreen.tsx
@@ -0,0 +1,148 @@
+import React, {memo} from 'react';
+import AppNavigation from 'navigation/AppNavigation';
+import PinOrBiometryLogin from 'screens/login/PinOrBiometryLogin';
+import CreatePIN from 'screens/onboarding/CreatePIN';
+import SecurityPrivacy from 'screens/drawer/security/SecurityPrivacy';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/native';
+import CreateWallet from 'screens/onboarding/CreateWallet';
+import {MainHeaderOptions} from 'navigation/NavUtils';
+import {useApplicationContext} from 'contexts/ApplicationContext';
+import {createStackNavigator} from '@react-navigation/stack';
+import BiometricsSDK from 'utils/BiometricsSDK';
+
+const SecurityStack = createStackNavigator();
+
+const SecurityPrivacyScreen = () => {
+  const nav = useNavigation<NativeStackNavigationProp<any>>();
+  return (
+    <SecurityPrivacy
+      onChangePin={() => nav.navigate(AppNavigation.SecurityPrivacy.PinChange)}
+      onShowRecoveryPhrase={() =>
+        nav.navigate(AppNavigation.SecurityPrivacy.ShowRecoveryPhrase)
+      }
+      onTurnOnBiometrics={() =>
+        nav.navigate(AppNavigation.SecurityPrivacy.TurnOnBiometrics)
+      }
+    />
+  );
+};
+
+const PinOrBiometryLoginForPassChange = memo(() => {
+  const {onSavedMnemonic} = useApplicationContext().appHook;
+  const nav = useNavigation<NativeStackNavigationProp<any>>();
+
+  return (
+    <PinOrBiometryLogin
+      onLoginSuccess={mnemonic => {
+        onSavedMnemonic(mnemonic, true);
+        nav.replace(AppNavigation.SecurityPrivacy.CreatePin);
+      }}
+      onSignInWithRecoveryPhrase={() => console.log('onSignIn')}
+      isResettingPin
+    />
+  );
+});
+
+const PinOrBiometryLoginForRecoveryReveal = memo(() => {
+  const {onSavedMnemonic} = useApplicationContext().appHook;
+  const nav = useNavigation<NativeStackNavigationProp<any>>();
+
+  return (
+    <PinOrBiometryLogin
+      onLoginSuccess={mnemonic => {
+        onSavedMnemonic(mnemonic, true);
+        nav.replace(AppNavigation.SecurityPrivacy.RecoveryPhrase);
+      }}
+      onSignInWithRecoveryPhrase={() => console.log('onSignIn')}
+      hideLoginWithMnemonic
+    />
+  );
+});
+
+const PinForBiometryEnable = memo(() => {
+  const nav = useNavigation<NativeStackNavigationProp<any>>();
+
+  return (
+    <PinOrBiometryLogin
+      onLoginSuccess={mnemonic => {
+        BiometricsSDK.storeWalletWithBiometry(mnemonic).then(() =>
+          nav.navigate(AppNavigation.SecurityPrivacy.SecurityPrivacy),
+        );
+      }}
+      onSignInWithRecoveryPhrase={() => console.log('onSignIn')}
+      isResettingPin
+    />
+  );
+});
+
+const CreatePinScreen = memo(() => {
+  const {onPinCreated} = useApplicationContext().appHook;
+  const nav = useNavigation<NativeStackNavigationProp<any>>();
+  return (
+    <CreatePIN
+      onBack={() => nav.goBack()}
+      onPinSet={pin => {
+        onPinCreated(pin, true).subscribe({
+          error: () => console.log('ignored'),
+        });
+        nav.goBack();
+      }}
+      isResettingPin
+    />
+  );
+});
+
+const CreateWalletScreen = memo(() => {
+  const nav = useNavigation<NativeStackNavigationProp<any>>();
+
+  return (
+    <CreateWallet onBack={() => nav.goBack()} isRevealingCurrentMnemonic />
+  );
+});
+
+function SecurityPrivacyStackScreen(): JSX.Element {
+  return (
+    <SecurityStack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+      }}>
+      <SecurityStack.Group>
+        <SecurityStack.Screen
+          options={MainHeaderOptions('Security & Privacy')}
+          name={AppNavigation.SecurityPrivacy.SecurityPrivacy}
+          component={SecurityPrivacyScreen}
+        />
+      </SecurityStack.Group>
+      <SecurityStack.Group screenOptions={{presentation: 'modal'}}>
+        <SecurityStack.Screen
+          options={MainHeaderOptions('Enter your pin')}
+          name={AppNavigation.SecurityPrivacy.PinChange}
+          component={PinOrBiometryLoginForPassChange}
+        />
+        <SecurityStack.Screen
+          options={MainHeaderOptions('Set new pin')}
+          name={AppNavigation.SecurityPrivacy.CreatePin}
+          component={CreatePinScreen}
+        />
+        <SecurityStack.Screen
+          options={MainHeaderOptions('Enter your pin')}
+          name={AppNavigation.SecurityPrivacy.ShowRecoveryPhrase}
+          component={PinOrBiometryLoginForRecoveryReveal}
+        />
+        <SecurityStack.Screen
+          options={MainHeaderOptions('Enter your pin')}
+          name={AppNavigation.SecurityPrivacy.TurnOnBiometrics}
+          component={PinForBiometryEnable}
+        />
+        <SecurityStack.Screen
+          options={MainHeaderOptions('Recovery phrase')}
+          name={AppNavigation.SecurityPrivacy.RecoveryPhrase}
+          component={CreateWalletScreen}
+        />
+      </SecurityStack.Group>
+    </SecurityStack.Navigator>
+  );
+}
+
+export default SecurityPrivacyStackScreen;
--- app/screens/drawer/security/SecurityPrivacy.tsx
+++ app/screens/drawer/security/SecurityPrivacy.tsx
@@ -2,95 +2,54 @@ import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 import {useApplicationContext} from 'contexts/ApplicationContext';
 import AvaListItem from 'components/AvaListItem';
-import {StackActions, useNavigation} from '@react-navigation/native';
-import BiometricsSDK, {KeystoreConfig} from 'utils/BiometricsSDK';
-import AppNavigation from 'navigation/AppNavigation';
+import BiometricsSDK from 'utils/BiometricsSDK';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {SECURE_ACCESS_SET} from 'resources/Constants';
-import {UserCredentials} from 'react-native-keychain';
 import Switch from 'components/Switch';
 
-function SecurityPrivacy() {
+function SecurityPrivacy({
+  onChangePin,
+  onShowRecoveryPhrase,
+  onTurnOnBiometrics,
+}: {
+  onChangePin: () => void;
+  onShowRecoveryPhrase: () => void;
+  onTurnOnBiometrics: () => void;
+}) {
   const theme = useApplicationContext().theme;
-  const {dispatch} = useNavigation();
   const [isBiometricSwitchEnabled, setIsBiometricSwitchEnabled] =
     useState(false);
   const [isBiometricEnabled, setIsBiometricEnabled] = useState(false);
-  const {onSavedMnemonic} = useApplicationContext().appHook;
 
   useEffect(() => {
     BiometricsSDK.canUseBiometry().then((biometryAvailable: boolean) => {
       setIsBiometricEnabled(biometryAvailable);
     });
-    AsyncStorage.getItem(SECURE_ACCESS_SET).then(type =>
-      setIsBiometricSwitchEnabled(type === 'BIO'),
-    );
+    AsyncStorage.getItem(SECURE_ACCESS_SET).then(type => {
+      setIsBiometricSwitchEnabled(type === 'BIO');
+    });
   }, []);
-
-  const resetPinAction = StackActions.push(
-    AppNavigation.CreateWallet.CreatePin,
-  );
-  const showMnemonicAction = StackActions.push(
-    AppNavigation.CreateWallet.CreateWallet,
-  );
-  const resetAction = StackActions.push(AppNavigation.Onboard.Login);
-  const revealAction = StackActions.push(AppNavigation.Onboard.Login, {
-    revealMnemonic: (mnemonic: string) => {
-      onSavedMnemonic(mnemonic, true);
-      dispatch(showMnemonicAction);
-    },
-  });
 
   const handleSwitchChange = (value: boolean) => {
     setIsBiometricSwitchEnabled(value);
     if (value) {
-      dispatch(
-        StackActions.push(AppNavigation.Onboard.Login, {
-          revealMnemonic: async (mnemonic: string) => {
-            await BiometricsSDK.storeWalletWithBiometry(mnemonic);
-          },
-        }),
-      );
+      onTurnOnBiometrics();
     } else {
       AsyncStorage.setItem(SECURE_ACCESS_SET, 'PIN');
-    }
-  };
-
-  const handleReset = async () => {
-    if (isBiometricSwitchEnabled) {
-      const mnemonic = (await BiometricsSDK.loadWalletKey(
-        KeystoreConfig.KEYSTORE_BIO_OPTIONS,
-      )) as UserCredentials;
-      onSavedMnemonic(mnemonic.password, true);
-      dispatch(resetPinAction);
-    } else {
-      dispatch(resetAction);
-    }
-  };
-
-  const handleReveal = async () => {
-    if (isBiometricSwitchEnabled) {
-      const mnemonic = (await BiometricsSDK.loadWalletKey(
-        KeystoreConfig.KEYSTORE_BIO_OPTIONS,
-      )) as UserCredentials;
-      onSavedMnemonic(mnemonic.password, true);
-      dispatch(showMnemonicAction);
-    } else {
-      dispatch(revealAction);
     }
   };
 
   return (
     <View style={{backgroundColor: theme.colorBg2}}>
       <AvaListItem.Base
-        title={'Change password'}
+        title={'Change PIN'}
         showNavigationArrow
-        onPress={handleReset}
+        onPress={onChangePin}
       />
       <AvaListItem.Base
         title={'Show recovery phrase'}
         showNavigationArrow
-        onPress={handleReveal}
+        onPress={onShowRecoveryPhrase}
       />
       {isBiometricEnabled && (
         <AvaListItem.Base
