3dbc84465526efc35a4d87283bae0a280dbc5434
Neven <piaf1986@gmail.com>
2021-12-17 17:41:29 +0100

    Add perform swap funcitonality (WIP)

--- app/swap/getSwapRate.ts
+++ app/swap/getSwapRate.ts
@@ -1,14 +1,11 @@
 import {WalletType} from '@avalabs/avalanche-wallet-sdk';
-import {
-  ERC20,
-  TokenWithBalance,
-  wallet$,
-} from '@avalabs/wallet-react-components';
+import {TokenWithBalance, wallet$} from '@avalabs/wallet-react-components';
 import {APIError, ParaSwap, SwapSide} from 'paraswap';
 import {OptimalRate} from 'paraswap-core';
 import {firstValueFrom} from 'rxjs';
 import {paraSwap$} from './swap';
 import {getDecimalsForEVM} from 'utils/TokenTools';
+import {getSrcToken, incrementalPromiseResolve, resolve} from 'swap/utils';
 
 const SERVER_BUSY_ERROR = 'Server too busy';
 
@@ -80,46 +77,9 @@ export async function getSwapRate(request: {
     checkForErrorsInResult,
   );
 
+  console.log('----------result', result);
+
   return {
     result,
   };
-}
-
-export function resolve<T = any>(promise: Promise<T>) {
-  try {
-    return promise.then(res => [res, null]).catch(err => [null, err]);
-  } catch (err) {
-    return Promise.resolve([null, err]);
-  }
-}
-
-async function incrementalPromiseResolve<T>(
-  prom: () => Promise<T>,
-  errorParser: (res: any) => boolean,
-  increment = 0,
-  maxTries = 10,
-) {
-  const res = await incrementAndCall(prom(), Math.pow(2, increment));
-  if (maxTries === increment) {
-    return res;
-  }
-  if (errorParser(res)) {
-    return incrementalPromiseResolve(() => prom(), errorParser, increment + 1);
-  }
-  return res;
-}
-
-function incrementAndCall<T>(prom: Promise<T>, interval = 0) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      prom.then(res => resolve(res));
-    }, 500 * interval);
-  });
-}
-
-function getSrcToken(token: TokenWithBalance) {
-  if (token.isErc20) {
-    return (token as ERC20).address;
-  }
-  return token.symbol;
 }
