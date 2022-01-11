cb170dd14340264fa017204b1d177863f34e62df
Neven <piaf1986@gmail.com>
2022-01-11 21:20:45 +0100

    Add loading screen for swap

--- app/screens/swap/components/SwapFeesBottomSheet.tsx
+++ app/screens/swap/components/SwapFeesBottomSheet.tsx
@@ -1,5 +1,8 @@
-import React, {useCallback, useEffect, useMemo, useRef} from 'react';
-import BottomSheet, {BottomSheetBackdrop, BottomSheetScrollView} from '@gorhom/bottom-sheet';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetScrollView,
+} from '@gorhom/bottom-sheet';
 import {InteractionManager, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import TabViewBackground from 'screens/portfolio/components/TabViewBackground';
@@ -11,11 +14,18 @@ import {Space} from 'components/Space';
 import {popableContent} from 'screens/swap/components/SwapTransactionDetails';
 import {useApplicationContext} from 'contexts/ApplicationContext';
 
-function SwapFeesBottomSheet(): JSX.Element {
+function SwapFeesBottomSheet({
+  onSave,
+}: {
+  onSave: (gasLimit: number, maxPriorityFee: number, maxFee: number) => void;
+}): JSX.Element {
   const navigation = useNavigation();
   const {theme} = useApplicationContext();
   const bottomSheetModalRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ['0%', '60%'], []);
+  const [gasLimit, setGasLimit] = useState<number>();
+  const [maxPriorityFee, setMaxPriorityFee] = useState<number>();
+  const [maxFee, setMaxFee] = useState<number>();
 
   useEffect(() => {
     // intentionally setting delay so animation is visible.
@@ -46,6 +56,18 @@ function SwapFeesBottomSheet(): JSX.Element {
     theme.colorBg3,
   );
 
+  const doSave = () => {
+    if (
+      gasLimit === undefined ||
+      maxPriorityFee === undefined ||
+      maxFee === undefined
+    ) {
+      handleClose();
+      return;
+    }
+    onSave(gasLimit, maxPriorityFee, maxFee);
+  };
+
   return (
     <BottomSheet
       backdropComponent={BottomSheetBackdrop}
@@ -70,20 +92,25 @@ function SwapFeesBottomSheet(): JSX.Element {
           </AvaText.Body3>
           <InputText
             label={'Gas Limit ⓘ'}
+            mode={'amount'}
             popOverInfoText={gasLimitInfoInfoMessage}
-            placeholder={'Gas Limit'}
+            onChangeText={text => setGasLimit(Number(text) || 0)}
           />
           <InputText
             label={'Max priority fee (GWEI) ⓘ'}
             popOverInfoText={priorityFeeInfoMessage}
             placeholder={'GWEI'}
+            onChangeText={text => setMaxPriorityFee(Number(text) || 0)}
           />
           <InputText
             label={'Max fee ⓘ'}
             popOverInfoText={maxFeeInfoMessage}
             placeholder={'GWEI'}
+            onChangeText={text => setMaxFee(Number(text) || 0)}
           />
-          <AvaButton.PrimaryLarge style={{marginHorizontal: 12}}>
+          <AvaButton.PrimaryLarge
+            style={{marginHorizontal: 12}}
+            onPress={doSave}>
             Save
           </AvaButton.PrimaryLarge>
         </BottomSheetScrollView>
