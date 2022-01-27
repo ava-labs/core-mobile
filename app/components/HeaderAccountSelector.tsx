29d8e5639d09b3bea690d38e6616348f081983a6
Neven <piaf1986@gmail.com>
2022-01-27 19:35:25 +0100

    Make HeaderAccountSelector navigation-agnostic

--- app/components/HeaderAccountSelector.tsx
+++ app/components/HeaderAccountSelector.tsx
@@ -1,37 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {useApplicationContext} from 'contexts/ApplicationContext';
-import {useNavigation} from '@react-navigation/native';
-import AppNavigation from 'navigation/AppNavigation';
 import AvaButton from 'components/AvaButton';
 import {StyleSheet, View} from 'react-native';
 import AvaText from 'components/AvaText';
 import CarrotSVG from 'components/svg/CarrotSVG';
 import {Account} from 'dto/Account';
 
-export default function HeaderAccountSelector() {
+export type Direction = 'up' | 'down';
+
+export default function HeaderAccountSelector({
+  onPressed,
+  direction,
+}: {
+  onPressed?: () => void;
+  direction?: Direction;
+}) {
   const {accounts} = useApplicationContext().repo.accountsRepo;
   const [activeAccount, setActiveAccount] = useState<Account | undefined>();
   const theme = useApplicationContext().theme;
-  const navigation = useNavigation();
+  const rotation = useMemo(
+    () => (direction === 'up' ? '-90deg' : '90deg'),
+    [direction],
+  );
 
   useEffect(() => {
     const activeAcc = [...accounts.values()].find(acc => acc.active);
     setActiveAccount(activeAcc);
   }, [accounts]);
 
-  function onAccountPressed() {
-    navigation.navigate(AppNavigation.Modal.AccountBottomSheet);
-  }
-
   return (
-    <AvaButton.Base onPress={onAccountPressed}>
+    <AvaButton.Base onPress={onPressed}>
       <View style={[styles.accountTitleContainer]}>
         <AvaText.Heading3
           ellipsizeMode={'middle'}
           textStyle={{marginRight: 16}}>
           {activeAccount?.title}
         </AvaText.Heading3>
-        <View style={{transform: [{rotate: '90deg'}]}}>
+        <View style={{transform: [{rotate: rotation}]}}>
           <CarrotSVG color={theme.txtListItem} />
         </View>
       </View>
