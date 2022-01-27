311161a600197f5fe0cd0fd456783c77fd52313e
Neven <piaf1986@gmail.com>
2022-01-27 16:54:19 +0100

    Make smaller changes:
    * Enable ellipsizeMode on TextHeading2 and TextButtonSmall
    * Remove alignItems from Row
    * Add colorAccent to Constants
    * Enable exhaustive-deps, disable no-inline-styles rules in eslintrc

--- app/components/Row.tsx
+++ app/components/Row.tsx
@@ -7,7 +7,6 @@ export const Row: FC<ViewProps> = ({style, children}) => {
       style={[
         {
           flexDirection: 'row',
-          alignItems: 'baseline',
         },
         style,
       ]}>
