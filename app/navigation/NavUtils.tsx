7580d66748ae21c38d7857876e7d3c9ccc10df2b
Neven <piaf1986@gmail.com>
2022-02-02 00:45:07 +0100

    Expand MainHeaderOptions to accept actionable component

--- app/navigation/NavUtils.tsx
+++ app/navigation/NavUtils.tsx
@@ -2,14 +2,28 @@ import {StackNavigationOptions} from '@react-navigation/stack';
 import AvaText from 'components/AvaText';
 import React from 'react';
 import {BottomTabNavigationOptions} from '@react-navigation/bottom-tabs';
+import {Row} from 'components/Row';
 
 export const MainHeaderOptions = (
   title: string,
   hideHeaderLeft = false,
+  actionComponent?: React.ReactNode,
 ): Partial<StackNavigationOptions | BottomTabNavigationOptions> => {
   return {
     headerShown: true,
-    headerTitle: () => <AvaText.Heading1>{title} </AvaText.Heading1>,
+    headerTitle: () => {
+      return (
+        <Row
+          style={{
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <AvaText.Heading1>{title} </AvaText.Heading1>
+          {actionComponent}
+        </Row>
+      );
+    },
     headerTitleAlign: 'left',
     headerLeft: hideHeaderLeft ? () => null : undefined,
     headerBackTitleVisible: false,
