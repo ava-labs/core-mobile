06dcec09a3904bd2d73e450c5db613d50ac7de91
Neven <piaf1986@gmail.com>
2022-02-02 21:50:30 +0100

    Bump wallet-react-components
    - requires promise.allsettled shim
    - we need to be synced with rxjs version

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ import {name as appName} from './app.json';
 
 require('node-libs-react-native/globals');
 require('react-native-crypto');
+require('promise.allsettled').shim();
+
 
 AppRegistry.registerComponent(appName, () => ContextApp);
