11179a43f7cb680906f54089a5c24bbcbc9f365c
Neven <piaf1986@gmail.com>
2022-02-02 01:32:34 +0100

    Remember recently selected contacts

--- app/Repo.ts
+++ app/Repo.ts
@@ -8,6 +8,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
  */
 const WALLET_ID = 'WALLET_ID';
 const ADDR_BOOK = 'ADDR_BOOK_1';
+const ADDR_BOOK_RECENTS = 'ADDR_BOOK_RECENTS';
 
 type AccountId = number;
 type UID = string;
@@ -27,21 +28,27 @@ export type Repo = {
   addressBookRepo: {
     addressBook: Map<UID, Contact>;
     saveAddressBook: (addressBook: Map<UID, Contact>) => void;
+    recentContacts: UID[];
+    addToRecentContacts: (contactId: UID) => void;
   };
 };
 
 export function useRepo(): Repo {
   const [accounts, setAccounts] = useState<Map<AccountId, Account>>(new Map());
   const [addressBook, setAddressBook] = useState<Map<UID, Contact>>(new Map());
+  const [recentContacts, setRecentContacts] = useState<UID[]>([]);
 
   useEffect(() => {
     loadAccountsFromStorage().then(value => setAccounts(value));
     loadAddressBookFromStorage().then(value => setAddressBook(value));
+    loadRecentContactsFromStorage().then(value => setRecentContacts(value));
   }, []);
 
   const saveAccounts = (accounts: Map<AccountId, Account>) => {
     setAccounts(new Map(accounts));
-    saveAccountsToStorage(WALLET_ID, accounts).catch(reason => console.error());
+    saveAccountsToStorage(WALLET_ID, accounts).catch(reason =>
+      console.error(reason),
+    );
   };
 
   const setActiveAccount = (accountIndex: number) => {
@@ -51,12 +58,28 @@ export function useRepo(): Repo {
 
   const saveAddressBook = (addrBook: Map<UID, Contact>) => {
     setAddressBook(new Map(addrBook));
-    saveAddressBookToStorage(addrBook).catch(reason => console.error());
+    saveAddressBookToStorage(addrBook).catch(reason => console.error(reason));
+  };
+
+  const addToRecentContacts = (contactId: UID) => {
+    const newRecents = [
+      contactId,
+      ...recentContacts.filter(value => value !== contactId),
+    ].slice(0, 9); //save max 10 recents
+    setRecentContacts(newRecents);
+    saveRecentContactsToStorage(newRecents).catch(reason =>
+      console.error(reason),
+    );
   };
 
   return {
     accountsRepo: {accounts, saveAccounts, setActiveAccount},
-    addressBookRepo: {addressBook, saveAddressBook},
+    addressBookRepo: {
+      addressBook,
+      saveAddressBook,
+      recentContacts,
+      addToRecentContacts,
+    },
   };
 }
 
@@ -72,6 +95,11 @@ async function loadAddressBookFromStorage() {
   return rawAddrBook
     ? (new Map(JSON.parse(rawAddrBook)) as Map<UID, Contact>)
     : new Map<UID, Contact>();
+}
+
+async function loadRecentContactsFromStorage() {
+  const rawRecents = await AsyncStorage.getItem(ADDR_BOOK_RECENTS);
+  return rawRecents ? (JSON.parse(rawRecents) as UID[]) : ([] as UID[]);
 }
 
 async function saveAccountsToStorage(
@@ -92,5 +120,14 @@ async function saveAddressBookToStorage(addrBook: Map<UID, Contact>) {
     console.error(addrBook);
   } else {
     await AsyncStorage.setItem(ADDR_BOOK, stringifiedAddrBook);
+  }
+}
+
+async function saveRecentContactsToStorage(recent: UID[]) {
+  const stringifiedRecentContacts = JSON.stringify([...recent]);
+  if (stringifiedRecentContacts === undefined) {
+    console.error(recent);
+  } else {
+    await AsyncStorage.setItem(ADDR_BOOK_RECENTS, stringifiedRecentContacts);
   }
 }
