c46901150f09678b23a42345194a30010c35d387
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2022-02-03 13:19:14 -0700

    some cleanup from previous PR

--- app/navigation/NavUtils.tsx
+++ app/navigation/NavUtils.tsx
@@ -1,14 +1,12 @@
 import {StackNavigationOptions} from '@react-navigation/stack';
 import AvaText from 'components/AvaText';
 import React from 'react';
-import {BottomTabNavigationOptions} from '@react-navigation/bottom-tabs';
 import {Row} from 'components/Row';
 
 export const MainHeaderOptions = (
   title: string,
   hideHeaderLeft = false,
-  actionComponent?: React.ReactNode,
-): Partial<StackNavigationOptions | BottomTabNavigationOptions> => {
+): Partial<StackNavigationOptions> => {
   return {
     headerShown: true,
     headerTitle: () => {
@@ -33,25 +31,4 @@ export const MainHeaderOptions = (
       shadowOpacity: 0,
     },
   };
-};
-
-export const SubHeaderOptions = (
-  title: string,
-  hideHeaderLeft = false,
-): Partial<StackNavigationOptions> => {
-  const options: Partial<StackNavigationOptions | BottomTabNavigationOptions> =
-    {
-      headerShown: true,
-      headerTitle: () => <AvaText.Heading1>{title}</AvaText.Heading1>,
-      headerTitleAlign: 'center',
-      headerLeft: hideHeaderLeft ? () => null : undefined,
-      headerBackTitleVisible: false,
-      headerStyle: {
-        shadowColor: 'transparent',
-        elevation: 0,
-        shadowOpacity: 0,
-      },
-    };
-
-  return options;
 };
