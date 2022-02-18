b0c4502a29d26cf8e8bad37e795aecdc1afb8573
Neven <piaf1986@gmail.com>
2022-02-18 16:43:07 +0100

    Added fancy animation of logo on splash screen

---
+++ app/components/AnimatedCoreXLogo.tsx
@@ -0,0 +1,58 @@
+import React, {useEffect, useRef} from 'react';
+import {Animated, Easing, View} from 'react-native';
+import SplashLogoSVG from 'components/svg/SplashLogoSVG';
+import CoreXLogoAnimated from 'components/CoreXLogoAnimated';
+
+const AnimatedCoreXLogo = ({finalState}: {finalState?: boolean}) => {
+  const animTranslateX = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.timing(animTranslateX, {
+      toValue: -82,
+      duration: finalState ? 0 : 600,
+      useNativeDriver: true,
+      easing: Easing.inOut(Easing.ease),
+      delay: finalState ? 0 : 3000,
+    }).start();
+  }, []);
+
+  const animWidth = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.timing(animWidth, {
+      toValue: 220,
+      duration: finalState ? 0 : 840,
+      useNativeDriver: false,
+      easing: Easing.inOut(Easing.ease),
+      delay: finalState ? 0 : 3000,
+    }).start();
+  }, []);
+  return (
+    <View
+      style={{
+        alignItems: 'center',
+        minHeight: 400,
+        justifyContent: 'center',
+      }}>
+      <Animated.View
+        style={{
+          position: 'absolute',
+          overflow: 'hidden',
+          alignItems: 'center',
+          width: animWidth,
+        }}>
+        <SplashLogoSVG />
+      </Animated.View>
+      <Animated.View
+        style={{
+          transform: [
+            {
+              translateX: animTranslateX,
+            },
+          ],
+        }}>
+        <CoreXLogoAnimated finalState={finalState} size={65} />
+      </Animated.View>
+    </View>
+  );
+};
+
+export default AnimatedCoreXLogo;
