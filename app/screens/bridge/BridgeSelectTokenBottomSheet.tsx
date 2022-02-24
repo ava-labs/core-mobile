3111e588116a51dfcce4620c7430debefd1b2776
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2022-02-23 17:39:44 -0700

    restructuring, deleted and new component and hooks

---
+++ app/screens/bridge/BridgeSelectTokenBottomSheet.tsx
@@ -0,0 +1,57 @@
+import React, {useCallback, useEffect, useMemo, useRef} from 'react';
+import BottomSheet, {BottomSheetBackdrop} from '@gorhom/bottom-sheet';
+import {InteractionManager} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import TabViewBackground from 'screens/portfolio/components/TabViewBackground';
+import AvaxSheetHandle from 'components/AvaxSheetHandle';
+import TokenSelector from 'screens/send/TokenSelector';
+import AvaText from 'components/AvaText';
+import {TokenWithBalance} from '@avalabs/wallet-react-components';
+
+function SelectTokenBottomSheet(): JSX.Element {
+  const navigation = useNavigation();
+  const bottomSheetModalRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ['0%', '90%'], []);
+  const route = useRoute();
+
+  useEffect(() => {
+    // intentionally setting delay so animation is visible.
+    setTimeout(() => {
+      bottomSheetModalRef?.current?.snapTo(1);
+    }, 100);
+  }, []);
+
+  function onTokenSelected(token: TokenWithBalance) {
+    handleClose();
+    route.params.onTokenSelected(token);
+  }
+
+  const handleClose = useCallback(() => {
+    bottomSheetModalRef?.current?.close();
+    InteractionManager.runAfterInteractions(() => navigation.goBack());
+  }, []);
+
+  const handleChange = useCallback(index => {
+    index === 0 && handleClose();
+  }, []);
+
+  return (
+    <BottomSheet
+      backdropComponent={BottomSheetBackdrop}
+      handleComponent={AvaxSheetHandle}
+      ref={bottomSheetModalRef}
+      index={0}
+      snapPoints={snapPoints}
+      backgroundComponent={TabViewBackground}
+      onChange={handleChange}>
+      <>
+        <AvaText.LargeTitleBold textStyle={{marginHorizontal: 16}}>
+          Select Token
+        </AvaText.LargeTitleBold>
+        <TokenSelector onTokenSelected={onTokenSelected} />
+      </>
+    </BottomSheet>
+  );
+}
+
+export default SelectTokenBottomSheet;
---
+++ app/screens/bridge/BridgeTokenSelector.tsx
@@ -0,0 +1,100 @@
+import React, {RefObject, useEffect, useRef} from 'react';
+import {
+  FlatList,
+  InteractionManager,
+  ListRenderItemInfo,
+  TextInput,
+  View,
+} from 'react-native';
+import {TokenWithBalance} from '@avalabs/wallet-react-components';
+import {useSearchableTokenList} from 'screens/portfolio/useSearchableTokenList';
+import Loader from 'components/Loader';
+import ZeroState from 'components/ZeroState';
+import PortfolioListItem from 'screens/portfolio/components/PortfolioListItem';
+import {Space} from 'components/Space';
+import {getTokenUID} from 'utils/TokenTools';
+import SearchBar from 'components/SearchBar';
+
+const DEFAULT_HORIZONTAL_MARGIN = 16;
+
+interface TokenSelectorProps {
+  onTokenSelected: (token: TokenWithBalance) => void;
+  horizontalMargin?: number;
+}
+
+function TokenSelector({
+  onTokenSelected,
+  horizontalMargin = DEFAULT_HORIZONTAL_MARGIN,
+}: TokenSelectorProps) {
+  const {filteredTokenList, searchText, setSearchText, loadTokenList} =
+    useSearchableTokenList(false);
+  const textInputRef = useRef() as RefObject<TextInput>;
+
+  useEffect(() => {
+    InteractionManager.runAfterInteractions(() => {
+      setTimeout(() => {
+        textInputRef.current?.focus();
+      }, 300); //delay is for some weird bug effect when opening select token on swap page
+    });
+  }, [textInputRef]);
+
+  function handleRefresh() {
+    loadTokenList();
+  }
+
+  const renderItem = (item: ListRenderItemInfo<TokenWithBalance>) => {
+    const token = item.item;
+    return (
+      <PortfolioListItem
+        tokenName={token.name}
+        tokenPrice={token.balanceDisplayValue ?? '0'}
+        tokenPriceUsd={token.balanceUsdDisplayValue}
+        image={token?.logoURI}
+        symbol={token.symbol}
+        onPress={() => {
+          onTokenSelected(token);
+        }}
+      />
+    );
+  };
+
+  const handleSearch = (text: string) => {
+    setSearchText(text);
+  };
+
+  function getNoResultsText() {
+    if (
+      !filteredTokenList ||
+      (filteredTokenList &&
+        filteredTokenList?.length === 0 &&
+        (!searchText || (searchText && searchText.length === 0)))
+    ) {
+      return 'You have no tokens to send';
+    }
+    return undefined;
+  }
+
+  return (
+    <View style={{flex: 1, marginHorizontal: horizontalMargin}}>
+      <SearchBar onTextChanged={handleSearch} searchText={searchText} />
+      <Space y={16} />
+      {!filteredTokenList ? (
+        <Loader />
+      ) : (
+        <FlatList
+          keyboardShouldPersistTaps="handled"
+          data={filteredTokenList}
+          renderItem={renderItem}
+          onRefresh={handleRefresh}
+          refreshing={false}
+          keyExtractor={(item: TokenWithBalance) => getTokenUID(item)}
+          ListEmptyComponent={
+            <ZeroState.NoResultsGraphical message={getNoResultsText()} />
+          }
+        />
+      )}
+    </View>
+  );
+}
+
+export default TokenSelector;
