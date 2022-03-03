75f3b99e5427f1e78b46746ba7d70dca2b9f67f1
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2022-03-03 08:34:27 -0700

    introducing storybook and dev debug

---
+++ app/utils/debugging/DevDebuggingConfig.ts
@@ -0,0 +1,8 @@
+const DevDebuggingConfig = {
+  STORYBOOK_ENABLED: false,
+  LOGBOX_DISABLED: false,
+  WHY_DID_YOU_RENDER_ENABLED: false,
+  LOGBOX_IGNORED_WARNINGS: [],
+};
+
+export default DevDebuggingConfig;
