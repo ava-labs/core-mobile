9b1b6d9a53df5326b9bc8fbcc830d301fc89ffc7
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2022-03-04 01:24:53 -0700

    sotrybook cleanup

--- storybook/index.js
+++ storybook/index.js
@@ -1,14 +1,12 @@
 // if you use expo remove this line
 import {AppRegistry} from 'react-native';
-import {getStorybookUI, configure, addDecorator} from '@storybook/react-native';
+import {addDecorator, configure, getStorybookUI} from '@storybook/react-native';
 import {withKnobs} from '@storybook/addon-knobs';
 import {loadStories} from './storyLoader';
-
 import './rn-addons';
 import {ApplicationContextProvider} from '../app/contexts/ApplicationContext';
-import {Component} from 'react';
+import {NetworkContextProvider} from '@avalabs/wallet-react-components';
 import useDevDebugging from '../app/utils/debugging/DevDebugging';
-import {FC} from 'react';
 
 // enables knobs for all stories
 addDecorator(withKnobs);
@@ -19,17 +17,16 @@ configure(() => {
 }, module);
 
 addDecorator(getStory => (
-  <ApplicationContextProvider>{getStory()}</ApplicationContextProvider>
+  <NetworkContextProvider>
+    <ApplicationContextProvider>{getStory()}</ApplicationContextProvider>
+  </NetworkContextProvider>
 ));
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({
-  port: 7007,
-  onDeviceUI: true,
   resetStorybook: true,
-  // asyncStorage: require('@react-native-async-storage/async-storage').default,
-  // asyncStorage: require('@react-native-community/async-storage').default,
+  asyncStorage: null,
 });
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
