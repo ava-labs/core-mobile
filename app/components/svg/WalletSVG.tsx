698048b5702dc4284bae8f9a38b52bcd5f707c6a
Neven <piaf1986@gmail.com>
2022-03-08 16:57:39 +0100

    UI fix: icons and text on Welcome screen

--- app/components/svg/WalletSVG.tsx
+++ app/components/svg/WalletSVG.tsx
@@ -21,22 +21,22 @@ function WalletSVG({size = 65, backgroundColor}: Prop) {
         rx="4.875"
         fill={bgColor}
         stroke="white"
-        stroke-width="3.25"
+        strokeWidth="3.25"
       />
       <Path
         d="M4.87518 18.0293H55.2502C57.9426 18.0293 60.1252 20.2119 60.1252 22.9043V51.9995C60.1252 54.6919 57.9426 56.8745 55.2502 56.8745H9.75018C7.05779 56.8745 4.87518 54.6919 4.87518 51.9995V18.0293Z"
         fill={bgColor}
         stroke="white"
-        stroke-width="3.25"
+        strokeWidth="3.25"
       />
       <Rect
-        x="43.8751"
+        x="42"
         y="30.0625"
         width="17.875"
         height="13.8125"
         rx="2.4375"
         stroke="white"
-        stroke-width="6.5"
+        strokeWidth="3.25"
       />
     </Svg>
   );
