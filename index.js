05f2755cf5b3fa9dd3211b2e319718a601842c02
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2022-03-23 16:05:06 -0600

    AvaListItem stories

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import {name as appName} from './app.json';
 import "./shims"
 import DevDebuggingConfig from "./app/utils/debugging/DevDebuggingConfig";
 
-// if (!DevDebuggingConfig.STORYBOOK_ENABLED) {
+if (!DevDebuggingConfig.STORYBOOK_ENABLED) {
   AppRegistry.registerComponent(appName, () => ContextApp);
-// } else {
-//   import('./storybook');
-// }
+} else {
+  import('./storybook');
+}
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,11 +4,13 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+  require('../app/components/__stories__/AvaListItem.stories');
   require('../app/components/__stories__/AvaText.stories');
   require('../app/components/__stories__/ConfirmationTracker.stories');
 }
 
 const stories = [
+  '../app/components/__stories__/AvaListItem.stories',
   '../app/components/__stories__/AvaText.stories',
   '../app/components/__stories__/ConfirmationTracker.stories',
 ];
