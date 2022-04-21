2d513410b107da952b04718006d453bb7609e758
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2022-04-20 23:50:28 -0600

    bridge sdk bump

--- app/screens/bridge/AddBitcoinInstructionsBottomSheet.tsx
+++ app/screens/bridge/AddBitcoinInstructionsBottomSheet.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useCallback, useEffect, useMemo, useRef } from 'react'
 import BottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet'
 import { InteractionManager, StyleSheet, View } from 'react-native'
@@ -25,10 +26,37 @@ function AddBitcoinInstructionsBottomSheet() {
   const wallet = useWalletContext().wallet
   const isMainnet = network?.config ? isMainnetNetwork(network.config) : false
   const btcAddress = wallet?.getAddressBTC(isMainnet ? 'bitcoin' : 'testnet')
+=======
+import React, {useCallback, useEffect, useMemo, useRef} from 'react';
+import BottomSheet, {BottomSheetBackdrop} from '@gorhom/bottom-sheet';
+import {InteractionManager} from 'react-native';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import TabViewBackground from 'screens/portfolio/components/TabViewBackground';
+import AvaxSheetHandle from 'components/AvaxSheetHandle';
+import AvaText from 'components/AvaText';
+import BridgeTokenSelector, {
+  SelectTokenMode,
+} from 'screens/bridge/BridgeTokenSelector';
+import {BridgeStackParamList} from 'navigation/wallet/BridgeScreenStack';
+import AppNavigation from 'navigation/AppNavigation';
+
+function BridgeSelectTokenBottomSheet(): JSX.Element {
+  const navigation = useNavigation();
+  const bottomSheetModalRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ['0%', '90%'], []);
+  const route =
+    useRoute<
+      RouteProp<
+        BridgeStackParamList,
+        typeof AppNavigation.Modal.BridgeSelectToken
+      >
+    >();
+>>>>>>> 538e4cb (bridge sdk bump)
 
   useEffect(() => {
     // intentionally setting delay so animation is visible.
     setTimeout(() => {
+<<<<<<< HEAD
       bottomSheetModalRef?.current?.snapTo(1)
     }, 100)
   }, [])
@@ -41,6 +69,25 @@ function AddBitcoinInstructionsBottomSheet() {
   const handleChange = useCallback(index => {
     index === 0 && handleClose()
   }, [])
+=======
+      bottomSheetModalRef?.current?.snapTo(1);
+    }, 100);
+  }, []);
+
+  function onTokenSelected(symbol: string) {
+    handleClose();
+    route.params.onTokenSelected(symbol);
+  }
+
+  const handleClose = useCallback(() => {
+    bottomSheetModalRef?.current?.close();
+    InteractionManager.runAfterInteractions(() => navigation.goBack());
+  }, []);
+
+  const handleChange = useCallback(index => {
+    index === 0 && handleClose();
+  }, []);
+>>>>>>> 538e4cb (bridge sdk bump)
 
   return (
     <BottomSheet
@@ -51,6 +98,7 @@ function AddBitcoinInstructionsBottomSheet() {
       snapPoints={snapPoints}
       backgroundComponent={TabViewBackground}
       onChange={handleChange}>
+<<<<<<< HEAD
       <View style={{ marginHorizontal: 16, top: 32 }}>
         <AvaText.LargeTitleBold>Add Bitcoin</AvaText.LargeTitleBold>
         <Space y={16} />
@@ -118,3 +166,19 @@ const styles = StyleSheet.create({
 })
 
 export default AddBitcoinInstructionsBottomSheet
+=======
+      <>
+        <AvaText.LargeTitleBold textStyle={{marginHorizontal: 16}}>
+          Select Token
+        </AvaText.LargeTitleBold>
+        <BridgeTokenSelector
+          onTokenSelected={onTokenSelected}
+          selectMode={SelectTokenMode.TRANSFER}
+        />
+      </>
+    </BottomSheet>
+  );
+}
+
+export default BridgeSelectTokenBottomSheet;
+>>>>>>> 538e4cb (bridge sdk bump)
