ac75acb4831d60772ec0bf5a7a2c6a2be3334980
An Nguyen <an.nguyen@avalabs.org>
2022-04-28 10:26:26 -0500

    refactored EditGasLimitBottomSheet and NetworkFeeSelector

--- app/screens/shared/EditGasLimitBottomSheet.tsx
+++ app/screens/shared/EditGasLimitBottomSheet.tsx
@@ -1,19 +1,25 @@
-// @ts-nocheck TODO CP-1725: Fix Typescript Errors - React Navigation
 import React, { useCallback, useEffect, useMemo, useRef } from 'react'
 import BottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet'
 import { InteractionManager } from 'react-native'
-import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
 import TabViewBackground from 'screens/portfolio/components/TabViewBackground'
 import AvaxSheetHandle from 'components/AvaxSheetHandle'
 import EditFees from 'components/EditFees'
-import { SendStackParamList } from 'navigation/wallet/SendScreenStack'
-import { StackNavigationProp } from '@react-navigation/stack'
 
-function EditGasLimitBottomSheet(): JSX.Element {
-  const { goBack } = useNavigation<StackNavigationProp<SendStackParamList>>()
+type Props = {
+  onClose: () => void
+  onSave: (newGasLimit: number) => void
+  gasLimit: string
+  networkFee: string
+}
+
+const EditGasLimitBottomSheet: React.FC<Props> = ({
+  onClose,
+  onSave,
+  gasLimit,
+  networkFee
+}) => {
   const bottomSheetModalRef = useRef<BottomSheet>(null)
   const snapPoints = useMemo(() => ['0%', '90%'], [])
-  const { params } = useRoute<RouteProp<SendStackParamList>>()
 
   useEffect(() => {
     // intentionally setting delay so animation is visible.
@@ -26,7 +32,7 @@ function EditGasLimitBottomSheet(): JSX.Element {
     bottomSheetModalRef?.current?.close()
 
     InteractionManager.runAfterInteractions(() => {
-      goBack()
+      onClose()
     })
   }, [])
 
@@ -45,11 +51,11 @@ function EditGasLimitBottomSheet(): JSX.Element {
       onChange={handleChange}>
       <EditFees
         onSave={newGasLimit => {
-          params.onSave(newGasLimit)
+          onSave(newGasLimit)
           handleClose()
         }}
-        gasLimit={params.gasLimit}
-        networkFee={params.networkFee}
+        gasLimit={gasLimit}
+        networkFee={networkFee}
       />
     </BottomSheet>
   )
