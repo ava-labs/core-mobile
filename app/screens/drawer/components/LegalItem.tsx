c84c5551feffd124511a95bd6cad697538573b78
An Nguyen <an.nguyen@avalabs.org>
2022-05-03 08:42:48 -0600

    CP-1572: add help center item to drawer (#226)

--- app/screens/drawer/components/LegalItem.tsx
+++ app/screens/drawer/components/LegalItem.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import AvaListItem from 'components/AvaListItem'
-import CarrotSVG from 'components/svg/CarrotSVG'
 import useInAppBrowser from 'hooks/useInAppBrowser'
 
 const LEGAL_URL = 'https://wallet.avax.network/legal'
@@ -11,10 +10,6 @@ const LegalItem = () => {
     <>
       <AvaListItem.Base
         title={'Legal'}
-        titleAlignment={'flex-start'}
-        leftComponent={null}
-        rightComponent={<CarrotSVG />}
-        rightComponentVerticalAlignment={'center'}
         onPress={() => {
           openUrl(LEGAL_URL)
         }}
