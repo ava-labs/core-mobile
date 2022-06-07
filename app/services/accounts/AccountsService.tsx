edb6ee72b3410cf607e185caec8e8a164145d662
Neven <piaf1986@gmail.com>
2022-06-07 18:33:06 +0200

    Separate logic from AccountsService from store

--- app/services/accounts/AccountsService.tsx
+++ app/services/accounts/AccountsService.tsx
@@ -1,21 +1,14 @@
 import { BITCOIN_NETWORK, FUJI_NETWORK } from 'store/network'
 import { WalletService } from 'services/wallet/WalletService'
 import { Account } from 'dto/Account'
-import {
-  AccountCollection,
-  addAccount as addAccountToStore,
-  setAccountTitle as setAccountTitleStore,
-  setActiveAccountIndex
-} from 'store/accounts'
-import { Store } from 'redux'
+import { AccountCollection } from 'store/accounts'
 
-export async function addAccount(
+export async function createNextAccount(
   walletService: WalletService,
-  accounts: AccountCollection,
-  store: Store
+  accounts: AccountCollection
 ) {
   const newIndex = Object.keys(accounts).length
-  const newAccount = {
+  return {
     index: newIndex,
     title: `Account ${newIndex + 1}`,
     addressBtc: (
@@ -23,21 +16,4 @@ export async function addAccount(
     ).getAddressBech32(),
     address: walletService.getEvmWallet(newIndex, FUJI_NETWORK).address
   } as Account
-
-  store.dispatch(addAccountToStore(newAccount))
-  return newAccount
-}
-
-export function setAccountTitle(
-  title: string,
-  accountIndex: number,
-  store: Store
-) {
-  store.dispatch(
-    setAccountTitleStore({ title: title, accountIndex: accountIndex })
-  )
-}
-
-export function activateAccount(accountIndex: number, store: Store) {
-  store.dispatch(setActiveAccountIndex(accountIndex))
 }
