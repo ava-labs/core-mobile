12350aad7d337e61aff8f853eef13aa887848af4
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2022-06-08 16:24:01 -0600

    AccountService as a class

--- app/services/accounts/AccountsService.tsx
+++ app/services/accounts/AccountsService.tsx
@@ -1,19 +1,29 @@
-import { WalletService } from 'services/wallet/WalletService'
+import walletService from 'services/wallet/WalletService'
 import { Account } from 'dto/Account'
 import { AccountCollection } from 'store/accounts'
-import { BITCOIN_NETWORK } from '@avalabs/chains-sdk'
+import {
+  BITCOIN_NETWORK,
+  BITCOIN_TEST_NETWORK,
+  Network
+} from '@avalabs/chains-sdk'
 
-export async function createNextAccount(
-  walletService: WalletService,
-  accounts: AccountCollection
-) {
-  const newIndex = Object.keys(accounts).length
-  return {
-    index: newIndex,
-    title: `Account ${newIndex + 1}`,
-    addressBtc: (
-      await walletService.getBtcWallet(newIndex, BITCOIN_NETWORK)
-    ).getAddressBech32(),
-    address: walletService.getEvmWallet(newIndex).address //fixme - get avax c chain
-  } as Account
+class AccountsService {
+  async createNextAccount(network: Network, accounts: AccountCollection) {
+    const newIndex = Object.keys(accounts).length
+    const addressBtc = (
+      await walletService.getBtcWallet(
+        newIndex,
+        network.isTestnet ? BITCOIN_TEST_NETWORK : BITCOIN_NETWORK
+      )
+    ).getAddressBech32()
+    const address = walletService.getEvmWallet(newIndex, network).address
+    return {
+      index: newIndex,
+      title: `Account ${newIndex + 1}`,
+      addressBtc,
+      address
+    } as Account
+  }
 }
+
+export default new AccountsService()
