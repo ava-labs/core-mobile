ea2afd970d8d772462612bfc28f3845cb4b1c50e
Eduardo Gueiros <eduardo@gueiros.net>
2022-06-13 08:12:27 -0600

    fix MoonPay call (#253)

--- app/hooks/useInAppBrowser.ts
+++ app/hooks/useInAppBrowser.ts
@@ -1,29 +1,33 @@
 import { InAppBrowser } from 'react-native-inappbrowser-reborn'
 import { useApplicationContext } from 'contexts/ApplicationContext'
 import { Alert, Linking } from 'react-native'
-import { useWalletStateContext } from '@avalabs/wallet-react-components'
 import { Moonpay } from '@avalabs/blizzard-sdk'
+import { resolve } from '@avalabs/utils-sdk'
+import { useSelector } from 'react-redux'
+import { selectActiveAccount } from 'store/account'
 
 const useInAppBrowser = () => {
   const { theme } = useApplicationContext()
   const moonAPI = new Moonpay({ baseUrl: 'https://blizzard.avax.network' })
-  const addressC = useWalletStateContext()?.addresses?.addrC ?? ''
+  const addressC = useSelector(selectActiveAccount)?.address ?? ''
 
   function failSafe(url: string) {
     Linking.openURL(url)
   }
 
   async function openMoonPay() {
-    const moonpayUrl = (
-      await moonAPI.getUrl(addressC, { color: theme.colorPrimary1 })
-    ).data.url
+    const [result, error] = await resolve(
+      moonAPI.getUrl(addressC, { color: theme.colorPrimary1 })
+    )
+
+    const moonpayUrl = result?.data?.url ?? ''
 
     Alert.alert(
-      moonpayUrl ? 'Attention' : 'Oh-oh',
-      moonpayUrl
+      !error ? 'Attention' : 'Oh-oh',
+      !error
         ? "Clicking “Continue” will take you to a page powered by our partner MoonPay, use is subject to MoonPay's terms and policies"
         : 'We cannot send your to our partner, MoonPay, at this time. Please try again soon',
-      moonpayUrl
+      !error
         ? [
             {
               text: 'Cancel',
