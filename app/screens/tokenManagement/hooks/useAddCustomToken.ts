e8237be60fef0a6754d60fa251d8c1d4f39c58da
An Nguyen <an.nguyen@avalabs.org>
2022-06-23 15:58:51 -0500

    refactor AddCustomToken component and hook

--- app/screens/tokenManagement/hooks/useAddCustomToken.ts
+++ app/screens/tokenManagement/hooks/useAddCustomToken.ts
@@ -1,76 +1,77 @@
-import {
-  TokenListDict,
-  useWalletStateContext
-} from '@avalabs/wallet-react-components'
-import { getContractDataErc20 } from '@avalabs/avalanche-wallet-sdk'
-import { Erc20TokenData } from '@avalabs/avalanche-wallet-sdk/dist/Asset/types'
-import { useApplicationContext } from 'contexts/ApplicationContext'
-import { useSelector } from 'react-redux'
-import { selectActiveNetwork } from 'store/network'
-import { useEffect } from 'react'
-import { customErc20Tokens$ } from '@avalabs/wallet-react-components'
+import { useDispatch, useSelector } from 'react-redux'
+import { isAddress } from '@ethersproject/address'
+import { selectActiveNetwork, selectNetworkContractTokens } from 'store/network'
+import TokenService from 'services/token/TokenService'
+import { addCustomToken as addCustomTokenAction } from 'store/customToken'
+import { useState, useEffect } from 'react'
+import { Network, NetworkContractToken } from '@avalabs/chains-sdk'
 
-export type CustomTokens = {
-  [chain: string]: TokenListDict
+const validateAddress = (
+  tokenAddress: string,
+  tokens: NetworkContractToken[]
+) => {
+  if (!tokenAddress || !isAddress(tokenAddress)) {
+    throw 'Invalid ERC-20 token address.'
+  }
+
+  const tokenAlreadyExists = tokens.some(
+    token => token.address === tokenAddress
+  )
+
+  if (tokenAlreadyExists) {
+    throw 'Token already exists in the wallet.'
+  }
 }
 
-const useAddCustomToken = () => {
+const fetchTokenData = async (network: Network, tokenAddress: string) => {
+  const networkContractToken = await TokenService.getTokenData(
+    tokenAddress,
+    network
+  )
+
+  if (!networkContractToken) {
+    throw `ERC20 contract ${tokenAddress} does not exist.`
+  }
+
+  return networkContractToken
+}
+
+const useAddCustomToken = (callback: () => void) => {
+  const [tokenAddress, setTokenAddress] = useState('')
+  const [errorMessage, setErrorMessage] = useState<string>()
+  const [token, setToken] = useState<NetworkContractToken>()
   const network = useSelector(selectActiveNetwork)
-  const walletState = useWalletStateContext()
-  const { customTokens, saveCustomTokens } =
-    useApplicationContext().repo.customTokenRepo
+  const tokens = useSelector(selectNetworkContractTokens)
+  const dispatch = useDispatch()
+  const chainId = network.chainId
 
   useEffect(() => {
-    customErc20Tokens$.next(customTokens[network.chainId])
-  }, [customTokens, network.chainId])
+    setErrorMessage('')
+    setToken(undefined)
 
-  async function addCustomToken(tokenAddress: string): Promise<Erc20TokenData> {
-    // make sure we have an erc20 token list
-    if (!walletState?.erc20Tokens) {
-      return Promise.reject('No ERC20 tokens found in wallet.')
-    }
-
-    // last check to see if the token is not already added to the state
-    const tokenAlreadyExists = walletState.erc20Tokens.reduce(
-      (exists, existingToken) =>
-        exists || existingToken.address === tokenAddress,
-      false
-    )
-    if (tokenAlreadyExists) {
-      return Promise.reject('Token already exists in the wallet.')
-    }
-
-    // we need to get the chain to be able to save custom token under correct chain
-    const chain = network.chainId
-
-    // we get the contract again...might change this since we already get it in the view
     try {
-      const tokenData = await getContractDataErc20(tokenAddress)
-      // no error, no data...reject promisse
-      if (!tokenData) {
-        return Promise.reject(`ERC20 contract ${tokenAddress} does not exist.`)
-      }
-
-      // create new object with new token
-      const copy = customTokens ?? ({} as CustomTokens)
-      const newCustomTokens = {
-        ...copy,
-        [chain]: {
-          ...copy[chain],
-          [tokenAddress]: tokenData
-        }
-      }
-
-      // save it :) if `await` fails, it will be caught by try/catch
-      await saveCustomTokens(newCustomTokens)
-
-      return Promise.resolve(tokenData)
+      validateAddress(tokenAddress, tokens)
     } catch (e: any) {
-      return Promise.reject(e.message)
+      // only start showing validation error after a certain length
+      if (tokenAddress.length > 10) {
+        setErrorMessage(e)
+      }
+
+      return
+    }
+
+    fetchTokenData(network, tokenAddress).then(setToken).catch(setErrorMessage)
+  }, [network, tokenAddress, tokens])
+
+  const addCustomToken = async () => {
+    if (token) {
+      dispatch(addCustomTokenAction({ chainId, token }))
+      setTokenAddress('')
+      callback()
     }
   }
 
-  return { addCustomToken }
+  return { tokenAddress, setTokenAddress, errorMessage, token, addCustomToken }
 }
 
 export default useAddCustomToken
