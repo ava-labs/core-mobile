b805980b3aceb832ea9e90fd037d20e63f6c509e
Neven <piaf1986@gmail.com>
2022-07-15 17:53:10 +0200

    Disable Bridge button on unsupported networks and show Toast

---
+++ app/components/toast/GeneralToast.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from 'react'
+import { Dimensions } from 'react-native'
+import { useApplicationContext } from 'contexts/ApplicationContext'
+import AvaText from 'components/AvaText'
+import AvaButton from 'components/AvaButton'
+
+const WINDOW_WIDTH = Dimensions.get('window').width
+
+interface Props {
+  message: string
+  toastId?: string
+}
+
+const GeneralToast: FC<Props> = ({ message, toastId }) => {
+  const theme = useApplicationContext().theme
+
+  function dismissToast() {
+    toastId && global?.toast?.hide(toastId)
+  }
+
+  return (
+    <AvaButton.Base
+      onPress={() => {
+        dismissToast()
+      }}
+      style={{
+        backgroundColor: theme.colorBg2 + 'FA',
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: WINDOW_WIDTH * 0.8,
+        paddingHorizontal: 8,
+        height: 72
+      }}>
+      {
+        <AvaText.ButtonLarge
+          color={theme.colorText1}
+          textStyle={{ textAlign: 'center' }}>
+          {message}
+        </AvaText.ButtonLarge>
+      }
+    </AvaButton.Base>
+  )
+}
+
+export default GeneralToast
