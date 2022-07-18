62accae58cc40212a09fa944bc65cedac860ce04
Eduardo Gueiros <eduardo.gueiros@avalabs.org>
2022-07-18 13:41:28 -0600

    last big recaftoring. removed useRpcTxHandler hook, moved to ConnectedDappsContext

---
+++ app/contexts/DappConnectionContext.tsx
@@ -0,0 +1,250 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import walletConnectService from 'services/walletconnect/WalletConnectService'
+import { MessageType, WalletConnectRequest } from 'services/walletconnect/types'
+import { InteractionManager } from 'react-native'
+import {
+  PeerMetadata,
+  RPC_EVENT,
+  Transaction,
+  TransactionParams
+} from 'screens/rpc/util/types'
+import { JsonRpcRequest } from '@walletconnect/jsonrpc-types'
+import { paramsToMessageParams } from 'screens/rpc/util/paramsToMessageParams'
+import { useActiveAccount } from 'hooks/useActiveAccount'
+import { useActiveNetwork } from 'hooks/useActiveNetwork'
+import Logger from 'utils/Logger'
+import { txToCustomEvmTx } from 'screens/rpc/util/txToCustomEvmTx'
+import { useNetworkFee } from 'hooks/useNetworkFee'
+import { JsonRpcBatchInternal } from '@avalabs/wallets-sdk'
+import networkService from 'services/network/NetworkService'
+import walletService from 'services/wallet/WalletService'
+
+interface AdditionalMessageParams {
+  data?: string
+  from?: string
+  password?: string
+}
+
+export type DappEvent = {
+  payload?: JsonRpcRequest<TransactionParams[]> & AdditionalMessageParams
+  peerMeta: PeerMetadata
+  eventType: RPC_EVENT
+}
+
+interface DappConnectionContext {
+  dappEvent?: DappEvent
+  onSessionApproved: () => void
+  onSessionRejected: () => void
+  onTransactionCallApproved: (
+    tx: Transaction
+  ) => Promise<{ hash?: string; error?: any }>
+  onMessageCallApproved: (
+    payload: DappEvent
+  ) => Promise<{ hash?: string; error?: any }>
+  onCallRejected: () => void
+}
+
+export const dappConnectionContext = createContext<DappConnectionContext>(
+  {} as any
+)
+
+export const DappConnectionContextProvider = ({
+  children
+}: {
+  children: any
+}) => {
+  const activeAccount = useActiveAccount()
+  const activeNetwork = useActiveNetwork()
+  const { networkFees } = useNetworkFee()
+  const [dappEvent, setDappEvent] = useState<DappEvent>()
+
+  useEffect(() => {
+    initializeWalletConnect()
+  }, [])
+
+  const initializeWalletConnect = () => {
+    if (!activeAccount || !activeNetwork) return
+    /**
+     * SESSION
+     */
+    walletConnectService.emitter.on(
+      WalletConnectRequest.SESSION,
+      sessionInfo => {
+        InteractionManager.runAfterInteractions(() => {
+          const meta: PeerMetadata = {
+            peerId: sessionInfo?.peerId,
+            name: sessionInfo?.peerMeta?.name,
+            description: sessionInfo?.peerMeta?.description,
+            url: sessionInfo?.peerMeta?.url,
+            icon: sessionInfo?.peerMeta?.icons?.[0]
+          }
+          setDappEvent({
+            peerMeta: meta,
+            eventType: RPC_EVENT.SESSION
+          })
+        })
+      }
+    )
+    /**
+     * CALL
+     */
+    walletConnectService.emitter.on(
+      WalletConnectRequest.CALL,
+      (data: { payload: any; peerMeta: any }) => {
+        InteractionManager.runAfterInteractions(() => {
+          const payload: JsonRpcRequest<TransactionParams[]> = data.payload
+          const meta: PeerMetadata = {
+            name: data?.peerMeta?.name,
+            description: data?.peerMeta?.description,
+            url: data?.peerMeta?.url,
+            icon: data?.peerMeta?.icons?.[0]
+          }
+          const { method } = payload
+          switch (method) {
+            case MessageType.ETH_SEND:
+              setDappEvent({
+                payload: payload,
+                peerMeta: meta,
+                eventType: RPC_EVENT.TRANSACTION
+              })
+              Logger.info('received CALL request, created transaction event')
+              break
+            case MessageType.ETH_SIGN:
+            case MessageType.SIGN_TYPED_DATA:
+            case MessageType.SIGN_TYPED_DATA_V1:
+            case MessageType.SIGN_TYPED_DATA_V3:
+            case MessageType.SIGN_TYPED_DATA_V4:
+            case MessageType.PERSONAL_SIGN: {
+              const messageParams = paramsToMessageParams(payload)
+              setDappEvent({
+                payload: { ...payload, ...messageParams },
+                peerMeta: meta,
+                eventType: RPC_EVENT.SIGN
+              })
+              Logger.info(
+                'received CALL request, created message signing event'
+              )
+            }
+          }
+        })
+      }
+    )
+    walletConnectService.init(activeAccount, activeNetwork)
+  }
+
+  function clearRequests() {
+    setDappEvent(undefined)
+  }
+
+  function onCallRejected() {
+    walletConnectService.emitter.emit(
+      WalletConnectRequest.CALL_REJECTED,
+      dappEvent?.payload?.id
+    )
+    clearRequests()
+  }
+
+  function onSessionApproved() {
+    walletConnectService.emitter.emit(
+      WalletConnectRequest.SESSION_APPROVED,
+      dappEvent?.peerMeta?.peerId
+    )
+    clearRequests()
+  }
+
+  function onSessionRejected() {
+    walletConnectService.emitter.emit(
+      WalletConnectRequest.SESSION_REJECTED,
+      dappEvent?.peerMeta?.peerId
+    )
+    clearRequests()
+  }
+
+  async function onMessageCallApproved(event: DappEvent) {
+    if (!activeAccount || !activeNetwork || !event?.payload) {
+      return Promise.reject({ error: 'not ready' })
+    }
+
+    const id = event.payload.id
+    const method = event.payload.method as MessageType
+    const dataToSign = event.payload.data
+
+    return walletService
+      .signMessage(method, dataToSign, activeAccount.index, activeNetwork)
+      .then(result => {
+        walletConnectService.emitter.emit(WalletConnectRequest.CALL_APPROVED, {
+          id,
+          hash: result
+        })
+        return { hash: result }
+      })
+      .catch(e => {
+        Logger.error('Error approving dapp tx', e)
+        return Promise.reject({ error: e })
+      })
+  }
+
+  async function onTransactionCallApproved(tx: Transaction) {
+    const params = tx.txParams
+    if (!activeAccount || !activeNetwork || !params) {
+      return Promise.reject({ error: 'not ready' })
+    }
+
+    const nonce = await (
+      networkService.getProviderForNetwork(
+        activeNetwork
+      ) as JsonRpcBatchInternal
+    ).getTransactionCount(params.from)
+
+    return txToCustomEvmTx(networkFees.low, params).then(evmPrams => {
+      return walletService
+        .sign(
+          {
+            nonce: nonce + 1,
+            chainId: activeNetwork.chainId,
+            gasPrice: evmPrams.gasPrice,
+            gasLimit: evmPrams.gasLimit,
+            data: evmPrams.data,
+            value: evmPrams.value
+          },
+          activeAccount.index,
+          activeNetwork
+        )
+        .then(signedTx => {
+          return networkService.sendTransaction(signedTx, activeNetwork)
+        })
+        .then(resultHash => {
+          walletConnectService.emitter.emit(
+            WalletConnectRequest.CALL_APPROVED,
+            {
+              id: tx.id,
+              hash: resultHash
+            }
+          )
+          return { hash: resultHash }
+        })
+        .catch(e => {
+          Logger.error('Error approving dapp tx', e)
+          return Promise.reject({ error: e })
+        })
+    })
+  }
+
+  return (
+    <dappConnectionContext.Provider
+      value={{
+        dappEvent,
+        onSessionApproved,
+        onSessionRejected,
+        onTransactionCallApproved,
+        onMessageCallApproved,
+        onCallRejected
+      }}>
+      {children}
+    </dappConnectionContext.Provider>
+  )
+}
+
+export function useDappConnectionContext() {
+  return useContext(dappConnectionContext)
+}
---
+++ app/screens/rpc/components/Transactions/SwapTransaction.tsx
@@ -0,0 +1,124 @@
+import { SwapExactTokensForTokenDisplayValues } from 'screens/rpc/util/types'
+import { useApplicationContext } from 'contexts/ApplicationContext'
+import AvaText from 'components/AvaText'
+import { Space } from 'components/Space'
+import { View } from 'react-native'
+import { Row } from 'components/Row'
+import TokenAddress from 'components/TokenAddress'
+import Separator from 'components/Separator'
+import Avatar from 'components/Avatar'
+import React from 'react'
+import AvaButton from 'components/AvaButton'
+import CarrotSVG from 'components/svg/CarrotSVG'
+import ArrowSVG from 'components/svg/ArrowSVG'
+import { useSelector } from 'react-redux'
+import { selectAccountByAddress } from 'store/account'
+
+export function SwapTransaction({
+  path,
+  fromAddress,
+  toAddress,
+  site
+}: SwapExactTokensForTokenDisplayValues) {
+  const theme = useApplicationContext().theme
+  const account = useSelector(selectAccountByAddress(fromAddress))
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const sentToken = path[0]!
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const receivingToken = path[path.length - 1]!
+
+  return (
+    <>
+      <AvaText.Heading1>Approve Swap</AvaText.Heading1>
+      <Space y={16} />
+      <Row style={{ justifyContent: 'space-between' }}>
+        <AvaText.Body2 color={theme.colorText1}>
+          Approve {site?.name} transaction
+        </AvaText.Body2>
+        <AvaButton.Base onPress={() => console.log('open data view')}>
+          <Row>
+            <CarrotSVG color={theme.colorText1} direction={'left'} size={12} />
+            <CarrotSVG color={theme.colorText1} size={12} />
+          </Row>
+        </AvaButton.Base>
+      </Row>
+      <View
+        style={[
+          {
+            marginTop: 8,
+            backgroundColor: theme.colorBg3,
+            borderRadius: 8,
+            padding: 16,
+            marginBottom: 16
+          }
+        ]}>
+        <Row style={{ justifyContent: 'space-between' }}>
+          <AvaText.Body3>Account</AvaText.Body3>
+          <AvaText.Body3>{account?.title}</AvaText.Body3>
+        </Row>
+        <Space y={8} />
+        <Row style={{ justifyContent: 'space-between' }}>
+          <AvaText.Body3>Contract</AvaText.Body3>
+          <TokenAddress address={toAddress} />
+        </Row>
+      </View>
+      <AvaText.Body2>Balance Change</AvaText.Body2>
+      <View
+        style={[
+          {
+            backgroundColor: theme.colorBg3,
+            marginTop: 8,
+            marginBottom: 16,
+            borderRadius: 10,
+            padding: 16
+          }
+        ]}>
+        <Row style={{ justifyContent: 'space-between' }}>
+          <AvaText.Body3>Transaction type</AvaText.Body3>
+          <AvaText.Body3>Swap</AvaText.Body3>
+        </Row>
+        <Space y={8} />
+        <Separator color={theme.colorDisabled} />
+        <Space y={12} />
+        <Row style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+          <Row style={{ alignItems: 'center' }}>
+            <Avatar.Token token={sentToken} />
+            <Space x={8} />
+            <AvaText.Body1>{sentToken?.symbol}</AvaText.Body1>
+          </Row>
+          <View style={{ alignItems: 'flex-end' }}>
+            <AvaText.Body2 color={theme.colorText1}>
+              {sentToken.amountIn?.value} {sentToken?.symbol}
+            </AvaText.Body2>
+            <AvaText.Body3 currency>
+              {sentToken?.amountCurrencyValue}
+            </AvaText.Body3>
+          </View>
+        </Row>
+        <Row
+          style={{
+            width: '100%',
+            marginStart: 8,
+            paddingVertical: 10
+          }}>
+          <ArrowSVG size={16} color={theme.colorIcon1} rotate={0} />
+        </Row>
+        <Row style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+          <Row style={{ alignItems: 'center' }}>
+            <Avatar.Token token={receivingToken} />
+            <Space x={8} />
+            <AvaText.Body1>{receivingToken?.symbol}</AvaText.Body1>
+          </Row>
+          <View style={{ alignItems: 'flex-end' }}>
+            <AvaText.Body2 color={theme.colorText1}>
+              {receivingToken.amountOut?.value} {sentToken?.symbol}
+            </AvaText.Body2>
+            <AvaText.Body3 currency>
+              {receivingToken?.amountCurrencyValue}
+            </AvaText.Body3>
+          </View>
+        </Row>
+      </View>
+    </>
+  )
+}
