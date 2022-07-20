629eb34d625ad698b49b2c6fa7fe07fde8ee329b
An Nguyen <an.nguyen@avalabs.org>
2022-07-20 17:24:57 -0500

    more optimization on WatchListItem and fix incorrect charts issue

--- app/screens/watchlist/components/WatchListItem.tsx
+++ app/screens/watchlist/components/WatchListItem.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useMemo, useRef, useState } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import { View, Dimensions } from 'react-native'
 import { useApplicationContext } from 'contexts/ApplicationContext'
 import AvaListItem from 'components/AvaListItem'
@@ -21,7 +21,6 @@ const deviceWidth = Dimensions.get('window').width
 interface Props {
   token: TokenWithBalance
   chartDays: number
-  tokenId?: string
   value?: string
   onPress?: () => void
   rank?: number
@@ -36,28 +35,99 @@ const WatchListItem: FC<Props> = ({
   rank,
   filterBy
 }) => {
-  const lastItemId = useRef(token.id)
+  const { symbol, name } = token
+
+  return (
+    <AvaListItem.Base
+      title={
+        <AvaText.Heading2 ellipsizeMode={'tail'}>{symbol}</AvaText.Heading2>
+      }
+      titleAlignment={'flex-start'}
+      subtitle={name}
+      embedInCard={false}
+      rightComponentMaxWidth={deviceWidth * 0.55}
+      leftComponent={<LeftComponent token={token} rank={rank} />}
+      rightComponent={
+        <RightComponent
+          token={token}
+          chartDays={chartDays}
+          value={value}
+          filterBy={filterBy}
+        />
+      }
+      onPress={onPress}
+    />
+  )
+}
+
+type LeftComponentProps = {
+  token: TokenWithBalance
+  rank?: number
+}
+
+const LeftComponent = ({ token, rank }: LeftComponentProps) => {
   const { logoUri, symbol, name } = token
+
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+      {rank && (
+        <>
+          <AvaText.Heading3>{rank}</AvaText.Heading3>
+          <Space x={9} />
+        </>
+      )}
+      <Avatar.Custom name={name} symbol={symbol} logoUri={logoUri} size={32} />
+    </View>
+  )
+}
+
+type RightComponentProps = {
+  token: TokenWithBalance
+  chartDays: number
+  value?: string
+  filterBy: WatchlistFilter
+}
+
+type ChartData = { x: number; y: number }[]
+
+type Ranges = {
+  minDate: number
+  maxDate: number
+  minPrice: number
+  maxPrice: number
+  diffValue: number
+  percentChange: number
+}
+
+const initialRanges = {
+  minDate: 0,
+  maxDate: 0,
+  minPrice: 0,
+  maxPrice: 0,
+  diffValue: 0,
+  percentChange: 0
+}
+
+const emptyArr: ChartData = []
+
+const RightComponent = ({
+  token,
+  chartDays,
+  value,
+  filterBy
+}: RightComponentProps) => {
+  const lastItemId = useRef(token.id)
   const { theme, appHook } = useApplicationContext()
   const { selectedCurrency } = appHook
-  const [ranges, setRanges] = useState<{
-    minDate: number
-    maxDate: number
-    minPrice: number
-    maxPrice: number
-    diffValue: number
-    percentChange: number
-  }>({
-    minDate: 0,
-    maxDate: 0,
-    minPrice: 0,
-    maxPrice: 0,
-    diffValue: 0,
-    percentChange: 0
-  })
-  const [chartData, setChartData] = useState<{ x: number; y: number }[]>([])
-  const [isLoadingChartData, setIsLoadingChartData] = useState(false)
   const network = useSelector(selectActiveNetwork)
+  const [ranges, setRanges] = useState<Ranges>(initialRanges)
+  const [chartData, setChartData] = useState<ChartData>(emptyArr)
+  const [isLoadingChartData, setIsLoadingChartData] = useState(false)
   const assetPlatformId =
     network.pricingProviders?.coingecko.assetPlatformId ?? ''
   const currency = selectedCurrency.toLowerCase() as VsCurrencyType
@@ -69,15 +139,8 @@ const WatchListItem: FC<Props> = ({
   // https://github.com/Shopify/flash-list/pull/529
   if (token.id !== lastItemId.current) {
     lastItemId.current = token.id
-    setRanges({
-      minDate: 0,
-      maxDate: 0,
-      minPrice: 0,
-      maxPrice: 0,
-      diffValue: 0,
-      percentChange: 0
-    })
-    setChartData([])
+    setRanges(initialRanges)
+    setChartData(emptyArr)
     setIsLoadingChartData(false)
   }
 
@@ -101,113 +164,81 @@ const WatchListItem: FC<Props> = ({
         })
       }
 
-      if (result) {
+      // only set data if token has not changed
+      if (result && token.id === lastItemId.current) {
         setChartData(result.dataPoints)
         setRanges(result.ranges)
       }
+
       setIsLoadingChartData(false)
     }
 
     fetchChartData()
   }, [assetPlatformId, chartDays, currency, token])
 
-  const usdBalance = useMemo(() => {
-    if (value) {
-      return (
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <View
-            style={{
-              width: 90,
-              alignItems: 'flex-end'
-            }}>
-            {isLoadingChartData ? (
-              <ActivityIndicator style={{ alignSelf: 'center' }} />
-            ) : (
-              <SparklineChart
-                width={90}
-                height={80}
-                animated={false}
-                data={chartData}
-                yRange={[ranges.minPrice, ranges.maxPrice]}
-                xRange={[ranges.minDate, ranges.maxDate]}
-                negative={ranges.diffValue < 0}
-              />
-            )}
-          </View>
-          <View
-            style={{
-              alignItems: 'flex-end',
-              flex: 1
-            }}>
-            <Row style={{ alignItems: 'flex-end' }}>
-              <AvaText.Heading3 ellipsizeMode={'tail'}>
-                {value}
-              </AvaText.Heading3>
-              <Space x={4} />
-              <AvaText.Body3
-                textStyle={{ color: theme.colorText2, lineHeight: 22 }}>
-                {selectedCurrency}
-              </AvaText.Body3>
-            </Row>
-            <MarketMovement
-              priceChange={ranges.diffValue}
-              percentChange={ranges.percentChange}
-              filterBy={filterBy}
-            />
-          </View>
-        </View>
-      )
-    }
-
-    return null
-  }, [
-    value,
-    isLoadingChartData,
-    theme.colorText2,
-    chartData,
-    ranges.minPrice,
-    ranges.maxPrice,
-    ranges.minDate,
-    ranges.maxDate,
-    ranges.diffValue,
-    ranges.percentChange,
-    selectedCurrency,
-    filterBy
-  ])
+  if (!value) return null
 
   return (
-    <AvaListItem.Base
-      title={
-        <AvaText.Heading2 ellipsizeMode={'tail'}>{symbol}</AvaText.Heading2>
-      }
-      titleAlignment={'flex-start'}
-      subtitle={name}
-      embedInCard={false}
-      rightComponentMaxWidth={deviceWidth * 0.55}
-      leftComponent={
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-          {rank && (
-            <>
-              <AvaText.Heading3>{rank}</AvaText.Heading3>
-              <Space x={9} />
-            </>
-          )}
-          <Avatar.Custom
-            name={name}
-            symbol={symbol}
-            logoUri={logoUri}
-            size={32}
-          />
-        </View>
-      }
-      rightComponent={usdBalance}
-      onPress={onPress}
-    />
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <MiddleComponent
+        chartData={chartData}
+        ranges={ranges}
+        isLoadingChartData={isLoadingChartData}
+      />
+      <View
+        style={{
+          alignItems: 'flex-end',
+          flex: 1
+        }}>
+        <Row style={{ alignItems: 'flex-end' }}>
+          <AvaText.Heading3 ellipsizeMode={'tail'}>{value}</AvaText.Heading3>
+          <Space x={4} />
+          <AvaText.Body3
+            textStyle={{ color: theme.colorText2, lineHeight: 22 }}>
+            {selectedCurrency}
+          </AvaText.Body3>
+        </Row>
+        <MarketMovement
+          priceChange={ranges.diffValue}
+          percentChange={ranges.percentChange}
+          filterBy={filterBy}
+        />
+      </View>
+    </View>
+  )
+}
+
+type MiddleComponentProps = {
+  chartData: ChartData
+  ranges: Ranges
+  isLoadingChartData: boolean
+}
+
+const MiddleComponent = ({
+  chartData,
+  ranges,
+  isLoadingChartData
+}: MiddleComponentProps) => {
+  return (
+    <View
+      style={{
+        width: 90,
+        alignItems: 'flex-end'
+      }}>
+      {isLoadingChartData ? (
+        <ActivityIndicator style={{ alignSelf: 'center' }} />
+      ) : (
+        <SparklineChart
+          width={90}
+          height={80}
+          animated={false}
+          data={chartData}
+          yRange={[ranges.minPrice, ranges.maxPrice]}
+          xRange={[ranges.minDate, ranges.maxDate]}
+          negative={ranges.diffValue < 0}
+        />
+      )}
+    </View>
   )
 }
 
