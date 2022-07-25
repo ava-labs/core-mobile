6beeafb9d9459d2d7c93b1f43ed6236191422abd
Joe Tanner <joe.tanner@avalabs.org>
2022-07-25 10:58:42 -0600

    feat: fetch Ethereum transactions from etherscan with pagination

--- app/services/activity/EvmActivityService.ts
+++ app/services/activity/EvmActivityService.ts
@@ -1,8 +1,11 @@
+import { getErc20Txs, getNormalTxs } from '@avalabs/etherscan-sdk'
 import { V1 as GlacierSdkV1 } from '@avalabs/glacier-sdk'
 import Config from 'react-native-config'
 import Logger from 'utils/Logger'
-import { GetActivitiesForAddressParams } from './types'
+import { isEthereumNetwork } from 'services/network/utils/isEthereumNetwork'
+import { ActivityResponse, GetActivitiesForAddressParams } from './types'
 import { convertTransaction } from './utils/evmTransactionConverter'
+import * as EtherscanConverter from './utils/etherscanTransactionConverter'
 
 const glacierUrl = __DEV__ ? Config.GLACIER_DEV_URL : Config.GLACIER_PROD_URL
 
@@ -13,37 +16,106 @@ const glacierSdk = new GlacierSdkV1({
 })
 
 export class EvmActivityService {
-  async getActivities({
-    network,
-    address,
-    nextPageToken,
-    pageSize,
-    criticalConfig
-  }: GetActivitiesForAddressParams) {
-    const response = await glacierSdk.listTransactions(
-      network.chainId.toString(),
-      address,
-      { pageToken: nextPageToken, pageSize }
-    )
-
-    const ethereumWrappedAssets = criticalConfig?.critical.assets
-    const bitcoinAssets = criticalConfig?.criticalBitcoin?.bitcoinAssets
-
-    const transactions = response.data.transactions.map(item =>
-      convertTransaction({
-        item,
-        network,
-        address,
-        ethereumWrappedAssets,
-        bitcoinAssets
-      })
-    )
-
-    return {
-      transactions,
-      nextPageToken: response.data.nextPageToken
-    }
+  async getActivities(params: GetActivitiesForAddressParams) {
+    return isEthereumNetwork(params.network)
+      ? getTransactionsFromEtherscan(params)
+      : getTransactionsFromGlacier(params)
   }
+}
+
+async function getTransactionsFromGlacier({
+  network,
+  address,
+  nextPageToken,
+  pageSize,
+  criticalConfig
+}: GetActivitiesForAddressParams): Promise<ActivityResponse> {
+  const response = await glacierSdk.listTransactions(
+    network.chainId.toString(),
+    address,
+    { pageToken: nextPageToken, pageSize }
+  )
+
+  const transactions = response.data.transactions.map(item =>
+    convertTransaction({
+      item,
+      network,
+      address,
+      criticalConfig
+    })
+  )
+
+  return {
+    transactions,
+    nextPageToken: response.data.nextPageToken
+  }
+}
+
+async function getTransactionsFromEtherscan({
+  network,
+  address,
+  nextPageToken,
+  pageSize: offset,
+  criticalConfig
+}: GetActivitiesForAddressParams): Promise<ActivityResponse> {
+  /*
+  Using JSON for nextPageToken because this function is managing both the Normal
+  and ERC20 queries. It encodes the current page and the queries that should be
+  run. For example, if 'normal' has no more records to fetch then it will be
+  excluded from the list and the JSON will be something like:
+  { page: 3, queries: ['erc20'] }
+  */
+  const parsedPageToken = nextPageToken
+    ? (JSON.parse(nextPageToken) as EtherscanPagination)
+    : undefined
+  const page = parsedPageToken?.page || 1
+  const queries = parsedPageToken?.queries || ['normal', 'erc20']
+
+  const normalHist = (
+    queries.includes('normal')
+      ? await getNormalTxs(address, !network.isTestnet, { page, offset })
+      : []
+  ).map(tx => EtherscanConverter.convertTransactionNormal(tx, network, address))
+
+  const erc20Hist = (
+    queries.includes('erc20')
+      ? await getErc20Txs(address, !network.isTestnet, undefined, {
+          page,
+          offset
+        })
+      : []
+  ).map(tx =>
+    EtherscanConverter.convertTransactionERC20(
+      tx,
+      network,
+      address,
+      criticalConfig
+    )
+  )
+
+  // Filter erc20 transactions from normal tx list
+  const erc20TxHashes = erc20Hist.map(tx => tx.hash)
+  const filteredNormalTxs = normalHist.filter(tx => {
+    return !erc20TxHashes.includes(tx.hash)
+  })
+
+  // Sort by timestamp
+  const transactions = [...filteredNormalTxs, ...erc20Hist]
+  transactions.sort((a, b) => b.timestamp - a.timestamp)
+
+  const next: EtherscanPagination = { queries: [], page: page + 1 }
+  if (normalHist.length) next.queries.push('normal')
+  if (erc20Hist.length) next.queries.push('erc20')
+
+  return {
+    transactions,
+    nextPageToken: next.queries.length ? JSON.stringify(next) : undefined // stop pagination
+  }
+}
+
+interface EtherscanPagination {
+  queries: ('normal' | 'erc20')[]
+  page: number | undefined
 }
 
 export default new EvmActivityService()
