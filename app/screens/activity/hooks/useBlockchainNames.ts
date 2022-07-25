d72e26116f9e8c2021fcabc10ddfb37fa3009528
Joe Tanner <joe.tanner@avalabs.org>
2022-07-25 11:44:04 -0600

    fix: better logic for displaying bridge transactions
    and cleans up temp TEST.t logic

--- app/screens/activity/hooks/useBlockchainNames.ts
+++ app/screens/activity/hooks/useBlockchainNames.ts
@@ -4,16 +4,7 @@ import { Transaction } from 'store/transaction'
 
 export function useBlockchainNames(item: Transaction | BridgeTransaction) {
   const pending = isPendingBridgeTransaction(item)
-  const { avalancheAssets } = useBridgeSDK()
-
-  const symbol = (
-    !pending
-      ? item.token?.symbol === 'TEST.t'
-        ? // TEMP: use "BTC" when "TEST.t" until testnet changes
-          'BTC'
-        : item.token?.symbol ?? ''
-      : ''
-  ).split('.')[0]
+  const { avalancheAssets, criticalConfig } = useBridgeSDK()
 
   if (pending) {
     return {
@@ -22,8 +13,17 @@ export function useBlockchainNames(item: Transaction | BridgeTransaction) {
     }
   }
 
-  const isToAvalanche =
-    item.from === '0x0000000000000000000000000000000000000000'
+  const symbol = (item.token?.symbol ?? '').split('.')[0]
+
+  const bridgeAddresses = criticalConfig
+    ? [
+        criticalConfig.critical.walletAddresses.avalanche?.toLowerCase(),
+        criticalConfig.critical.walletAddresses.ethereum?.toLowerCase(),
+        criticalConfig.criticalBitcoin.walletAddresses.avalanche.toLowerCase(),
+        criticalConfig.criticalBitcoin.walletAddresses.btc.toLowerCase()
+      ]
+    : []
+  const isToAvalanche = bridgeAddresses.includes(item.to.toLowerCase())
   const txBlockchain = titleCase(
     avalancheAssets[symbol]?.nativeNetwork || 'N/A'
   )
