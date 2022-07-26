311c29bb0d248ac673332b9684dc1e1d1de940db
Joe Tanner <joe.tanner@avalabs.org>
2022-07-26 14:50:22 -0600

    fix: check each glacier url and only in dev

--- app/utils/glacierUtils.ts
+++ app/utils/glacierUtils.ts
@@ -11,7 +11,12 @@ export const GLACIER_URL = __DEV__
  * Glacier needs an API key for development, this adds the key if needed.
  */
 export function addGlacierAPIKeyIfNeeded(url: string): string {
-  if (Config.GLACIER_API_KEY && url.startsWith(GLACIER_URL)) {
+  if (
+    __DEV__ &&
+    Config.GLACIER_API_KEY &&
+    (url.startsWith(Config.GLACIER_DEV_URL) ||
+      url.startsWith(Config.GLACIER_PROD_URL))
+  ) {
     const urlObj = new URL(url)
     const search_params = urlObj.searchParams // copy, does not update the URL
     search_params.set('token', Config.GLACIER_API_KEY)
