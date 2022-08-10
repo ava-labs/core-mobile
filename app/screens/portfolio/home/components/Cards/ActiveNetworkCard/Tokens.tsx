18707725e1cdae533a01e4b9173ccb2a82fcb496
Joe Tanner <joe.tanner@avalabs.org>
2022-08-10 13:50:06 -0600

    fix: show token initials when logoUri is blank

--- app/screens/portfolio/home/components/Cards/ActiveNetworkCard/Tokens.tsx
+++ app/screens/portfolio/home/components/Cards/ActiveNetworkCard/Tokens.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { TokenWithBalance } from 'store/balance'
 import AvaText from 'components/AvaText'
 import { useApplicationContext } from 'contexts/ApplicationContext'
 import { useSearchableTokenList } from 'screens/portfolio/useSearchableTokenList'
+import Avatar from 'components/Avatar'
 
 const Tokens = () => {
   const { filteredTokenList: tokens } = useSearchableTokenList()
@@ -27,7 +28,7 @@ const Tokens = () => {
 
     return (
       <View style={[styles.tokenRow, { marginBottom }]} key={index.toString()}>
-        <Image source={{ uri: token.logoUri }} style={styles.smallIcon} />
+        <Avatar.Custom size={16} name={token.name} logoUri={token.logoUri} />
         <AvaText.ButtonSmall
           color={tokenTextColor}
           textStyle={styles.tokenNameText}>
@@ -62,11 +63,6 @@ const Tokens = () => {
 }
 
 const styles = StyleSheet.create({
-  smallIcon: {
-    width: 16,
-    height: 16,
-    borderRadius: 16 / 2
-  },
   tokenRow: {
     flexDirection: 'row',
     alignItems: 'center'
