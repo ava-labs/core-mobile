ec940ec4a6b29531704273e1589994d84b50b3cf
Joe Tanner <joe.tanner@avalabs.org>
2022-08-31 11:15:54 -0600

    fix: use es6-promise so the allSettled polyfill is consistent

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import 'react-native-get-random-values'
 import 'react-native-url-polyfill/auto'
+import 'es6-promise/auto'
 import { AppRegistry } from 'react-native'
 import ContextApp from './app/ContextApp'
 import { name as appName } from './app.json'
