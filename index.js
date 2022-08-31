3543be6d4465b5701bf9bdf9484aa57ea09d6618
An Nguyen <an.nguyen@avalabs.org>
2022-08-31 11:41:32 -0500

    remove react-native-crypto and its dependencies

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
  * @format
  */
 
+import './shim'
+import './read_as_array_buffer_shim'
 import 'react-native-get-random-values'
 import 'react-native-url-polyfill/auto'
 import 'es6-promise/auto'
 import { AppRegistry } from 'react-native'
 import ContextApp from './app/ContextApp'
 import { name as appName } from './app.json'
-import './read_as_array_buffer_shim'
-import './shim'
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import DevDebuggingConfig from './app/utils/debugging/DevDebuggingConfig'
 
