6990a42549812f9e051b770fd3d1f853af6256d5
An Nguyen <an.nguyen@avalabs.org>
2022-08-31 10:34:09 -0500

    CP-3201 + CP-3202: Switch key derivation algorithm and use dynamic salt (#370)
    * use argon2id instead of pbkdf2 to derive key + use dynamic salt
    
    * add migration logic

---
+++ app/utils/EncryptionHelper.ts
@@ -0,0 +1,54 @@
+import { NativeModules } from 'react-native'
+import argon2 from 'react-native-argon2'
+
+const Aes = NativeModules.Aes
+
+export interface EncryptedData {
+  cipher: string
+  iv: string
+  salt: string
+}
+
+export class NoSaltError extends Error {
+  constructor() {
+    super('data has no salt')
+    this.name = 'NoSaltError'
+  }
+}
+
+async function getDerivedKey(password: string, salt: string): Promise<string> {
+  const { rawHash } = await argon2(password, salt, {
+    iterations: 2,
+    memory: 32 * 1024, // 32 MB
+    parallelism: 6,
+    hashLength: 32, // 256-bit output
+    mode: 'argon2id'
+  })
+  return rawHash
+}
+
+export async function encrypt(data: string, password: string): Promise<string> {
+  const salt = await Aes.randomKey(16) // 128-bit salt
+  const iv = await Aes.randomKey(16) // 128-bit iv
+  const key = await getDerivedKey(password, salt)
+  const cipher = await Aes.encrypt(data, key, iv)
+
+  return JSON.stringify({
+    cipher,
+    iv,
+    salt
+  })
+}
+
+export async function decrypt(
+  encryptedData: string,
+  password: string
+): Promise<string> {
+  const { cipher, iv, salt } = JSON.parse(encryptedData) as EncryptedData
+
+  if (salt === undefined) throw new NoSaltError()
+
+  const key = await getDerivedKey(password, salt)
+
+  return Aes.decrypt(cipher, key, iv)
+}
