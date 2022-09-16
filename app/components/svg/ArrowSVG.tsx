3b5e748dfd6d35a5c38a862d970b61dfd7199b4a
An Nguyen <an.nguyen@avalabs.org>
2022-09-16 09:02:19 -0500

    CP-3444: update arrow icon (#406)

--- app/components/svg/ArrowSVG.tsx
+++ app/components/svg/ArrowSVG.tsx
@@ -9,31 +9,29 @@ interface Prop {
   rotate?: number
 }
 
-function ArrowSVG({ color = '#FFF', size = 16, rotate }: Prop) {
+const Arrow = ({ size, color }: { size: number; color: string }) => (
+  <Svg width={size} height={size} viewBox="0 0 15 15" fill="none">
+    <Path
+      d="M10 14c0-.56-.45-1-1-1H3.41L14.3 2.11A.996.996 0 1 0 12.89.7L2 11.59V6c0-.55-.45-1-1-1s-1 .45-1 1v8c0 .55.45 1 1 1h8c.55 0 1-.45 1-1Z"
+      fill={color}
+    />
+  </Svg>
+)
+
+function ArrowSVG({ color = '#FFF', size = 15, rotate = 0 }: Prop) {
   const context = useApplicationContext()
 
   const iconColor = color ?? context.theme.colorIcon1
 
-  const Arrow = () => (
-    <Svg width={size} height={size} viewBox="0 0 16 16" fill="none">
-      <Path
-        d="M7.29289 14.7071C7.68342 15.0976 8.31658 15.0976 8.70711 14.7071L15.0711 8.34314C15.4616 7.95262 15.4616 7.31945 15.0711 6.92893C14.6805 6.53841 14.0474 6.53841 13.6569 6.92893L8 12.5858L2.34315 6.92893C1.95262 6.53841 1.31946 6.53841 0.928931 6.92893C0.538407 7.31945 0.538407 7.95262 0.928931 8.34314L7.29289 14.7071ZM7 1L7 14H9V1L7 1Z"
-        fill={iconColor}
-      />
-    </Svg>
-  )
-
-  return rotate ? (
+  return (
     <View
       style={{
         justifyContent: 'center',
         alignItems: 'center',
         transform: [{ rotate: `${rotate}deg` }]
       }}>
-      <Arrow />
+      <Arrow size={size} color={iconColor} />
     </View>
-  ) : (
-    <Arrow />
   )
 }
 
