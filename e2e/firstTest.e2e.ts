48ca48975c4e281ceeceeccacab459d65c1514b9
Tyler Hackett <tyler.hackett@avalabs.org>
2022-09-19 09:52:43 -0700

    functional first test

--- e2e/firstTest.e2e.ts
+++ e2e/firstTest.e2e.ts
@@ -1,4 +1,3 @@
-
 describe('Example', () => {
   beforeAll(async () => {
     await device.launchApp()
@@ -12,7 +11,6 @@ describe('Example', () => {
     await expect(element(by.text('Existing Wallet')).atIndex(1)).toBeVisible()
   })
 
-  // eslint-disable-next-line jest/expect-expect
   it('should show recovery phrase after tap', async () => {
     await element(by.id('walletSVG')).atIndex(1).tap()
     await element(by.id('recoveryPhraseTextInput')).typeText(
@@ -22,5 +20,9 @@ describe('Example', () => {
     for (let i = 0; i < 12; i++) {
       await element(by.id('0')).tap()
     }
+    await element(by.id('checkBoxEmpty')).atIndex(1).tap()
+    await element(by.id('checkBoxEmpty')).tap()
+    await element(by.text('Next')).tap()
+    await expect(await element(by.text('Collectibles'))).toBeVisible()
   })
 })
