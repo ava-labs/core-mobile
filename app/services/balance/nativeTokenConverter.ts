2e546cbc1fd1f5be1a3c0042465716ea7b881f2b
Neven <piaf1986@gmail.com>
2022-10-07 09:37:27 +0200

    Create GlacierBalanceService

---
+++ app/services/balance/nativeTokenConverter.ts
@@ -0,0 +1,41 @@
+import { NativeTokenBalance } from '@avalabs/glacier-sdk'
+import { NetworkTokenWithBalance, TokenType } from 'store/balance'
+import { BN } from 'bn.js'
+import { balanceToDisplayValue, bnToBig } from '@avalabs/utils-sdk'
+
+export function convertNativeToTokenWithBalance(
+  native: NativeTokenBalance
+): NetworkTokenWithBalance {
+  const id = getTokenId(native)
+  const balance = new BN(native.balance)
+  const balanceDisplayValue = balanceToDisplayValue(balance, native.decimals)
+  const priceInCurrency = native.price?.value ?? 0
+  const balanceInCurrency = bnToBig(balance, native.decimals)
+    .mul(priceInCurrency)
+    .toNumber()
+  const balanceCurrencyDisplayValue =
+    native.balanceValue?.value.toString() ?? '0'
+
+  return {
+    name: native.name,
+    symbol: native.symbol,
+    description: '',
+    decimals: native.decimals,
+    logoUri: native.logoUri ?? '',
+    id,
+    type: TokenType.NATIVE,
+    balance,
+    balanceDisplayValue,
+    balanceInCurrency,
+    balanceCurrencyDisplayValue,
+    priceInCurrency,
+    marketCap: 0,
+    vol24: 0,
+    change24: 0,
+    coingeckoId: ''
+  }
+}
+
+function getTokenId(native: NativeTokenBalance): string {
+  return `${native.chainId}-${native.name}-${native.symbol}`
+}
