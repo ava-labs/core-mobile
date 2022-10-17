b0f6b2a4b8407b1fbac25a606592d70192f90cdc
An Nguyen <an.nguyen@avalabs.org>
2022-10-17 16:18:15 -0500

    add coinlist parse script and coinlist json files

---
+++ scripts/parseCoinlist.js
@@ -0,0 +1,72 @@
+// TODO: remove this script once glacier returns coingecko id for each contract token
+const fs = require('fs')
+
+const assetsPath = '../app/assets/'
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0
+}
+
+// coinlistRaw.json is from https://api.coingecko.com/api/v3/coins/list?include_platform=true
+fs.readFile('../coinlistRaw.json', 'utf8', (err, dataRaw) => {
+  if (err) {
+    console.log(`Error reading file from disk: ${err}`)
+  } else {
+    const data = JSON.parse(dataRaw)
+
+    const coinByAddress = {}
+    data.forEach(item => {
+      if (!isEmpty(item.platforms)) {
+        for (const platform in item.platforms) {
+          if (
+            item.platforms[platform] &&
+            !coinByAddress[item.platforms[platform]]
+          ) {
+            coinByAddress[item.platforms[platform]] = {
+              id: item.id,
+              symbol: item.symbol,
+              name: item.name
+            }
+          }
+        }
+      }
+    })
+
+    const coinBySymbol = {}
+    data.forEach(item => {
+      if (!coinBySymbol[item.symbol]) {
+        coinBySymbol[item.symbol] = {
+          id: item.id,
+          symbol: item.symbol,
+          name: item.name
+        }
+      }
+    })
+
+    fs.writeFile(
+      assetsPath + 'coinByAddress.json',
+      JSON.stringify(coinByAddress),
+      'utf8',
+      err => {
+        if (err) {
+          console.log(`Error writing file: ${err}`)
+        } else {
+          console.log(`File is written successfully!`)
+        }
+      }
+    )
+
+    fs.writeFile(
+      assetsPath + 'coinBySymbol.json',
+      JSON.stringify(coinBySymbol),
+      'utf8',
+      err => {
+        if (err) {
+          console.log(`Error writing file: ${err}`)
+        } else {
+          console.log(`File is written successfully!`)
+        }
+      }
+    )
+  }
+})
