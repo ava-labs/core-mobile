90b79ca6d644ca7fbdd56254c6b28e52ff639126
An Nguyen <an.nguyen@avalabs.org>
2022-10-17 16:56:55 -0500

    make useFocusedSelector return correct typing

--- app/utils/performance/useFocusedSelector.ts
+++ app/utils/performance/useFocusedSelector.ts
@@ -1,5 +1,5 @@
 import { useIsFocused } from '@react-navigation/native'
-import { shallowEqual, useSelector } from 'react-redux'
+import { Selector, shallowEqual, useSelector } from 'react-redux'
 import { RootState } from 'store'
 
 const returnTrueFn = () => true
@@ -8,7 +8,9 @@ const returnTrueFn = () => true
  * an enhanced useSelector that subscribes to/unsubscribes from store updates
  * whenever component is focused/unfocused
  */
-export const useFocusedSelector = (selectorFn: (state: RootState) => any) => {
+export const useFocusedSelector = <Result>(
+  selectorFn: Selector<RootState, Result>
+) => {
   const isFocused = useIsFocused()
   const equalityFn = isFocused ? shallowEqual : returnTrueFn
   return useSelector(selectorFn, equalityFn)
