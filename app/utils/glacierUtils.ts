a6e6e44cb9dfd4a9e603e8cf59b4984e9b8c8f04
An Nguyen <an.nguyen@avalabs.org>
2022-10-19 10:40:50 -0500

    Revert "adjust glacier url env"
    This reverts commit e97421dddf8b2f2c1b7ca16782d8d696e7ced3e6.

--- app/utils/glacierUtils.ts
+++ app/utils/glacierUtils.ts
@@ -1,16 +1,24 @@
 import Config from 'react-native-config'
 import Logger from './Logger'
 
-if (!Config.GLACIER_URL || !Config.GLACIER_API_KEY)
-  Logger.error('GLACIER_URL or GLACIER_API_KEY env is missing')
+/**
+ * After constructing a full URL make sure to call `addGlacierAPIKeyIfNeeded`.
+ */
+export const GLACIER_URL = __DEV__
+  ? Config.GLACIER_DEV_URL
+  : Config.GLACIER_PROD_URL
 
-export const GLACIER_URL = Config.GLACIER_URL
+if (!GLACIER_URL) Logger.error('GLACIER URL ENV is missing')
 
 /**
  * Glacier needs an API key for development, this adds the key if needed.
  */
 export function addGlacierAPIKeyIfNeeded(url: string): string {
-  if (url.startsWith(Config.GLACIER_URL) && Config.GLACIER_API_KEY) {
+  if (
+    Config.GLACIER_API_KEY &&
+    (url.startsWith(Config.GLACIER_DEV_URL) ||
+      url.startsWith(Config.GLACIER_PROD_URL))
+  ) {
     const urlObj = new URL(url)
     const search_params = urlObj.searchParams // copy, does not update the URL
     search_params.set('token', Config.GLACIER_API_KEY)
