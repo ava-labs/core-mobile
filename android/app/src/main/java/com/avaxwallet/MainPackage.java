b35d16ca97ae3e1f80ee80e3586173fe98d8724d
Neven <piaf1986@gmail.com>
2022-10-28 14:51:23 +0200

    Enable FLAG_SECURE flag for android builds when on RevealMnemonic screen

--- android/app/src/main/java/com/avaxwallet/MainPackage.java
+++ android/app/src/main/java/com/avaxwallet/MainPackage.java
@@ -17,6 +17,7 @@ public class MainPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new FullScreenActivity(reactContext));
+        modules.add(new SecureActivity(reactContext));
         return modules;
     }
 
