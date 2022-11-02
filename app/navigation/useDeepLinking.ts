212d2e051bb5f01157b4ffdd1b0f1ab864cc2863
An Nguyen <an.nguyen@avalabs.org>
2022-11-02 12:11:09 -0500

    clean up deeplink logic

--- app/navigation/useDeepLinking.ts
+++ app/navigation/useDeepLinking.ts
@@ -2,7 +2,6 @@ import { Alert } from 'react-native'
 import {
   ACTIONS,
   DeepLinkOrigin,
-  PREFIXES,
   PROTOCOLS
 } from 'services/walletconnect/types'
 import URL from 'url-parse'
@@ -12,6 +11,7 @@ import { CORE_UNIVERSAL_LINK_HOST } from 'resources/Constants'
 import walletConnectService from 'services/walletconnect/WalletConnectService'
 import { Network } from '@avalabs/chains-sdk'
 import { Account } from 'store/account'
+import Logger from 'utils/Logger'
 
 /******************************************************************************
  * 3. Parsing of the url string happens here.
@@ -22,11 +22,7 @@ export function parseUrl(
   activeAccount?: Account,
   activeNetwork?: Network
 ) {
-  const urlObj = new URL(
-    url
-      .replace(`${PROTOCOLS.DAPP}/${PROTOCOLS.HTTPS}://`, `${PROTOCOLS.DAPP}/`)
-      .replace(`${PROTOCOLS.DAPP}/${PROTOCOLS.HTTP}://`, `${PROTOCOLS.DAPP}/`)
-  )
+  const urlObj = new URL(url)
 
   let params: ParsedQs | undefined
 
@@ -34,19 +30,23 @@ export function parseUrl(
     try {
       params = qs.parse(urlObj.query.substring(1))
     } catch (e) {
-      console.error(e)
+      Logger.error('failed to parse url params', e)
     }
   }
-
   if (urlObj && url) {
-    navigateWithProtocol(
-      urlObj,
-      url,
-      params,
-      origin,
-      activeAccount,
-      activeNetwork
-    )
+    try {
+      navigateWithProtocol(
+        urlObj,
+        url,
+        params,
+        origin,
+        activeAccount,
+        activeNetwork
+      )
+    } catch (e) {
+      Logger.error('failed to process dapp link', e)
+      Alert.alert((e as Error)?.message)
+    }
   }
 }
 
@@ -65,26 +65,22 @@ export function navigateWithProtocol(
   const protocol = urlObj.protocol.replace(':', '')
   switch (protocol) {
     // handles general WalletConnect custom url protocol
-    case PROTOCOLS.WC: {
-      try {
-        const wcCleanUrl = originalUrl.replace('wc://wc?uri=', '')
-        if (!walletConnectService.isValidUri(wcCleanUrl)) {
-          return
-        }
-        walletConnectService.newSession(
-          wcCleanUrl,
-          !!params?.autosign,
-          origin,
-          activeAccount,
-          activeNetwork
-        )
-      } catch (e) {
-        Alert.alert((e as Error)?.message)
+    case PROTOCOLS.WC:
+      if (!walletConnectService.isValidUri(originalUrl)) {
+        return
       }
+      walletConnectService.newSession(
+        originalUrl,
+        !!params?.autosign,
+        origin,
+        activeAccount,
+        activeNetwork
+      )
       break
-    }
     // handles Universal Link (iOS) url protocol
     case PROTOCOLS.HTTP:
+      Logger.error('http protocol is not not supported for dapps', originalUrl)
+      break
     case PROTOCOLS.HTTPS:
       if (urlObj.hostname === CORE_UNIVERSAL_LINK_HOST) {
         const action = urlObj.pathname.split('/')[1]
@@ -99,32 +95,7 @@ export function navigateWithProtocol(
         } else if (action === ACTIONS.WC) {
           // This is called from WC just to open the app, and it's not supposed to do anything
           return
-        } else if (PREFIXES[action]) {
-          const url = urlObj.href.replace(
-            `https://${CORE_UNIVERSAL_LINK_HOST}/${action}/`,
-            PREFIXES[action]
-          )
-          // loops back to open the link with the right protocol
-          parseUrl(url, origin)
         }
-      }
-      break
-    // Handles Metamask url protocol - Not sure yet if we want to handle this
-    case PROTOCOLS.METAMASK:
-      if (originalUrl.startsWith('metamask://wc')) {
-        const cleanUrlObj = new URL(urlObj.query.replace('?uri=', ''))
-        const href = cleanUrlObj.href
-
-        if (!walletConnectService.isValidUri(href)) {
-          return
-        }
-        walletConnectService.newSession(
-          href,
-          !!params?.autosign,
-          origin,
-          activeAccount,
-          activeNetwork
-        )
       }
       break
     default:
