18a12d1945d5e0506b222c980cc66213f2470459
An Nguyen <an.nguyen@avalabs.org>
2022-11-15 09:25:09 -0600

    CP-3998: Fix release builds failing to build due to the minification of @avalabs/avalanchejs-v2 (#470)

--- metro.config.js
+++ metro.config.js
@@ -17,6 +17,11 @@ module.exports = {
         experimentalImportSupport: false,
         inlineRequires: true
       }
-    })
+    }),
+    // using metro-minify-esbuild as the minifier because:
+    // 1. react native metro bundler uses uglify-es, which doesn't support bigint syntax (0n, 1n,...)
+    // 2. metro-minify-esbuild is ~46x faster
+    minifierPath: require.resolve('metro-minify-esbuild'),
+    minifierConfig: {}
   }
 }
