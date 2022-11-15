f027c06338f71b70b90c512e7be693ac35da9e42
Gergely Lovas <36536513+gergelylovas@users.noreply.github.com>
2022-11-15 19:29:44 +0100

    CP-4021: kill all walletconnect sessions on logout (#472)
    * fix: kill all walletconnect sessions on logout
    
    * fix: change clear order
    
    Co-authored-by: An Nguyen <an.nguyen@avalabs.org>

--- app/store/app/listeners.ts
+++ app/store/app/listeners.ts
@@ -2,6 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { isAnyOf } from '@reduxjs/toolkit'
 import { differenceInSeconds } from 'date-fns'
 import { AppState, AppStateStatus, Platform } from 'react-native'
+import { default as WalletConnectService } from 'services/walletconnect/WalletConnectService'
 import { AppListenerEffectAPI } from 'store'
 import {
   onRehydrationComplete,
@@ -122,6 +123,8 @@ const clearData = async (action: any, listenerApi: AppListenerEffectAPI) => {
   const { dispatch } = listenerApi
   dispatch(setWalletState(WalletState.NONEXISTENT))
   await BiometricsSDK.clearWalletKey()
+  // do before clearing AsyncStorage since it writes it
+  await WalletConnectService.killAllSessions()
   await AsyncStorage.clear()
 }
 
