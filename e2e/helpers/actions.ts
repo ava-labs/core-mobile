f70f5a0b89f80d4a1e5809d4d86798a91f79168c
Tyler Hackett <thackett86181@gmail.com>
2022-11-16 16:17:49 -0800

    rebase conflicts

--- e2e/helpers/actions.ts
+++ e2e/helpers/actions.ts
@@ -1,5 +1,6 @@
 import { element, waitFor } from 'detox'
 
+<<<<<<< HEAD
 const tap = async (item: Detox.NativeMatcher) => {
   await element(item).tap()
 }
@@ -45,6 +46,41 @@ const swipeDown = async (
   speed: undefined,
   normalizedOffset: number
 ) => {
+=======
+const tap = async item => {
+  await element(item).tap()
+}
+
+const tapElementAtIndex = async (item, num) => {
+  await element(item).atIndex(num).tap()
+}
+
+const longPress = async item => {
+  await element(item).longPress()
+}
+
+const setColumnToValue = async (item, index, value) => {
+  await element(item).setColumnToValue(index, value)
+}
+
+const setInputText = async (item, value) => {
+  await element(item).replaceText(value)
+}
+
+const waitForElement = async (item, timeout = 2000) => {
+  await waitFor(element(item)).toBeVisible(item).withTimeout(timeout)
+}
+
+const getAttributes = async item => {
+  return await element(item).getAttributes()
+}
+
+const swipeUp = async (item, speed, normalizedOffset) => {
+  return await element(item).swipe('up', speed, normalizedOffset)
+}
+
+const swipeDown = async (item, speed, normalizedOffset) => {
+>>>>>>> 5cfbd55 (add base pages)
   return await element(item).swipe('down', speed, normalizedOffset)
 }
 
