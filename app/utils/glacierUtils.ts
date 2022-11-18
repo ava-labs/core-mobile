848046d4e68e1071f3f3e30f960d125d12494c11
An Nguyen <an.nguyen@avalabs.org>
2022-11-18 11:14:44 -0600

    switch glacier proxy with dedicated proxy service

--- app/utils/glacierUtils.ts
+++ app/utils/glacierUtils.ts
@@ -1,25 +1,20 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import Config from 'react-native-config'
-import Logger from './Logger'
+
+if (!Config.GLACIER_URL) throw Error('GLACIER_URL ENV is missing')
 
 /**
  * After constructing a full URL make sure to call `addGlacierAPIKeyIfNeeded`.
  */
-export const GLACIER_URL = __DEV__
-  ? Config.GLACIER_DEV_URL!
-  : Config.GLACIER_PROD_URL!
+export const GLACIER_URL = Config.GLACIER_URL
 
-if (!GLACIER_URL) Logger.error('GLACIER URL ENV is missing')
+// RPC urls returned in the token list are always using the production URL
+const knownHosts = ['glacier-api.avax.network', 'proxy-api.avax.network']
 
 /**
  * Glacier needs an API key for development, this adds the key if needed.
  */
 export function addGlacierAPIKeyIfNeeded(url: string): string {
-  if (
-    Config.GLACIER_API_KEY &&
-    (url.startsWith(Config.GLACIER_DEV_URL!) ||
-      url.startsWith(Config.GLACIER_PROD_URL!))
-  ) {
+  if (Config.GLACIER_API_KEY && knownHosts.includes(url)) {
     const urlObj = new URL(url)
     const search_params = urlObj.searchParams // copy, does not update the URL
     search_params.set('token', Config.GLACIER_API_KEY)
