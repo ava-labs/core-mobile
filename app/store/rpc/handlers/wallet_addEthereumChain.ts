b6bcbc60886b5b3e8b6a6526c85bac3eec869527
Gergely Lovas <gergely.lovas@avalabs.org>
2022-11-29 18:48:26 +0100

    feat: add wallet_addEthereumChain and wallet_switchEthereumChain

---
+++ app/store/rpc/handlers/wallet_addEthereumChain.ts
@@ -0,0 +1,186 @@
+import { Network, NetworkVMType } from '@avalabs/chains-sdk'
+import { PayloadAction } from '@reduxjs/toolkit'
+import { ethErrors } from 'eth-rpc-errors'
+import { isValidRPCUrl } from 'services/network/utils/isValidRpcUrl'
+import { RpcMethod } from 'services/walletconnect/types'
+import { AppListenerEffectAPI } from 'store'
+import {
+  addCustomNetwork,
+  selectActiveNetwork,
+  selectNetworks,
+  setActive
+} from 'store/network'
+import {
+  addRequest,
+  removeRequest,
+  sendRpcError,
+  sendRpcResult
+} from '../slice'
+import { DappRpcRequest, RpcRequestHandler } from './types'
+
+interface AddEthereumChainParameter {
+  chainId: string
+  blockExplorerUrls?: string[]
+  chainName?: string
+  iconUrls?: string[]
+  nativeCurrency?: {
+    name: string
+    symbol: string
+    decimals: number
+  }
+  rpcUrls?: string[]
+}
+
+export interface WalletAddEthereumChainRpcRequest
+  extends DappRpcRequest<
+    RpcMethod.WALLET_ADD_ETHEREUM_CHAIN,
+    AddEthereumChainParameter[]
+  > {
+  network: Network
+  isExisting: boolean
+}
+
+class WalletAddEthereumChainHandler
+  implements RpcRequestHandler<WalletAddEthereumChainRpcRequest>
+{
+  methods = [RpcMethod.WALLET_ADD_ETHEREUM_CHAIN]
+
+  handle = async (
+    action: PayloadAction<WalletAddEthereumChainRpcRequest['payload'], string>,
+    listenerApi: AppListenerEffectAPI
+  ) => {
+    const { dispatch } = listenerApi
+    const store = listenerApi.getState()
+
+    const requestedChain: AddEthereumChainParameter | undefined =
+      action.payload.params?.[0]
+
+    if (!requestedChain) {
+      dispatch(
+        sendRpcError({
+          request: { payload: action.payload },
+          error: ethErrors.rpc.invalidParams({
+            message: 'missing chain params'
+          })
+        })
+      )
+      return
+    }
+
+    const chains = selectNetworks(store)
+    const currentActiveNetwork = selectActiveNetwork(store)
+    const supportedChainIds = Object.keys(chains ?? {})
+    const requestedChainId = Number(requestedChain.chainId)
+    const chainRequestedIsSupported =
+      requestedChain && supportedChainIds.includes(requestedChainId.toString())
+    const isSameNetwork = requestedChainId === currentActiveNetwork?.chainId
+
+    if (isSameNetwork) {
+      dispatch(
+        sendRpcResult({
+          request: { payload: action.payload },
+          result: null
+        })
+      )
+      return
+    }
+
+    const rpcUrl = requestedChain?.rpcUrls?.[0]
+    if (!rpcUrl) {
+      dispatch(
+        sendRpcError({
+          request: { payload: action.payload },
+          error: ethErrors.rpc.invalidParams({
+            message: 'RPC url missing'
+          })
+        })
+      )
+      return
+    }
+
+    if (!requestedChain.nativeCurrency) {
+      dispatch(
+        sendRpcError({
+          request: { payload: action.payload },
+          error: ethErrors.rpc.invalidParams({
+            message: 'Expected nativeCurrency param to be defined'
+          })
+        })
+      )
+      return
+    }
+
+    const customNetwork: Network = {
+      chainId: requestedChainId,
+      chainName: requestedChain.chainName || '',
+      description: '',
+      explorerUrl: requestedChain.blockExplorerUrls?.[0] || '',
+      isTestnet: false,
+      logoUri: requestedChain.iconUrls?.[0] || '',
+      mainnetChainId: 0,
+      networkToken: {
+        symbol: requestedChain.nativeCurrency.symbol,
+        name: requestedChain.nativeCurrency.name,
+        description: '',
+        decimals: requestedChain.nativeCurrency.decimals,
+        logoUri: requestedChain.iconUrls?.[0] || ''
+      },
+      platformChainId: '',
+      rpcUrl,
+      subnetId: '',
+      vmId: '',
+      vmName: NetworkVMType.EVM
+    }
+
+    if (chainRequestedIsSupported) {
+      dispatch(
+        addRequest({
+          payload: action.payload,
+          network: customNetwork,
+          isExisting: true
+        })
+      )
+      return
+    }
+
+    const isValid = await isValidRPCUrl(
+      customNetwork.chainId,
+      customNetwork.rpcUrl
+    )
+    if (!isValid) {
+      dispatch(
+        sendRpcError({
+          request: { payload: action.payload },
+          error: ethErrors.rpc.invalidParams({
+            message: 'ChainID does not match the rpc url'
+          })
+        })
+      )
+      return
+    }
+    dispatch(
+      addRequest({
+        payload: action.payload,
+        network: customNetwork,
+        isExisting: false
+      })
+    )
+  }
+
+  onApprove = async (
+    action: PayloadAction<
+      { request: WalletAddEthereumChainRpcRequest; result?: unknown },
+      string
+    >,
+    listenerApi: AppListenerEffectAPI
+  ) => {
+    const { dispatch } = listenerApi
+    const request = action.payload.request
+
+    dispatch(addCustomNetwork(request.network))
+    dispatch(setActive(request.network.chainId))
+    dispatch(removeRequest(request.payload.id))
+    dispatch(sendRpcResult({ request }))
+  }
+}
+export const walletAddEthereumChainHandler = new WalletAddEthereumChainHandler()
---
+++ app/store/rpc/handlers/wallet_switchEthereumChain.ts
@@ -0,0 +1,96 @@
+import { Network } from '@avalabs/chains-sdk'
+import { PayloadAction } from '@reduxjs/toolkit'
+import { ethErrors } from 'eth-rpc-errors'
+import { RpcMethod } from 'services/walletconnect/types'
+import { AppListenerEffectAPI } from 'store'
+import { selectActiveNetwork, selectNetworks, setActive } from 'store/network'
+import {
+  addRequest,
+  removeRequest,
+  sendRpcError,
+  sendRpcResult
+} from '../slice'
+import { DappRpcRequest, RpcRequestHandler } from './types'
+
+export interface WalletSwitchEthereumChainRpcRequest
+  extends DappRpcRequest<
+    RpcMethod.WALLET_SWITCH_ETHEREUM_CHAIN,
+    { chainId: string }[]
+  > {
+  network: Network
+}
+
+class WalletSwitchEthereumChainHandler
+  implements RpcRequestHandler<WalletSwitchEthereumChainRpcRequest>
+{
+  methods = [RpcMethod.WALLET_SWITCH_ETHEREUM_CHAIN]
+
+  handle = async (
+    action: PayloadAction<
+      WalletSwitchEthereumChainRpcRequest['payload'],
+      string
+    >,
+    listenerApi: AppListenerEffectAPI
+  ) => {
+    const { dispatch } = listenerApi
+    const store = listenerApi.getState()
+
+    const params = action.payload.params
+    const targetChainID = params?.[0]?.chainId // chain ID is hex with 0x perfix
+    const networks = selectNetworks(store)
+    const supportedNetwork = networks[Number(targetChainID)]
+    const currentActiveNetwork = selectActiveNetwork(store)
+
+    // Verify if the wallet is not currently on the requested network.
+    // If it is, we just need to return early to prevent an unnecessary UX
+    if (Number(targetChainID) === currentActiveNetwork?.chainId) {
+      dispatch(
+        sendRpcResult({
+          request: { payload: action.payload },
+          result: null
+        })
+      )
+      return
+    }
+    // If the network is not currently on the requested network and we currently support the network
+    // then we need to show a confirmation popup to confirm user wants to switch to the requested network
+    // from the dApp they are on.
+    if (supportedNetwork?.chainId) {
+      dispatch(
+        addRequest({
+          payload: action.payload,
+          network: supportedNetwork
+        })
+      )
+      return
+    } else {
+      dispatch(
+        sendRpcError({
+          request: { payload: action.payload },
+          error: ethErrors.provider.custom({
+            code: 4902, // To-be-standardized "unrecognized chain ID" error
+            message: `Unrecognized chain ID "${targetChainID}". Try adding the chain using ${RpcMethod.WALLET_ADD_ETHEREUM_CHAIN} first.`
+          })
+        })
+      )
+      return
+    }
+  }
+
+  onApprove = async (
+    action: PayloadAction<
+      { request: WalletSwitchEthereumChainRpcRequest; result?: unknown },
+      string
+    >,
+    listenerApi: AppListenerEffectAPI
+  ) => {
+    const { dispatch } = listenerApi
+    const request = action.payload.request
+
+    dispatch(setActive(request.network.chainId))
+    dispatch(removeRequest(request.payload.id))
+    dispatch(sendRpcResult({ request }))
+  }
+}
+export const walletSwitchEthereumChainHandler =
+  new WalletSwitchEthereumChainHandler()
