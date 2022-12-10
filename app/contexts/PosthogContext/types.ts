eed215a4b3c6c635e7b154b078c5e3f12a24e347
An Nguyen <an.nguyen@avalabs.org>
2022-12-09 18:41:39 -0600

    add separate send nft flags for ios and android

--- app/contexts/PosthogContext/types.ts
+++ app/contexts/PosthogContext/types.ts
@@ -1,4 +1,25 @@
-import { FeatureGates, FeatureVars } from 'contexts/types'
+import { JsonMap } from 'posthog-react-native/src/bridge'
+
+export enum FeatureGates {
+  EVERYTHING = 'everything',
+  EVENTS = 'events',
+  SWAP = 'swap-feature',
+  BRIDGE = 'bridge-feature',
+  BRIDGE_BTC = 'bridge-feature-btc',
+  BRIDGE_ETH = 'bridge-feature-eth',
+  SEND = 'send-feature',
+  SEND_NFT_IOS = 'send-nft-ios-feature',
+  SEND_NFT_ANDROID = 'send-nft-android-feature'
+}
+
+export enum FeatureVars {
+  SENTRY_SAMPLE_RATE = 'sentry-sample-rate'
+}
+
+export type PosthogCapture = (
+  event: string,
+  properties?: JsonMap
+) => Promise<void>
 
 // posthog response can be an empty object when all features are disabled
 // thus, we need to use Partial
