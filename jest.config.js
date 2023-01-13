18a8b789aab0b55d6a05a5f09863dc127460cec5
Tyler Hackett <thackett86181@gmail.com>
2023-01-13 15:12:08 -0800

    fix jest config imports

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
-import setupTestRun from './e2e/setup'
 import sendResults from './e2e/sendResults'
 
 module.exports = async () => {
   await require('detox/runners/jest/index').globalSetup()
-  await setupTestRun()
 }
 
 module.exports = async () => {
