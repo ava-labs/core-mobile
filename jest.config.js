79512dae37d3be83ba86d9166e2d02c62327c635
Tyler Hackett <thackett86181@gmail.com>
2023-01-13 16:32:25 -0800

    fix imports

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,4 @@
-import sendResults from './e2e/sendResults'
-
-module.exports = async () => {
-  await require('detox/runners/jest/index').globalSetup()
-}
-
-module.exports = async () => {
-  try {
-    await sendResults()
-  } finally {
-    await require('detox/runners/jest/index').globalTeardown()
-  }
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
