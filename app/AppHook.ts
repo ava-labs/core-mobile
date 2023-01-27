eb7d11b8d81310bc200e31ce9790a931b363b5d3
Neven <piaf1986@gmail.com>
2023-01-27 16:43:25 +0100

    Add tokenInCurrencyFormatter to show up to 8 fraction digits when showing token value in currency

--- app/AppHook.ts
+++ app/AppHook.ts
@@ -2,7 +2,7 @@ import { asyncScheduler, AsyncSubject, concat, Observable, of } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { BackHandler } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { WalletSetupHook } from 'hooks/useWalletSetup'
 import { AppNavHook } from 'useAppNav'
 import { Repo } from 'Repo'
@@ -19,6 +19,7 @@ export type AppHook = {
   selectedCurrency: string
   signOut: () => Promise<void>
   currencyFormatter(num: number | string): string
+  tokenInCurrencyFormatter(num: number | string): string
 }
 
 export function useApp(
@@ -33,13 +34,23 @@ export function useApp(
   const { getSetting } = repository.userSettingsRepo
   const { setAnalyticsConsent } = usePosthogContext()
 
-  useEffect(waitForNavigationContainer, [])
+  const signOut = useCallback(async () => {
+    walletSetupHook.destroyWallet()
+    repository.flush()
+    dispatch(onLogOut())
+    dispatch(resetLoginAttempt())
+    appNavHook.resetNavToRoot()
+  }, [appNavHook, dispatch, repository, walletSetupHook])
+
+  useEffect(waitForNavigationContainer, [appNavHook.navigation])
   useEffect(watchCoreAnalyticsFlagFx, [getSetting, setAnalyticsConsent])
   useEffect(decideInitialRoute, [
     appNavHook,
+    dispatch,
     initRouteSet,
     navigationContainerSet,
-    repository
+    repository,
+    signOut
   ])
 
   function watchCoreAnalyticsFlagFx() {
@@ -80,14 +91,6 @@ export function useApp(
     })
   }
 
-  async function signOut() {
-    walletSetupHook.destroyWallet()
-    repository.flush()
-    dispatch(onLogOut())
-    dispatch(resetLoginAttempt())
-    appNavHook.resetNavToRoot()
-  }
-
   function onExit(): Observable<ExitEvents> {
     const exitPrompt = new AsyncSubject<ExitPromptAnswers>()
     const dialogOp: Observable<ExitFinished> = exitPrompt.pipe(
@@ -114,9 +117,6 @@ export function useApp(
    * Localized currency formatter
    */
   const currencyFormatter = useMemo(() => {
-    /**
-     * For performance reasons we want to instantiate this as little as possible
-     */
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: selectedCurrency ?? 'USD',
@@ -125,41 +125,73 @@ export function useApp(
       minimumFractionDigits: 2
     })
 
-    return (amount: number) => {
-      /**
-       * This formats the currency to return:
-       *   <symbol><amount>
-       *   e.g. $10.00, €10.00
-       * If <symbol> matches the currency code (e.g. "CHF 10") then it returns:
-       *   <amount> <symbol>
-       *   e.g. 10 CHF
-       */
-
-      const parts = formatter.formatToParts(amount)
-      // match "CHF 10"
-      if (parts[0]?.value === selectedCurrency) {
-        const flatArray = parts.map(x => x.value)
-        flatArray.push(` ${flatArray.shift() || ''}`)
-        return flatArray.join('').trim()
-      }
-      // match "-CHF 10"
-      if (parts[1]?.value === selectedCurrency) {
-        const flatArray = parts.map(x => x.value)
-        // remove the currency code after the sign
-        flatArray.splice(1, 1)
-        flatArray.push(` ${selectedCurrency}`)
-        return flatArray.join('').trim()
-      }
-
-      return formatter.format(amount)
-    }
+    return (amount: number) =>
+      formatCurrency(amount, formatter, formatter, selectedCurrency)
   }, [selectedCurrency])
+
+  /**
+   * When displaying token value in currency we keep max 8 fraction digits
+   */
+  const tokenInCurrencyFormatter = useMemo(() => {
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: selectedCurrency ?? 'USD',
+      currencyDisplay: 'symbol', // the extension uses 'narrowSymbol'
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2 // must be set for the `formatToParts` call below
+    })
+    const smallNumberFormatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: selectedCurrency ?? 'USD',
+      currencyDisplay: 'symbol', // the extension uses 'narrowSymbol'
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 8 // must be set for the `formatToParts` call below
+    })
+
+    return (amount: number) =>
+      formatCurrency(amount, formatter, smallNumberFormatter, selectedCurrency)
+  }, [selectedCurrency])
+
+  /**
+   * formatCurrency formats the currency to return:
+   *   <symbol><amount>
+   *   e.g. $10.00, €10.00
+   * If <symbol> matches the currency code (e.g. "CHF 10") then it returns:
+   *   <amount> <symbol>
+   *   e.g. 10 CHF
+   */
+  function formatCurrency(
+    amount: number,
+    bigNumberFormatter: Intl.NumberFormat,
+    smallNumberFormatter: Intl.NumberFormat,
+    currency: string
+  ) {
+    const formatter = amount < 1 ? smallNumberFormatter : bigNumberFormatter
+    const parts = formatter.formatToParts(amount)
+    // match "CHF 10"
+    if (parts[0]?.value === currency) {
+      const flatArray = parts.map(x => x.value)
+      flatArray.push(` ${flatArray.shift() || ''}`)
+      return flatArray.join('').trim()
+    }
+    // match "-CHF 10"
+    if (parts[1]?.value === currency) {
+      const flatArray = parts.map(x => x.value)
+      // remove the currency code after the sign
+      flatArray.splice(1, 1)
+      flatArray.push(` ${currency}`)
+      return flatArray.join('').trim()
+    }
+
+    return formatter.format(amount)
+  }
 
   return {
     signOut,
     onExit,
     selectedCurrency,
-    currencyFormatter
+    currencyFormatter,
+    tokenInCurrencyFormatter
   }
 }
 
