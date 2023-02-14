3afa4ddb02c4218dd9262d60cec1590cebc53316
Tyler Hackett <thackett86181@gmail.com>
2023-02-14 12:08:42 -0800

    an comment fixes

--- tests/msw/handlers.js
+++ tests/msw/handlers.js
@@ -1,35 +1,28 @@
 import { rest } from 'msw'
-import coingeckoResponse from './coingeckoResponse.json'
-import glacierTokenList from './glacierTokenList.json'
-import coingeckoSimplePrice from './coingeckoSimplePrice.json'
-import coingeckoBitcoin from './coingeckoBitcoin.json'
+import coingeckoResponse from 'tests/fixtures/coingeckoResponse'
+import glacierTokenList from 'tests/fixtures/glacierTokenList.json'
+import coingeckoSimplePrice from 'tests/fixtures/coingeckoSimplePrice.json'
+import coingeckoBitcoin from 'tests/fixtures/coingeckoBitcoin.json'
+
+const coingeckoApi = 'https://api.coingecko.com/api/v3'
 
 export const handlers = [
-  rest.get(
-    'https://api.coingecko.com/api/v3/coins/markets',
-    async (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(coingeckoResponse))
-    }
-  ),
+  rest.get(`${coingeckoApi}/coins/markets`, async (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(coingeckoResponse))
+  }),
 
   rest.get(
-    'https://glacier-api-dev.avax.network/tokenlist',
+    `${process.env.GLACIER_URL}/tokenlist`,
     async (req, res, ctx) => {
       return res(ctx.status(200), ctx.json(glacierTokenList))
     }
   ),
 
-  rest.get(
-    'https://api.coingecko.com/api/v3/simple/price',
-    async (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(coingeckoSimplePrice))
-    }
-  ),
+  rest.get(`${coingeckoApi}/simple/price`, async (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(coingeckoSimplePrice))
+  }),
 
-  rest.get(
-    'https://api.coingecko.com/api/v3/coins/bitcoin',
-    async (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(coingeckoBitcoin))
-    }
-  )
+  rest.get(`${coingeckoApi}/coins/bitcoin`, async (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(coingeckoBitcoin))
+  })
 ]
