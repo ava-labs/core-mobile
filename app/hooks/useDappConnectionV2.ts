9bd3cdf96b911599e98414f8cad87992b50cc3a4
An Nguyen <an.nguyen@avalabs.org>
2023-02-24 09:27:09 -0600

    create hooks for common dapp interactions

---
+++ app/hooks/useDappConnectionV2.ts
@@ -0,0 +1,49 @@
+import { ethErrors } from 'eth-rpc-errors'
+import { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { Session } from 'services/walletconnectv2/WalletConnectService'
+import {
+  onRequestApproved,
+  onRequestRejected,
+  killSessions as killSessionsAction,
+  Request
+} from 'store/walletConnectV2'
+
+export const useDappConnectionV2 = () => {
+  const dispatch = useDispatch()
+
+  const onUserApproved = useCallback(
+    (request: Request, data?: unknown) => {
+      dispatch(
+        onRequestApproved({
+          request,
+          data
+        })
+      )
+    },
+    [dispatch]
+  )
+
+  const onUserRejected = useCallback(
+    (request: Request, message?: string) => {
+      dispatch(
+        onRequestRejected({
+          request,
+          error: message
+            ? ethErrors.rpc.internal(message)
+            : ethErrors.provider.userRejectedRequest()
+        })
+      )
+    },
+    [dispatch]
+  )
+
+  const killSessions = useCallback(
+    async (sessionsToKill: Session[]) => {
+      dispatch(killSessionsAction(sessionsToKill))
+    },
+    [dispatch]
+  )
+
+  return { onUserApproved, onUserRejected, killSessions }
+}
