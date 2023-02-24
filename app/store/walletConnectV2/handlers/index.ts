2b76fc64ed4ba1677aaed69f9058a911a1c990d2
An Nguyen <an.nguyen@avalabs.org>
2023-02-24 08:53:59 -0600

    add handler map

---
+++ app/store/walletConnectV2/handlers/index.ts
@@ -0,0 +1,36 @@
+import { avalancheGetAccountsHandler } from './account/avalanche_getAccounts/avalanche_getAccounts'
+import { avalancheGetContactsHandler } from './contact/avalanche_getContacts'
+import { avalancheUpdateContactHandler } from './contact/avalanche_updateContact/avalanche_updateContact'
+import { avalancheCreateContactHandler } from './contact/avalanche_createContact/avalanche_createContact'
+import { avalancheRemoveContactHandler } from './contact/avalanche_removeContact/avalanche_removeContact'
+import { ethSendTransactionHandler } from './eth_sendTransaction/eth_sendTransaction'
+import { ethSignHandler } from './eth_sign/eth_sign'
+import { sessionRequestHandler } from './session_request/session_request'
+import { walletAddEthereumChainHandler } from './chain/wallet_addEthereumChain/wallet_addEthereumChain'
+import { walletSwitchEthereumChainHandler } from './chain/wallet_switchEthereumChain/wallet_switchEthereumChain'
+import { avalancheBridgeAssetHandler } from './avalanche_bridgeAsset/avalanche_bridgeAsset'
+import { avalancheSelectAccountHandler } from './account/avalanche_selectAccount/avalanche_selectAccount'
+import { RpcRequestHandler } from './types'
+
+const handlerMap = [
+  avalancheSelectAccountHandler,
+  avalancheBridgeAssetHandler,
+  avalancheCreateContactHandler,
+  avalancheRemoveContactHandler,
+  avalancheUpdateContactHandler,
+  avalancheGetAccountsHandler,
+  avalancheGetContactsHandler,
+  ethSendTransactionHandler,
+  ethSignHandler,
+  sessionRequestHandler,
+  walletAddEthereumChainHandler,
+  walletSwitchEthereumChainHandler
+].reduce((acc, current) => {
+  current.methods.forEach(method => {
+    acc.set(method, current)
+  })
+  return acc
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+}, new Map<string, RpcRequestHandler<any>>())
+
+export default handlerMap
