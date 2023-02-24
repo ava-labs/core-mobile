55412856c1358e23efb046a54737dbf2727eb57e
An Nguyen <an.nguyen@avalabs.org>
2023-02-24 07:59:22 -0600

    add custom type for rn-flipper-async-storage-advanced

--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,6 @@ import { RootScreenStackParamList } from './app/navigation/RootScreenStack'
  * Necessary type file for react-native-toast-notification so it's inserted in the global context.
  * Documentation: https://github.com/arnnis/react-native-toast-notifications#--how-to-call-toast-outside-react-components
  */
-export {}
-
 type ToastType = import('react-native-toast-notifications').ToastType
 
 declare global {
