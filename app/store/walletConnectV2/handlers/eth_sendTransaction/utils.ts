45030a445a325a3ca242ac97e4465b1fc49b38c4
An Nguyen <an.nguyen@avalabs.org>
2023-02-24 08:52:08 -0600

    add v2 eth_sendTransaction handler

---
+++ app/store/walletConnectV2/handlers/eth_sendTransaction/utils.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+
+const transactionSchema = z.object({
+  from: z.string(),
+  to: z.string(),
+  value: z.string(),
+  data: z.string().optional(),
+  gas: z.string().or(z.number()).optional(),
+  gasPrice: z.string().optional()
+})
+
+const paramsSchema = z.tuple([transactionSchema])
+
+const approveDataSchema = z.object({
+  txParams: transactionSchema
+})
+
+export const parseRequestParams = (params: unknown) => {
+  return paramsSchema.safeParse(params)
+}
+
+export const parseApproveData = (data: unknown) => {
+  return approveDataSchema.safeParse(data)
+}
+
+export type TransactionParams = {
+  from: string
+  to: string
+  value: string
+  data?: string
+  gas?: string | number
+  gasPrice?: string
+}
