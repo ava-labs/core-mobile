8dc9da207996a1cde66946b2104b5af1137e21fd
An Nguyen <an.nguyen@avalabs.org>
2023-02-24 09:43:16 -0600

    clean up components

--- app/screens/rpc/components/SignMessage/types.ts
+++
@@ -1,9 +0,0 @@
-import { PeerMeta } from 'services/walletconnect/types'
-
-export interface MessageAction {
-  id?: string | number
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  displayData: any
-  method: string
-  site: PeerMeta
-}
