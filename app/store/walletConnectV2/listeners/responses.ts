0366c95e1f77e2673f71d9abf8f677c5dcd65fad
An Nguyen <an.nguyen@avalabs.org>
2023-02-24 08:31:28 -0600

    add walletConnectV2 listeners

---
+++ app/store/walletConnectV2/listeners/responses.ts
@@ -0,0 +1,106 @@
+import { AppListenerEffectAPI } from 'store'
+import { SessionTypes } from '@walletconnect/types'
+import WalletConnectService from 'services/walletconnectv2/WalletConnectService'
+import { ethErrors } from 'eth-rpc-errors'
+import { capture } from 'store/posthog'
+import { showSimpleToast, showDappToastError } from 'components/Snackbar'
+import Logger from 'utils/Logger'
+import { onSendRpcError, onSendRpcResult } from '../slice'
+import { isSessionProposal } from './utils'
+
+export const sendRpcResult = async (
+  action: ReturnType<typeof onSendRpcResult>,
+  listenerApi: AppListenerEffectAPI
+) => {
+  const { request, result } = action.payload
+  const { dispatch } = listenerApi
+
+  if (isSessionProposal(request)) {
+    const relayProtocol = request.data.params.relays[0]?.protocol
+
+    const approveData = {
+      id: request.data.id,
+      relayProtocol,
+      namespaces: result as SessionTypes.Namespaces
+    }
+
+    try {
+      const session = await WalletConnectService.approveSession(approveData)
+
+      const { name, url } = session.peer.metadata
+      const requiredNamespaces = JSON.stringify(session.requiredNamespaces)
+      const message = `Connected to ${name}`
+
+      showSimpleToast(message)
+
+      dispatch(
+        capture({
+          event: 'WalletConnectSessionApprovedV2',
+          properties: {
+            requiredNamespaces,
+            dappUrl: url
+          }
+        })
+      )
+    } catch (e) {
+      Logger.error('Unable to approve session proposal', e)
+      showDappToastError(
+        'Unable to approve session proposal',
+        request.data.params.proposer.metadata.name
+      )
+    }
+  } else {
+    const topic = request.data.topic
+    const requestId = request.data.id
+
+    try {
+      await WalletConnectService.approveRequest(topic, requestId, result)
+    } catch (e) {
+      Logger.error('Unable to approve request', e)
+      showDappToastError(
+        'Unable to approve request',
+        request.session.peer.metadata.name
+      )
+    }
+  }
+}
+
+export const sendRpcError = async (
+  action: ReturnType<typeof onSendRpcError>
+) => {
+  const { request, error } = action.payload
+
+  // only show error toast if it is not a user rejected error
+  const shouldShowErrorToast =
+    error.code !== ethErrors.provider.userRejectedRequest().code
+
+  if (isSessionProposal(request)) {
+    const dappName = request.data.params.proposer.metadata.name
+
+    shouldShowErrorToast && showDappToastError(error.message, dappName)
+
+    try {
+      await WalletConnectService.rejectSession(request.data.id)
+    } catch (e) {
+      Logger.error('Unable to reject session proposal', e)
+      showDappToastError('Unable to reject session proposal', dappName)
+    }
+  } else {
+    const topic = request.data.topic
+    const requestId = request.data.id
+    const dappName = request.session.peer.metadata.name
+
+    shouldShowErrorToast && showDappToastError(error.message, dappName)
+
+    try {
+      await WalletConnectService.rejectRequest(
+        topic,
+        requestId,
+        error ?? ethErrors.rpc.internal()
+      )
+    } catch (e) {
+      Logger.error('Unable to reject request', e)
+      showDappToastError('Unable to reject request', dappName)
+    }
+  }
+}
---
+++ app/store/walletConnectV2/listeners/sessions.ts
@@ -0,0 +1,91 @@
+import { AppListenerEffectAPI } from 'store/index'
+import WalletConnectService, {
+  WalletConnectCallbacks
+} from 'services/walletconnectv2/WalletConnectService'
+import { InteractionManager } from 'react-native'
+import { showSimpleToast } from 'components/Snackbar'
+import Logger from 'utils/Logger'
+import {
+  onRehydrationComplete,
+  selectWalletState,
+  WalletState
+} from 'store/app'
+import { AnyAction } from '@reduxjs/toolkit'
+import { killSessions, newSession, onDisconnect, onRequest } from '../slice'
+import { RpcMethod } from '../types'
+
+const callbacks = (
+  listenerApi: AppListenerEffectAPI
+): WalletConnectCallbacks => {
+  const { dispatch } = listenerApi
+
+  return {
+    onSessionProposal: data =>
+      dispatch(
+        onRequest({
+          method: RpcMethod.SESSION_REQUEST,
+          data
+        })
+      ),
+    onSessionRequest: (data, session) =>
+      dispatch(
+        onRequest({
+          method: data.params.request.method,
+          data,
+          session
+        })
+      ),
+    onDisconnect: data => dispatch(onDisconnect(data))
+  }
+}
+
+export const initWalletConnect = async (
+  action: AnyAction,
+  listenerApi: AppListenerEffectAPI
+) => {
+  try {
+    if (onRehydrationComplete.match(action)) {
+      const state = listenerApi.getState()
+      const walletState = selectWalletState(state)
+
+      if (walletState === WalletState.NONEXISTENT) return
+    }
+
+    await WalletConnectService.init(callbacks(listenerApi))
+  } catch (e) {
+    Logger.error('Unable to init wallet connect', e)
+  }
+}
+
+export const startSession = async (action: ReturnType<typeof newSession>) => {
+  const uri = action.payload
+
+  try {
+    await WalletConnectService.pair(uri)
+  } catch (e) {
+    Logger.error('Unable to pair with dapp', e)
+    showSimpleToast('Unable to pair with dapp')
+  }
+}
+
+export const killAllSessions = async () =>
+  WalletConnectService.killAllSessions()
+
+export const killSomeSessions = async (
+  action: ReturnType<typeof killSessions>
+) => {
+  const sessionsToKill = action.payload
+  const topics = sessionsToKill.map(session => session.topic)
+
+  WalletConnectService.killSessions(topics)
+}
+
+export const handleDisconnect = async (
+  action: ReturnType<typeof onDisconnect>
+) => {
+  const peerMeta = action.payload
+
+  InteractionManager.runAfterInteractions(() => {
+    showSimpleToast(`${peerMeta.name} was disconnected`)
+  })
+}
