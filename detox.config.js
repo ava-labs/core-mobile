3d70f0a41d85a782b3b9dfd20c8513c64486a7e5
Tyler Hackett <thackett86181@gmail.com>
2023-03-01 10:47:47 -0800

    change sim target

--- detox.config.js
+++ detox.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     simulator: {
       type: 'ios.simulator',
-      device: { type: 'iPhone 13' }
+      device: { type: 'iPhone 14' }
     },
     emulator_ci: {
       type: 'android.emulator',
