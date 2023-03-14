7b7ea8e3db5dbc2e4030228b40775eb4cff23d0d
An Nguyen <an.nguyen@avalabs.org>
2023-03-14 09:09:51 -0500

    CP-4906: Add tests for wallet connect v2 (#608)

--- app/store/walletConnectV2/handlers/chain/utils.ts
+++ app/store/walletConnectV2/handlers/chain/utils.ts
@@ -8,18 +8,20 @@ const networkContractTokenResourceLinkSchema = z.object({
   url: z.string()
 })
 
-const networkContractTokenSchema = z.object({
-  address: z.string(),
-  name: z.string(),
-  symbol: z.string(),
-  contractType: z.literal('ERC-20'),
-  decimals: z.number(),
-  logoUri: z.string().optional(),
-  resourceLinks: z.array(networkContractTokenResourceLinkSchema).optional(),
-  description: z.string().optional(),
-  tags: z.array(z.string()).optional(),
-  email: z.string().optional()
-})
+const networkContractTokenSchema = z
+  .object({
+    address: z.string(),
+    name: z.string(),
+    symbol: z.string(),
+    contractType: z.literal('ERC-20'),
+    decimals: z.number(),
+    logoUri: z.string().optional(),
+    resourceLinks: z.array(networkContractTokenResourceLinkSchema).optional(),
+    description: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    email: z.string().optional()
+  })
+  .passthrough()
 
 const networkTokenSchema = z.object({
   name: z.string(),
@@ -29,34 +31,36 @@ const networkTokenSchema = z.object({
   logoUri: z.string()
 })
 
-export const networkSchema = z.object({
-  chainName: z.string(),
-  description: z.string().optional(),
-  chainId: z.number(),
-  platformChainId: z.string().optional(),
-  subnetId: z.string().optional(),
-  vmId: z.string().optional(),
-  vmName: networkVMTypeSchema,
-  rpcUrl: z.string(),
-  wsUrl: z.string().optional(),
-  isTestnet: z.boolean().optional(),
-  mainnetChainId: z.number().optional(),
-  networkToken: networkTokenSchema,
-  logoUri: z.string(),
-  tokens: z.array(networkContractTokenSchema).optional(),
-  utilityAddresses: z
-    .object({
-      multicall: z.string()
-    })
-    .optional(),
-  pricingProviders: z
-    .object({
-      coingecko: z.object({
-        assetPlatformId: z.string().optional(),
-        nativeTokenId: z.string().optional()
+export const networkSchema = z
+  .object({
+    chainName: z.string(),
+    description: z.string().optional(),
+    chainId: z.number(),
+    platformChainId: z.string().optional(),
+    subnetId: z.string().optional(),
+    vmId: z.string().optional(),
+    vmName: networkVMTypeSchema,
+    rpcUrl: z.string(),
+    wsUrl: z.string().optional(),
+    isTestnet: z.boolean().optional(),
+    mainnetChainId: z.number().optional(),
+    networkToken: networkTokenSchema,
+    logoUri: z.string(),
+    tokens: z.array(networkContractTokenSchema).optional(),
+    utilityAddresses: z
+      .object({
+        multicall: z.string()
       })
-    })
-    .optional(),
-  explorerUrl: z.string().optional(),
-  subnetExplorerUriId: z.string().optional()
-})
+      .optional(),
+    pricingProviders: z
+      .object({
+        coingecko: z.object({
+          assetPlatformId: z.string().optional(),
+          nativeTokenId: z.string().optional()
+        })
+      })
+      .optional(),
+    explorerUrl: z.string().optional(),
+    subnetExplorerUriId: z.string().optional()
+  })
+  .passthrough()
---
+++ app/store/walletConnectV2/handlers/eth_sign/utils/parseApproveData.test.ts
@@ -0,0 +1,70 @@
+/* eslint-disable jest/expect-expect */
+import mockAccounts from 'tests/fixtures/accounts.json'
+import mockNetworks from 'tests/fixtures/networks.json'
+import { primitiveTypedData, typedData } from 'tests/fixtures/rpc/typedData'
+import { parseApproveData } from './parseApproveData'
+
+const testParseInvalidApproveData = (data: unknown) => {
+  const result = parseApproveData(data)
+  expect(result.success).toEqual(false)
+}
+
+const testParseValidApproveData = (data: unknown) => {
+  const result = parseApproveData(data)
+  expect(result.success).toEqual(true)
+  // @ts-expect-error data does exist
+  expect(result.data.data).toEqual(data.data)
+  // @ts-expect-error data does exist
+  expect(result.data.network).toEqual(data.network)
+  // @ts-expect-error data does exist
+  expect(result.data.account).toEqual(data.account)
+}
+
+describe('parseApproveData', () => {
+  it('should fail to parse when data is invalid', () => {
+    const invalidDataScenarios = [
+      null,
+      {},
+      { network: mockNetworks[43114] },
+      { account: mockAccounts[0] },
+      { data: '48656c6c6f2031323321' }
+    ]
+
+    for (const scenario of invalidDataScenarios) {
+      testParseInvalidApproveData(scenario)
+    }
+  })
+
+  it('should parse correctly when data is valid', () => {
+    const testAccount = mockAccounts[0]
+    const testNetwork = mockNetworks[43114]
+
+    const validDataScenarios = [
+      {
+        data: {
+          data: '48656c6c6f2031323321',
+          account: testAccount,
+          network: testNetwork
+        }
+      },
+      {
+        data: {
+          data: primitiveTypedData,
+          account: testAccount,
+          network: testNetwork
+        }
+      },
+      {
+        data: {
+          data: typedData,
+          account: testAccount,
+          network: testNetwork
+        }
+      }
+    ]
+
+    for (const scenario of validDataScenarios) {
+      testParseValidApproveData(scenario.data)
+    }
+  })
+})
