127258fbe86c915a2877e96e07cc2e83776aaa6b
ARTEM BESPALOV <40455398+artyombespalov@users.noreply.github.com>
2023-03-20 14:56:50 -0700

    CP-5144: New e2e automation Empty Assets / Add custom token / Add account (#623)

---
+++ e2e/tests/network/addCustomToken.e2e.ts
@@ -0,0 +1,25 @@
+/* eslint-disable jest/expect-expect */
+import Assert from '../../helpers/assertions'
+import LoginRecoverWallet from '../../helpers/loginRecoverWallet'
+import PortfolioPage from '../../pages/portfolio.page'
+import ManageTokensPage from '../../pages/manageTokens.page'
+import { warmup } from '../../helpers/warmup'
+
+describe('Change Network', () => {
+  beforeAll(async () => {
+    await warmup()
+    await LoginRecoverWallet.recoverWalletLogin()
+  })
+
+  it('should add custom network', async () => {
+    await PortfolioPage.tapAvaxNetwork()
+    await PortfolioPage.tapManageTokens()
+    await ManageTokensPage.tapAddcustomToken()
+    await ManageTokensPage.inputCustomToken()
+    await ManageTokensPage.tapAddButton()
+    await ManageTokensPage.tapAddcustomToken()
+    await ManageTokensPage.inputCustomToken()
+
+    await Assert.isVisible(ManageTokensPage.addedMessage)
+  })
+})
