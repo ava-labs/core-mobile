09fa317cb575f0d2db540fe32161d3c518129229
ARTEM BESPALOV <40455398+artyombespalov@users.noreply.github.com>
2023-03-21 09:39:06 -0700

    CP-5146: Fixed flaky test Activity Tab (#624)

--- e2e/locators/activityTab.loc.ts
+++ e2e/locators/activityTab.loc.ts
@@ -9,6 +9,5 @@ export default {
   bridgeTransaction: 'bridge_transaction__item',
   bridgeSVG: 'bridge_svg',
   arrowSVG: 'arrow_svg',
-  linkIcon: 'link_svg',
-  usdCoin: 'USD Coin'
+  linkIcon: 'link_svg'
 }
