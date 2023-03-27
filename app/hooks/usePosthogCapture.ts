762d6a217121750bb069a515be22cdb7844332d2
An Nguyen <an.nguyen@avalabs.org>
2023-03-27 10:36:06 -0500

    add useCallback to prevent infinite rerender

--- app/hooks/usePosthogCapture.ts
+++ app/hooks/usePosthogCapture.ts
@@ -2,13 +2,17 @@ import { JsonMap } from 'store/posthog/types'
 
 import { useDispatch } from 'react-redux'
 import { capture as captureAction } from 'store/posthog'
+import { useCallback } from 'react'
 
 export const usePostCapture = () => {
   const dispatch = useDispatch()
 
-  const capture = (event: string, properties?: JsonMap) => {
-    dispatch(captureAction({ event, properties }))
-  }
+  const capture = useCallback(
+    (event: string, properties?: JsonMap) => {
+      dispatch(captureAction({ event, properties }))
+    },
+    [dispatch]
+  )
 
   return {
     capture
