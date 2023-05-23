dcd7694f7d5e15fbe46d6cfb6e7d076410c24ee2
An Nguyen <an.nguyen@avalabs.org>
2023-05-23 16:19:22 -0500

    add necessary providers

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+
+import React from 'react';
+import { EncryptedStoreProvider } from '../app/contexts/EncryptedStoreProvider'
+import { ApplicationContextProvider } from '../app/contexts/ApplicationContext'
+import { PosthogContextProvider } from '../app/contexts/PosthogContext'
+
 export const parameters = {
   controls: {
     matchers: {
@@ -6,3 +12,15 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+    (Story) => (
+      <EncryptedStoreProvider>
+        <PosthogContextProvider>
+          <ApplicationContextProvider>
+            <Story />
+          </ApplicationContextProvider>
+        </PosthogContextProvider>
+      </EncryptedStoreProvider>
+    )
+  ]
