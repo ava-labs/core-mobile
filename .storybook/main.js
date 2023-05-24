82d15eea73206b346f9ef17cd507fb49d4262c5d
An Nguyen <an.nguyen@avalabs.org>
2023-05-24 11:23:11 -0500

    fix background addon not selectable

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../storybook/stories/**/*.stories.?(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
+  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions', '@storybook/addon-ondevice-backgrounds'],
 };
