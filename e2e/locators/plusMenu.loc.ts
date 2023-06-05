fee20f0f59185c5ec56d1463f937145a2eef9846
ARTEM BESPALOV <40455398+artyombespalov@users.noreply.github.com>
2023-06-05 13:39:10 -0700

    CP-5194:Playwright integration (#700)
    Co-authored-by: Tyler Hackett <thackett86181@gmail.com>

--- e2e/locators/plusMenu.loc.ts
+++ e2e/locators/plusMenu.loc.ts
@@ -1,4 +1,7 @@
 export default {
+  connectionURI: 'Connection URI',
   sendSVG: 'arrow_svg',
-  send: 'tab_navigator__send_button'
+  send: 'tab_navigator__send_button',
+  walletConnectButton: 'WalletConnect',
+  inputTextField: 'input_text'
 }
