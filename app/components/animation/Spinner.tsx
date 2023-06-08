17f5e636e658b8a8eb1ad220f140bddc955b93b9
An Nguyen <an.nguyen@avalabs.org>
2023-06-08 11:29:46 -0500

    CP-5413: fix android animation not starting (#704)

--- app/components/animation/Spinner.tsx
+++ app/components/animation/Spinner.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
 import LottieView from 'lottie-react-native'
 import SpinnerSource from 'assets/lotties/spinner.json'
@@ -11,10 +11,22 @@ interface Props {
 
 const Spinner: FC<Props> = ({ size = 40, color }) => {
   const theme = useApplicationContext().theme
+  const lottieRef = React.useRef<LottieView | null>(null)
+
   const spinnerColor = color ?? theme.colorPrimary1
+
+  // temp workaround for autoplay issue
+  // https://github.com/lottie-react-native/lottie-react-native/issues/832
+  useEffect(() => {
+    setTimeout(() => {
+      lottieRef.current?.play()
+    }, 0)
+  }, [])
+
   return (
     <View style={styles.container}>
       <LottieView
+        ref={lottieRef}
         autoPlay={true}
         loop={true}
         source={SpinnerSource}
