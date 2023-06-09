73ea7e8eb1f207fa8a9fe073c7ec69192f57b161
An Nguyen <an.nguyen@avalabs.org>
2023-06-09 09:09:26 -0500

    refetch on app focus and reconnect

--- app/contexts/ReactQueryProvider.tsx
+++ app/contexts/ReactQueryProvider.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   QueryCache,
   QueryClient,
-  QueryClientProvider
+  QueryClientProvider,
+  focusManager
 } from '@tanstack/react-query'
+import NetInfo from '@react-native-community/netinfo'
+import { onlineManager } from '@tanstack/react-query'
+import { AppState, AppStateStatus } from 'react-native'
 
 const queryCache = new QueryCache()
 const queryClient = new QueryClient({
@@ -15,7 +19,31 @@ const queryClient = new QueryClient({
   }
 })
 
+const onAppStateChange = (status: AppStateStatus) => {
+  focusManager.setFocused(status === 'active')
+}
+
 export const ReactQueryProvider: React.FC = ({ children }) => {
+  // manage online status
+  useEffect(() => {
+    return NetInfo.addEventListener(state => {
+      const online =
+        state.isConnected != null &&
+        state.isConnected &&
+        Boolean(state.isInternetReachable)
+
+      onlineManager.setOnline(online)
+    })
+  }, [])
+
+  // manage app focus status
+  useEffect(() => {
+    const sub = AppState.addEventListener('change', onAppStateChange)
+    return () => {
+      sub.remove()
+    }
+  }, [])
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
