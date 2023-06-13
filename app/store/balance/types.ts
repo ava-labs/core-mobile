423c730765c7d486b274d535641f0c220ea335ee
Ade Amos <ade@adeamos.com>
2023-06-13 18:15:55 -0500

    Fixed type issue

--- app/store/balance/types.ts
+++ app/store/balance/types.ts
@@ -38,7 +38,6 @@ export type TokenWithBalanceERC20 = TokenBalanceData &
   TokenMarketData &
   NetworkContractToken & {
     type: TokenType.ERC20
-    description: string
   }
 
 export type NftTokenWithBalance = TokenBalanceData &
