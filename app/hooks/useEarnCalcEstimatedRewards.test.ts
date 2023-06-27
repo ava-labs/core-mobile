ca610b6127071ba2cdf5f7f83989c2029fd3e771
Neven <piaf1986@gmail.com>
2023-06-27 23:34:08 +0200

    Fix github commit

--- app/hooks/useEarnCalcEstimatedRewards.test.ts
+++ app/hooks/useEarnCalcEstimatedRewards.test.ts
@@ -3,27 +3,26 @@ import { bnToBig } from '@avalabs/utils-sdk'
 import EarnService from 'services/earn/EarnService'
 
 describe('calcReward', () => {
-    it('should return zero if current supply is max', () => {
-      expect(
-        EarnService.calcReward(
-          bnToBig(Avax.muln(25)),
-          7 * 24 * Hour,
-          bnToBig(MainnetParams.stakingConfig.RewardConfig.SupplyCap),
-          2,
-          true
-        )
-      ).toEqual('0')
-    })
-    it('should return non zero if current supply is less than max', () => {
-      expect(
-        EarnService.calcReward(
-          bnToBig(Avax.muln(2000000)),
-          7 * 24 * Hour,
-          bnToBig(MegaAvax.muln(400)),
-          2,
-          true
-        )
-      ).toEqual('3018657459186')
-    })
+  it('should return zero if current supply is max', () => {
+    expect(
+      EarnService.calcReward(
+        bnToBig(Avax.muln(25)),
+        7 * 24 * Hour,
+        bnToBig(MainnetParams.stakingConfig.RewardConfig.SupplyCap),
+        2,
+        true
+      )
+    ).toEqual('0')
+  })
+  it('should return non zero if current supply is less than max', () => {
+    expect(
+      EarnService.calcReward(
+        bnToBig(Avax.muln(2000000)),
+        7 * 24 * Hour,
+        bnToBig(MegaAvax.muln(400)),
+        2,
+        true
+      )
+    ).toEqual('3018657459186')
   })
 })
