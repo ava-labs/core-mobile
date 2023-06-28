e1f12827c893ad2146056257f77d9f3ffc92d4e8
Tyler Hackett <thackett86181@gmail.com>
2023-06-28 11:57:34 -0700

    change index arrow_svg

--- e2e/tests/activityTab/activityTab.e2e.smoke.ts
+++ e2e/tests/activityTab/activityTab.e2e.smoke.ts
@@ -16,7 +16,7 @@ describe('Activity Tab', () => {
   it('should show contract call only in activity list', async () => {
     await PortfolioPage.tapActivityTab()
     const startTime = new Date().getTime()
-    await actions.waitForElement(ActivityTabPage.arrowSVG, 10000, 0)
+    await actions.waitForElement(ActivityTabPage.arrowSVG, 10000, 1)
     const endTime = new Date().getTime()
     await actions.reportUIPerformance(
       startTime,
