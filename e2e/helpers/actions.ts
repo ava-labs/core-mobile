700032991fcb4b72ac733cfb119df36dbc642336
Tyler Hackett <thackett86181@gmail.com>
2023-06-28 11:40:45 -0700

    add file check cond

--- e2e/helpers/actions.ts
+++ e2e/helpers/actions.ts
@@ -221,11 +221,25 @@ const reportUIPerformance = async (
 
   let data = ''
 
-  try {
-    data = fs.readFileSync(reportUIPerformanceFilePath, 'utf8')
-  } catch (err) {
-    console.error('Error reading file:', err)
-    // continue
+  if (!fs.existsSync(reportUIPerformanceFilePath)) {
+    fs.writeFile(reportUIPerformanceFilePath, '', function (err: any) {
+      if (err) {
+        console.log(err)
+      }
+    })
+    try {
+      data = fs.readFileSync(reportUIPerformanceFilePath, 'utf8')
+    } catch (err) {
+      console.error('Error reading file:', err)
+      // continue
+    }
+  } else {
+    try {
+      data = fs.readFileSync(reportUIPerformanceFilePath, 'utf8')
+    } catch (err) {
+      console.error('Error reading file:', err)
+      // continue
+    }
   }
 
   const existingLines = data.trim().split('\n')
