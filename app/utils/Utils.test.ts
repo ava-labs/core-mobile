68036dbf16c6b2da01d5fc8c454a3bced411d991
Ade Amos <ade@adeamos.com>
2023-06-30 16:04:36 -0700

    Updated with pr comments

--- app/utils/Utils.test.ts
+++ app/utils/Utils.test.ts
@@ -1,4 +1,5 @@
-import { truncateNodeId } from './Utils'
+import Big from 'big.js'
+import { calculateMaxWeight, truncateNodeId } from './Utils'
 
 describe('/app/utils/Utils', () => {
   describe('truncateNodeId', () => {
@@ -24,6 +25,33 @@ describe('/app/utils/Utils', () => {
     it('handles <1 size', () => {
       expect(truncateNodeId(nodeID, 0)).toBe('NodeID-')
       expect(truncateNodeId(nodeID, -2)).toBe('NodeID-')
+    })
+  })
+  describe('calculateMaxWeight', () => {
+    it('returns the correct maxWeight and maxDelegation', () => {
+      const maxValidatorStake = new Big(3000000e9)
+      const stakeAmount = new Big('1900264376785214')
+
+      const expectedMaxWeight = {
+        maxDelegation: new Big('1099735623214786'),
+        maxWeight: new Big(3000000e9)
+      }
+      expect(calculateMaxWeight(maxValidatorStake, stakeAmount)).toStrictEqual(
+        expectedMaxWeight
+      )
+    })
+
+    it('returns the correct maxWeight when stakeWeight is less than maxValidatorStake', () => {
+      const maxValidatorStake = new Big(2000000e9)
+      const stakeAmount = new Big('4376785214')
+
+      const expectedMaxWeight = {
+        maxDelegation: new Big('17507140856'),
+        maxWeight: new Big('21883926070')
+      }
+      expect(calculateMaxWeight(maxValidatorStake, stakeAmount)).toStrictEqual(
+        expectedMaxWeight
+      )
     })
   })
 })
