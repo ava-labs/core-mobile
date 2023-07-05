051965e9f0284553ad8d787b7093502606b6b670
neven-s <piaf1986@gmail.com>
2023-07-06 00:15:57 +0200

    CP-5756 Implement issue add delegator transaction (#738)

--- app/services/wallet/WalletService.tsx
+++ app/services/wallet/WalletService.tsx
@@ -11,7 +11,11 @@ import {
   getXpubFromMnemonic
 } from '@avalabs/wallets-sdk'
 import { now } from 'moment'
-import { PubKeyType, SignTransactionRequest } from 'services/wallet/types'
+import {
+  AddDelegatorProps,
+  PubKeyType,
+  SignTransactionRequest
+} from 'services/wallet/types'
 import { Wallet } from 'ethers'
 import networkService from 'services/network/NetworkService'
 import { Network, NetworkVMType } from '@avalabs/chains-sdk'
@@ -27,6 +31,7 @@ import { Account } from 'store/account'
 import { RpcMethod } from 'store/walletConnectV2/types'
 import Logger from 'utils/Logger'
 import { UnsignedTx } from '@avalabs/avalanchejs-v2'
+import { add, getUnixTime } from 'date-fns'
 
 class WalletService {
   private mnemonic?: string
@@ -298,6 +303,60 @@ class WalletService {
       baseFee,
       undefined,
       destinationAddress
+    )
+  }
+
+  async createAddDelegatorTx({
+    accountIndex,
+    avaxXPNetwork,
+    nodeId,
+    stakeAmount,
+    startDate,
+    endDate,
+    rewardAddress,
+    isDevMode
+  }: AddDelegatorProps): Promise<UnsignedTx> {
+    if (!nodeId.startsWith('NodeID-')) {
+      throw Error('Invalid node id: ' + nodeId)
+    }
+    const oneAvax = BigInt(1e9)
+    const minStakingAmount = isDevMode ? oneAvax : BigInt(25) * oneAvax
+    if (stakeAmount < minStakingAmount) {
+      throw Error('Staking amount less than minimum')
+    }
+    const unixNow = getUnixTime(new Date())
+    if (unixNow > startDate) {
+      throw Error('Start date must be in future: ' + startDate)
+    }
+    const minimalStakeEndDate = isDevMode
+      ? add(new Date(), { hours: 24 })
+      : add(new Date(), { weeks: 2 })
+    if (endDate < getUnixTime(minimalStakeEndDate)) {
+      throw Error('Staking duration too short')
+    }
+    if (
+      !rewardAddress.startsWith('P-') ||
+      !Avalanche.isBech32Address(rewardAddress, true)
+    ) {
+      throw Error('Reward address must be from P chain')
+    }
+
+    const wallet = (await this.getWallet(
+      accountIndex,
+      avaxXPNetwork
+    )) as Avalanche.StaticSigner
+
+    const utxoSet = await wallet.getUTXOs('P')
+    const config = {
+      rewardAddress
+    }
+    return wallet.addDelegator(
+      utxoSet,
+      nodeId,
+      stakeAmount,
+      BigInt(startDate),
+      BigInt(endDate),
+      config
     )
   }
 
