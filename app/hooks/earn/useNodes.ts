1a093d8d8fed3c1a6a7c3d893fe6a41f4d1e5af6
RuiJia Lin <ruijia.lin@avalabs.org>
2023-07-06 11:30:17 -0400

    update EarnService hook
    - update useNodes
    - update useEarnCalcEstimatedRewards

--- app/hooks/earn/useNodes.ts
+++ app/hooks/earn/useNodes.ts
@@ -1,15 +1,31 @@
 import { useQuery } from '@tanstack/react-query'
+import BN from 'bn.js'
 import { useSelector } from 'react-redux'
 import EarnService from 'services/earn/EarnService'
 import { selectIsDeveloperMode } from 'store/settings/advanced'
+import { getFilteredValidators } from 'utils/getFilteredValidators'
 
 export const useNodes = () => {
   const isDeveloperMode = useSelector(selectIsDeveloperMode)
 
   return useQuery({
-    queryKey: ['nodes', isDeveloperMode],
+    queryKey: [
+      'nodes',
+      isDeveloperMode,
+      stakingDuration,
+      stakingAmount,
+      minUpTime
+    ],
     queryFn: async () => {
-      return EarnService.getCurrentValidators(isDeveloperMode)
+      const result = await EarnService.getCurrentValidators(isDeveloperMode)
+      const filteredValidators = getFilteredValidators(
+        result.validators,
+        stakingAmount,
+        isDeveloperMode,
+        stakingDuration,
+        minUpTime
+      )
+      return filteredValidators
     }
   })
 }
