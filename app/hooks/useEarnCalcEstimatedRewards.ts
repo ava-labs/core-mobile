cd6ac73ebd95711b7e2940c41a1f3b800aa36ba1
RuiJia Lin <ruijia.lin@avalabs.org>
2023-07-06 11:30:17 -0400

    update EarnService hook
    - update useNodes
    - update useEarnCalcEstimatedRewards

--- app/hooks/useEarnCalcEstimatedRewards.ts
+++ app/hooks/useEarnCalcEstimatedRewards.ts
@@ -3,20 +3,53 @@ import { useSelector } from 'react-redux'
 import { selectIsDeveloperMode } from 'store/settings/advanced'
 import Big from 'big.js'
 import EarnService from 'services/earn/EarnService'
+import { selectAvaxPrice } from 'store/balance'
+import { useQuery } from '@tanstack/react-query'
 
-export const useEarnCalcEstimatedRewards = (
-  amount: Big,
-  duration: number,
-  currentSupply: Big
-): BN => {
+export type StakingRewards = {
+  estimatedTokenReward: BN
+  estimatedRewardAmount: number
+}
+
+export type useEarnCalcEstimatedRewardsProps = {
+  amount: Big
+  duration: number
+  delegationFee: number
+}
+
+export const useEarnCalcEstimatedRewards = ({
+  amount,
+  duration,
+  delegationFee
+}: useEarnCalcEstimatedRewardsProps) => {
   const isDeveloperMode = useSelector(selectIsDeveloperMode)
+  const avaxPrice = useSelector(selectAvaxPrice)
 
-  const rewardStr = EarnService.calcReward(
-    amount,
-    duration,
-    currentSupply,
-    2,
-    isDeveloperMode
-  )
-  return new BN(rewardStr)
+  return useQuery({
+    queryKey: [
+      'nodes',
+      amount,
+      duration,
+      delegationFee,
+      isDeveloperMode,
+      avaxPrice
+    ],
+    queryFn: async () => {
+      const { supply } = await EarnService.getCurrentSupply(isDeveloperMode)
+      const currentSupply = new Big(supply.toString())
+
+      const rewardStr = EarnService.calcReward(
+        amount,
+        duration,
+        currentSupply,
+        delegationFee,
+        isDeveloperMode
+      )
+
+      return {
+        estimatedTokenReward: rewardStr,
+        estimatedRewardAmount: Number(rewardStr) * avaxPrice
+      }
+    }
+  })
 }
