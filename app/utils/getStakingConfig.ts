103f35a49980f60ce285eb3b79bc60d5f08060cb
RuiJia Lin <ruijia.lin@avalabs.org>
2023-07-07 16:36:42 -0400

    address pr comments
    - remove console.log
    - update function name
    - move utils into earn/utils

--- app/utils/getStakingConfig.ts
+++
@@ -1,7 +0,0 @@
-import { FujiParams, MainnetParams } from './NetworkParams'
-
-export const getStakingConfig = (isDeveloperMode: boolean) => {
-  return isDeveloperMode
-    ? FujiParams.stakingConfig
-    : MainnetParams.stakingConfig
-}
