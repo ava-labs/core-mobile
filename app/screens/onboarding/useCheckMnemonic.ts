a80b90b74544eb434abd79ae557ce648a34c3951
Tyler Hackett <thackett86181@gmail.com>
2023-07-11 14:56:10 -0700

    more comments

--- app/screens/onboarding/useCheckMnemonic.ts
+++ app/screens/onboarding/useCheckMnemonic.ts
@@ -93,7 +93,7 @@ function selectXRandNumbers(numOfNumbers: number, pool: number[]) {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     ;[pool[i], pool[randomIndex]] = [pool[randomIndex]!, pool[i]!] //put it on front of pool
   }
-  return pool.splice(0, numOfNumbers) //first [numOfNumbers] nums are now non-repeating random from pool and won't be duplicated in UI
+  return pool.splice(0, numOfNumbers) //first [numOfNumbers] nums are now non-repeating random from pool and won't be duplicated in UI causing test failures
 }
 
 function selectXRandWordsIncluding(
