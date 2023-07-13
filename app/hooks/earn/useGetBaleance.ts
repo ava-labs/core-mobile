150035acb32d092c8066d1f58b8934061cb2b063
Ade Amos <ade@adeamos.com>
2023-07-13 08:54:10 -0700

    Code clean-up and re-orginization

--- app/hooks/earn/useGetBaleance.ts
+++ app/hooks/earn/useGetBaleance.ts
@@ -2,14 +2,15 @@ import { useQuery } from '@tanstack/react-query'
 import { useSelector } from 'react-redux'
 import EarnService from 'services/earn/EarnService'
 import { selectIsDeveloperMode } from 'store/settings/advanced'
+import { Network } from '@avalabs/chains-sdk'
 
-export const useNodes = () => {
+export const useGetBalance = (network: Network, addresses: string[]) => {
   const isDeveloperMode = useSelector(selectIsDeveloperMode)
 
   return useQuery({
-    queryKey: ['nodes', isDeveloperMode],
+    queryKey: ['stakingBalances', isDeveloperMode, network, addresses],
     queryFn: async () => {
-      return EarnService.getCurrentValidators(isDeveloperMode)
+      return EarnService.getPChainBalance(network, addresses)
     }
   })
 }
