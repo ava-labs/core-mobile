322d28300bd9197aa1d7c033e03e1b0681601a62
Neven <piaf1986@gmail.com>
2023-07-18 17:35:33 +0200

    Make type converters

--- app/hooks/earn/useIssueDelegation.ts
+++ app/hooks/earn/useIssueDelegation.ts
@@ -2,26 +2,31 @@ import { useMutation } from '@tanstack/react-query'
 import { useSelector } from 'react-redux'
 import EarnService from 'services/earn/EarnService'
 import { selectIsDeveloperMode } from 'store/settings/advanced'
-import { BigIntNAvax } from 'types/denominations'
-import { bnToBigint } from 'utils/bigNumbers/bnToBigint'
+import { BigIntNAvax, BigIntWeiAvax } from 'types/denominations'
 import { BN } from 'bn.js'
 import { selectNativeTokenBalanceForNetworkAndAccount } from 'store/balance'
 import { selectActiveAccount } from 'store/account'
 import { selectActiveNetwork } from 'store/network'
+import BigIntConverter from 'types/converters/BigIntConverter'
+import TypeConverter from 'types/converters/TypeConverter'
 
 export const useIssueDelegation = (onSuccess: (txId: string) => void) => {
   const activeNetwork = useSelector(selectActiveNetwork)
   const activeAccount = useSelector(selectActiveAccount)
   const isDeveloperMode = useSelector(selectIsDeveloperMode)
 
-  const cChainBalance = useSelector(
+  const cChainBalanceWei = useSelector(
     selectNativeTokenBalanceForNetworkAndAccount(
       activeNetwork.chainId,
       activeAccount?.index
     )
   )
-  const cChainBalanceNAvax: BigIntNAvax =
-    bnToBigint(cChainBalance || new BN(0)) / BigInt(1e9) //TODO: make function for converting between denominations
+  const cChainBalanceBigIntWei = TypeConverter.bnToBigInt(
+    cChainBalanceWei || new BN(0)
+  ) as BigIntWeiAvax
+  const cChainBalanceNAvax: BigIntNAvax = BigIntConverter.weiToNAvax(
+    cChainBalanceBigIntWei
+  )
 
   const issueDelegationMutation = useMutation({
     mutationFn: (data: {
