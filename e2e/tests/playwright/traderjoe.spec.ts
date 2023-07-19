7bfd7d15e1b41e18d14452fd3dbe096e13a6bc80
Tyler Hackett <thackett86181@gmail.com>
2023-07-19 11:00:46 -0700

    Qa tyler add uniswap dapp test (#764)

--- e2e/tests/playwright/traderjoe.spec.ts
+++ e2e/tests/playwright/traderjoe.spec.ts
@@ -2,38 +2,21 @@ import { test, expect } from '@playwright/test'
 import actions from '../../helpers/actions'
 import TraderJoePage from '../../pages/traderjoe.page'
 
-const fs = require('fs')
-
 test('check wallet connect button', async ({ page }) => {
   const traderJoePage = new TraderJoePage(page)
-  const context = page.context()
 
   await actions.openPage(traderJoePage.page, traderJoePage.homePage)
   await expect(traderJoePage.connectWalletButtonText).toBeVisible()
-  await page.waitForTimeout(1000)
   await traderJoePage.clickConnectWalletButton()
-  await page.waitForTimeout(1000)
+  await expect(traderJoePage.walletConnectButton).toBeVisible()
   await traderJoePage.clickWalletConnectButton()
-  await page.waitForTimeout(1000)
+  await expect(traderJoePage.oldWalletConnectModal).toBeVisible()
   await traderJoePage.clickOldWalletConnectModal()
-  await page.waitForTimeout(1000)
-  await traderJoePage.clickWalletConnectCode()
-  await page.waitForTimeout(1500)
+  const qrUri = await traderJoePage.qrUriValue()
 
-  await context.grantPermissions(['clipboard-read'])
+  if (qrUri) {
+    await actions.writeQrCodeToFile(qrUri)
+  }
 
-  const clipboardValue = await page.evaluate(() => {
-    return navigator.clipboard.readText()
-  })
-
-  fs.writeFile(
-    '../plusIcon/walletConnect/qr_codes.txt',
-    clipboardValue,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (err: any) => {
-      if (err) throw err
-    }
-  )
-
-  console.log('Clipboard value:', clipboardValue)
+  console.log('Clipboard value:', qrUri)
 })
