03482efdd6c18e8da3d1e57c2beff0a4507d487b
An Nguyen <an.nguyen@avalabs.org>
2023-07-24 11:36:43 -0500

    CP-6021: Decouple C Balance from Portfolio Balance (#774)

--- app/hooks/earn/useStakingParams.tsx
+++ app/hooks/earn/useStakingParams.tsx
@@ -1,28 +1,16 @@
-import { selectNativeTokenBalanceForNetworkAndAccount } from 'store/balance'
 import { useSelector } from 'react-redux'
-import { selectActiveAccount } from 'store/account'
 import { selectIsDeveloperMode } from 'store/settings/advanced'
-import { ChainId } from '@avalabs/chains-sdk'
 import { BigIntWeiAvax } from 'types/denominations'
 
 export interface StakeParamsHook {
   minStakeAmount: BigIntWeiAvax
-  nativeTokenBalance: BigIntWeiAvax | undefined
 }
 
 export default function useStakingParams(): StakeParamsHook {
-  const activeAccount = useSelector(selectActiveAccount)
   const isDeveloperMode = useSelector(selectIsDeveloperMode)
-  const chainId = isDeveloperMode
-    ? ChainId.AVALANCHE_TESTNET_ID
-    : ChainId.AVALANCHE_MAINNET_ID
   const minStakeAmount = isDeveloperMode ? BigInt(1e18) : BigInt(25e18)
-  const nativeTokenBalance = useSelector(
-    selectNativeTokenBalanceForNetworkAndAccount(chainId, activeAccount?.index)
-  )
 
   return {
-    minStakeAmount,
-    nativeTokenBalance
+    minStakeAmount
   } as StakeParamsHook
 }
