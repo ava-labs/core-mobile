254b84b9566870760d25c7c8704c8b3e86070a0e
Tyler Hackett <thackett86181@gmail.com>
2023-07-24 16:57:59 -0700

    Qa tyler add oasis dapp test (#783)

--- e2e/tests/playwright/wallet_connect_runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd ~/avalanche-wallet-apps/e2e/tests/playwright/ && npx playwright test traderjoe.spec.ts && sleep 5
-
-cd ~/avalanche-wallet-apps && node_modules/.bin/detox test traderjoe --configuration ios.internal.debug
--- e2e/tests/plusIcon/walletConnect/android_dapp_test_runner.sh
+++ e2e/tests/plusIcon/walletConnect/android_dapp_test_runner.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-cmds=('npx playwright test e2e/tests/playwright/traderjoe.spec.ts' 'npx playwright test e2e/tests/playwright/uniswap.spec.ts')
+cmds=(
+'npx playwright test e2e/tests/playwright/traderjoe.spec.ts' 
+'npx playwright test e2e/tests/playwright/uniswap.spec.ts' 
+'npx playwright test e2e/tests/playwright/oasis.spec.ts'
+)
 for cmd in "${cmds[@]}"; do
     echo "Running ${cmd}..."
     $cmd
     echo "${cmd} finished"
-    node_modules/.bin/detox test dappIntegrations.e2e.ts --configuration android.internal.debug
+    node_modules/.bin/detox test dappIntegrations.e2e.ts --configuration android.internal.debug 
     echo "Android Test Finished!!!"
 done
