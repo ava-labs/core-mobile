0caf5adbc327911c38d795d7bc5665eda4892957
RuiJia Lin <ruijia.lin@avalabs.org>
2023-07-26 13:16:42 -0400

    add wrongNetwork screen

--- app/screens/earn/StakeDashboard.tsx
+++ app/screens/earn/StakeDashboard.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import AvaText from 'components/AvaText'
+import { useSelector } from 'react-redux'
+import { selectActiveNetwork } from 'store/network'
+import { selectIsDeveloperMode } from 'store/settings/advanced'
+import { ChainId } from '@avalabs/chains-sdk'
 import { Balance } from './components/Balance'
 import { StakeTabs } from './StakeTabs'
+import { WrongNetwork } from './WrongNetwork'
 
 export const StakeDashboard = () => {
+  const network = useSelector(selectActiveNetwork)
+  const isDeveloperMode = useSelector(selectIsDeveloperMode)
+  const avalancheChainId = isDeveloperMode
+    ? ChainId.AVALANCHE_TESTNET_ID
+    : ChainId.AVALANCHE_MAINNET_ID
+
+  if (network.chainId !== avalancheChainId) {
+    return <WrongNetwork />
+  }
+
   return (
     <View style={styles.container}>
       <AvaText.LargeTitleBold>Stake</AvaText.LargeTitleBold>
---
+++ app/screens/earn/WrongNetwork.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react'
+import { View } from 'react-native'
+import AvaText from 'components/AvaText'
+import SwitchSVG from 'components/svg/SwitchSVG'
+import { Space } from 'components/Space'
+import AvaButton from 'components/AvaButton'
+import { selectActiveNetwork, setActive } from 'store/network'
+import { selectIsDeveloperMode } from 'store/settings/advanced'
+import { useDispatch, useSelector } from 'react-redux'
+import { ChainId } from '@avalabs/chains-sdk'
+import { EarnScreenProps } from 'navigation/types'
+import AppNavigation from 'navigation/AppNavigation'
+import { useNavigation } from '@react-navigation/native'
+
+type ScreenProps = EarnScreenProps<typeof AppNavigation.Earn.StakeDashboard>
+
+export const WrongNetwork = () => {
+  const { navigate } = useNavigation<ScreenProps['navigation']>()
+
+  const dispatch = useDispatch()
+  const network = useSelector(selectActiveNetwork)
+  const isDeveloperMode = useSelector(selectIsDeveloperMode)
+  const avalancheChainId = isDeveloperMode
+    ? ChainId.AVALANCHE_TESTNET_ID
+    : ChainId.AVALANCHE_MAINNET_ID
+
+  const onSwitchNetwork = () => {
+    dispatch(setActive(avalancheChainId))
+  }
+
+  useEffect(() => {
+    if (network.chainId === avalancheChainId) {
+      navigate(AppNavigation.Earn.StakeDashboard)
+    }
+  }, [avalancheChainId, navigate, network])
+
+  return (
+    <View style={{ padding: 16, flex: 1 }}>
+      <AvaText.LargeTitleBold>Stake</AvaText.LargeTitleBold>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <SwitchSVG />
+        <Space y={24} />
+        <AvaText.Heading5>Switch Network to Stake</AvaText.Heading5>
+        <Space y={8} />
+        <AvaText.Body2
+          textStyle={{
+            textAlign: 'center',
+            lineHeight: 20
+          }}>
+          Staking is only available on the Avalanche Network. Please switch
+          networks to continue.
+        </AvaText.Body2>
+        <Space y={24} />
+        <AvaButton.PrimaryLarge
+          onPress={onSwitchNetwork}
+          style={{ width: '100%' }}>
+          Switch to Avalanche Network
+        </AvaButton.PrimaryLarge>
+      </View>
+    </View>
+  )
+}
