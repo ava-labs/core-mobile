30c68d69c5f82abc73eec0806d7a3cb6cd5ef980
Neven <piaf1986@gmail.com>
2023-07-28 18:54:49 +0200

    Extract calculateAmountForCrossChainTransfer function

--- app/hooks/earn/useGetAmountForCrossChainTransfer.ts
+++ app/hooks/earn/useGetAmountForCrossChainTransfer.ts
@@ -11,12 +11,18 @@ export const useGetAmountForCrossChainTransfer = (
     if (claimableBalance === undefined) {
       return undefined
     }
-    if (claimableBalance.gt(stakingAmount)) {
-      return Avax.fromBase(0)
-    } else {
-      return stakingAmount.sub(claimableBalance)
-    }
+    return calculateAmountForCrossChainTransfer(stakingAmount, claimableBalance)
   }, [claimableBalance, stakingAmount])
-
   return amountForCrossChainTransfer
+}
+
+export const calculateAmountForCrossChainTransfer = (
+  stakingAmount: Avax,
+  claimableBalance: Avax
+): Avax => {
+  if (claimableBalance.gt(stakingAmount)) {
+    return Avax.fromBase(0)
+  } else {
+    return stakingAmount.sub(claimableBalance)
+  }
 }
