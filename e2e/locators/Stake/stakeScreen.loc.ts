a38c20e7f3fb47c5eb0ea4072597dd8272193e11
ARTEM BESPALOV <40455398+artyombespalov@users.noreply.github.com>
2023-08-10 15:08:02 -0700

    Wrong Network for Staking Automation test Added

--- e2e/locators/Stake/stakeScreen.loc.ts
+++ e2e/locators/Stake/stakeScreen.loc.ts
@@ -1,8 +1,19 @@
 export default {
+  availableAvaxText: 'Available',
+  avaLogo: 'ava_logo',
+  stakedAvaxText: 'Staked',
+  claimableAvaxText: 'Claimable',
   stakeTitle: 'Stake',
   notEnoughAvaxTitle: 'You don’t have enough AVAX!',
   notEnoughAvaxDescription: 'Buy or Swap AVAX to begin staking.',
   swapAvaxButton: 'Swap AVAX',
   recieveAvaxButton: 'Receive AVAX',
-  buyAvaxButton: 'Buy AVAX'
+  buyAvaxButton: 'Buy AVAX',
+  switchNetworkTitle: 'Switch Network to Stake',
+  switchNetworkDescription:
+    'Staking is only available on the Avalanche Network. Please switch networks to continue.',
+  switchNetworkButton: 'btnPrimary',
+  switchNetworkButtonText: 'Switch to Avalanche Network',
+  stakeButton: 'btnPrimary',
+  stakeButtonText: 'Stake'
 }
--- e2e/pages/Stake/stake.page.ts
+++ e2e/pages/Stake/stake.page.ts
@@ -1,10 +1,35 @@
 // import Actions from '../../helpers/actions'
 import stakeScreenLoc from '../../locators/Stake/stakeScreen.loc'
 import Assert from '../../helpers/assertions'
+import Actions from '../../helpers/actions'
 
 class StakePage {
+  get avaLogo() {
+    return by.id(stakeScreenLoc.avaLogo)
+  }
+
+  get availableAvaxText() {
+    return by.text(stakeScreenLoc.availableAvaxText)
+  }
+
+  get stakedAvaxText() {
+    return by.text(stakeScreenLoc.stakedAvaxText)
+  }
+
+  get claimableAvaxText() {
+    return by.text(stakeScreenLoc.claimableAvaxText)
+  }
+
   get stakeTitle() {
     return by.text(stakeScreenLoc.stakeTitle)
+  }
+
+  get stakeButton() {
+    return by.id(stakeScreenLoc.stakeButton)
+  }
+
+  get stakeButtonText() {
+    return by.text(stakeScreenLoc.stakeButtonText)
   }
 
   get notEnoughAvaxTitle() {
@@ -27,6 +52,22 @@ class StakePage {
     return by.text(stakeScreenLoc.buyAvaxButton)
   }
 
+  get switchNetworkTitle() {
+    return by.text(stakeScreenLoc.switchNetworkTitle)
+  }
+
+  get switchNetworkDescription() {
+    return by.text(stakeScreenLoc.switchNetworkDescription)
+  }
+
+  get switchNetworkButton() {
+    return by.id(stakeScreenLoc.switchNetworkButton)
+  }
+
+  get switchNetworkButtonText() {
+    return by.text(stakeScreenLoc.switchNetworkButtonText)
+  }
+
   async verifyStakeScreenItems() {
     await Assert.isVisible(this.stakeTitle)
     await Assert.isVisible(this.notEnoughAvaxTitle)
@@ -34,6 +75,26 @@ class StakePage {
     await Assert.isVisible(this.swapAvaxButton)
     await Assert.isVisible(this.recieveAvaxButton)
     await Assert.isVisible(this.buyAvaxButton)
+  }
+
+  async verifySwitchNetworkScreenItems() {
+    await Assert.isVisible(this.switchNetworkTitle)
+    await Assert.isVisible(this.switchNetworkDescription)
+    await Assert.isVisible(this.switchNetworkButton)
+    await Assert.isVisible(this.switchNetworkButtonText)
+  }
+
+  async tapSwitchNetworkButton() {
+    await Actions.tap(this.switchNetworkButton)
+  }
+
+  async verifyStakeTopItems() {
+    await Assert.isVisible(this.availableAvaxText)
+    await Assert.isVisible(this.stakedAvaxText)
+    await Assert.isVisible(this.claimableAvaxText)
+    await Assert.isVisible(this.stakeButton)
+    await Assert.isVisible(this.stakeButtonText, 1)
+    await Assert.isVisible(this.avaLogo)
   }
 }
 
