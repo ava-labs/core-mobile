1207472d0ae0b97133a3d777b94cc4cb096816a3
Neven <piaf1986@gmail.com>
2023-08-14 09:57:43 +0200

    Import stuck funds in Balance component and show InaccurateBalancePoppable

---
+++ app/screens/earn/components/BalanceItem.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import AvaText from 'components/AvaText'
+import { useApplicationContext } from 'contexts/ApplicationContext'
+import { Row } from 'components/Row'
+
+export type BalanceItemProps = {
+  balanceType: string
+  iconColor: string
+  balance: string
+  poppableItem?: Element
+}
+
+export const BalanceItem = ({
+  balanceType,
+  iconColor,
+  balance,
+  poppableItem
+}: BalanceItemProps) => {
+  const { theme } = useApplicationContext()
+
+  return (
+    <Row key={balanceType} style={{ alignItems: 'center' }}>
+      <View style={[styles.dot, { backgroundColor: iconColor }]} />
+      <AvaText.Subtitle2
+        textStyle={{
+          color: theme.neutral50,
+          lineHeight: 24.5,
+          marginHorizontal: 8
+        }}>
+        {`${balance} AVAX`}
+      </AvaText.Subtitle2>
+      <AvaText.Caption
+        textStyle={{
+          alignSelf: 'flex-end',
+          color: theme.neutral400,
+          lineHeight: 19.92
+        }}>
+        {balanceType}
+      </AvaText.Caption>
+      {poppableItem && poppableItem}
+    </Row>
+  )
+}
+
+const styles = StyleSheet.create({
+  dot: {
+    width: 16,
+    height: 16,
+    borderRadius: 8
+  }
+})
