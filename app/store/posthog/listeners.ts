01c81f3f59566c6ded285325e5e0a71992232f36
An Nguyen <an.nguyen@avalabs.org>
2023-08-18 15:38:37 -0500

    CP-4272: Refactor feature flags to redux (#857)

--- app/store/posthog/listeners.ts
+++ app/store/posthog/listeners.ts
@@ -1,15 +1,20 @@
 import { AppStartListening } from 'store/middleware/listener'
-import { onLogOut } from 'store/app'
+import { onLogOut, onRehydrationComplete } from 'store/app'
 import {
   capture,
   regenerateUserId,
+  setFeatureFlags,
   selectUserID,
   selectDistinctID,
   selectIsAnalyticsEnabled
 } from 'store/posthog/slice'
 import Logger from 'utils/Logger'
 import PostHogService from 'services/posthog/PostHogService'
+import { AppListenerEffectAPI } from 'store'
+import { Action } from '@reduxjs/toolkit'
 import { JsonMap } from './types'
+
+const FEATURE_FLAGS_FETCH_INTERVAL = 60000 // 1 minute
 
 export const posthogCapture = ({
   distinctId,
@@ -24,6 +29,25 @@ export const posthogCapture = ({
 }) => {
   Logger.info(`posthog capture: ${event}`, properties)
   return PostHogService.capture(event, distinctId, posthogUserId, properties)
+}
+
+const fetchFeatureFlagsPeriodically = async (
+  _: Action,
+  listenerApi: AppListenerEffectAPI
+) => {
+  const { dispatch } = listenerApi
+
+  async function fetchFeatureFlags() {
+    const featureFlags = await PostHogService.fetchFeatureFlags()
+    featureFlags && dispatch(setFeatureFlags(featureFlags))
+  }
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    await listenerApi.pause(fetchFeatureFlags())
+
+    await listenerApi.delay(FEATURE_FLAGS_FETCH_INTERVAL)
+  }
 }
 
 export const addPosthogListeners = (startListening: AppStartListening) => {
@@ -47,5 +71,10 @@ export const addPosthogListeners = (startListening: AppStartListening) => {
         posthogCapture({ distinctId, posthogUserId, event, properties })
       }
     }
+  })
+
+  startListening({
+    actionCreator: onRehydrationComplete,
+    effect: fetchFeatureFlagsPeriodically
   })
 }
