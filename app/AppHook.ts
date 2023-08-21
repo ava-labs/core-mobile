76424ce22499a033087d80b8c7f51815862b56e9
An Nguyen <an.nguyen@avalabs.org>
2023-08-21 13:30:41 -0500

    remove decideInitialRoute since it is not necessary

--- app/AppHook.ts
+++ app/AppHook.ts
@@ -1,23 +1,17 @@
 import { asyncScheduler, AsyncSubject, concat, Observable, of } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { BackHandler } from 'react-native'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo } from 'react'
 import { WalletSetupHook } from 'hooks/useWalletSetup'
 import { AppNavHook } from 'useAppNav'
 import { Repo } from 'Repo'
-import { SECURE_ACCESS_SET } from 'resources/Constants'
-import AppNavigation from 'navigation/AppNavigation'
 import { usePosthogContext } from 'contexts/PosthogContext'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectSelectedCurrency } from 'store/settings/currency'
-import { onLogOut, setWalletState, WalletState } from 'store/app'
+import { onLogOut } from 'store/app'
 import { resetLoginAttempt } from 'store/security'
 import { formatCurrency } from 'utils/FormatCurrency'
-import {
-  selectCoreAnalyticsConsent,
-  selectTouAndPpConsent
-} from 'store/settings/securityPrivacy'
+import { selectCoreAnalyticsConsent } from 'store/settings/securityPrivacy'
 
 export type AppHook = {
   onExit: () => Observable<ExitEvents>
@@ -35,11 +29,8 @@ export function useApp(
 ): AppHook {
   const dispatch = useDispatch()
   const selectedCurrency = useSelector(selectSelectedCurrency)
-  const [navigationContainerSet, setNavigationContainerSet] = useState(false)
-  const [initRouteSet, setInitRouteSet] = useState(false)
   const { setAnalyticsConsent } = usePosthogContext()
   const coreAnalyticsConsentSetting = useSelector(selectCoreAnalyticsConsent)
-  const touAndPpConsentSetting = useSelector(selectTouAndPpConsent)
 
   const deleteWallet = useCallback(() => {
     walletSetupHook.destroyWallet()
@@ -53,56 +44,13 @@ export function useApp(
     appNavHook.resetNavToRoot()
   }, [appNavHook, deleteWallet])
 
-  useEffect(waitForNavigationContainer, [appNavHook.navigation])
   useEffect(watchCoreAnalyticsFlagFx, [
     coreAnalyticsConsentSetting,
     setAnalyticsConsent
   ])
-  useEffect(decideInitialRoute, [
-    appNavHook,
-    dispatch,
-    initRouteSet,
-    navigationContainerSet,
-    repository,
-    signOut,
-    touAndPpConsentSetting
-  ])
 
   function watchCoreAnalyticsFlagFx() {
     setAnalyticsConsent(coreAnalyticsConsentSetting)
-  }
-
-  function waitForNavigationContainer() {
-    async function onFirstLoad() {
-      if (!appNavHook.navigation.current) {
-        setTimeout(() => onFirstLoad(), 1000)
-        return
-      }
-      setNavigationContainerSet(true)
-    }
-
-    onFirstLoad().then()
-  }
-
-  function decideInitialRoute() {
-    if (!navigationContainerSet || initRouteSet || !repository.initialized) {
-      return
-    }
-    setInitRouteSet(true)
-    AsyncStorage.getItem(SECURE_ACCESS_SET).then(result => {
-      if (result) {
-        if (!touAndPpConsentSetting) {
-          //User has probably killed app before consent to TOU, so we'll clear all data and
-          //return him to onboarding
-          signOut()
-        } else {
-          dispatch(setWalletState(WalletState.INACTIVE))
-          appNavHook.navigation.current?.navigate(AppNavigation.Root.NoWallet)
-        }
-      } else {
-        appNavHook.navigation.current?.navigate(AppNavigation.Root.NoWallet)
-      }
-    })
   }
 
   function onExit(): Observable<ExitEvents> {
