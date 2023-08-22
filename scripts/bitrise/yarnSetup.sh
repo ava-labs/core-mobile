0acaf4703a55d07fa9d8d270ad767880052dda96
An Nguyen <an.nguyen@avalabs.org>
2023-08-22 16:20:11 -0500

    CP-7053: Fix Android Intermittent Build Failure on Bitrise (#873)

---
+++ scripts/bitrise/yarnSetup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
+
+set -o pipefail
+# debug log
+set -x
+
+if ! cat /etc/issue 2>/dev/null
+then
+yarn setup:ci
+else 
+  stack=$( cat /etc/issue )
+fi
+
+if [[ $stack == *Ubuntu* ]]; then
+    # on ubuntu, yarn setup command will fail
+    # as patch-package doesn't have write access to the node_modules folder
+    # thus, we need to set write permission manually and rerun yarn
+    # Bitrise issue link https://support.bitrise.io/hc/en-us/requests/39436?page=1
+    sudo chown root .
+
+    yarn setup:ci
+fi
