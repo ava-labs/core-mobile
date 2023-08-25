e6d24648ecc3754ec68470bc1fbfaeaacc46ddd6
ARTEM BESPALOV <40455398+artyombespalov@users.noreply.github.com>
2023-08-25 09:41:58 -0700

    QA-63: New Automation verify staking amount screen items, verify duration screen items on testnet, verify staking on testnet (#883)

---
+++ e2e/locators/Stake/durationScreen.loc.ts
@@ -0,0 +1,18 @@
+export default {
+  advancedSetup: 'Advanced Set Up',
+  disclaimerText: 'Estimates are provided for informational purposes only...￼￼',
+  disclaimerTooltip: 'info_svg',
+  tooltipText:
+    'Estimates are provided for informational purposes only, without any representation, warranty or guarantee, and do not represent any assurance that you will achieve the same results.',
+  tooltipTextLink: 'Learn more',
+  durationTitle: 'Duration',
+  durationDescription: 'How long would you like to stake?',
+  nextButton: 'btnPrimary',
+  oneDayText: '1 Day',
+  oneMonthText: '1 Month',
+  threeMonthsText: '3 Months',
+  sixMonthsText: '6 Months',
+  oneYearText: '1 Year',
+  customText: 'Custom',
+  customDescription: 'Enter your desired end date'
+}
