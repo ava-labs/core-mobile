636891c61ae3cf7db2e30e0978eb38cdc6a31ca9
ruijialin <ruijia.lin@avalabs.org>
2023-08-28 16:37:17 -0400

    add detail skeleton loader

--- app/screens/portfolio/home/components/Loaders/PortfolioDeFiDetailsLoader.tsx
+++ app/screens/portfolio/home/components/Loaders/PortfolioDeFiDetailsLoader.tsx
@@ -6,7 +6,7 @@ import { useSkeletonColors } from 'hooks/colors/useSkeletonColors'
 const deviceWidth = Dimensions.get('screen').width
 const aspectRatio = 359 / 518
 
-export const PortfolioDeFiLoader = () => {
+export const PortfolioDeFiDetailsLoader = () => {
   const { backgroundColor, foregroundColor } = useSkeletonColors()
 
   return (
@@ -25,13 +25,11 @@ export const PortfolioDeFiLoader = () => {
         viewBox="0 0 359 518"
         backgroundColor={backgroundColor}
         foregroundColor={foregroundColor}>
-        <Rect x="0" y="8" rx="8" ry="8" width="343" height="72" />
-        <Rect x="0" y="88" rx="8" ry="8" width="343" height="72" />
-        <Rect x="0" y="168" rx="8" ry="8" width="343" height="72" />
-        <Rect x="0" y="248" rx="8" ry="8" width="343" height="72" />
-        <Rect x="0" y="328" rx="8" ry="8" width="343" height="72" />
-        <Rect x="0" y="408" rx="8" ry="8" width="343" height="72" />
-        <Rect x="0" y="488" rx="8" ry="8" width="343" height="72" />
+        <Rect x="0" y="8" rx="8" ry="8" width="343" height="357" />
+        <Rect x="16" y="24" rx="8" ry="8" width="311" height="56" />
+        <Rect x="16" y="96" rx="8" ry="8" width="311" height="1" />
+        <Rect x="16" y="113" rx="8" ry="8" width="106" height="24" />
+        <Rect x="16" y="153" rx="8" ry="8" width="311" height="176" />
       </ContentLoader>
     </View>
   )
