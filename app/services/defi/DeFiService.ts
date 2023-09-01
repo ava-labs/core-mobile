a327399ca7735f4cd0074549f50a2c0bdb3be257
ruijialin <ruijia.lin@avalabs.org>
2023-09-01 08:32:09 -0400

    address pr comments

--- app/services/defi/DeFiService.ts
+++ app/services/defi/DeFiService.ts
@@ -9,22 +9,18 @@ import { CHAIN_LIST, PROTOCOL, SIMPLE_PROTOCOL_LIST } from './constants'
 
 if (!Config.PROXY_URL) throw Error('PROXY_URL is missing')
 
+const baseUrl = Config.PROXY_URL + '/proxy/debank/v1'
+const chainListUrl = `${baseUrl}/${CHAIN_LIST}`
+const userProtocolUrl = `${baseUrl}/${PROTOCOL}`
+const userProtocolListUrl = `${baseUrl}/${SIMPLE_PROTOCOL_LIST}`
+const headers = {
+  'Content-Type': 'application/json'
+}
+
 class DeFiService {
-  private baseUrl: string
-  private headers: Record<string, string>
-
-  constructor() {
-    this.baseUrl = Config.PROXY_URL + '/proxy/debank/v1'
-    this.headers = {
-      'Content-Type': 'application/json'
-    }
-  }
-
   async getSupportedChainList(): Promise<DeFiChainObject[]> {
-    const chainListUrl = `${this.baseUrl}/${CHAIN_LIST}`
-
     const supportedChainList = await fetch(chainListUrl, {
-      headers: this.headers
+      headers
     })
     const jsonRes = (await supportedChainList.json()) as DeFiChainObject[]
     return jsonRes
@@ -34,15 +30,13 @@ class DeFiService {
     userAddress: string,
     protocolId: string
   ): Promise<DeFiProtocolObject> {
-    const userProtocolUrl = `${this.baseUrl}/${PROTOCOL}`
     const params = new URLSearchParams({
       id: userAddress,
       protocol_id: protocolId
     })
-
     const urlWithQueryParam = `${userProtocolUrl}?${params}`
     const userProtocolList = await fetch(urlWithQueryParam, {
-      headers: this.headers
+      headers
     })
     const jsonRes = (await userProtocolList.json()) as DeFiProtocolObject
     return jsonRes
@@ -51,14 +45,12 @@ class DeFiService {
   async getDeFiProtocolList(
     userAddress: string
   ): Promise<DeFiSimpleProtocolObject[]> {
-    const userProtocolListUrl = `${this.baseUrl}/${SIMPLE_PROTOCOL_LIST}`
     const params = new URLSearchParams({
       id: userAddress
     })
-
     const urlWithQueryParam = `${userProtocolListUrl}?${params}`
     const userProtocolList = await fetch(urlWithQueryParam, {
-      headers: this.headers
+      headers
     })
     const jsonRes =
       (await userProtocolList.json()) as DeFiSimpleProtocolObject[]
