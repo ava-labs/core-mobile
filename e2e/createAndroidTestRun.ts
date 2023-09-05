3aa2e84e15c97115db28126c6ff51ceabd6072a5
Tyler Hackett <thackett86181@gmail.com>
2023-09-05 14:16:42 -0700

    change timestamp

--- e2e/createAndroidTestRun.ts
+++ e2e/createAndroidTestRun.ts
@@ -1,18 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createEmptyTestRun } from './generateTestrailObjects'
-import { testRunTimestamp } from './getResultsFromLogs'
 const fs = require('fs')
 
 export async function createAndroidTestRun() {
-  const timestamp = await testRunTimestamp('Android')
+  const timestamp = generateUtcTimestamp()
   const runID = createEmptyTestRun('Android', timestamp)
   return runID
 }
 
 export async function writeRunIdToTextFile(runId: string) {
-  fs.writeFile('e2e/testrailRunID.txt', runId, (err: any) => {
+  fs.writeFile('./e2e/testrailRunID.txt', runId, (err: any) => {
     if (err) throw err
   })
+}
+
+export function generateUtcTimestamp() {
+  const date = new Date()
+  const utcDate = new Date(
+    date.getTime() + date.getTimezoneOffset() * 60000
+  ).toISOString()
+  return utcDate
 }
 
 writeRunIdToTextFile(`${createAndroidTestRun()}`)
