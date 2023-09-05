ec7322e4f760b455a193298c74bc0b7c76d311d8
Tyler Hackett <thackett86181@gmail.com>
2023-09-05 14:48:34 -0700

    update node command

--- e2e/createTestRunID.sh
+++ e2e/createTestRunID.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # debug log
 set -x
 
-./node_modules/.bin/ts-node -e 'require ("./e2e/generateTestrailObjects").createAndroidTestRun();'
+./node_modules/.bin/ts-node -e 'import ("./e2e/generateTestrailObjects").then(a => a.createAndroidTestRun());'
 
 RUN_ID=$(head -n 1 ./e2e/testrailRunID.txt) 
 
