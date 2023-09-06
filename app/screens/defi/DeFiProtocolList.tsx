b6d697f9f0ab4181c413763873acf70244754be4
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2023-09-06 15:47:53 -0400

    CP-6032: add DeFi details page with header only (#896)

--- app/screens/defi/DeFiProtocolList.tsx
+++ app/screens/defi/DeFiProtocolList.tsx
@@ -1,6 +1,6 @@
 import { useDeFiProtocolList } from 'hooks/defi/useDeFiProtocolList'
 import React from 'react'
-import { FlatList, Image, Linking, View, Pressable, Alert } from 'react-native'
+import { FlatList, View } from 'react-native'
 import DeFiService from 'services/defi/DeFiService'
 import Card from 'components/Card'
 import { DeFiSimpleProtocol } from 'services/defi/types'
@@ -11,10 +11,21 @@ import { useApplicationContext } from 'contexts/ApplicationContext'
 import { Space } from 'components/Space'
 import { useDeFiChainList } from 'hooks/defi/useDeFiChainList'
 import AvaButton from 'components/AvaButton'
+import { PortfolioScreenProps } from 'navigation/types'
+import AppNavigation from 'navigation/AppNavigation'
+import { useNavigation } from '@react-navigation/native'
+import { openURL } from 'utils/openURL'
 import { ErrorState } from './components/ErrorState'
 import { ZeroState } from './components/ZeroState'
+import { ProtocolLogo } from './components/ProtocolLogo'
+import { NetworkLogo } from './components/NetworkLogo'
+
+type ScreenProps = PortfolioScreenProps<
+  typeof AppNavigation.Portfolio.Portfolio
+>['navigation']
 
 export const DeFiProtocolList = () => {
+  const { navigate } = useNavigation<ScreenProps>()
   const {
     theme,
     appHook: { currencyFormatter }
@@ -27,12 +38,19 @@ export const DeFiProtocolList = () => {
     return DeFiService.sortSimpleProtocols(data)
   }, [data])
 
-  const handleGoToDetail = () => {
-    Alert.alert('Not Implemented')
+  const handleGoToDetail = (protocolId: string) => {
+    navigate({
+      name: AppNavigation.Wallet.DeFiProtocolDetails,
+      params: { protocolId }
+    })
+  }
+
+  const goToProtocolPage = async (siteUrl?: string) => {
+    openURL(siteUrl)
   }
 
   const handleExploreEcosystem = () => {
-    Linking.openURL('https://core.app/discover/')
+    openURL('https://core.app/discover/')
   }
 
   if (isLoading) return <PortfolioDeFiHomeLoader />
@@ -43,39 +61,27 @@ export const DeFiProtocolList = () => {
   const renderItem = ({ item }: { item: DeFiSimpleProtocol }) => {
     const netUsdValue = currencyFormatter(item.netUsdValue)
     const networkLogo = chainList?.[item.chain]?.logoUrl
+    const protocolId = item.id
 
     const renderLogo = () => {
       return (
         <View>
-          <Image
-            source={{ uri: item.logoUrl }}
+          <ProtocolLogo uri={item.logoUrl} />
+          <NetworkLogo
+            uri={networkLogo}
             style={{
-              width: 40,
-              height: 40,
               marginRight: 16,
-              borderRadius: 20
+              position: 'absolute',
+              bottom: 0,
+              right: 0
             }}
           />
-          {networkLogo && (
-            <Image
-              source={{ uri: networkLogo }}
-              style={{
-                width: 12,
-                height: 12,
-                marginRight: 16,
-                borderRadius: 6,
-                position: 'absolute',
-                bottom: 0,
-                right: 0
-              }}
-            />
-          )}
         </View>
       )
     }
 
     return (
-      <AvaButton.Base onPress={handleGoToDetail}>
+      <AvaButton.Base onPress={() => handleGoToDetail(protocolId)}>
         <Card
           key={item.id}
           style={{
@@ -90,7 +96,7 @@ export const DeFiProtocolList = () => {
             <AvaText.Heading5>{item.name}</AvaText.Heading5>
           </View>
           <View>
-            <Pressable onPress={handleExploreEcosystem}>
+            <AvaButton.Base onPress={() => goToProtocolPage(item.siteUrl)}>
               <View style={{ alignItems: 'flex-end' }}>
                 <AvaText.Body2 color={theme.neutral50}>
                   {netUsdValue}
@@ -98,7 +104,7 @@ export const DeFiProtocolList = () => {
                 <Space y={6} />
                 <LinkSVG color={theme.white} />
               </View>
-            </Pressable>
+            </AvaButton.Base>
           </View>
         </Card>
       </AvaButton.Base>
