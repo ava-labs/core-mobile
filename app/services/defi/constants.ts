924801cfb8b12e03f49ab38948aa4a6c7650570a
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2023-09-07 12:34:37 -0400

    CP-7155: add isPaused to handled no network (#899)

--- app/services/defi/constants.ts
+++ app/services/defi/constants.ts
@@ -1,3 +1,8 @@
 export const CHAIN_LIST = 'chain/list'
 export const PROTOCOL = 'user/protocol'
 export const SIMPLE_PROTOCOL_LIST = 'user/all_simple_protocol_list'
+
+export const refetchIntervals = {
+  deFiProtocolList: 30000, // 30 seconds
+  deFiProtocol: 30000 // 30 seconds
+}
