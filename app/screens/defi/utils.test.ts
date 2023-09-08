d9eac5e5226f0c0e392fa005b264c3dd14080df8
ruijialin <ruijia.lin@avalabs.org>
2023-09-08 16:24:07 -0400

    added unit tests

---
+++ app/screens/defi/utils.test.ts
@@ -0,0 +1,25 @@
+import { DeFiPortfolioItem } from 'services/defi/types'
+import { mapPortfolioItems } from './utils'
+import MOCK_DATA from './mockProtocolDetailsData.json'
+
+describe('mapPortfolioItems', () => {
+  it('should have returned item with highest net usd value from sorted item group', () => {
+    const groupedItem = mapPortfolioItems(
+      MOCK_DATA as unknown as DeFiPortfolioItem[]
+    )
+    expect(groupedItem[0]?.totalUsdValue).toStrictEqual(1.803)
+  })
+  it('should have returned correct count of grouped items', () => {
+    const groupedItem = mapPortfolioItems(
+      MOCK_DATA as unknown as DeFiPortfolioItem[]
+    )
+    expect(groupedItem.length).toStrictEqual(4)
+  })
+  it('should have return correct number of grouped Liquidity Pool items', () => {
+    const groupedItem = mapPortfolioItems(
+      MOCK_DATA as unknown as DeFiPortfolioItem[]
+    )
+    expect(groupedItem[0]?.name).toStrictEqual('Liquidity Pool')
+    expect(groupedItem[0]?.items.length).toStrictEqual(2)
+  })
+})
