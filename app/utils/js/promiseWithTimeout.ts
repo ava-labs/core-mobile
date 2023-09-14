a283174b98ad8d8dac677ad0f0e4ed3c22c4d311
neven-s <piaf1986@gmail.com>
2023-09-14 15:30:15 +0200

    CP-7131: Fix Cannot add custom token  (#904)

---
+++ app/utils/js/promiseWithTimeout.ts
@@ -0,0 +1,32 @@
+import { ErrorBase } from 'errors/ErrorBase'
+
+export class TimeoutError extends ErrorBase<'TIMEOUT_ERROR'> {}
+
+/**
+ * If param **promise** does not resolve within **ms** this promise will throw **timeoutError**.
+ * You can catch error thrown in try catch block and check against **timeoutError** if throw was
+ * because of timeout or coming from wrapped **promise**
+ *
+ * @param promise Promise to race against
+ * @param ms Timeout in milliseconds
+ */
+export default function promiseWithTimeout<T>(
+  promise: Promise<T>,
+  ms: number
+): Promise<T> {
+  let timer: NodeJS.Timeout | undefined
+  return Promise.race([
+    promise,
+    new Promise<T>(
+      (_r, reject) =>
+        (timer = setTimeout(
+          reject,
+          ms,
+          new TimeoutError({
+            message: 'Promise timed out!',
+            name: 'TIMEOUT_ERROR'
+          })
+        ))
+    )
+  ]).finally(() => timer && clearTimeout(timer))
+}
