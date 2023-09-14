c7f87596e355640ef5cad9fb46f858a68df58355
Tyler Hackett <thackett86181@gmail.com>
2023-09-14 10:18:06 -0700

    add correct PnL formatting

--- app/screens/defi/components/DeFiPortfolioPerpetual.tsx
+++ app/screens/defi/components/DeFiPortfolioPerpetual.tsx
@@ -4,6 +4,7 @@ import { DefiPerpetualItem } from 'services/defi/types'
 import React from 'react'
 import { Row } from 'components/Row'
 import { View } from 'react-native'
+import { useApplicationContext } from 'contexts/ApplicationContext'
 
 interface Props {
   items: DefiPerpetualItem[]
@@ -13,21 +14,22 @@ const roundValueTwoDecimals = (value: number) => {
   return (Math.round(value * 100) / 100).toFixed(2)
 }
 
+const pNLtextColor = (value: number) => {
+  return value > 0 ? '#53C26E' : '#F34333'
+}
+
 export const DeFiPortfolioPerpetual: FC<Props> = ({ items }) => {
+  const { currencyFormatter } = useApplicationContext().appHook
   return (
     <View style={{ marginTop: 8 }}>
       <Row
         style={{
           justifyContent: 'space-between',
-          marginTop: 8
+          marginTop: 8,
+          marginRight: 8
         }}>
         <AvaText.Body1>Token Pair</AvaText.Body1>
-        <View style={{ marginLeft: 20 }}>
-          <AvaText.Body1>Profit</AvaText.Body1>
-        </View>
-        <View style={{ marginRight: 8 }}>
-          <AvaText.Body1>Value</AvaText.Body1>
-        </View>
+        <AvaText.Body1>Value</AvaText.Body1>
       </Row>
       {items.map(
         (
@@ -40,12 +42,17 @@ export const DeFiPortfolioPerpetual: FC<Props> = ({ items }) => {
               style={{ justifyContent: 'space-between', marginTop: 8 }}>
               <AvaText.Body2>
                 {positionToken.symbol}/{marginToken.symbol}
+                {'\n'}
+                {'PnL'}
               </AvaText.Body2>
-              <AvaText.Body2>
-                {roundValueTwoDecimals(profitUsdValue)}
-              </AvaText.Body2>
-              <AvaText.Body2>
-                {roundValueTwoDecimals(netUsdValue)}
+              <AvaText.Body2 currency={true}>
+                {currencyFormatter(roundValueTwoDecimals(netUsdValue))}
+                {'\n'}
+                <AvaText.Body2
+                  color={pNLtextColor(profitUsdValue)}
+                  currency={true}>
+                  {currencyFormatter(roundValueTwoDecimals(profitUsdValue))}
+                </AvaText.Body2>
               </AvaText.Body2>
             </Row>
           )
