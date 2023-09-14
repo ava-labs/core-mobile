95feabc5e565ef1c7bb41386fc744988dd14e5ae
ruijialin <ruijia.lin@avalabs.org>
2023-09-14 16:47:11 -0400

    add defi error/zero state stories

---
+++ storybook/stories/defi/ZeroStateSkipBodyText.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import type { Meta } from '@storybook/react-native'
+import { ZeroState } from 'screens/defi/components/ZeroState'
+
+export default {
+  title: 'DeFi'
+} as Meta
+
+export const ZeroStateSkipBodyText = () => <ZeroState skipBodyText />
