e0fc93c7c94c42fd3aa7dca62e8fd827602ed101
Tyler Hackett <thackett86181@gmail.com>
2023-09-13 23:21:23 -0700

    add portfolio perpetual component

--- app/screens/defi/components/DeFiPortfolioPerpetual.tsx
+++ app/screens/defi/components/DeFiPortfolioPerpetual.tsx
@@ -9,12 +9,25 @@ interface Props {
   items: DefiPerpetualItem[]
 }
 
+const roundValueTwoDecimals = (value: number) => {
+  return (Math.round(value * 100) / 100).toFixed(2)
+}
+
 export const DeFiPortfolioPerpetual: FC<Props> = ({ items }) => {
   return (
     <View style={{ marginTop: 8 }}>
-      <Row style={{ justifyContent: 'space-between', marginTop: 8 }}>
+      <Row
+        style={{
+          justifyContent: 'space-between',
+          marginTop: 8
+        }}>
         <AvaText.Body1>Token Pair</AvaText.Body1>
-        <AvaText.Body1>Value</AvaText.Body1>
+        <View style={{ marginLeft: 20 }}>
+          <AvaText.Body1>Profit</AvaText.Body1>
+        </View>
+        <View style={{ marginRight: 8 }}>
+          <AvaText.Body1>Value</AvaText.Body1>
+        </View>
       </Row>
       {items.map(
         (
@@ -28,8 +41,12 @@ export const DeFiPortfolioPerpetual: FC<Props> = ({ items }) => {
               <AvaText.Body2>
                 {positionToken.symbol}/{marginToken.symbol}
               </AvaText.Body2>
-              <AvaText.Body2>{profitUsdValue}</AvaText.Body2>
-              <AvaText.Body2>{netUsdValue}</AvaText.Body2>
+              <AvaText.Body2>
+                {roundValueTwoDecimals(profitUsdValue)}
+              </AvaText.Body2>
+              <AvaText.Body2>
+                {roundValueTwoDecimals(netUsdValue)}
+              </AvaText.Body2>
             </Row>
           )
         }
