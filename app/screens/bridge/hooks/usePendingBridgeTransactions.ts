9e71e90dd6d9dbae806689308a1d1395b2940b1d
Junghwan Jang <onghwan@gmail.com>
2023-09-15 10:43:33 -0400

    make network prop optional

--- app/screens/bridge/hooks/usePendingBridgeTransactions.ts
+++ app/screens/bridge/hooks/usePendingBridgeTransactions.ts
@@ -2,12 +2,16 @@ import { Network } from '@avalabs/chains-sdk'
 import { useSelector } from 'react-redux'
 import { selectBridgeTransactions } from 'store/bridge'
 
-const usePendingBridgeTransactions = (network: Network) => {
+const usePendingBridgeTransactions = (network?: Network) => {
   const pendingBridgeByTxId = useSelector(selectBridgeTransactions)
 
-  return Object.values(pendingBridgeByTxId).filter(
-    tx => tx.symbol === network.networkToken.symbol
-  )
+  if (network) {
+    return Object.values(pendingBridgeByTxId).filter(
+      tx => tx.symbol === network.networkToken.symbol
+    )
+  } else {
+    return Object.values(pendingBridgeByTxId)
+  }
 }
 
 export default usePendingBridgeTransactions
