215af2b942f5e2ad9aeffaa6375961afd6996ae3
ARTEM BESPALOV <40455398+artyombespalov@users.noreply.github.com>
2023-09-19 13:41:14 -0700

    QA-72: new automation tests for stake feature (#940)

---
+++ e2e/locators/Stake/claimScreen.loc.ts
@@ -0,0 +1,11 @@
+export default {
+  avaLogo: 'ava_logo',
+  claimRewardsTitle: 'Claim Rewards',
+  claimNowButton: 'btnPrimary',
+  claimableAmountText: 'Claimable Amount',
+  networkFeeText: 'Network Fee',
+  claimableAvaxAmount: 'claimable_balance',
+  claimableBalanceCurrency: 'claimable_balance_currency',
+  networkFeeAmount: 'network_fee',
+  networkFeeCurrency: 'network_fee_currency'
+}
