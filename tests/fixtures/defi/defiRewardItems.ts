8bc5f4ff55fe434454bb5aac3171dadfe94d4f32
Tyler Hackett <thackett86181@gmail.com>
2023-09-19 11:21:46 -0700

    CP-7148: create component for Reward portfolioItemObject type (#935)

---
+++ tests/fixtures/defi/defiRewardItems.ts
@@ -0,0 +1,101 @@
+import { DeFiProtocolDetailTypes, DeFiRewardItem } from 'services/defi/types'
+
+export const defiRewardItems: DeFiRewardItem[] = [
+  {
+    type: DeFiProtocolDetailTypes.REWARD,
+    netUsdValue: 390247.3,
+    name: 'Rewards',
+    tokens: [
+      {
+        id: '0xb72b31907c1c95f3650b64b2469e08edacee5e8f',
+        chain: 'eth',
+        name: 'bZx Vesting Token',
+        symbol: 'vBZRX',
+        displaySymbol: '',
+        optimizedSymbol: 'vBZRX',
+        decimals: 18,
+        logoUrl:
+          'https://static.debank.com/image/token/logo_url/0xb72b31907c1c95f3650b64b2469e08edacee5e8f/03ffd36c7ec2ffa954fa9ed3ac93790b.png',
+        protocolId: '',
+        price: 0.2715782119251945,
+        isVerified: true,
+        isCore: false,
+        isWallet: false,
+        timeAt: 1594509894,
+        amount: 13.620476800650975
+      },
+      {
+        id: '0xb72b31907c1c95f3650b64b2469e08edacee5e8f',
+        chain: 'eth',
+        name: 'bZx Vesting Token',
+        symbol: 'AHOL',
+        displaySymbol: null,
+        optimizedSymbol: 'AHOL',
+        decimals: 18,
+        logoUrl:
+          'https://static.debank.com/image/token/logo_url/0xb72b31907c1c95f3650b64b2469e08edacee5e8f/03ffd36c7ec2ffa954fa9ed3ac93790b.png',
+        protocolId: '',
+        price: 0.2715782119251945,
+        isVerified: true,
+        isCore: false,
+        isWallet: false,
+        timeAt: 1594509894,
+        amount: 13.620476800650975
+      },
+      {
+        id: '0xb72b31907c1c95f3650b64b2469e08edacee5e8f',
+        chain: 'eth',
+        name: 'bZx Vesting Token',
+        symbol: 'vBZRX',
+        displaySymbol: null,
+        optimizedSymbol: 'vBZRX',
+        decimals: 18,
+        logoUrl:
+          'https://static.debank.com/image/token/logo_url/0xb72b31907c1c95f3650b64b2469e08edacee5e8f/03ffd36c7ec2ffa954fa9ed3ac93790b.png',
+        protocolId: '',
+        price: 0.2715782119251945,
+        isVerified: true,
+        isCore: false,
+        isWallet: false,
+        timeAt: 1594509894,
+        amount: 13.620476800650975
+      },
+      {
+        id: '0xb72b31907c1c95f3650b64b2469e08edacee5e8f',
+        chain: 'eth',
+        name: 'bZx Vesting Token',
+        symbol: 'vBZRX',
+        displaySymbol: null,
+        optimizedSymbol: 'vBZRX',
+        decimals: 18,
+        logoUrl:
+          'https://static.debank.com/image/token/logo_url/0xb72b31907c1c95f3650b64b2469e08edacee5e8f/03ffd36c7ec2ffa954fa9ed3ac93790b.png',
+        protocolId: '',
+        price: 0.2715782119251945,
+        isVerified: true,
+        isCore: false,
+        isWallet: false,
+        timeAt: 1594509894,
+        amount: 13.620476800650975
+      },
+      {
+        id: '0xb72b31907c1c95f3650b64b2469e08edacee5e8f',
+        chain: 'eth',
+        name: 'bZx Vesting Token',
+        symbol: 'vBZRX',
+        displaySymbol: null,
+        optimizedSymbol: 'vBZRX',
+        decimals: 18,
+        logoUrl:
+          'https://static.debank.com/image/token/logo_url/0xb72b31907c1c95f3650b64b2469e08edacee5e8f/03ffd36c7ec2ffa954fa9ed3ac93790b.png',
+        protocolId: '',
+        price: 0.2715782119251945,
+        isVerified: true,
+        isCore: false,
+        isWallet: false,
+        timeAt: 1594509894,
+        amount: 13.620476800650975
+      }
+    ]
+  }
+]
