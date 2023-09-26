14c56ec045449e9f32058a8d001a62e209fd552a
An Nguyen <an.nguyen@avalabs.org>
2023-09-26 08:19:19 -0500

    adjust hooks

--- app/hooks/useNetworkFee.tsx
+++ app/hooks/useNetworkFee.tsx
@@ -1,11 +1,13 @@
 import { Network } from '@avalabs/chains-sdk'
 import { useQuery } from '@tanstack/react-query'
 import { ReactQueryKeys } from 'consts/reactQueryKeys'
+import { queryClient } from 'contexts/ReactQueryProvider'
 import { useSelector } from 'react-redux'
 import NetworkFeeService from 'services/networkFee/NetworkFeeService'
 import { selectActiveNetwork } from 'store/network'
+import Logger from 'utils/Logger'
 
-const initialData = {
+const defaultData = {
   low: 0n,
   medium: 0n,
   high: 0n,
@@ -18,18 +20,34 @@ const initialData = {
 
 const REFETCH_INTERVAL = 30000 // 30 seconds
 
+export const getQueryKey = (network: Network) => [
+  ReactQueryKeys.NETWORK_FEE,
+  network.chainId
+]
+
+const getQueryFn = (network: Network) => () =>
+  NetworkFeeService.getNetworkFee(network)
+
+export const prefetchNetworkFee = (network: Network | undefined) => {
+  if (network) {
+    Logger.info('prefetching network fee', network.chainId)
+
+    queryClient.prefetchQuery({
+      queryKey: getQueryKey(network),
+      queryFn: getQueryFn(network)
+    })
+  }
+}
+
 export const useNetworkFee = (network?: Network) => {
   const activeNetwork = useSelector(selectActiveNetwork)
   const networkToRequest = network || activeNetwork
 
-  return useQuery({
-    initialData,
-    // eslint-disable-next-line @tanstack/query/exhaustive-deps
-    queryKey: [ReactQueryKeys.NETWORK_FEE, networkToRequest.chainId],
-    queryFn: () => NetworkFeeService.getNetworkFee(networkToRequest),
-    select: data => {
-      return data ?? initialData
-    },
+  const query = useQuery({
+    queryKey: getQueryKey(networkToRequest),
+    queryFn: getQueryFn(networkToRequest),
     refetchInterval: REFETCH_INTERVAL
   })
+
+  return { ...query, data: query.data || defaultData }
 }
