89890436b351e0684d35c1f062a53653c3b6c174
Tyler Hackett <thackett86181@gmail.com>
2023-09-28 16:49:46 -0700

    fix jq args

--- scripts/bitrise/detox/createAndroidTestRun.sh
+++ scripts/bitrise/detox/createAndroidTestRun.sh
@@ -6,13 +6,13 @@ TEST_RUN_NAME="Android Smoke Test run $TIMESTAMP"
 
 echo 'Creating new android test run...'
 
-JSON_DATA=$(jq -n \ 
+JSON_DATA=$(jq --null-input \ 
             --arg test_name "$TEST_RUN_NAME" \
-            --arg all_tcs_bool "false")
+            --arg all_tcs_bool "false") \
 
 TEST_RUN_DETAILS=$(curl -H "Content-Type: application/json" \
 -u "mobiledevs@avalabs.org:$TESTRAIL_API_KEY" \
--d "$JSON_DATA" \
+-d "${JSON_DATA}" \
 -X POST "https://avalabs.testrail.net/index.php?/api/v2/add_run/$TESTRAIL_PROJECT_ID")
 
 echo "Test run details: $TEST_RUN_DETAILS"
@@ -21,4 +21,4 @@ TESTRAIL_RUN_ID="$TEST_RUN_DETAILS" | jq -r '.id'
 
 echo "Created test run with id: $TESTRAIL_RUN_ID"
 
-envman add --key TESTRAIL_RUN --value "$TESTRAIL_RUN_ID"
+#envman add --key TESTRAIL_RUN --value "$TESTRAIL_RUN_ID"
