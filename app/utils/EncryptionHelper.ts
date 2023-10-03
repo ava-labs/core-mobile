c97caea3196dcf8c4ba04793fe9895ece75e1522
An Nguyen <an.nguyen@avalabs.org>
2023-10-03 10:27:58 -0500

    adjust argon config when encrypting mnemonic phrase

--- app/utils/EncryptionHelper.ts
+++ app/utils/EncryptionHelper.ts
@@ -1,54 +1,107 @@
+import { ErrorBase } from 'errors/ErrorBase'
 import { NativeModules } from 'react-native'
 import argon2 from 'react-native-argon2'
 
 const Aes = NativeModules.Aes
 
-export interface EncryptedData {
-  cipher: string
-  iv: string
-  salt: string
+const VERSION = 2
+
+type Mode = 'argon2id' | 'argon2d' | 'argon2i'
+
+type ArgonConfig = {
+  iterations: number
+  memory: number
+  parallelism: number
+  hashLength: number
+  mode: Mode
 }
 
-export class NoSaltError extends Error {
-  constructor() {
-    super('data has no salt')
-    this.name = 'NoSaltError'
-  }
+type DecryptedData = {
+  version: number // what version of the encryption algo was used
+  data: string
 }
 
-async function getDerivedKey(password: string, salt: string): Promise<string> {
-  const { rawHash } = await argon2(password, salt, {
+const versionConfigMap: Record<number, ArgonConfig> = {
+  1: {
     iterations: 2,
     memory: 32 * 1024, // 32 MB
     parallelism: 6,
     hashLength: 32, // 256-bit output
     mode: 'argon2id'
-  })
+  },
+  2: {
+    iterations: 3,
+    memory: 64 * 1024, // 64 MB
+    parallelism: 4,
+    hashLength: 32, // 256-bit output
+    mode: 'argon2id'
+  }
+}
+
+interface EncryptedData {
+  cipher: string
+  iv: string
+  salt: string
+  version: string
+}
+
+export class NoSaltError extends ErrorBase<'NoSaltError'> {}
+
+export class InvalidVersionError extends ErrorBase<'InvalidVersionError'> {}
+
+async function getDerivedKey(
+  password: string,
+  salt: string,
+  version: number
+): Promise<string> {
+  const config = versionConfigMap[version]
+
+  if (!config)
+    throw new InvalidVersionError({
+      message: `no config for version ${VERSION}`
+    })
+
+  const { rawHash } = await argon2(password, salt, config)
+
   return rawHash
 }
 
 export async function encrypt(data: string, password: string): Promise<string> {
   const salt = await Aes.randomKey(16) // 128-bit salt
   const iv = await Aes.randomKey(16) // 128-bit iv
-  const key = await getDerivedKey(password, salt)
+  const key = await getDerivedKey(password, salt, VERSION)
   const cipher = await Aes.encrypt(data, key, iv)
 
   return JSON.stringify({
     cipher,
     iv,
-    salt
+    salt,
+    version: VERSION
   })
 }
 
 export async function decrypt(
   encryptedData: string,
   password: string
-): Promise<string> {
-  const { cipher, iv, salt } = JSON.parse(encryptedData) as EncryptedData
+): Promise<DecryptedData> {
+  const { cipher, iv, salt, version } = JSON.parse(
+    encryptedData
+  ) as EncryptedData
 
-  if (salt === undefined) throw new NoSaltError()
+  if (salt === undefined)
+    throw new NoSaltError({
+      message: 'data has no salt'
+    })
 
-  const key = await getDerivedKey(password, salt)
+  // we only start storing version info from version 2
+  // so if version is undefined, it means data was encrypted using version 1 config
+  const versionToUse = version === undefined ? 1 : Number(version)
 
-  return Aes.decrypt(cipher, key, iv)
+  const key = await getDerivedKey(password, salt, versionToUse)
+  const data = (await Aes.decrypt(cipher, key, iv)) as string
+
+  return {
+    version: versionToUse,
+    data
+  }
 }
