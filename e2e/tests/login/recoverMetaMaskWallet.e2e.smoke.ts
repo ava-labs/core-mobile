ee63d11e26cf4b1068145b850eadca1b8f581f01
Tyler Hackett <thackett86181@gmail.com>
2023-10-04 16:27:40 -0700

    add metamask seed phrase test

---
+++ e2e/tests/login/recoverMetaMaskWallet.e2e.smoke.ts
@@ -0,0 +1,18 @@
+/* eslint-disable jest/expect-expect */
+/* eslint-env detox/detox, jest */
+/**
+ * @jest-environment ./jestCustomEnv
+ */
+import { warmup } from '../../helpers/warmup'
+import ExistingRecoveryPhrasePage from '../../pages/existingRecoveryPhrase.page'
+
+describe('Add existing metamask wallet', () => {
+  beforeAll(async () => {
+    await warmup()
+  })
+
+  it('should successfully add an existing metamask wallet', async () => {
+    const recoveryPhrase: string = process.env.E2E_METAMASK as string
+    await ExistingRecoveryPhrasePage.recoverWallet(recoveryPhrase)
+  })
+})
