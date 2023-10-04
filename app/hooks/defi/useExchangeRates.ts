a766af71c6149656a38723367786e08cb0d8e0c8
Junghwan Jang <onghwan@gmail.com>
2023-10-04 14:38:48 -0400

    remove unnecessary export

--- app/hooks/defi/useExchangeRates.ts
+++ app/hooks/defi/useExchangeRates.ts
@@ -4,7 +4,7 @@ import { ReactQueryKeys } from 'consts/reactQueryKeys'
 import { ExchangeRateSchema } from 'services/defi/types'
 
 // We're only loading exchange rates for USD at the moment.
-export const CURRENCY_EXCHANGE_RATES_URL =
+const CURRENCY_EXCHANGE_RATES_URL =
   'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json'
 
 const apiClient = new Zodios(CURRENCY_EXCHANGE_RATES_URL, [
