585985c19fe835a809413d31c9138d275731ff05
JungHwan Jang <onghwan@gmail.com>
2023-10-06 11:10:54 -0400

    CP-7237: Http client with schema validation (#970)

--- app/services/defi/DeFiService.ts
+++ app/services/defi/DeFiService.ts
@@ -1,80 +1,28 @@
-import Config from 'react-native-config'
+import { defiApiClient, exchangeRateApiClient } from './apiClient'
 import {
-  DeFiProtocolObject,
-  DeFiSimpleProtocolObject,
   DeFiChainObject,
-  DeFiSimpleProtocolSchema,
-  DeFiChainSchema,
-  DeFiProtocolSchema
+  DeFiProtocolObject,
+  DeFiSimpleProtocolObject
 } from './debankTypes'
-import { DeFiSimpleProtocol, ExchangeRate, ExchangeRateSchema } from './types'
-import {
-  CHAIN_LIST,
-  CURRENCY_EXCHANGE_RATES_URL,
-  PROTOCOL,
-  SIMPLE_PROTOCOL_LIST
-} from './constants'
-
-if (!Config.PROXY_URL) throw Error('PROXY_URL is missing')
-
-const baseUrl = Config.PROXY_URL + '/proxy/debank/v1'
-const chainListUrl = `${baseUrl}/${CHAIN_LIST}`
-const userProtocolUrl = `${baseUrl}/${PROTOCOL}`
-const userProtocolListUrl = `${baseUrl}/${SIMPLE_PROTOCOL_LIST}`
-const headers = {
-  'Content-Type': 'application/json'
-}
 
 class DeFiService {
-  async getSupportedChainList(): Promise<DeFiChainObject[]> {
-    const supportedChainList = await fetch(chainListUrl, {
-      headers
-    })
-    const json = await supportedChainList.json()
-    return DeFiChainSchema.array().parse(json)
-  }
+  static getSupportedChainList = (): Promise<DeFiChainObject[]> =>
+    defiApiClient.getSupportedChainList()
 
-  async getDeFiProtocol(
+  static getDeFiProtocol = (
     userAddress: string,
     protocolId: string
-  ): Promise<DeFiProtocolObject> {
-    const params = new URLSearchParams({
-      id: userAddress,
-      protocol_id: protocolId
+  ): Promise<DeFiProtocolObject> =>
+    defiApiClient.getDeFiProtocol({
+      queries: { id: userAddress, protocol_id: protocolId }
     })
-    const urlWithQueryParam = `${userProtocolUrl}?${params}`
-    const userProtocolList = await fetch(urlWithQueryParam, {
-      headers
-    })
-    const json = await userProtocolList.json()
-    return DeFiProtocolSchema.parse(json)
-  }
 
-  async getDeFiProtocolList(
+  static getDeFiProtocolList = (
     userAddress: string
-  ): Promise<DeFiSimpleProtocolObject[]> {
-    const params = new URLSearchParams({
-      id: userAddress
-    })
-    const urlWithQueryParam = `${userProtocolListUrl}?${params}`
-    const userProtocolList = await fetch(urlWithQueryParam, {
-      headers
-    })
-    const json = await userProtocolList.json()
-    return DeFiSimpleProtocolSchema.array().parse(json)
-  }
+  ): Promise<DeFiSimpleProtocolObject[]> =>
+    defiApiClient.getDeFiProtocolList({ queries: { id: userAddress } })
 
-  sortSimpleProtocols(protocols: DeFiSimpleProtocol[]): DeFiSimpleProtocol[] {
-    return [...protocols].sort(
-      ({ netUsdValue: valueA }, { netUsdValue: valueB }) => valueB - valueA
-    )
-  }
-
-  async getExchangeRates(): Promise<ExchangeRate> {
-    const response = await fetch(CURRENCY_EXCHANGE_RATES_URL)
-    const json = await response.json()
-    return ExchangeRateSchema.parse(json)
-  }
+  static getExchangeRates = exchangeRateApiClient.getExchangeRates
 }
 
-export default new DeFiService()
+export default DeFiService
---
+++ app/services/defi/apiClient.ts
@@ -0,0 +1,62 @@
+import { Zodios } from '@zodios/core'
+import Config from 'react-native-config'
+import { z } from 'zod'
+import {
+  DeFiChainSchema,
+  DeFiProtocolSchema,
+  DeFiSimpleProtocolSchema
+} from './debankTypes'
+import { ExchangeRateSchema } from './types'
+
+if (!Config.PROXY_URL) throw Error('PROXY_URL is missing')
+
+const baseUrl = Config.PROXY_URL + '/proxy/debank/v1'
+
+export const defiApiClient = new Zodios(
+  baseUrl,
+  [
+    {
+      method: 'get',
+      path: '/chain/list',
+      alias: 'getSupportedChainList',
+      response: z.array(DeFiChainSchema)
+    },
+    {
+      method: 'get',
+      path: '/user/protocol',
+      parameters: [
+        { name: 'id', type: 'Query', schema: z.string() },
+        { name: 'protocol_id', type: 'Query', schema: z.string() }
+      ],
+      alias: 'getDeFiProtocol',
+      response: DeFiProtocolSchema
+    },
+    {
+      method: 'get',
+      path: '/user/all_simple_protocol_list',
+      parameters: [{ name: 'id', type: 'Query', schema: z.string() }],
+      alias: 'getDeFiProtocolList',
+      response: z.array(DeFiSimpleProtocolSchema)
+    }
+  ],
+  {
+    axiosConfig: {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  }
+)
+
+// We're only loading exchange rates for USD at the moment.
+const CURRENCY_EXCHANGE_RATES_URL =
+  'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json'
+
+export const exchangeRateApiClient = new Zodios(CURRENCY_EXCHANGE_RATES_URL, [
+  {
+    method: 'get',
+    path: '',
+    alias: 'getExchangeRates',
+    response: ExchangeRateSchema
+  }
+])
