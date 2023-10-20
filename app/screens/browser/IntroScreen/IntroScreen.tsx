7a71b1917ff9957064d8d753e86e5102d164efcc
Tyler Hackett <thackett86181@gmail.com>
2023-10-19 17:28:03 -0700

    use ava components

--- app/screens/browser/IntroScreen/IntroScreen.tsx
+++ app/screens/browser/IntroScreen/IntroScreen.tsx
@@ -13,11 +13,15 @@ import {
   rrect,
   rect,
   Box,
-  Mask,
-  Text,
-  useFont
+  Mask
 } from '@shopify/react-native-skia'
-import { RocketIcon, SearchIcon, WalletConnectIcon, CoreOwlIcon } from './Icons'
+import AvaText from 'components/AvaText'
+import { Space } from 'components/Space'
+import { Row } from 'components/Row'
+import WalletConnectSVG from 'components/svg/WalletConnectSVG'
+import CoreOwl from 'assets/icons/core_owl.svg'
+import RocketLaunch from 'assets/icons/rocket_launch.svg'
+import SearchIcon from 'assets/icons/search.svg'
 
 const TOO_COLOR = '#000000'
 const FROM_COLOR = '#007AFF'
@@ -37,7 +41,7 @@ const BlueBackground = (): JSX.Element => {
 }
 
 const TokenImageWithGradient = (): JSX.Element => {
-  const image = useImage(require('assets/icons/browser_intro_screen_logos.jpg'))
+  const image = useImage(require('assets/icons/browser_intro_screen_logos.png'))
   return (
     <Mask
       mask={
@@ -57,140 +61,123 @@ const TokenImageWithGradient = (): JSX.Element => {
 }
 
 const HowToUseTheCoreBrowser = (): JSX.Element | null => {
-  const font = useFont(require('assets/fonts/Inter-Bold.ttf'), 34)
-  if (!font) {
-    return null
-  }
   return (
-    <Group>
-      <Text x={16} y={250} text="How to use the" font={font} color={'white'} />
-      <Text x={16} y={292} text="Core browser..." font={font} color={'white'} />
-    </Group>
+    <View style={{ paddingHorizontal: 32 }}>
+      <AvaText.Heading3
+        textStyle={{ fontSize: 34, lineHeight: 44, fontWeight: '700' }}>
+        How to use the Core Browser...
+      </AvaText.Heading3>
+    </View>
   )
 }
 
 const SearchText = (): JSX.Element | null => {
-  const smallFont = useFont(require('assets/fonts/Inter-Bold.ttf'), 16)
-
-  if (!smallFont) {
-    return null
-  }
   return (
-    <Group>
-      <Text
-        x={55}
-        y={345}
-        text="Search for a website or browse"
-        font={smallFont}
-        color={'white'}
-      />
-      <Text
-        x={55}
-        y={369}
-        text="suggested apps"
-        font={smallFont}
-        color={'white'}
-      />
-    </Group>
+    <View style={{ paddingHorizontal: 16 }}>
+      <AvaText.Heading6
+        textStyle={{ fontSize: 16, lineHeight: 24, fontWeight: '700' }}>
+        Search for a website or browse suggested apps
+      </AvaText.Heading6>
+    </View>
   )
 }
 
 const WalletConnectText = (): JSX.Element | null => {
-  const smallFont = useFont(require('assets/fonts/Inter-Bold.ttf'), 16)
-
-  if (!smallFont) {
-    return null
-  }
   return (
-    <Group>
-      <Text
-        x={55}
-        y={408}
-        text="On the website find “Connect”"
-        font={smallFont}
-        color={'white'}
-      />
-      <Text
-        x={55}
-        y={433}
-        text="then tap Wallet Connect"
-        font={smallFont}
-        color={'white'}
-      />
-    </Group>
+    <View style={{ paddingHorizontal: 16 }}>
+      <AvaText.Heading6
+        textStyle={{ fontSize: 16, lineHeight: 24, fontWeight: '700' }}>
+        On the website find “Connect” then tap Wallet Connect
+      </AvaText.Heading6>
+    </View>
   )
 }
 
 const CoreOwlText = (): JSX.Element | null => {
-  const smallFont = useFont(require('assets/fonts/Inter-Bold.ttf'), 16)
-
-  if (!smallFont) {
-    return null
-  }
   return (
-    <Group>
-      <Text
-        x={55}
-        y={472}
-        text="Find Core and tap “Connect”"
-        font={smallFont}
-        color={'white'}
-      />
-    </Group>
+    <View style={{ paddingHorizontal: 16 }}>
+      <AvaText.Heading6
+        textStyle={{ fontSize: 16, lineHeight: 24, fontWeight: '700' }}>
+        Find core and tap "Connect"
+      </AvaText.Heading6>
+    </View>
   )
 }
 
 const RocketText = (): JSX.Element | null => {
-  const smallFont = useFont(require('assets/fonts/Inter-Bold.ttf'), 16)
-
-  if (!smallFont) {
-    return null
-  }
   return (
-    <Group>
-      <Text
-        x={55}
-        y={512}
-        text="Conquer the cryptoverse!"
-        font={smallFont}
-        color={'white'}
-      />
-    </Group>
+    <View style={{ paddingHorizontal: 16 }}>
+      <AvaText.Heading6
+        textStyle={{ fontSize: 16, lineHeight: 24, fontWeight: '700' }}>
+        Conquer the cryptoverse!
+      </AvaText.Heading6>
+    </View>
   )
 }
 
 export default function IntroScreen(): JSX.Element | null {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, paddingHorizontal: 16 }}>
+      <View style={{ marginTop: 290, zIndex: 1, position: 'absolute' }}>
+        <HowToUseTheCoreBrowser />
+        <Space y={16} />
+        <Row style={{ alignItems: 'flex-start' }}>
+          <View style={{ paddingLeft: 32 }}>
+            <SearchIcon />
+          </View>
+          <View style={{ flex: 1, marginRight: -16 }}>
+            <SearchText />
+          </View>
+        </Row>
+        <Space y={16} />
+        <Row style={{ alignItems: 'flex-start' }}>
+          <View style={{ paddingLeft: 32 }}>
+            <WalletConnectSVG color="white" />
+          </View>
+          <View style={{ flex: 1, marginRight: -16 }}>
+            <WalletConnectText />
+          </View>
+        </Row>
+        <Space y={16} />
+        <Row style={{ alignItems: 'flex-end' }}>
+          <View style={{ paddingLeft: 32 }}>
+            <CoreOwl width={24} height={24} />
+          </View>
+          <View style={{ flex: 1, marginRight: -16 }}>
+            <CoreOwlText />
+          </View>
+        </Row>
+        <Space y={16} />
+        <Row style={{ alignItems: 'flex-end' }}>
+          <View style={{ paddingLeft: 32 }}>
+            <RocketLaunch />
+          </View>
+          <View style={{ flex: 1, marginRight: -16 }}>
+            <RocketText />
+          </View>
+        </Row>
+      </View>
       <Canvas
         style={{
           flex: 1,
-          marginLeft: 16,
           marginTop: 70,
-          marginRight: 16,
           marginBottom: 80
         }}>
         <Group>
           <BlueBackground />
           <TokenImageWithGradient />
         </Group>
-        <HowToUseTheCoreBrowser />
-        <SearchIcon />
-        <SearchText />
-        <WalletConnectIcon />
-        <WalletConnectText />
-        <CoreOwlIcon />
-        <CoreOwlText />
-        <RocketIcon />
-        <RocketText />
       </Canvas>
       <View
         style={{
+          flex: 1,
           position: 'absolute',
+          alignContent: 'center',
           bottom: 0,
-          left: 32,
-          paddingBottom: 95,
-          width: '80%'
+          left: 16,
+          paddingHorizontal: 16,
+          paddingBottom: 45,
+          width: '100%'
         }}>
         <AvaButton.PrimaryLarge>Get started!</AvaButton.PrimaryLarge>
       </View>
--- storybook/stories/IntroScreen.stories.tsx
+++ storybook/stories/IntroScreen.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type { Meta } from '@storybook/react-native'
-import IntroScreen from 'screens/browser/IntroScreenStub'
+import IntroScreen from 'screens/browser/IntroScreen/IntroScreen'
 
 export default {
   title: 'Browser/IntroScreenStub'
