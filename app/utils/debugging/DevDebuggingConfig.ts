383072ce916836ea3710986fbad7e8ca8aced22b
Tyler Hackett <thackett86181@gmail.com>
2023-10-19 17:33:52 -0700

    fix logbox

--- app/utils/debugging/DevDebuggingConfig.ts
+++ app/utils/debugging/DevDebuggingConfig.ts
@@ -5,7 +5,7 @@
 const DevDebuggingConfig = {
   WDYR: false,
   STORYBOOK_ENABLED: false,
-  LOGBOX_DISABLED: true,
+  LOGBOX_DISABLED: false,
   REDSCREEN_DISABLED: false,
   LOGBOX_IGNORED_WARNINGS: [],
   SHOW_DEMO_NFTS: false,
