e09ee497c4bd745839275dd15d5dcbe7281fb091
An Nguyen <an.nguyen@avalabs.org>
2023-10-24 09:06:13 -0500

    CP-7549: Monorepo (#991)

---
+++ packages/core-mobile/e2e/pages/Stake/claim.page.ts
@@ -0,0 +1,67 @@
+import { expect as jestExpect } from 'expect'
+import claimScreenLoc from '../../locators/Stake/claimScreen.loc'
+import Assert from '../../helpers/assertions'
+import Actions from '../../helpers/actions'
+
+class ClaimPage {
+  get avaLogo() {
+    return by.id(claimScreenLoc.avaLogo)
+  }
+
+  get claimRewardsTitle() {
+    return by.text(claimScreenLoc.claimRewardsTitle)
+  }
+
+  get claimButton() {
+    return by.id(claimScreenLoc.claimNowButton)
+  }
+
+  get claimableAmountText() {
+    return by.text(claimScreenLoc.claimableAmountText)
+  }
+
+  get claimableAvaxAmount() {
+    return by.id(claimScreenLoc.claimableAvaxAmount)
+  }
+
+  get claimableBalanceCurrency() {
+    return by.id(claimScreenLoc.claimableBalanceCurrency)
+  }
+
+  get networkFeeAmount() {
+    return by.id(claimScreenLoc.networkFeeAmount)
+  }
+
+  get networkFeeCurrency() {
+    return by.id(claimScreenLoc.networkFeeCurrency)
+  }
+
+  get networkFeeText() {
+    return by.text(claimScreenLoc.networkFeeText)
+  }
+
+  async tapClaimNowButton() {
+    await Actions.tap(this.claimButton)
+  }
+
+  async verifyClaimRewardsScreenItems(topClaimableBalance: number | null) {
+    if (Actions.platform() === 'android') {
+      const claimableBalance = await Actions.balanceToNumber(
+        this.claimableAvaxAmount
+      )
+
+      jestExpect(claimableBalance).toBe(topClaimableBalance)
+    }
+    await Assert.isVisible(this.avaLogo)
+    await Assert.isVisible(this.claimRewardsTitle)
+    await Assert.isVisible(this.claimableAmountText)
+    await Assert.isVisible(this.networkFeeText)
+    await Assert.isVisible(this.claimButton)
+    await Assert.isVisible(this.claimableAvaxAmount)
+    await Assert.isVisible(this.claimableBalanceCurrency)
+    await Assert.isVisible(this.networkFeeAmount)
+    await Assert.isVisible(this.networkFeeCurrency)
+  }
+}
+
+export default new ClaimPage()
