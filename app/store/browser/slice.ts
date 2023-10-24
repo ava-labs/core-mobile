ab34fb07b80504468a5a5fadd54c03e973dfee05
Tyler Hackett <thackett86181@gmail.com>
2023-10-24 09:43:29 -0700

    minor update

--- app/store/browser/slice.ts
+++ app/store/browser/slice.ts
@@ -1,12 +1,9 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from 'store/index'
-import { ActiveTabId, BrowserState } from 'store/browser/types'
+import { ActiveTabId } from 'store/browser/types'
+import { initialState } from './types'
 
 const reducerName = 'browser'
-
-const initialState = {
-  activeTabId: '' as ActiveTabId
-} as BrowserState
 
 const browserSlice = createSlice({
   name: reducerName,
@@ -14,13 +11,17 @@ const browserSlice = createSlice({
   reducers: {
     setActiveTabId: (state, action: PayloadAction<ActiveTabId>) => {
       state.activeTabId = action.payload
-    }
+    },
+    clear: () => initialState
   }
 })
 
 // selectors
 export const selectActiveTabId = (state: RootState): string | undefined =>
   state.browser.activeTabId
+
+export const clearBrowser = (): { payload: undefined; type: string } =>
+  browserSlice.actions.clear()
 
 // actions
 export const { setActiveTabId } = browserSlice.actions
--- app/store/browser/types.ts
+++ app/store/browser/types.ts
@@ -2,6 +2,9 @@ export type TabId = string
 
 export type ActiveTabId = TabId
 
+export const initialState: BrowserState = {
+  activeTabId: ''
+}
 export interface BrowserState {
   activeTabId?: ActiveTabId
 }
