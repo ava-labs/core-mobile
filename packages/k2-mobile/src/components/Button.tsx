11063b39fe89a57fd52e5444b23c367efc550fdf
An Nguyen <an.nguyen@avalabs.org>
2023-10-25 10:19:07 -0500

    add a test button

---
+++ packages/k2-mobile/src/components/Button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Button as RNButton } from 'react-native'
+
+export const Button = (): JSX.Element => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  return <RNButton title="Test button" onPress={() => {}} />
+}
