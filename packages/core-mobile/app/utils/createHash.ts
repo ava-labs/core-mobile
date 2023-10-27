f498ab37b3a9ee5634da0ed31a529847b256be13
ruijialin <ruijia.lin@avalabs.org>
2023-10-27 14:56:28 -0400

    update sha256 to md5

--- packages/core-mobile/app/utils/createHash.ts
+++ packages/core-mobile/app/utils/createHash.ts
@@ -1,5 +1,5 @@
 import * as Crypto from 'crypto'
 
 export const createHash = (input: string): string => {
-  return Crypto.createHash('sha256').update(input).digest('hex')
+  return Crypto.createHash('md5').update(input).digest().toString()
 }
