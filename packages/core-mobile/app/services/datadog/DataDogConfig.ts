748a28007d187db0c589db8a822e3bdd45dd8928
ARTEM BESPALOV <40455398+artyombespalov@users.noreply.github.com>
2023-11-03 14:31:37 -0700

    process.env.CI added to ignore DD on local launches

--- packages/core-mobile/app/services/datadog/DataDogConfig.ts
+++ packages/core-mobile/app/services/datadog/DataDogConfig.ts
@@ -3,18 +3,13 @@ import Config from 'react-native-config'
 import DeviceInfo from 'react-native-device-info'
 
 let DataDogConfig: DdSdkReactNativeConfiguration | null = null
-let localLaunch = false
-
-if (DeviceInfo.getBuildNumber() === '1') {
-  localLaunch = true
-}
 
 if (
   Config.DD_CLIENT_TOKEN &&
   Config.ENVIRONMENT &&
   Config.DD_APPLICATION_ID &&
   Config.DD_SITE &&
-  !localLaunch
+  process.env.CI
 ) {
   DataDogConfig = new DdSdkReactNativeConfiguration(
     Config.DD_CLIENT_TOKEN,
