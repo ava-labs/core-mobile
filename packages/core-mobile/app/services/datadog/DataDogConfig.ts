31af346aac84355229b87acee977789aadf5608e
ARTEM BESPALOV <40455398+artyombespalov@users.noreply.github.com>
2023-11-03 14:59:12 -0700

    DataDog service added (#1006)
    Signed-off-by: ARTEM BESPALOV <40455398+artyombespalov@users.noreply.github.com>
    Co-authored-by: An Nguyen <an.nguyen@avalabs.org>

---
+++ packages/core-mobile/app/services/datadog/DataDogConfig.ts
@@ -0,0 +1,31 @@
+import { DdSdkReactNativeConfiguration } from '@datadog/mobile-react-native'
+import Config from 'react-native-config'
+import DeviceInfo from 'react-native-device-info'
+
+let DataDogConfig: DdSdkReactNativeConfiguration | null = null
+
+if (
+  Config.DD_CLIENT_TOKEN &&
+  Config.ENVIRONMENT &&
+  Config.DD_APPLICATION_ID &&
+  Config.DD_SITE &&
+  process.env.CI
+) {
+  DataDogConfig = new DdSdkReactNativeConfiguration(
+    Config.DD_CLIENT_TOKEN,
+    Config.ENVIRONMENT,
+    Config.DD_APPLICATION_ID,
+    true,
+    true,
+    true
+  )
+
+  DataDogConfig.site = Config.DD_SITE
+  DataDogConfig.nativeCrashReportEnabled = true
+  DataDogConfig.nativeViewTracking = true
+  DataDogConfig.sessionSamplingRate = 80
+  DataDogConfig.resourceTracingSamplingRate = 80
+  DataDogConfig.version = DeviceInfo.getBuildNumber()
+}
+
+export default DataDogConfig
