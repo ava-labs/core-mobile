88325553805f8c521a12c3e993ee8919ac4a7c65
neven-s <piaf1986@gmail.com>
2023-11-08 17:44:30 +0100

    CP-7508 Create Tab View screen (#1017)
    Co-authored-by: Tyler Hackett <thackett86181@gmail.com>

---
+++ packages/core-mobile/app/screens/browser/TabViewScreen.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View } from 'react-native'
+import { useSelector } from 'react-redux'
+import { selectActiveTab } from 'store/browser/slices/tabs'
+import { Text } from '@avalabs/k2-mobile'
+
+export default function TabViewScreen(): JSX.Element {
+  const activeTab = useSelector(selectActiveTab)
+  const showEmptyTab = (activeTab?.historyIds?.length ?? 0) === 0
+  const showWebView = !showEmptyTab
+
+  return (
+    <View>
+      {showEmptyTab && (
+        <View>
+          <Text variant="heading3">EmptyTab</Text>
+        </View>
+      )}
+      {showWebView && (
+        <View>
+          <Text variant="heading3">Browser</Text>
+        </View>
+      )}
+    </View>
+  )
+}
