3ec8f64daf082a52671524047ba14aabb23569ca
JungHwan Jang <onghwan@gmail.com>
2023-11-07 22:34:08 -0500

    CP-7679: Implement K2 Button component + Iconography (#1019)

---
+++ packages/k2-mobile/src/theme/tokens/Icons.stories.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import type { Meta } from '@storybook/react-native'
+import { FlatList } from 'react-native'
+import { SvgProps } from 'react-native-svg'
+import Link from '../../utils/Link'
+import { useTheme } from '../..'
+import { Icons } from './Icons'
+
+export default {
+  title: 'Icons'
+} as Meta
+
+export const All = (): JSX.Element => {
+  return (
+    <Link
+      title="Figma Source"
+      url="https://www.figma.com/file/hDSl4OUgXorDAtqPZtCUhB/K2-Foundation?type=design&node-id=376%3A4252&mode=design&t=HOIixbVhKpxGrRwG-1"
+    />
+  )
+}
+
+const Template = ({
+  icons,
+  resourceURL
+}: {
+  icons: React.FC<SvgProps>[]
+  resourceURL: string
+}): JSX.Element => {
+  const {
+    theme: { colors }
+  } = useTheme()
+  const renderItem = ({ item }: { item: React.FC<SvgProps> }): JSX.Element => {
+    const IconComponent = item
+
+    return <IconComponent color={colors.$neutral50} />
+  }
+
+  return (
+    <FlatList
+      contentContainerStyle={{ padding: 16 }}
+      data={icons}
+      numColumns={12}
+      renderItem={renderItem}
+      ListHeaderComponent={
+        <Link
+          title="Figma Source"
+          url={resourceURL}
+          style={{ marginBottom: 20 }}
+        />
+      }
+    />
+  )
+}
+
+export const Navigation = (): JSX.Element =>
+  Template({
+    icons: [Icons.Navigation.Check, Icons.Navigation.ExpandMore],
+    resourceURL:
+      'https://www.figma.com/file/hDSl4OUgXorDAtqPZtCUhB/K2-Foundation?type=design&node-id=378-5642&mode=design&t=HOIixbVhKpxGrRwG-4'
+  })
