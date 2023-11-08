8a08e1e57e0ba6adeab54bbfe10d1b9277499478
Neven <piaf1986@gmail.com>
2023-11-08 19:50:41 +0100

    Extract hooks from Browser

---
+++ packages/core-mobile/app/hooks/browser/useRecentWalletHack.ts
@@ -0,0 +1,133 @@
+export type RecentWalletHackScripts = {
+  injectCoreAsRecent: string
+  /**
+   * Use injectMetamaskAsRecent only for testing or demo
+   */
+  injectMetamaskAsRecent: string
+  /**
+   * Use injectLogRecentWallet if you need to find out what is stored in localStorage under WCM_RECENT_WALLET_DATA key.
+   * To use it put this string into WebView.injectedJavaScript, and watch for messages in WebView.onMessage
+   */
+  injectLogRecentWallet: string
+}
+
+/**
+ * This hook offers js scripts to be injected into WebView so that WalletConnect modal window shows our
+ * app on top of the list.
+ * The way it works is by putting stringified JSON into setting localStorage of visited website under WCM_RECENT_WALLET_DATA
+ * key. This data is read by
+ * [WalletConnect modal]{@link https://github.com/WalletConnect/modal/blob/95571fb4e96bd2a5c36214e657dc66aae0f1c8b4/packages/modal-ui/src/utils/UiUtil.ts#L146}
+ * and it sets this wallet as first in the list of supported wallets.
+ */
+export default function useRecentWalletHack(): RecentWalletHackScripts {
+  const coreMobileWalletConnectObject = `
+    {
+      id: 'f323633c1f67055a45aac84e321af6ffe46322da677ffdd32f9bc1e33bafe29c',
+      name: 'Core',
+      homepage:
+        'https://core.app/?utm_source=referral&utm_medium=website&utm_campaign=walletconnect',
+      image_id: '35f9c46e-cc57-4aa7-315d-e6ccb2a1d600',
+      order: 3230,
+      app: {
+        browser: null,
+        ios: 'https://apps.apple.com/us/app/core-crypto-wallet-nfts/id6443685999',
+        android:
+          'https://play.google.com/store/apps/details?id=com.avaxwallet&hl=en_US&gl=US',
+        mac: null,
+        windows: null,
+        linux: null,
+        chrome:
+          'https://chrome.google.com/webstore/detail/core-crypto-wallet-nft-ex/agoakfejjabomempkjlepdflaleeobhb',
+        firefox: null,
+        safari: null,
+        edge: null,
+        opera: null
+      },
+      injected: [
+        {
+          injected_id: 'isAvalanche',
+          namespace: 'eip155'
+        }
+      ],
+      rdns: null,
+      mobile: {
+        native: 'core://',
+        universal: 'https://core.app'
+      },
+      desktop: {
+        native: null,
+        universal: null
+      }
+    };
+  `
+
+  const metamaskWalletConnectObject = `
+    {
+      id: 'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+      name: 'MetaMask',
+      homepage: 'https://metamask.io/',
+      image_id: '5195e9db-94d8-4579-6f11-ef553be95100',
+      order: 10,
+      app: {
+        browser: null,
+        ios: 'https://apps.apple.com/us/app/metamask/id1438144202',
+        android: 'https://play.google.com/store/apps/details?id=io.metamask',
+        mac: null,
+        windows: null,
+        linux: null,
+        chrome:
+          'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
+        firefox: 'https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/',
+        safari: null,
+        edge: 'https://microsoftedge.microsoft.com/addons/detail/metamask/ejbalbakoplchlghecdalmeeeajnimhm?hl=en-US',
+        opera: 'https://addons.opera.com/en-gb/extensions/details/metamask-10/'
+      },
+      injected: [
+        {
+          injected_id: 'isMetaMask',
+          namespace: 'eip155'
+        }
+      ],
+      rdns: 'io.metamask',
+      mobile: {
+        native: 'metamask://',
+        universal: 'https://metamask.app.link'
+      },
+      desktop: {
+        native: null,
+        universal: null
+      }
+    };
+  `
+
+  const injectCoreAsRecent = `(async function(){ 
+    const wallet = ${coreMobileWalletConnectObject}
+    window.localStorage.setItem('WCM_RECENT_WALLET_DATA', JSON.stringify(coreWallet));
+    const recentWallet = window.localStorage.getItem('WCM_RECENT_WALLET_DATA');
+    window.ReactNativeWebView.postMessage(recentWallet)
+  })();`
+
+  const injectMetamaskAsRecent = `(async function(){ 
+    const wallet = ${metamaskWalletConnectObject}
+    window.localStorage.setItem('WCM_RECENT_WALLET_DATA', JSON.stringify(coreWallet));
+    const recentWallet = window.localStorage.getItem('WCM_RECENT_WALLET_DATA');
+    window.ReactNativeWebView.postMessage(recentWallet)
+  })();`
+
+  const injectLogRecentWallet = `(async function(){
+    let printRecentWallet = async function(){
+      const recentWallet = window.localStorage.getItem('WCM_RECENT_WALLET_DATA');
+      window.ReactNativeWebView.postMessage(recentWallet)
+      await new Promise(r => setTimeout(r, 2000));
+    }
+    while (true){
+      await printRecentWallet();
+    }
+  })();`
+
+  return {
+    injectCoreAsRecent,
+    injectMetamaskAsRecent,
+    injectLogRecentWallet
+  }
+}
--- packages/core-mobile/app/screens/browser/Browser.tsx
+++ packages/core-mobile/app/screens/browser/Browser.tsx
@@ -1,126 +1,40 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View } from '@avalabs/k2-mobile'
 import WebView from 'react-native-webview'
 import Logger from 'utils/Logger'
 import { useDispatch, useSelector } from 'react-redux'
 import { addHistoryForTab, selectActiveTab } from 'store/browser/slices/tabs'
-import Clipboard from '@react-native-clipboard/clipboard'
 import { useDeeplink } from 'contexts/DeeplinkContext/DeeplinkContext'
 import { DeepLink, DeepLinkOrigin } from 'contexts/DeeplinkContext/types'
-import { WebViewScrollEvent } from 'react-native-webview/lib/WebViewTypes'
 import { AddHistoryPayload } from 'store/browser'
 import InputText from 'components/InputText'
-
-const THROTTLE = 300
-const DEBOUNCE = 500
+import useClipboardWatcher from 'hooks/useClipboardWatcher'
+import useScrollHandler from 'hooks/browser/useScrollHandler'
+import useRecentWalletHack from 'hooks/browser/useRecentWalletHack'
 
 export default function Browser(): JSX.Element {
   const dispatch = useDispatch()
   const { setPendingDeepLink } = useDeeplink()
-  const [scrollDirection, setScrollDirection] = useState<
-    'up' | 'down' | 'idle'
-  >('idle')
   const activeTab = useSelector(selectActiveTab)
   const [urlEntry, setUrlEntry] = useState('')
   const [urlToLoad, setUrlToLoad] = useState('')
+  const clipboard = useClipboardWatcher()
+  const { scrollState, onScrollHandler } = useScrollHandler()
+  const { injectCoreAsRecent } = useRecentWalletHack()
 
   useEffect(() => {
-    Logger.trace('------> Browser:scrollDirection', scrollDirection)
-  }, [scrollDirection])
+    Logger.trace('TODO: pass this to Dock', scrollState)
+  }, [scrollState])
 
   useEffect(() => {
-    Clipboard.addListener(async () => {
-      const clipboard = await Clipboard.getString()
-      if (clipboard.startsWith('wc:')) {
-        setPendingDeepLink({
-          url: clipboard,
-          origin: DeepLinkOrigin.ORIGIN_QR_CODE
-        } as DeepLink)
-      }
-    })
-    return () => {
-      Clipboard.removeAllListeners()
+    //initiate deep link if user copies WC link to clipboard
+    if (clipboard.startsWith('wc:')) {
+      setPendingDeepLink({
+        url: clipboard,
+        origin: DeepLinkOrigin.ORIGIN_QR_CODE
+      } as DeepLink)
     }
-  }, [setPendingDeepLink])
-
-  const scrollHandler = useMemo(() => {
-    let lastY = 0
-    let lastCall = 0
-    let timeout: NodeJS.Timeout | undefined
-    return (event: WebViewScrollEvent) => {
-      const now = Date.now()
-      if (now - lastCall >= THROTTLE) {
-        if (timeout) {
-          clearTimeout(timeout)
-        }
-        timeout = setTimeout(() => {
-          setScrollDirection('idle')
-        }, DEBOUNCE)
-        lastCall = now
-      }
-      if (lastY !== 0) {
-        const direction = event.nativeEvent.contentOffset.y - lastY
-        direction > 0 ? setScrollDirection('down') : setScrollDirection('up')
-      }
-      lastY = event.nativeEvent.contentOffset.y
-    }
-  }, [])
-
-  // const getRecentWallet = `(async function(){
-  //   let printRecentWallet = async function(){
-  //     const recentWallet = window.localStorage.getItem('WCM_RECENT_WALLET_DATA');
-  //     window.ReactNativeWebView.postMessage(recentWallet)
-  //     await new Promise(r => setTimeout(r, 1000));
-  //   }
-  //   while (true){
-  //     await printRecentWallet();
-  //   }
-  // })();`
-
-  const setRecentWallet = `(async function(){ 
-    const coreWallet = {
-      id: 'f323633c1f67055a45aac84e321af6ffe46322da677ffdd32f9bc1e33bafe29c',
-      name: 'Core',
-      homepage:
-        'https://core.app/?utm_source=referral&utm_medium=website&utm_campaign=walletconnect',
-      image_id: '35f9c46e-cc57-4aa7-315d-e6ccb2a1d600',
-      order: 3230,
-      app: {
-        browser: null,
-        ios: 'https://apps.apple.com/us/app/core-crypto-wallet-nfts/id6443685999',
-        android:
-          'https://play.google.com/store/apps/details?id=com.avaxwallet&hl=en_US&gl=US',
-        mac: null,
-        windows: null,
-        linux: null,
-        chrome:
-          'https://chrome.google.com/webstore/detail/core-crypto-wallet-nft-ex/agoakfejjabomempkjlepdflaleeobhb',
-        firefox: null,
-        safari: null,
-        edge: null,
-        opera: null
-      },
-      injected: [
-        {
-          injected_id: 'isAvalanche',
-          namespace: 'eip155'
-        }
-      ],
-      rdns: null,
-      mobile: {
-        native: 'core://',
-        universal: 'https://core.app'
-      },
-      desktop: {
-        native: null,
-        universal: null
-      }
-    }
-  
-    window.localStorage.setItem('WCM_RECENT_WALLET_DATA', JSON.stringify(coreWallet));
-    const recentWallet = window.localStorage.getItem('WCM_RECENT_WALLET_DATA');
-    window.ReactNativeWebView.postMessage(recentWallet)
-  })();`
+  }, [clipboard, setPendingDeepLink])
 
   return (
     <View style={{ width: '100%', height: '100%' }}>
@@ -130,14 +44,9 @@ export default function Browser(): JSX.Element {
         onSubmit={() => setUrlToLoad(urlEntry)}
       />
       <WebView
-        injectedJavaScript={setRecentWallet}
+        injectedJavaScript={injectCoreAsRecent}
         source={{ uri: urlToLoad }}
-        onShouldStartLoadWithRequest={request => {
-          Logger.trace('WebView on should load', request.url)
-          return true
-        }}
         onNavigationStateChange={event => {
-          Logger.trace('------> onNavigationStateChange', event.navigationType)
           if (activeTab) {
             const history: AddHistoryPayload = {
               tabId: activeTab.id,
@@ -146,15 +55,11 @@ export default function Browser(): JSX.Element {
                 url: event.url
               }
             }
-            Logger.trace('------> Browser:add to history', history)
             dispatch(addHistoryForTab(history))
           }
         }}
         setSupportMultipleWindows={false}
-        onLoadStart={event => {
-          Logger.trace('------> Browser:onload start', event.nativeEvent.url)
-        }}
-        onScroll={scrollHandler}
+        onScroll={onScrollHandler}
         onError={event => {
           Logger.error('WebView onError', event.nativeEvent.description)
         }}
