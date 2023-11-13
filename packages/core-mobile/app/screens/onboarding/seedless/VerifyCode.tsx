01b39ce8c8c9c9a48e81eb9ce886c2183f8def9c
ruijialin <ruijia.lin@avalabs.org>
2023-11-13 09:26:54 -0500

    add button to verify code screen

--- packages/core-mobile/app/screens/onboarding/seedless/VerifyCode.tsx
+++ packages/core-mobile/app/screens/onboarding/seedless/VerifyCode.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Pressable,
   Text,
   TextInput,
@@ -34,6 +35,12 @@ export const VerifyCode = (): JSX.Element => {
     }
   }
 
+  const handleVerifyCode = (): void => {
+    // todo: if code is incorrect, show error
+    setShowError(true)
+    // todo: else go to next screen
+  }
+
   const textInputStyle = showError
     ? {
         borderColor: '$dangerLight',
@@ -43,53 +50,63 @@ export const VerifyCode = (): JSX.Element => {
 
   return (
     <BottomSheet snapPoints={['99%']}>
-      <View sx={{ marginHorizontal: 16 }}>
-        <View
-          sx={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center'
-          }}>
-          <Text variant="heading4">VerifyCode</Text>
-          <Pressable onPress={onGoBack}>
-            <ClearSVG
-              backgroundColor={alpha(colors.$neutral700, 0.5)}
-              color={colors.$neutral500}
-              size={30}
-            />
-          </Pressable>
-        </View>
-        <Space y={24} />
-        <Text variant="body1">
-          Enter the code generated from your authenticator app.
-        </Text>
-        <Space y={24} />
-        <TextInput
-          onChangeText={setCode}
-          keyboardType="number-pad"
-          keyboardAppearance="dark"
-          multiline
-          value={code}
-          sx={{
-            height: 116,
-            backgroundColor: alpha(colors.$neutral700, 0.5),
-            borderColor: '$dangerLight',
-            borderRadius: 8,
-            fontFamily: 'Inter-Bold',
-            fontSize: 34,
-            lineHeight: 44,
-            color: '$neutral50',
-            padding: 8,
-            ...textInputStyle
-          }}
-        />
-        {showError && (
-          <Text
-            variant="helperText"
-            sx={{ color: '$dangerLight', marginTop: 8 }}>
-            Incorrect code. Try again.
+      <View
+        sx={{ marginHorizontal: 16, justifyContent: 'space-between', flex: 1 }}>
+        <View>
+          <View
+            sx={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center'
+            }}>
+            <Text variant="heading4">VerifyCode</Text>
+            <Pressable onPress={onGoBack}>
+              <ClearSVG
+                backgroundColor={alpha(colors.$neutral700, 0.5)}
+                color={colors.$neutral500}
+                size={30}
+              />
+            </Pressable>
+          </View>
+          <Space y={24} />
+          <Text variant="body1">
+            Enter the code generated from your authenticator app.
           </Text>
-        )}
+          <Space y={24} />
+          <TextInput
+            onChangeText={setCode}
+            keyboardType="number-pad"
+            keyboardAppearance="dark"
+            multiline
+            value={code}
+            sx={{
+              height: 116,
+              backgroundColor: alpha(colors.$neutral700, 0.5),
+              borderColor: '$dangerLight',
+              borderRadius: 8,
+              fontFamily: 'Inter-Bold',
+              fontSize: 34,
+              lineHeight: 44,
+              color: '$neutral50',
+              padding: 8,
+              ...textInputStyle
+            }}
+          />
+          {showError && (
+            <Text
+              variant="helperText"
+              sx={{ color: '$dangerLight', marginTop: 8 }}>
+              Incorrect code. Try again.
+            </Text>
+          )}
+        </View>
+        <Button
+          type="primary"
+          size="large"
+          style={{ marginVertical: 16 }}
+          onPress={handleVerifyCode}>
+          Confirm
+        </Button>
       </View>
     </BottomSheet>
   )
