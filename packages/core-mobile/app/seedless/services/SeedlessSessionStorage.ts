8b619f94967b0f89ab1b07f51c1b020f0e4aa4d5
An Nguyen <an.nguyen@avalabs.org>
2023-11-15 17:37:12 -0600

    store signer session data in memory temporarily

--- packages/core-mobile/app/seedless/services/SeedlessSessionStorage.ts
+++ packages/core-mobile/app/seedless/services/SeedlessSessionStorage.ts
@@ -1,16 +1,16 @@
 import { SessionStorage, SignerSessionData } from '@cubist-dev/cubesigner-sdk'
 import { assertNotUndefined } from 'utils/assertions'
 
+let signerSessionData: SignerSessionData | undefined
+
 // TODO: This is a stub. It should be implemented.
 export class SeedlessTokenStorage implements SessionStorage<SignerSessionData> {
-  #data: SignerSessionData | undefined
-
   async save(data: SignerSessionData): Promise<void> {
-    this.#data = data
+    signerSessionData = data
   }
 
   async retrieve(): Promise<SignerSessionData> {
-    assertNotUndefined(this.#data)
-    return this.#data
+    assertNotUndefined(signerSessionData)
+    return signerSessionData
   }
 }
