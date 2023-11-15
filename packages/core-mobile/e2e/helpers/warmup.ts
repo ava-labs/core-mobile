c908ff933bd5aa4a39dd96984265f943cd7d5633
Tyler Hackett <thackett86181@gmail.com>
2023-11-15 09:32:36 -0800

    fix jailbrokenWarning index

--- packages/core-mobile/e2e/helpers/warmup.ts
+++ packages/core-mobile/e2e/helpers/warmup.ts
@@ -12,7 +12,7 @@ export const warmup = async () => {
   // if we are running Android e2e on Bitrise, we also need to handle the Jailbroken overlay
   if (process.env.E2E === 'true' && Action.platform() === Platform.Android) {
     console.log('Handling Jailbroken warning...')
-    await Assert.isVisible(CommonElsPage.jailbrokenWarning, 1)
+    await Assert.isVisible(CommonElsPage.jailbrokenWarning, 0)
     await Action.tapElementAtIndex(by.text('Ok'), 0)
     console.log('Jailbroken warning handled!!!')
   }
