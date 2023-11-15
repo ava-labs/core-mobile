af0b8b76e9dbac4b8cb138eea3c20476a9e01d3e
Tyler Hackett <thackett86181@gmail.com>
2023-11-15 08:14:52 -0800

    Qa fix e2e var failure android (#1032)

--- packages/core-mobile/e2e/helpers/warmup.ts
+++ packages/core-mobile/e2e/helpers/warmup.ts
@@ -1,19 +1,19 @@
 import { device } from 'detox'
 import CommonElsPage from '../pages/commonEls.page'
-import existingRecoveryPhrasePage from '../pages/existingRecoveryPhrase.page'
-import Assert from './assertions'
+import Assert from '../helpers/assertions'
 import Action from './actions'
 import { Platform } from './constants'
 
 export const warmup = async () => {
-  await device.launchApp({ permissions: { camera: 'YES' } })
+  await device.launchApp({
+    permissions: { notifications: 'YES', camera: 'YES' }
+  })
 
   // if we are running Android e2e on Bitrise, we also need to handle the Jailbroken overlay
-  if (Boolean(process.env.E2E) && Action.platform() === Platform.Android) {
-    await Assert.isVisible(CommonElsPage.jailbrokenWarning, 0)
-    await Action.tap(by.text('Ok'))
+  if (process.env.E2E === 'true' && Action.platform() === Platform.Android) {
+    console.log('Handling Jailbroken warning...')
+    await Assert.isVisible(CommonElsPage.jailbrokenWarning, 1)
+    await Action.tapElementAtIndex(by.text('Ok'), 0)
     console.log('Jailbroken warning handled!!!')
   }
-
-  await Assert.isVisible(existingRecoveryPhrasePage.alreadyHaveAWalletBtn, 0)
 }
