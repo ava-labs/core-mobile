253a7177923bd11b9a4c6c3b95bf4b505e74a090
Neven <piaf1986@gmail.com>
2023-11-16 17:34:08 +0100

    Implement SeedlessSessionStorage

--- packages/core-mobile/app/seedless/services/SeedlessSessionStorage.ts
+++ packages/core-mobile/app/seedless/services/SeedlessSessionStorage.ts
@@ -1,17 +1,18 @@
 import { SessionStorage, SignerSessionData } from '@cubist-dev/cubesigner-sdk'
 import { assertNotUndefined } from 'utils/assertions'
+import SecurityService, { KeySlot } from 'security/SecurityService'
 
-let signerSessionData: SignerSessionData | undefined
-
-// TODO: This is a stub. It should be implemented.
 export class SeedlessSessionStorage
   implements SessionStorage<SignerSessionData>
 {
   async save(data: SignerSessionData): Promise<void> {
-    signerSessionData = data
+    await SecurityService.store(KeySlot.SeedlessSessionStorage, data)
   }
 
   async retrieve(): Promise<SignerSessionData> {
+    const signerSessionData = await SecurityService.load<SignerSessionData>(
+      KeySlot.SeedlessSessionStorage
+    )
     assertNotUndefined(signerSessionData)
     return signerSessionData
   }
