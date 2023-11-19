fd286e467a32646d7b133fa67a7aba7d5961f169
An Nguyen <an.nguyen@avalabs.org>
2023-11-19 09:50:44 -0600

    add logging to assertNotUndefined

--- packages/core-mobile/app/utils/assertions.ts
+++ packages/core-mobile/app/utils/assertions.ts
@@ -1,10 +1,17 @@
+import Logger from './Logger'
+
 type NonUndefined<T> = T extends undefined ? never : T
 type NonNull<T> = T extends null ? never : T
 
 export function assertNotUndefined<T>(
   input: T
 ): asserts input is NonUndefined<T> {
-  if (input === undefined) throw new Error('input is undefined')
+  if (input === undefined) {
+    const message = 'input is undefined'
+    const error = new Error(message)
+    Logger.error(message, error)
+    throw error
+  }
 }
 
 export function assertNotNull<T>(
