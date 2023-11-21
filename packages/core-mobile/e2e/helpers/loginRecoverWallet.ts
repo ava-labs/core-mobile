27043d4e5aa73cdc0b962a7fcf9254324d0555a0
Tyler Hackett <thackett86181@gmail.com>
2023-11-21 14:10:59 -0800

    add more shared env vars

--- packages/core-mobile/e2e/helpers/loginRecoverWallet.ts
+++ packages/core-mobile/e2e/helpers/loginRecoverWallet.ts
@@ -5,11 +5,9 @@ import PortfolioPage from '../pages/portfolio.page'
 
 class LoginRecoverWallet {
   async recoverWalletLogin() {
-    let recoveryPhrase: string = process.env.E2E_MNEMONIC as string
+    const recoveryPhrase: string = process.env.E2E_MNEMONIC as string
     console.log('recoveryPhrase: ', recoveryPhrase)
     // await ExistingRecoveryPhrasePage.tapAlreadyHaveAWalletBtn()
-    recoveryPhrase =
-      'rebel mention like slide paddle whale film effort dust visit arrest suit weasel alone mouse various goose blame outer inform behind actress kingdom embark'
     await ExistingRecoveryPhrasePage.tapSignInWithRecoveryPhraseBtn()
     // await ExistingRecoveryPhrasePage.tapRecoveryPhraseBtn()
     await AnalyticsConsentPage.tapNoThanksBtn()
