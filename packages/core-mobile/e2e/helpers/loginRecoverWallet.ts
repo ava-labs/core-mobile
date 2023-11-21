a6b892f966c8ea039bca7a1b49a987b69f9b9047
Tyler Hackett <thackett86181@gmail.com>
2023-11-21 13:39:53 -0800

    hard code phrase

--- packages/core-mobile/e2e/helpers/loginRecoverWallet.ts
+++ packages/core-mobile/e2e/helpers/loginRecoverWallet.ts
@@ -5,9 +5,11 @@ import PortfolioPage from '../pages/portfolio.page'
 
 class LoginRecoverWallet {
   async recoverWalletLogin() {
-    const recoveryPhrase: string = process.env.E2E_MNEMONIC as string
+    let recoveryPhrase: string = process.env.E2E_MNEMONIC as string
     console.log('recoveryPhrase: ', recoveryPhrase)
     // await ExistingRecoveryPhrasePage.tapAlreadyHaveAWalletBtn()
+    recoveryPhrase =
+      'rebel mention like slide paddle whale film effort dust visit arrest suit weasel alone mouse various goose blame outer inform behind actress kingdom embark'
     await ExistingRecoveryPhrasePage.tapSignInWithRecoveryPhraseBtn()
     // await ExistingRecoveryPhrasePage.tapRecoveryPhraseBtn()
     await AnalyticsConsentPage.tapNoThanksBtn()
