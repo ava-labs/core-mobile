7cc8524a90d251a035a2c081eb3454b1bf07be4f
Junghwan Jang <onghwan@gmail.com>
2023-11-28 15:22:30 -0500

    add comment

--- packages/core-mobile/app/seedless/types.ts
+++ packages/core-mobile/app/seedless/types.ts
@@ -1,9 +1,13 @@
 export type MFA =
   | {
+      /** type for autenticator */
       type: 'totp'
     }
   | {
+      /** A unique credential id */
       id: string
+      /** A human-readable name given to the key */
       name: string
+      /** type for fido(i.e. passkey, yubikey) */
       type: 'fido'
     }
