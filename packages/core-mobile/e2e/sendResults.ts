f63c0190eec1eea802aa90c8a0e885172eed2a41
Tyler Hackett <thackett86181@gmail.com>
2023-12-06 15:13:59 -0800

    adjust testResultObject

--- packages/core-mobile/e2e/sendResults.ts
+++ packages/core-mobile/e2e/sendResults.ts
@@ -197,21 +197,19 @@ async function generatePlatformResults(
     for (let i = 0; i < resultArray.length; i++) {
       const resultObject = resultArray[i]
       const statusId = Number(resultObject?.status_id)
-      const payload = {
-        status_id: statusId,
-        comment: `Test case result for ${resultObject?.case_id} and has a status of ${statusId} for ${platform}`
-      }
+      const comment = `Test case result for ${resultObject?.case_id} and has a status of ${statusId} for ${platform}`
+
       if (resultObject) {
         const testResult = {
-          run_id: Number(runId),
           case_id: resultObject?.case_id,
-          payload
+          status_id: statusId,
+          comment: comment
         }
         testResults.push(testResult)
       }
     }
     // Send the results to testrail
-    await api.addResultsForCases(Number(runId), { testResults })
+    await api.addResultsForCases(Number(runId), testResults)
     console.log(testResults, ' is the result for testrail')
 
     // Adds the screenshot to the test case in testrail if the test failed
