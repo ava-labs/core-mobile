1353571c1ae1835c628eb80d9549cd6e58b115d8
An Nguyen <an.nguyen@avalabs.org>
2023-12-07 18:49:03 -0600

    Adjust Pin Screen UI (#1085)
    Co-authored-by: Junghwan Jang <onghwan@gmail.com>

--- packages/k2-mobile/src/theme/tokens/Icons.ts
+++ packages/k2-mobile/src/theme/tokens/Icons.ts
@@ -3,11 +3,15 @@ import IconExpandMore from '../../assets/icons/expand_more.svg'
 import IconQRCode from '../../assets/icons/qr_code.svg'
 import IconKey from '../../assets/icons/key.svg'
 import IconUSB from '../../assets/icons/usb.svg'
+import IconBackspace from '../../assets/icons/backspace.svg'
 
 export const Icons = {
   Communication: {
     IconKey: IconKey,
     IconQRCode: IconQRCode
+  },
+  Content: {
+    IconBackspace: IconBackspace
   },
   Device: {
     IconUSB: IconUSB
