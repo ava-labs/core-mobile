829ba8867ff05a3e5dcb668c329c9f442181996c
Neven <piaf1986@gmail.com>
2023-12-18 20:33:54 +0100

    Remove metamaskWalletConnectObject

--- packages/core-mobile/app/hooks/browser/useRecentWalletHack.ts
+++ packages/core-mobile/app/hooks/browser/useRecentWalletHack.ts
@@ -1,10 +1,6 @@
 export type RecentWalletHackScripts = {
   injectCoreAsRecent: string
   /**
-   * Use injectMetamaskAsRecent only for testing or demo
-   */
-  injectMetamaskAsRecent: string
-  /**
    * Use injectLogRecentWallet if you need to find out what is stored in localStorage under WCM_RECENT_WALLET_DATA key.
    * To use it put this string into WebView.injectedJavaScript, and watch for messages in WebView.onMessage
    */
@@ -61,54 +57,8 @@ export default function useRecentWalletHack(): RecentWalletHackScripts {
     };
   `
 
-  const metamaskWalletConnectObject = `
-    {
-      id: 'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-      name: 'MetaMask',
-      homepage: 'https://metamask.io/',
-      image_id: '5195e9db-94d8-4579-6f11-ef553be95100',
-      order: 10,
-      app: {
-        browser: null,
-        ios: 'https://apps.apple.com/us/app/metamask/id1438144202',
-        android: 'https://play.google.com/store/apps/details?id=io.metamask',
-        mac: null,
-        windows: null,
-        linux: null,
-        chrome:
-          'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
-        firefox: 'https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/',
-        safari: null,
-        edge: 'https://microsoftedge.microsoft.com/addons/detail/metamask/ejbalbakoplchlghecdalmeeeajnimhm?hl=en-US',
-        opera: 'https://addons.opera.com/en-gb/extensions/details/metamask-10/'
-      },
-      injected: [
-        {
-          injected_id: 'isMetaMask',
-          namespace: 'eip155'
-        }
-      ],
-      rdns: 'io.metamask',
-      mobile: {
-        native: 'metamask://',
-        universal: 'https://metamask.app.link'
-      },
-      desktop: {
-        native: null,
-        universal: null
-      }
-    };
-  `
-
   const injectCoreAsRecent = `(async function(){ 
     const wallet = ${coreMobileWalletConnectObject}
-    window.localStorage.setItem('WCM_RECENT_WALLET_DATA', JSON.stringify(wallet));
-    const recentWallet = window.localStorage.getItem('WCM_RECENT_WALLET_DATA');
-    window.ReactNativeWebView.postMessage(recentWallet)
-  })();`
-
-  const injectMetamaskAsRecent = `(async function(){ 
-    const wallet = ${metamaskWalletConnectObject}
     window.localStorage.setItem('WCM_RECENT_WALLET_DATA', JSON.stringify(wallet));
     const recentWallet = window.localStorage.getItem('WCM_RECENT_WALLET_DATA');
     window.ReactNativeWebView.postMessage(recentWallet)
@@ -127,7 +77,6 @@ export default function useRecentWalletHack(): RecentWalletHackScripts {
 
   return {
     injectCoreAsRecent,
-    injectMetamaskAsRecent,
     injectLogRecentWallet
   }
 }
