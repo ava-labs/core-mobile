903190a79135497bbd52a4beda092300da8a8cae
Neven <piaf1986@gmail.com>
2023-12-19 19:43:10 +0100

    Connect Dock and Browser

--- packages/core-mobile/app/screens/browser/TabViewScreen.tsx
+++ packages/core-mobile/app/screens/browser/TabViewScreen.tsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View } from 'react-native'
 import { useSelector } from 'react-redux'
 import { selectActiveTab } from 'store/browser/slices/tabs'
 import Browser from 'screens/browser/Browser'
-import { EmptyTab } from './components/EmptyTab'
+import { Dock } from 'screens/browser/components/Dock'
+import { ScrollState } from 'hooks/browser/useScrollHandler'
+import { EmptyTab } from 'screens/browser/components/EmptyTab'
 
 export default function TabViewScreen(): JSX.Element {
   const activeTab = useSelector(selectActiveTab)
   const showEmptyTab = (activeTab?.historyIds?.length ?? 0) === 0
   const showWebView = !showEmptyTab
+  const [dockVisible, setDockVisible] = useState(true)
+
+  function onNewScrollState(scrollState: ScrollState): void {
+    switch (scrollState) {
+      case ScrollState.down:
+        setDockVisible(false)
+        break
+      case ScrollState.up:
+      case ScrollState.idle:
+        setDockVisible(true)
+        break
+    }
+  }
 
   return (
     <View>
       {showEmptyTab && <EmptyTab />}
-      {showWebView && <Browser />}
+      {showWebView && <Browser onNewScrollState={onNewScrollState} />}
+      {dockVisible && <Dock />}
     </View>
   )
 }
