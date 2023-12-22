f9d4d1ca5f4c2d3d1a2e0db9dfba3a41bed71781
Tyler Hackett <thackett86181@gmail.com>
2023-12-22 15:24:12 -0800

    log titlearray from test run

--- packages/core-mobile/e2e/generateTestrailObjects.ts
+++ packages/core-mobile/e2e/generateTestrailObjects.ts
@@ -574,7 +574,7 @@ export async function getTestCasesFromRun(runId: number): Promise<object[]> {
       status_id: testResult
     })
   }
-  console.log(titleArray, ' this is the title array')
+  console.log(JSON.stringify(titleArray), ' this is the title array')
   return titleArray
 }
 
