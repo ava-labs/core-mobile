e5a732b8ec91e91a6988ca3101a5a71ef06a3636
Neven <piaf1986@gmail.com>
2024-01-17 16:42:32 +0100

    Add DividerLine to k2

---
+++ packages/k2-mobile/src/components/DividerLine/DividerLine.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import type { Meta } from '@storybook/react-native'
+import { ScrollView, View, Text } from '../Primitives'
+import Link from '../../utils/Link'
+import { DividerLine } from './DividerLine'
+
+export default {
+  title: 'DividerLine'
+} as Meta
+
+export const All = (): JSX.Element => {
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        padding: 16,
+        flex: 1
+      }}>
+      <Link
+        title="Figma Source"
+        url={FIGMA_LINK}
+        style={{ marginBottom: 20 }}
+      />
+      <View>
+        <Text>Regular</Text>
+        <DividerLine />
+      </View>
+      <View>
+        <Text>Prominent</Text>
+        <DividerLine type={'prominent'} />
+      </View>
+    </ScrollView>
+  )
+}
+
+const FIGMA_LINK =
+  'https://www.figma.com/file/TAXtaoLGSNNt8nAqqcYH2H/K2-Component-Library?type=design&node-id=1596-32923&mode=design&t=jtcHUoKrPX76W8SZ-4'
