d5d3ff03d8c4c0c8c012ee5e9ea6d7cb884f0521
Junghwan Jang <onghwan@gmail.com>
2024-02-06 14:19:19 -0500

    move PolyfillCrypto to WalletScreenStack

--- packages/core-mobile/app/utils/hpke.ts
+++ packages/core-mobile/app/utils/hpke.ts
@@ -1,12 +1,9 @@
-import React from 'react'
 import {
   CipherSuite,
   DhkemP521HkdfSha512,
   HkdfSha512,
   Aes256Gcm
 } from '@hpke/core'
-import RootSiblingsManager from 'react-native-root-siblings'
-import PolyfillCrypto from 'react-native-webview-crypto'
 
 const suite = new CipherSuite({
   kem: new DhkemP521HkdfSha512(),
@@ -19,8 +16,6 @@ export async function encrypt(
   key: string,
   keyID: string
 ): Promise<{ encrypted: string; enc: string }> {
-  const rootNode = new RootSiblingsManager(<PolyfillCrypto />)
-
   const publicKey = await suite.kem.deserializePublicKey(
     Buffer.from(key, 'base64')
   )
@@ -35,8 +30,6 @@ export async function encrypt(
 
   const encrypted = Buffer.from(ct).toString('base64')
   const enc = Buffer.from(sender.enc).toString('base64')
-
-  rootNode?.destroy()
 
   return { encrypted, enc }
 }
