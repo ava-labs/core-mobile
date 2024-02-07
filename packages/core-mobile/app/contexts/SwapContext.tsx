44ebaa12383f3d00cd7dec157dd83699dd26d7b2
Junghwan Jang <onghwan@gmail.com>
2024-02-07 16:05:19 -0500

    code review

--- packages/core-mobile/app/contexts/SwapContext.tsx
+++ packages/core-mobile/app/contexts/SwapContext.tsx
@@ -218,7 +218,7 @@ export const SwapContextProvider = ({
           slippage: swapSlippage,
           activeNetwork,
           provider: avalancheProvider,
-          transactionSend: async signedTx =>
+          transactionSend: signedTx =>
             NetworkService.sendTransaction(signedTx, activeNetwork),
           transactionSign: tx =>
             WalletService.sign(tx, activeAccount.index, activeNetwork),
