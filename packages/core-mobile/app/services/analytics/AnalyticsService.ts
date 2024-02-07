19b8e7fc8efc77a75ed150cb3a16366347dd9c95
Junghwan Jang <onghwan@gmail.com>
2024-02-07 09:52:28 -0500

    move encrypt function into AnalyticsService

--- packages/core-mobile/app/services/analytics/AnalyticsService.ts
+++ packages/core-mobile/app/services/analytics/AnalyticsService.ts
@@ -1,7 +1,12 @@
 import PostHogService from 'services/posthog/PostHogService'
 import { AnalyticsEvents } from 'types/analytics'
-import { encrypt } from 'utils/hpke'
 import Config from 'react-native-config'
+import {
+  CipherSuite,
+  DhkemP521HkdfSha512,
+  HkdfSha512,
+  Aes256Gcm
+} from '@hpke/core'
 import { AnalyticsEventName, CaptureEventProperties } from './types'
 
 if (!Config.ANALYTICS_ENCRYPTION_KEY) {
@@ -22,6 +27,11 @@ const ANALYTICS_ENCRYPTION_KEY_ID = Config.ANALYTICS_ENCRYPTION_KEY_ID
 
 class AnalyticsService {
   private isEnabled: boolean | undefined
+  private suite = new CipherSuite({
+    kem: new DhkemP521HkdfSha512(),
+    kdf: new HkdfSha512(),
+    aead: new Aes256Gcm()
+  })
 
   setEnabled(isEnabled: boolean): void {
     this.isEnabled = isEnabled
@@ -47,17 +57,38 @@ class AnalyticsService {
     }
 
     const stringifiedProperties = JSON.stringify(properties)
-    const { encrypted, enc } = await encrypt(
-      stringifiedProperties,
-      ANALYTICS_ENCRYPTION_KEY,
-      ANALYTICS_ENCRYPTION_KEY_ID
-    )
+    const { encrypted, enc } = await this.encrypt(stringifiedProperties)
 
     return PostHogService.capture(eventName, {
       data: encrypted,
       enc,
       keyID: ANALYTICS_ENCRYPTION_KEY_ID
     })
+  }
+
+  private async encrypt(
+    message: string
+  ): Promise<{ encrypted: string; enc: string }> {
+    if (!crypto.subtle) {
+      throw new Error('crypto.subtle is not available')
+    }
+
+    const deserializedPublicKey = await this.suite.kem.deserializePublicKey(
+      Buffer.from(ANALYTICS_ENCRYPTION_KEY, 'base64')
+    )
+
+    const sender = await this.suite.createSenderContext({
+      recipientPublicKey: deserializedPublicKey
+    })
+
+    const aad = new TextEncoder().encode(ANALYTICS_ENCRYPTION_KEY_ID)
+    const data = new TextEncoder().encode(message)
+    const ct = await sender.seal(data, aad)
+
+    const encrypted = Buffer.from(ct).toString('base64')
+    const enc = Buffer.from(sender.enc).toString('base64')
+
+    return { encrypted, enc }
   }
 }
 
