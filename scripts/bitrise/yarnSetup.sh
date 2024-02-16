7c50b3591e43c0ef0b5f21ca37b276d4bb2d8c8c
An Nguyen <an.nguyen@avalabs.org>
2024-02-16 09:22:49 -0600

    CP-8203: Adjust internal builds to point to seedless gamma (#1191)

--- scripts/bitrise/yarnSetup.sh
+++ scripts/bitrise/yarnSetup.sh
@@ -4,8 +4,6 @@ set -e
 # make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
 
 set -o pipefail
-# debug log
-set -x
 
 if ! cat /etc/issue 2>/dev/null
 then
