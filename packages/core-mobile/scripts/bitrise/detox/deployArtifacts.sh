2ae10357bc85c34d9eb9871cbf0a8ae537a753d8
An Nguyen <an.nguyen@avalabs.org>
2024-02-16 09:22:49 -0600

    CP-8203: Adjust internal builds to point to seedless gamma (#1191)

--- packages/core-mobile/scripts/bitrise/detox/deployArtifacts.sh
+++ packages/core-mobile/scripts/bitrise/detox/deployArtifacts.sh
@@ -3,8 +3,6 @@
 set -e
 # make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
 set -o pipefail
-# debug log
-set -x
 
 if [ "$PLATFORM" = "iOS" ]; then
   cp -R e2e/artifacts/ios $BITRISE_DEPLOY_DIR/detox    
