442959793f9545e30d463f7e788e896b781c03d1
Tyler Hackett <thackett86181@gmail.com>
2024-03-12 14:01:01 -0700

    setup android script for ci

--- packages/core-mobile/e2e/tests/plusIcon/walletConnect/android_core_web_test_runner.sh
+++ packages/core-mobile/e2e/tests/plusIcon/walletConnect/android_core_web_test_runner.sh
@@ -4,4 +4,4 @@ npx playwright install
 
 npx playwright test e2e/tests/playwright/core.spec.ts --project='firefox'
 
-node_modules/.bin/detox test dappIntegrations.e2e.ts --configuration android.internal.debug  --loglevel trace
+node_modules/.bin/detox test dappIntegrations.e2e.ts --configuration android.internal.release.ci 
