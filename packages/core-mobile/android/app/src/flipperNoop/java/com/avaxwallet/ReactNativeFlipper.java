9ddd3e74455121ef8a70dfbabebfa203efe006d6
An Nguyen <an.nguyen@avalabs.org>
2024-03-13 09:43:38 -0500

    got android to build and bundle with latest RN

--- packages/core-mobile/android/app/src/flipperNoop/java/com/avaxwallet/ReactNativeFlipper.java
+++ packages/core-mobile/android/app/src/flipperNoop/java/com/avaxwallet/ReactNativeFlipper.java
@@ -1,16 +1,16 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
- * directory of this source tree.
- */
-package com.avaxwallet;
-
-import android.content.Context;
-import com.facebook.react.ReactInstanceManager;
-
-public class ReactNativeFlipper {
-    public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
-        // Do nothing
-    }
-}
+///**
+// * Copyright (c) Meta Platforms, Inc. and affiliates.
+// *
+// * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
+// * directory of this source tree.
+// */
+//package com.avaxwallet;
+//
+//import android.content.Context;
+//import com.facebook.react.ReactInstanceManager;
+//
+//public class ReactNativeFlipper {
+//    public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+//        // Do nothing
+//    }
+//}
--- packages/core-mobile/metro.config.js
+++ packages/core-mobile/metro.config.js
@@ -1,8 +1,79 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
+const { createSentryMetroSerializer } = require('@sentry/react-native/metro')
 const merge = require('lodash.merge')
-const { getBaseConfig } = require('./metro.base.config')
-const monorepoConfig = require('./metro.monorepo.config')
 
-module.exports = (async () => {
-  const baseConfig = await getBaseConfig()
-  return merge(baseConfig, monorepoConfig)
-})()
+const monorepoConfig = require('./metro.monorepo.config')
+const defaultConfig = getDefaultConfig(__dirname)
+const { assetExts, sourceExts } = defaultConfig.resolver
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const baseConfig = {
+  resetCache: true,
+  serializer: {
+    customSerializer: createSentryMetroSerializer()
+  },
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer')
+  },
+  resolver: {
+    extraNodeModules: {
+      // this is for any modules that use require('crypto')
+      crypto: require.resolve('react-native-quick-crypto')
+    },
+    // sbmodern is needed for storybook
+    resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+    resolveRequest: (context, moduleName, platform) => {
+      if (moduleName.startsWith('@ledgerhq/cryptoassets')) {
+        return context.resolveRequest(
+          context,
+          moduleName.replace(
+            '@ledgerhq/cryptoassets',
+            '@ledgerhq/cryptoassets/lib-es'
+          ),
+          platform
+        )
+      }
+      if (moduleName.startsWith('@ledgerhq/domain-service')) {
+        return context.resolveRequest(
+          context,
+          moduleName.replace(
+            '@ledgerhq/domain-service',
+            '@ledgerhq/domain-service/lib-es'
+          ),
+          platform
+        )
+      }
+      if (moduleName.startsWith('@ledgerhq/evm-tools')) {
+        return context.resolveRequest(
+          context,
+          moduleName.replace(
+            '@ledgerhq/evm-tools',
+            '@ledgerhq/evm-tools/lib-es'
+          ),
+          platform
+        )
+      }
+      if (moduleName.startsWith('@ledgerhq/live-network')) {
+        return context.resolveRequest(
+          context,
+          moduleName.replace(
+            '@ledgerhq/live-network',
+            '@ledgerhq/live-network/lib-es'
+          ),
+          platform
+        )
+      }
+      // optionally, chain to the standard Metro resolver.
+      return context.resolveRequest(context, moduleName, platform)
+    }
+  }
+}
+
+module.exports = mergeConfig(defaultConfig, merge(baseConfig, monorepoConfig))
