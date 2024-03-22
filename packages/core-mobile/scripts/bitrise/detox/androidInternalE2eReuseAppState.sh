21ba04fc46e0c7dc6eaf2368c6c0314b4c5fca3a
Tyler Hackett <thackett86181@gmail.com>
2024-03-22 14:06:55 -0700

    log tests to run

--- packages/core-mobile/scripts/bitrise/detox/androidInternalE2eReuseAppState.sh
+++ packages/core-mobile/scripts/bitrise/detox/androidInternalE2eReuseAppState.sh
@@ -11,7 +11,7 @@ adb install -r $BITRISE_TEST_APK_PATH
 
 ./node_modules/.bin/detox test --listTests --configuration android.internal.release.smoke.reuse_state.ci
 
-QT_QPA_PLATFORM=xcb; ./node_modules/.bin/detox test --configuration android.internal.release.smoke.reuse_state.ci --reuse --headless --retries 1 --loglevel trace; test_result=$?
+QT_QPA_PLATFORM=xcb; ./node_modules/.bin/detox test --configuration android.internal.release.smoke.reuse_state.ci --headless --loglevel trace; test_result=$?
 
 if ((test_result != 0)); then
   exit 1
