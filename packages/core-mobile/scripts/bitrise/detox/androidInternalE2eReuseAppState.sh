8da47de11934bc30e8a54365df7f3801a734702c
Tyler Hackett <thackett86181@gmail.com>
2024-03-25 09:11:09 -0700

    add debug sleep

--- packages/core-mobile/scripts/bitrise/detox/androidInternalE2eReuseAppState.sh
+++ packages/core-mobile/scripts/bitrise/detox/androidInternalE2eReuseAppState.sh
@@ -9,7 +9,7 @@ npm rebuild detox
 
 adb install -r $BITRISE_TEST_APK_PATH
 
-./node_modules/.bin/detox test --listTests --configuration android.internal.release.smoke.reuse_state.ci
+./node_modules/.bin/detox test --listTests --configuration android.internal.release.smoke.reuse_state.ci && sleep 999999999
 
 QT_QPA_PLATFORM=xcb; ./node_modules/.bin/detox test --configuration android.internal.release.smoke.reuse_state.ci --headless --loglevel trace; test_result=$?
 
