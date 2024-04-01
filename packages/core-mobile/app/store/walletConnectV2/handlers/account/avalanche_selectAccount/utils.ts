9898f165031aa41062ea705677008b88edc6a3c9
ruijialin <ruijia.lin@avalabs.org>
2024-04-01 13:10:58 -0400

    fixed addPermissionValidatorTx

--- packages/core-mobile/app/store/walletConnectV2/handlers/account/avalanche_selectAccount/utils.ts
+++ packages/core-mobile/app/store/walletConnectV2/handlers/account/avalanche_selectAccount/utils.ts
@@ -10,16 +10,19 @@ export const accountSchema = z.object({
   addressCoreEth: z.string().optional()
 })
 
-const paramsSchema = z.tuple([z.number().nonnegative()])
+const paramsSchema = z.tuple([z.coerce.number().nonnegative()])
 
 const approveDataSchema = z.object({
   account: accountSchema
 })
 
-export const parseRequestParams = (params: unknown) => {
+export const parseRequestParams = (
+  params: unknown
+): z.SafeParseReturnType<[number], [number]> => {
   return paramsSchema.safeParse(params)
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const parseApproveData = (data: unknown) => {
   return approveDataSchema.safeParse(data)
 }
