585f1409e5764423f3aa6db6dd00e6cc6a0f454b
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2024-04-02 11:02:04 -0400

    Fixed addPermissionValidatorTx (#1238)

--- packages/core-mobile/app/store/walletConnectV2/handlers/avalanche_sendTransaction/utils.ts
+++ packages/core-mobile/app/store/walletConnectV2/handlers/avalanche_sendTransaction/utils.ts
@@ -4,9 +4,11 @@ const paramsSchema = z.object({
   transactionHex: z.string(),
   chainAlias: z.enum(['X', 'P', 'C']),
   externalIndices: z.number().array().optional(),
-  internalIndices: z.number().array().optional()
+  internalIndices: z.number().array().optional(),
+  utxos: z.string().array().optional()
 })
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const parseRequestParams = (params: unknown) => {
   return paramsSchema.safeParse(params)
 }
