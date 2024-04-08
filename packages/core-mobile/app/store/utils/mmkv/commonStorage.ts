2089562a6dbd284cd273878bb5f1995c57f72a11
ruijialin <ruijia.lin@avalabs.org>
2024-04-08 10:11:33 -0400

    update asyncStorage migration logic

--- packages/core-mobile/app/store/utils/mmkv/commonStorage.ts
+++ packages/core-mobile/app/store/utils/mmkv/commonStorage.ts
@@ -1,5 +1,8 @@
 import { MMKV } from 'react-native-mmkv'
+import { SECURE_ACCESS_SET } from 'resources/Constants'
 import { baseStorage } from './baseStorage'
+
+export const commonStorageKeys = ['POSTHOG_SUSPENDED', SECURE_ACCESS_SET]
 
 const storage = new MMKV({
   id: `common`
