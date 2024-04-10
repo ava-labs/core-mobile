e863dc9a969ade1257135175a9f090c0fa25d730
Tyler Hackett <thackett86181@gmail.com>
2024-04-10 13:47:46 -0700

    reduce workers to 2

--- packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
@@ -4,11 +4,16 @@
 set -o pipefail
 
 npm rebuild detox
-./node_modules/.bin/detox test --maxWorkers 3 --configuration ios.internal.release.smoke.ci --retries 1; test_result=$?
+./node_modules/.bin/detox test --maxWorkers 2 --configuration ios.internal.release.smoke.ci --retries 1; test_result=$?
 
-RUN_ID=$(head -n 1 ./e2e/testrailRunId.txt) 
+if test -f ./e2e/testrailRunId.txt; then
+  echo "TestRail run id file exists"
+  RUN_ID=$(head -n 1 ./e2e/testrailRunId.txt) 
+  envman add --key TESTRAIL_RUN_ID --value $RUN_ID
+else
+  echo "TestRail run id file does not exist"
+fi
 
-envman add --key TESTRAIL_RUN_ID --value $RUN_ID
 
 if ((test_result != 0)); then
   exit 1
