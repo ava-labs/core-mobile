f0448a666eb8f26316902f9b93439ad0309dce25
ruijialin <ruijia.lin@avalabs.org>
2024-04-17 10:16:55 -0400

    move listener to reactueryProcier

---
+++ packages/core-mobile/app/hooks/networks/useNetworksListener.ts
@@ -0,0 +1,29 @@
+import { addListener, isAnyOf } from '@reduxjs/toolkit'
+import { ReactQueryKeys } from 'consts/reactQueryKeys'
+import { queryClient } from 'contexts/ReactQueryProvider'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { onAppUnlocked } from 'store/app'
+import { onNetworksFetched } from 'store/network'
+import { toggleDeveloperMode } from 'store/settings/advanced'
+
+export const useNetworksListener = (): void => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = dispatch(
+      addListener({
+        matcher: isAnyOf(toggleDeveloperMode, onAppUnlocked),
+        effect: async () => {
+          await queryClient.invalidateQueries({
+            queryKey: [ReactQueryKeys.NETWORKS]
+          })
+          dispatch(onNetworksFetched)
+        }
+      })
+    )
+    return () => {
+      unsubscribe
+    }
+  }, [dispatch])
+}
