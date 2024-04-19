27aaec17ea66fafffa97dbf692de6cfa953b2c1e
Tyler Hackett <thackett86181@gmail.com>
2024-04-19 13:52:11 -0700

    fail activity tab test

--- packages/core-mobile/e2e/tests/activityTab/activityTab.e2e.smoke.ts
+++ packages/core-mobile/e2e/tests/activityTab/activityTab.e2e.smoke.ts
@@ -12,7 +12,7 @@ describe('Activity Tab', () => {
 
   it('should show contract call only in activity list', async () => {
     await PortfolioPage.tapAvaxNetwork()
-    await PortfolioPage.tapActivityTab()
+    // await PortfolioPage.tapActivityTab()
     await actions.waitForElement(ActivityTabPage.arrowSVG, 10000, 1)
     await ActivityTabPage.tapFilterDropdown()
     await ActivityTabPage.tapContractCallFilterOption()
