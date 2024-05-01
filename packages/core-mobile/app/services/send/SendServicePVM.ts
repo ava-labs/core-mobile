6dfc95e51c81724631805c0dd08fe801b0ebee47
Neven <piaf1986@gmail.com>
2024-05-01 15:43:23 +0200

    Extract getInternalExternalAddrs to util file

--- packages/core-mobile/app/services/send/SendServicePVM.ts
+++ packages/core-mobile/app/services/send/SendServicePVM.ts
@@ -11,7 +11,8 @@ import SentryWrapper from 'services/sentry/SentryWrapper'
 import WalletService from 'services/wallet/WalletService'
 import { Avax } from 'types'
 import { Avalanche } from '@avalabs/wallets-sdk'
-import { networkIDs, utils, Utxo } from '@avalabs/avalanchejs'
+import { utils } from '@avalabs/avalanchejs'
+import { getInternalExternalAddrs } from 'services/send/utils'
 import { AvalancheTxParams } from 'store/rpc/handlers/avalanche_sendTransaction/avalanche_sendTransaction'
 import { GAS_LIMIT_FOR_XP_CHAIN } from 'consts/fees'
 
@@ -138,70 +139,4 @@ export class SendServicePVM {
       canSubmit: false
     }
   }
-}
-
-type SearchSpace = 'i' | 'e'
-type XPAddressData = { index: number; space: SearchSpace }
-type XPAddressDictionary = Record<string, XPAddressData>
-
-const getHrp = (isTestnet: boolean): string =>
-  isTestnet ? networkIDs.FujiHRP : networkIDs.MainnetHRP
-
-/**
- * Formats an `avax…` or `fuji…` style Avalanche address for the correct network.
- * This will throw errors for `['C-', 'X-', 'P-']` prefixed addresses.
- */
-const formatAvalancheAddress = (
-  addressString: string,
-  isTestnet: boolean
-): string => {
-  const [, bytes] = utils.parseBech32(addressString)
-  const hrp = getHrp(isTestnet)
-
-  // Safe cast because otherwise the parse above would throw.
-  return utils.formatBech32(hrp, bytes)
-}
-
-const getInternalExternalAddrs = (
-  utxos: Utxo[],
-  xpAddressDict: XPAddressDictionary,
-  isTestnet: boolean
-): {
-  externalIndices: number[]
-  internalIndices: number[]
-} => {
-  const utxosAddrs = new Set<string>(
-    utxos.flatMap(utxo =>
-      utxo
-        .getOutputOwners()
-        .addrs.map(String)
-        .map(addressString => formatAvalancheAddress(addressString, isTestnet))
-    )
-  )
-
-  return [...utxosAddrs].reduce(
-    (accumulator, address) => {
-      // This can happen when the CoreEth address owns a UTXO.
-      const xpAddressDictElement = xpAddressDict[address]
-      if (xpAddressDictElement === undefined) {
-        return accumulator
-      }
-      const { space, index } = xpAddressDictElement
-
-      return {
-        internalIndices: [
-          ...accumulator.internalIndices,
-          ...(space === 'i' ? [index] : [])
-        ],
-        externalIndices: [
-          ...accumulator.externalIndices,
-          ...(space === 'e' ? [index] : [])
-        ]
-      }
-    },
-    {
-      externalIndices: [] as number[],
-      internalIndices: [] as number[]
-    }
-  )
 }
