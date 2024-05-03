16c0f091c67a2b1324ebd93b1e0f20bec3e4354a
Tyler Hackett <thackett86181@gmail.com>
2024-05-03 08:37:34 -0700

    Qa fix change pin test (#1285)

--- packages/core-mobile/e2e/tests/settings/changePin.e2e.smoke.ts
+++ packages/core-mobile/e2e/tests/settings/changePin.e2e.smoke.ts
@@ -9,6 +9,8 @@ import { warmup } from '../../helpers/warmup'
 import CreatePinPage from '../../pages/createPin.page'
 import SecurityAndPrivacyPage from '../../pages/burgerMenu/securityAndPrivacy.page'
 import portfolioPage from '../../pages/portfolio.page'
+import commonElsPage from '../../pages/commonEls.page'
+import actions from '../../helpers/actions'
 
 describe('Change Pin', () => {
   beforeAll(async () => {
@@ -26,6 +28,15 @@ describe('Change Pin', () => {
     await CreatePinPage.createNewPin()
     await Assert.isVisible(BurgerMenuPage.securityAndPrivacy)
     await device.reloadReactNative()
+    if (device.getPlatform() === 'android') {
+      await Assert.isVisible(commonElsPage.jailbrokenWarning, 0)
+      await actions.tapElementAtIndex(by.text('Ok'), 0)
+      await actions.waitForElementNotVisible(
+        commonElsPage.jailbrokenWarning,
+        20,
+        0
+      )
+    }
     await CreatePinPage.enterNewCurrentPin()
     await portfolioPage.verifyPorfolioScreen()
     await BurgerMenuPage.tapBurgerMenuButton()
