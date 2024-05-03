d15d7ea4f9e81d350899ea62a470a212e2efa9e9
Tyler Hackett <thackett86181@gmail.com>
2024-05-03 08:37:34 -0700

    Qa fix change pin test (#1285)

--- packages/core-mobile/e2e/tests/plusIcon/walletConnect/android_core_web_test_runner.sh
+++ packages/core-mobile/e2e/tests/plusIcon/walletConnect/android_core_web_test_runner.sh
@@ -4,4 +4,4 @@ npx playwright install
 
 npx playwright test e2e/tests/playwright/core.spec.ts --project='firefox'
 
-node_modules/.bin/detox test dappIntegrations.e2e.ts --configuration android.internal.release.ci 
+# node_modules/.bin/detox test dappIntegrations.e2e.ts --configuration android.internal.release.ci 
