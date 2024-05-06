db275994d4cf72d637df9415072c44cffdcc7cce
Tyler Hackett <thackett86181@gmail.com>
2024-05-06 12:43:50 -0700

    update apple sim utils

--- packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
@@ -3,8 +3,12 @@
 # make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
 set -o pipefail
 
+brew update && brew upgrade applesimutils
+
+./node_modules/.bin/detox clean-framework-cache && ./node_modules/.bin/detox build-framework-cache
+
 npm rebuild detox
-./node_modules/.bin/detox test --maxWorkers 2 --configuration ios.internal.release.smoke.ci.reuse_state --retries 1 --reuse; test_result=$?
+./node_modules/.bin/detox test --maxWorkers 1 --configuration ios.internal.release.smoke.ci.reuse_state --retries 1 --reuse; test_result=$?
 
 if ((test_result != 0)); then
   exit 1
