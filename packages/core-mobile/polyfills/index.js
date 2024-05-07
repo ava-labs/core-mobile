e18c7997720e08b354b4302af972bd2887ba8fe9
An Nguyen <an.nguyen@avalabs.org>
2024-05-06 22:26:58 -0500

    Update index.js

--- packages/core-mobile/polyfills/index.js
+++ packages/core-mobile/polyfills/index.js
@@ -8,3 +8,23 @@ import './text_encoding'
 import './base64_shim'
 import './bigInt_to_string'
 import './ethers'
+
+setTimeout(() => {
+  const OriginalPromise = Promise
+
+  // Override the Promise object
+  class FakePromise extends OriginalPromise {
+    constructor() {
+      super(resolve => {
+        resolve()
+        // // Immediately reject
+        // reject(new Error('Promise rejected immediately'))
+      })
+    }
+  }
+
+  // Assign the overridden Promise globally
+  window.Promise = FakePromise // If running in a browser
+
+  global.Promise = FakePromise // If running in Node.js
+}, 7000)
