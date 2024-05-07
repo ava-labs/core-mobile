c8c24e2e30c112a3d92aecc86a82109c3495865e
An Nguyen <an.nguyen@avalabs.org>
2024-05-07 00:26:03 -0500

    disable sentry

--- packages/core-mobile/polyfills/index.js
+++ packages/core-mobile/polyfills/index.js
@@ -9,22 +9,22 @@ import './base64_shim'
 import './bigInt_to_string'
 import './ethers'
 
-setTimeout(() => {
-  const OriginalPromise = Promise
+// setTimeout(() => {
+//   const OriginalPromise = Promise
 
-  // Override the Promise object
-  class FakePromise extends OriginalPromise {
-    constructor() {
-      super(resolve => {
-        resolve()
-        // // Immediately reject
-        // reject(new Error('Promise rejected immediately'))
-      })
-    }
-  }
+//   // Override the Promise object
+//   class FakePromise extends OriginalPromise {
+//     constructor() {
+//       super(resolve => {
+//         resolve()
+//         // // Immediately reject
+//         // reject(new Error('Promise rejected immediately'))
+//       })
+//     }
+//   }
 
-  // Assign the overridden Promise globally
-  window.Promise = FakePromise // If running in a browser
+//   // Assign the overridden Promise globally
+//   window.Promise = FakePromise // If running in a browser
 
-  global.Promise = FakePromise // If running in Node.js
-}, 7000)
+//   global.Promise = FakePromise // If running in Node.js
+// }, 7000)
