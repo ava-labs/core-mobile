4418a216779f694a9a69b2b7dd117e271d495432
Neven <piaf1986@gmail.com>
2024-05-14 15:55:39 +0200

    Make it clearer

--- packages/core-mobile/app/store/rpc/handlers/avalanche_signMessage/utils.ts
+++ packages/core-mobile/app/store/rpc/handlers/avalanche_signMessage/utils.ts
@@ -2,9 +2,10 @@ import { z } from 'zod'
 
 const paramsSchema = z.union([
   z.tuple([z.string()]).describe('message to sign'),
-  z
-    .tuple([z.string(), z.number().nonnegative()])
-    .describe('message to sign, account index')
+  z.tuple([
+    z.string().describe('message to sign'),
+    z.number().nonnegative().describe('account index')
+  ])
 ])
 
 export const parseRequestParams = (
