2634b331bd02252548b9effb572252782d271d34
JungHwan Jang <onghwan@gmail.com>
2024-05-17 14:40:28 -0400

    CP-8669: Fix the spacing of inactive network cards in the portfolio. (#1319)

--- packages/core-mobile/app/screens/portfolio/home/Portfolio.tsx
+++ packages/core-mobile/app/screens/portfolio/home/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ListRenderItemInfo, Platform } from 'react-native'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { useSearchableTokenList } from 'screens/portfolio/useSearchableTokenList'
@@ -22,6 +22,7 @@ import AnalyticsService from 'services/analytics/AnalyticsService'
 import { fetchWatchlist } from 'store/watchlist'
 import { useNetworks } from 'hooks/networks/useNetworks'
 import { selectIsBalanceLoadedForNetworks } from 'store/balance/slice'
+import { setActive } from 'store/network'
 import InactiveNetworkCard from './components/Cards/InactiveNetworkCard'
 import PortfolioHeader from './components/PortfolioHeader'
 import { PortfolioInactiveNetworksLoader } from './components/Loaders/PortfolioInactiveNetworksLoader'
@@ -82,9 +83,15 @@ const Portfolio = (): JSX.Element => {
 const Separator = (): JSX.Element => <Space y={16} />
 
 const TokensTab = (): JSX.Element => {
+  const { navigate } = useNavigation<PortfolioNavigationProp['navigation']>()
   const { inactiveNetworks } = useNetworks()
   const { isRefetching, refetch } = useSearchableTokenList()
   const dispatch = useDispatch()
+  const [
+    inactiveNetworkCardContentHeights,
+    setInactiveNetworkCardContentHeights
+  ] = useState<Record<number, number>>({})
+  const numberOfColumns = 2
 
   const isBalanceLoadedForInactiveNetworks = useSelector(
     selectIsBalanceLoadedForNetworks(
@@ -105,6 +112,37 @@ const TokensTab = (): JSX.Element => {
     return <PortfolioInactiveNetworksLoader />
   }
 
+  const getInactiveNetworkCardHeight = (index: number): number | undefined => {
+    const row = Math.floor(index / numberOfColumns)
+    const startIndex = row * numberOfColumns
+    const endIndex = startIndex + numberOfColumns
+
+    const contentHeightsInRow = []
+    for (let i = startIndex; i < endIndex; i++) {
+      const height = inactiveNetworkCardContentHeights[i]
+      if (height !== undefined) {
+        contentHeightsInRow.push(height)
+      }
+    }
+
+    return contentHeightsInRow.length > 0
+      ? Math.max(...contentHeightsInRow)
+      : undefined
+  }
+
+  const handlePressInactiveNetwork = (network: Network): void => {
+    AnalyticsService.capture('PortfolioSecondaryNetworkClicked', {
+      chainId: network.chainId
+    })
+    dispatch(setActive(network.chainId))
+    setTimeout(
+      () => {
+        navigate(AppNavigation.Portfolio.NetworkTokens)
+      },
+      Platform.OS === 'ios' ? 700 : 0
+    )
+  }
+
   const renderInactiveNetwork = (
     item: ListRenderItemInfo<Network>
   ): JSX.Element => {
@@ -117,7 +155,17 @@ const TokensTab = (): JSX.Element => {
         }
         exiting={FadeOutUp.duration(300)}
         entering={FadeInDown.delay(300).duration(300)}>
-        <InactiveNetworkCard network={item.item} />
+        <InactiveNetworkCard
+          network={item.item}
+          height={getInactiveNetworkCardHeight(item.index)}
+          onPress={handlePressInactiveNetwork}
+          onContentLayout={({ nativeEvent }) => {
+            setInactiveNetworkCardContentHeights(prev => ({
+              ...prev,
+              [item.index]: nativeEvent.layout.height
+            }))
+          }}
+        />
       </Animated.View>
     )
   }
@@ -137,7 +185,7 @@ const TokensTab = (): JSX.Element => {
           paddingHorizontal: 16,
           paddingBottom: 100
         }}
-        numColumns={2}
+        numColumns={numberOfColumns}
         data={itemsToRender}
         renderItem={
           !isBalanceLoadedForInactiveNetworks
