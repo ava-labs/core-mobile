90e05d3fbbe380b3423efc1253170f4bc988488a
ruijialin <ruijia.lin@avalabs.org>
2024-06-24 12:52:38 -0400

    remove activity detail logic

--- packages/core-mobile/e2e/tests/plusIcon/send/sendUsdcTo2ndAccount.e2e.ts
+++ packages/core-mobile/e2e/tests/plusIcon/send/sendUsdcTo2ndAccount.e2e.ts
@@ -1,6 +1,5 @@
 import AccountManagePage from '../../../pages/accountManage.page'
 import ActivityTabPage from '../../../pages/activityTab.page'
-import ActivityTabLoc from '../../../locators/activityTab.loc'
 import PortfolioPage from '../../../pages/portfolio.page'
 import SendPage from '../../../pages/send.page'
 import sendLoc from '../../../locators/send.loc'
@@ -20,10 +19,6 @@ describe('Send USDC to another account', () => {
     )
     await PortfolioPage.tapAvaxNetwork()
     await PortfolioPage.tapActivityTab()
-    await ActivityTabPage.verifyOutgoingTransaction(
-      5000,
-      secondAccountAddress,
-      ActivityTabLoc.usdcOutgoingTransactionDetail
-    )
+    await ActivityTabPage.verifyOutgoingTransaction(5000, secondAccountAddress)
   })
 })
