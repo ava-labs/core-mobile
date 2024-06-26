7a8aaee90c778924aec41d1e79c200149b2cc726
Tyler Hackett <thackett86181@gmail.com>
2024-06-26 12:42:34 -0700

    fix detox config dapp test

--- packages/core-mobile/scripts/bitrise/detox/iosCoreWebTestRunner.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosCoreWebTestRunner.sh
@@ -6,4 +6,4 @@ npx playwright test e2e/tests/playwright/core.spec.ts --project='firefox'
 
 npm rebuild detox
 
-node_modules/.bin/detox test dappIntegrations.e2e.dapp.ts --configuration ios.internal.release.ci --loglevel trace
+node_modules/.bin/detox test dappIntegrations.e2e.dapp.ts --configuration ios.internal.release.dapp.ci --loglevel trace
