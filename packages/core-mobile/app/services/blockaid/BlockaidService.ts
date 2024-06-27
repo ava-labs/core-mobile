911486efbe2a501346874afd255ec076776b1a93
onghwan <onghwan@gmail.com>
2024-06-27 12:28:57 -0400

    use chainId instead of chain name as the param

--- packages/core-mobile/app/services/blockaid/BlockaidService.ts
+++ packages/core-mobile/app/services/blockaid/BlockaidService.ts
@@ -1,12 +1,10 @@
 import { TransactionParams } from 'store/rpc/handlers/eth_sendTransaction/utils'
-import { ChainId } from '@avalabs/chains-sdk'
 import Blockaid from '@blockaid/client'
 import Config from 'react-native-config'
 import {
   JsonRpcRequestData,
   SiteScanResponse,
-  TransactionScanResponse,
-  TransactionScanSupportedChain
+  TransactionScanResponse
 } from './types'
 
 if (!Config.PROXY_URL) throw Error('PROXY_URL is missing')
@@ -23,13 +21,13 @@ class BlockaidService {
     blockaid.site.scan({ url })
 
   static scanTransaction = async (
-    chainId: number,
+    chainId: string,
     params: TransactionParams,
     domain?: string
   ): Promise<TransactionScanResponse> =>
     blockaid.evm.transaction.scan({
       account_address: params.from,
-      chain: BlockaidService.getTransactionScanSupportedChain(chainId),
+      chain: chainId,
       options: ['validation', 'simulation'],
       data: {
         from: params.from,
@@ -49,48 +47,19 @@ class BlockaidService {
     data,
     domain
   }: {
-    chainId: number
+    chainId: string
     accountAddress: string
     data: JsonRpcRequestData
     domain?: string
   }): Promise<TransactionScanResponse> =>
     blockaid.evm.jsonRpc.scan({
-      chain: BlockaidService.getTransactionScanSupportedChain(chainId),
+      chain: chainId,
       options: ['validation', 'simulation'],
       account_address: accountAddress,
       data: data,
       // @ts-ignore
       metadata: domain && domain.length > 0 ? { domain } : { non_dapp: true }
     })
-
-  private static getTransactionScanSupportedChain = (
-    chainId: number
-  ): TransactionScanSupportedChain => {
-    switch (chainId) {
-      case ChainId.ETHEREUM_HOMESTEAD:
-        return 'ethereum'
-      case ChainId.AVALANCHE_MAINNET_ID:
-        return 'avalanche'
-      case 42161:
-        return 'arbitrum'
-      case 8453:
-        return 'base'
-      case 56:
-        return 'bsc'
-      case 137:
-        return 'polygon'
-      case 324:
-        return 'zksync'
-      case 7777777:
-        return 'zora'
-      case 59144:
-        return 'linea'
-      case 238:
-        return 'blast'
-      default:
-        throw new Error(`[Blockaid] Unsupported chainId: ${chainId}`)
-    }
-  }
 }
 
 export default BlockaidService
--- packages/core-mobile/app/store/rpc/handlers/eth_sendTransaction/utils.ts
+++ packages/core-mobile/app/store/rpc/handlers/eth_sendTransaction/utils.ts
@@ -56,17 +56,17 @@ export type TransactionParams = {
 
 export const getChainIdFromRequest = (
   request: RpcRequest<RpcMethod>
-): number => {
+): string => {
   if (!request.data.params.chainId) {
     throw new Error('chainId is missing from the request')
   }
 
   const parts = request.data.params.chainId.split(':')
-  if (parts.length < 2 || isNaN(Number(parts[1]))) {
+  if (parts.length < 2 || parts[1] === undefined || isNaN(Number(parts[1]))) {
     throw new Error('chainId is not in a valid format')
   }
 
-  return Number(parts[1])
+  return parts[1]
 }
 
 export const scanAndSignTransaction = async (
