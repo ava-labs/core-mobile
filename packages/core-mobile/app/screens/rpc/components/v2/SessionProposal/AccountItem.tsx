338cf79d6b51b0d7c7329f0c14ea13a6ec2f303b
JungHwan Jang <onghwan@gmail.com>
2024-07-01 14:49:22 -0400

    CP-8696: update P&L balance when toggling testnet mode (#1379)

--- packages/core-mobile/app/screens/rpc/components/v2/SessionProposal/AccountItem.tsx
+++ packages/core-mobile/app/screens/rpc/components/v2/SessionProposal/AccountItem.tsx
@@ -6,7 +6,6 @@ import { Row } from 'components/Row'
 import { Space } from 'components/Space'
 import ReloadSVG from 'components/svg/ReloadSVG'
 import { useApplicationContext } from 'contexts/ApplicationContext'
-import { useBalanceTotalInCurrencyForAccount } from 'hooks/balance/useBalanceTotalInCurrencyForAccount'
 import { useNetworks } from 'hooks/networks/useNetworks'
 import React, { useCallback, useEffect, useState, JSX } from 'react'
 import { StyleSheet, View } from 'react-native'
@@ -16,6 +15,7 @@ import {
   fetchBalanceForAccount,
   QueryStatus,
   selectBalanceStatus,
+  selectBalanceTotalInCurrencyForAccount,
   selectIsBalanceLoadedForAddress
 } from 'store/balance'
 import { truncateAddress } from 'utils/Utils'
@@ -29,7 +29,9 @@ type Props = {
 const AccountItem = ({ account, onSelect, selected }: Props): JSX.Element => {
   const { theme } = useApplicationContext()
   const { activeNetwork } = useNetworks()
-  const accountBalance = useBalanceTotalInCurrencyForAccount(account.index)
+  const accountBalance = useSelector(
+    selectBalanceTotalInCurrencyForAccount(account.index)
+  )
 
   const isBalanceLoaded = useSelector(
     selectIsBalanceLoadedForAddress(account.index, activeNetwork.chainId)
