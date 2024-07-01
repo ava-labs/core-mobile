c256d6584e8a31d3badd96285de3bdbb55213696
Neven <piaf1986@gmail.com>
2024-07-01 02:23:11 +0200

    Turn off SENTRY_SPOTLIGHT

--- packages/core-mobile/app/utils/debugging/DevDebuggingConfig.ts
+++ packages/core-mobile/app/utils/debugging/DevDebuggingConfig.ts
@@ -10,7 +10,7 @@ const DevDebuggingConfig = {
   LOGBOX_IGNORED_WARNINGS: [],
   SHOW_DEMO_NFTS: false,
   API_MOCKING: false,
-  SENTRY_SPOTLIGHT: true
+  SENTRY_SPOTLIGHT: false
 }
 
 export default DevDebuggingConfig
