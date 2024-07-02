a7b583ac806d21a1265545001b75d63f1339edff
Tyler Hackett <thackett86181@gmail.com>
2024-07-02 10:23:16 -0700

    debug test run

--- packages/core-mobile/scripts/bitrise/detox/iosInternalRegressionRunner.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalRegressionRunner.sh
@@ -7,7 +7,7 @@ yarn start &
 
 npm rebuild detox
 
-QT_QPA_PLATFORM=xcb; ./node_modules/.bin/detox test --maxWorkers 3 -c ios.internal.release.ci --headless --retries 1 --detectOpenHandles; test_result=$?
+QT_QPA_PLATFORM=xcb; ./node_modules/.bin/detox test addressBook.e2e.smoke.ts enableTestnet.e2e.smoke.ts emptyAssets.e2e.ts changeNetwork.e2e.smoke.ts  --maxWorkers 3 -c ios.internal.release.ci --retries 1; test_result=$?
 
 if ((test_result != 0)); then
   exit 1
