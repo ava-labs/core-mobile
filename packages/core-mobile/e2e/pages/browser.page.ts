f642234cbd6c3ff12e51e5dccb3a01e779b3a499
eunjisong <eunji.l.song@gmail.com>
2024-07-16 11:26:50 -0400

    QA - Test connect Dapp via inAppBrowser

--- packages/core-mobile/e2e/pages/browser.page.ts
+++ packages/core-mobile/e2e/pages/browser.page.ts
@@ -1,7 +1,10 @@
 import Actions from '../helpers/actions'
+import Asserts from '../helpers/assertions'
 import BrowserLoc from '../locators/browser.loc'
 
 class BrowserPage {
+  wb = web(by.id('myWebview'))
+
   get searchBar() {
     return by.id(BrowserLoc.searchBar)
   }
@@ -13,6 +16,65 @@ class BrowserPage {
   async enterBrowserSearchQuery(query: string) {
     await Actions.setInputText(this.searchBar, query)
     await element(this.searchBar).tapReturnKey()
+  }
+
+  async tapAccept() {
+    await this.wb.element(by.web.xpath('//*[text()="Accept"]')).tap()
+  }
+
+  async tapConnectWallet() {
+    await this.wb
+      .element(by.web.xpath('//*[@data-testid="connect-wallet-button"]'))
+      .tap()
+  }
+
+  async tapWalletConnect() {
+    await this.wb
+      .element(
+        by.web.xpath(
+          '//*[@data-testid="connect-wallet-connect-button"]//p[text()="WalletConnect"]'
+        )
+      )
+      .tap()
+  }
+
+  async connectTermAndContinue() {
+    await this.wb
+      .element(by.web.xpath('//*[@data-testid="connect-terms-checkbox"]'))
+      .tap()
+    await this.wb
+      .element(by.web.xpath('//*[@data-testid="connect-terms-continue-btn"]'))
+      .tap()
+  }
+
+  async connectCore() {
+    await this.wb
+      .element(by.web.tag('w3m-modal'))
+      .runScript(function (element) {
+        element.shadowRoot
+          .querySelector('wui-flex > wui-card > w3m-router')
+          .shadowRoot.querySelector('w3m-connect-view')
+          .shadowRoot.querySelector('wui-list-wallet[name="Core"]')
+          .click()
+      })
+  }
+
+  async selectAccountAndconnect() {
+    await Actions.tap(by.text('Select Accounts'))
+    await Actions.tapElementAtIndex(by.id('account_check_box'), 0)
+    await Actions.tap(by.text('Approve'))
+  }
+
+  async verifyDappConnected() {
+    await Asserts.isVisible(by.text('Connected to Core'))
+    try {
+      await this.wb
+        .element(by.web.xpath('//*[@data-testid="connect-wallet-button"]'))
+        .tap()
+      fail('We should not have connect wallet button after connected')
+    } catch (e) {
+      console.log('Verify there is no connect wallet button')
+    }
   }
 }
 
