02d55b8a64761dd37ea1519e28b76ffed5ea1fff
ruijialin <ruijia.lin@avalabs.org>
2024-07-26 09:35:54 -0400

    bump vm-module version

--- packages/core-mobile/app/hooks/earn/usePChainBalance.ts
+++ packages/core-mobile/app/hooks/earn/usePChainBalance.ts
@@ -43,7 +43,7 @@ export const usePChainBalance = (): UseQueryResult<
       ) {
         return
       }
-      return pChainBalance.balancePerType.unlockedUnstaked
+      return pChainBalance
     }
   })
 }
