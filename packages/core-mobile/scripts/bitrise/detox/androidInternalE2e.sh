ccf00ec9e3277988e1cb63abf6cf037472202fda
Tyler Hackett <thackett86181@gmail.com>
2024-07-30 14:15:21 -0700

    add script after teardown

--- packages/core-mobile/scripts/bitrise/detox/androidInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/androidInternalE2e.sh
@@ -9,6 +9,8 @@ npm rebuild detox
 
 QT_QPA_PLATFORM=xcb; ./node_modules/.bin/detox test addCustomToken.e2e.smoke.ts createNewWallet.e2e.smoke.ts unlockWithRecoveryPhrase.e2e.ts recoverMetaMaskWallet.e2e.smoke.ts showRecoveryPhrase.e2e.smoke.ts performanceHelpUsImproveScreen.e2e.ts recoverWallet.e2e.smoke.ts --configuration android.internal.release.smoke.ci --headless --retries 1; test_result=$?
 
+npx ts-node ./e2e/attachLogsToTestrailResults.ts
+
 if ((test_result != 0)); then
   exit 1
 fi
