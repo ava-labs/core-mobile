57ccb6476116c9a5bed01d872c8427908f26efe1
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2024-08-02 10:05:32 -0400

    CP-9025: update XP chainId (#1456)

--- packages/core-mobile/app/services/network/utils/isAvalancheNetwork.ts
+++ packages/core-mobile/app/services/network/utils/isAvalancheNetwork.ts
@@ -1,5 +1,4 @@
 import { Network, ChainId } from '@avalabs/core-chains-sdk'
-import { absoluteChain } from 'utils/network/isAvalancheNetwork'
 
 export function isAvalancheNetwork(network: Network): boolean {
   return isAvalancheCChainId(network.chainId)
@@ -14,11 +13,13 @@ export function isAvalancheCChainId(chainId: number): boolean {
 }
 
 export function isAvalancheChainId(chainId: number): boolean {
-  return (
-    ChainId.AVALANCHE_MAINNET_ID === chainId ||
-    ChainId.AVALANCHE_TESTNET_ID === chainId ||
-    ChainId.AVALANCHE_LOCAL_ID === chainId ||
-    ChainId.AVALANCHE_XP === absoluteChain(chainId) ||
-    ChainId.AVALANCHE_TEST_XP === absoluteChain(chainId)
-  )
+  return [
+    ChainId.AVALANCHE_MAINNET_ID,
+    ChainId.AVALANCHE_TESTNET_ID,
+    ChainId.AVALANCHE_LOCAL_ID,
+    ChainId.AVALANCHE_X,
+    ChainId.AVALANCHE_TEST_X,
+    ChainId.AVALANCHE_P,
+    ChainId.AVALANCHE_TEST_P
+  ].includes(chainId)
 }
