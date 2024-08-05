c72ebd99660c2fd6fb63a411494278498236ad0b
Tyler Hackett <thackett86181@gmail.com>
2024-08-05 15:23:37 -0700

    uncomment envman vars

--- packages/core-mobile/scripts/bitrise/detox/distributeAndroidTests.sh
+++ packages/core-mobile/scripts/bitrise/detox/distributeAndroidTests.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Command that returns the test files to be run and stores in TESTS_TO_BE_RUN
-# if (($IS_REGRESSION_RUN=='true')); then
-#   echo "This is a regression run"
-#   TESTS_TO_BE_RUN=$(./node_modules/.bin/detox test --configuration android.internal.release.regression.ci --listTests)
-# else
-#   echo "This is a smoke test run"
-#   TESTS_TO_BE_RUN=$(./node_modules/.bin/detox test --configuration android.internal.release.smoke.ci --listTests)
-# fi
-
-TESTS_TO_BE_RUN=$(./node_modules/.bin/detox test --configuration android.internal.smoke.debug.no_reuse_state --listTests)
+if (($IS_REGRESSION_RUN=='true')); then
+  echo "This is a regression run"
+  TESTS_TO_BE_RUN=$(./node_modules/.bin/detox test --configuration android.internal.release.regression.ci --listTests)
+else
+  echo "This is a smoke test run"
+  TESTS_TO_BE_RUN=$(./node_modules/.bin/detox test --configuration android.internal.release.smoke.ci --listTests)
+fi
 
 
 # This splits the string into an array of strings based on the newline character
@@ -40,6 +38,6 @@ echo "Test count2: $testCnt2"
 echo "Test count3: $testCnt3"
 
 # These env vars are passed to the test emulators via the detox config TESTS_ONE to ui_test_pixel_4_one, TESTS_TWO to ui_test_pixel_4_two, and TESTS_THREE to ui_test_pixel_4_three
-# envman add --key TESTS_ONE --value "$testCnt1"
-# envman add --key TESTS_TWO --value "$testCnt2"
-# envman add --key TESTS_THREE --value "$testCnt3"
+envman add --key TESTS_ONE --value "$testCnt1"
+envman add --key TESTS_TWO --value "$testCnt2"
+envman add --key TESTS_THREE --value "$testCnt3"
