64804e9c7a5aa16ec4e7d60734b1028471ac6dc2
Eunji Song <32075932+eunjisong@users.noreply.github.com>
2024-08-07 09:29:29 -0400

    QA: New parameterized send tests (#1459)

---
+++ packages/core-mobile/e2e/helpers/tokens.ts
@@ -0,0 +1,14 @@
+export const Tokens = [
+  { symbol: 'AVAX', name: 'Avalanche' },
+  { symbol: 'BTC.b', name: 'Bitcoin' },
+  { symbol: 'COQ', name: 'Coq Inu' },
+  { symbol: 'WETH.e', name: 'Wrapped ETH' },
+  { symbol: 'USDC', name: 'USD Coin' },
+  { symbol: 'USDC.e', name: 'USD Coin' },
+  { symbol: 'ALPHA.e', name: 'AlphaToken' },
+  { symbol: 'SUSHI.e', name: 'Avalanche' },
+  { symbol: 'JOE', name: 'JoeToken' },
+  { symbol: 'APE', name: 'ApeCoin' },
+  { symbol: 'AAVE', name: 'Aave Token' },
+  { symbol: 'AAVE.e', name: 'Aave Token' }
+]
