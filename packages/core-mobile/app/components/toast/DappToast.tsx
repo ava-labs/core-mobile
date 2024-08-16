a03423f622170e6541f2f127532bf2ab7ed2da89
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2024-08-16 11:05:48 -0400

    CP-9028: update successful toast (#1492)

--- packages/core-mobile/app/components/toast/DappToast.tsx
+++ packages/core-mobile/app/components/toast/DappToast.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react'
 import { Dimensions, Pressable, StyleProp, View, ViewStyle } from 'react-native'
-import { useApplicationContext } from 'contexts/ApplicationContext'
-import AvaText from 'components/AvaText'
 import AvaButton from 'components/AvaButton'
 import ClearSVG from 'components/svg/ClearSVG'
+import { Text, useTheme } from '@avalabs/k2-mobile'
 
 const WINDOW_WIDTH = Dimensions.get('window').width
 
 export enum DappToastTypes {
-  ERROR = 'ERROR'
+  ERROR = 'ERROR',
+  SUCCESS = 'SUCCESS'
 }
 
 interface Props {
@@ -23,18 +23,23 @@ const dismissToast = (toastId?: string): void => {
 }
 
 const DappToast: FC<Props> = ({ toastId, message, type, dappName }) => {
-  const theme = useApplicationContext().theme
+  const {
+    theme: { colors }
+  } = useTheme()
 
   const renderContent = (): JSX.Element | undefined => {
     if (type === DappToastTypes.ERROR) {
       return <Error dappName={dappName} message={message} />
+    }
+    if (type === DappToastTypes.SUCCESS) {
+      return <Success dappName={dappName} message={message} />
     }
   }
 
   const hideToast = (): void => dismissToast(toastId)
 
   const style = {
-    backgroundColor: theme.neutral850,
+    backgroundColor: colors.$neutral850,
     borderRadius: 8,
     justifyContent: 'center',
     width: WINDOW_WIDTH * 0.89,
@@ -47,9 +52,36 @@ const DappToast: FC<Props> = ({ toastId, message, type, dappName }) => {
       <Pressable
         style={{ position: 'absolute', top: 10, right: 10 }}
         onPress={hideToast}>
-        <ClearSVG color={theme.white} backgroundColor={theme.transparent} />
+        <ClearSVG color={colors.$white} backgroundColor={colors.$transparent} />
       </Pressable>
     </AvaButton.Base>
+  )
+}
+
+const Success = ({
+  dappName,
+  message
+}: {
+  dappName: string
+  message: string
+}): JSX.Element => {
+  return (
+    <View style={{ backgroundColor: 'transparent' }}>
+      <Text
+        variant="buttonSmall"
+        sx={{ width: '80%', color: '$successMain' }}
+        ellipsizeMode="tail"
+        numberOfLines={1}>
+        {dappName}
+      </Text>
+      <Text
+        variant="buttonLarge"
+        sx={{ lineHeight: 28, color: '$neutral50' }}
+        ellipsizeMode="tail"
+        numberOfLines={5}>
+        {message}
+      </Text>
+    </View>
   )
 }
 
@@ -60,24 +92,22 @@ const Error = ({
   dappName: string
   message: string
 }): JSX.Element => {
-  const theme = useApplicationContext().theme
-
   return (
     <View style={{ backgroundColor: 'transparent' }}>
-      <AvaText.ButtonSmall
-        color={theme.colorError}
-        textStyle={{ width: '80%' }}
+      <Text
+        variant="buttonSmall"
+        sx={{ width: '80%', color: '$dangerMain' }}
         ellipsizeMode="tail"
         numberOfLines={1}>
         {dappName}
-      </AvaText.ButtonSmall>
-      <AvaText.ButtonLarge
-        color={theme.colorText1}
-        textStyle={{ lineHeight: 28 }}
+      </Text>
+      <Text
+        variant="buttonLarge"
+        sx={{ lineHeight: 28, color: '$neutral50' }}
         ellipsizeMode="tail"
         numberOfLines={5}>
         {message}
-      </AvaText.ButtonLarge>
+      </Text>
     </View>
   )
 }
