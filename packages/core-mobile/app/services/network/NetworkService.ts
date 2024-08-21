4aa49f55f851dbe320ada12d5c35d25120972b41
neven-s <piaf1986@gmail.com>
2024-08-21 22:31:19 +0200

    CP-8996 Implement support for DeBank tokens (#1505)
    Signed-off-by: neven-s <piaf1986@gmail.com>

--- packages/core-mobile/app/services/network/NetworkService.ts
+++ packages/core-mobile/app/services/network/NetworkService.ts
@@ -8,15 +8,18 @@ import {
   BITCOIN_TEST_NETWORK,
   ChainId,
   Network,
+  NetworkToken,
   NetworkVMType
 } from '@avalabs/core-chains-sdk'
 import SentryWrapper from 'services/sentry/SentryWrapper'
 import { Transaction } from '@sentry/types'
 import { avaxSerial } from '@avalabs/avalanchejs'
 import { TransactionResponse } from 'ethers'
-import { Networks } from 'store/network/types'
+import { ChainID, Networks } from 'store/network/types'
 import Config from 'react-native-config'
 import Logger from 'utils/Logger'
+import { DebankNetwork } from 'services/network/types'
+import { addIdToPromise, settleAllIdPromises } from '@avalabs/evm-module'
 import { getBitcoinProvider, getEvmProvider } from './utils/providerUtils'
 import { NETWORK_P, NETWORK_P_TEST, NETWORK_X, NETWORK_X_TEST } from './consts'
 
@@ -26,11 +29,13 @@ if (!Config.PROXY_URL)
 class NetworkService {
   async getNetworks(): Promise<Networks> {
     const erc20Networks = await this.fetchERC20Networks()
+    const deBankNetworks = await this.fetchDeBankNetworks()
 
     delete erc20Networks[ChainId.AVALANCHE_LOCAL_ID]
 
     return {
       ...erc20Networks,
+      ...deBankNetworks,
       [ChainId.BITCOIN]: BITCOIN_NETWORK,
       [ChainId.BITCOIN_TESTNET]: BITCOIN_TEST_NETWORK,
       [ChainId.AVALANCHE_P]: this.getAvalancheNetworkP(false),
@@ -120,6 +125,7 @@ class NetworkService {
   getAvalancheNetworkX(isDeveloperMode: boolean): Network {
     return isDeveloperMode ? NETWORK_X_TEST : NETWORK_X
   }
+
   /**
    * Returns the provider used by Avalanche X/P/CoreEth chains.
    * Using either X or P Network will result in same provider.
@@ -137,6 +143,81 @@ class NetworkService {
       acc[network.chainId] = network
       return acc
     }, {} as Networks)
+  }
+
+  private async fetchDeBankNetworks(): Promise<Networks> {
+    const response = await fetch(
+      `${Config.PROXY_URL}/proxy/debank/v1/chain/list`
+    )
+    if (!response.ok) {
+      throw Error('fetchDeBankNetworks failed: ' + response.statusText)
+    }
+    const deBankNetworks: DebankNetwork[] = await response.json()
+
+    const networks = deBankNetworks
+      .filter(network =>
+        ['arb', 'bsc', 'op', 'matic', 'base'].includes(network.id)
+      )
+      .reduce(
+        (acc, network) => {
+          acc[network.community_id] = {
+            platformChainId: network.id,
+            chainId: network.community_id,
+            chainName: network.name,
+            logoUri: network.logo_url,
+            vmName: NetworkVMType.EVM,
+            vmId: network.id,
+            isTestnet: false,
+            networkToken: {} as NetworkToken,
+            nativeTokenId: network.native_token_id
+          } as Network & { nativeTokenId: string }
+          return acc
+        },
+        {} as {
+          [chainId: ChainID]: Network & { nativeTokenId: string }
+        }
+      )
+
+    //fetch info about native tokens
+    const promises = Object.keys(networks).map(chainId => {
+      const network = networks[Number(chainId)]
+      if (!network) {
+        return Promise.reject('invalid chain id: ' + chainId)
+      }
+      return addIdToPromise(
+        (async () => {
+          const tokenResponse = await fetch(
+            `${Config.PROXY_URL}/proxy/debank/v1/token?chain_id=${chainId}&id=${network.nativeTokenId}`
+          )
+          if (!tokenResponse.ok) {
+            throw Error('Failed to fetch debank/v1/token')
+          }
+          return await tokenResponse.json() //as DeBankToken
+        })(),
+        chainId
+      )
+    })
+
+    const nativeTokenInfos = await settleAllIdPromises(promises)
+    const networksWithToken: Networks = {}
+    for (const chainId in nativeTokenInfos) {
+      const nativeTokenInfo = nativeTokenInfos[chainId]
+      if (!nativeTokenInfo || 'error' in nativeTokenInfo) {
+        continue
+      }
+      const { nativeTokenId, ...networkWithToken } = {
+        ...networks[Number(chainId)],
+        networkToken: {
+          symbol: nativeTokenInfo.symbol,
+          logoUri: nativeTokenInfo.logo_url,
+          decimals: nativeTokenInfo.decimals,
+          name: nativeTokenInfo.name
+        } as NetworkToken
+      }
+      networksWithToken[Number(chainId)] = networkWithToken as Network
+    }
+
+    return networksWithToken
   }
 }
 
