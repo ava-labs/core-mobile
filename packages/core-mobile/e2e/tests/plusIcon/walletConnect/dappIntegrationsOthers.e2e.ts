da07673326679b0f509f3f395217c87439a7c1d8
Eunji Song <32075932+eunjisong@users.noreply.github.com>
2024-08-26 15:49:49 -0400

    QA - distribute tests to android pixel devices & fix some tests  (#1490)
    Co-authored-by: Tyler Hackett <thackett86181@gmail.com>

--- packages/core-mobile/e2e/tests/plusIcon/walletConnect/dappIntegrationsOthers.e2e.ts
+++ packages/core-mobile/e2e/tests/plusIcon/walletConnect/dappIntegrationsOthers.e2e.ts
@@ -2,47 +2,33 @@ import { warmup } from '../../../helpers/warmup'
 import browserPage from '../../../pages/browser.page'
 import plusMenuPage from '../../../pages/plusMenu.page'
 import connectToSitePage from '../../../pages/connectToSite.page'
-import connectedSitesPage from '../../../pages/connectedSites.page'
-import securityAndPrivacyPage from '../../../pages/burgerMenu/securityAndPrivacy.page'
 
-describe('Dapp Wallet Connect', () => {
-  beforeAll(async () => {
-    await warmup()
+describe('Dapp Wallet Connect - Others', () => {
+  beforeEach(async () => {
+    await warmup(true)
   })
 
-  enum Dapps {
-    aave = 'Aave - Open Source Liquidity Protocol',
-    traderjoe = 'Trader Joe',
-    openSea = 'OpenSea, the largest NFT marketplace'
-  }
-
-  it('should connect Aave', async () => {
+  it('should connect Aave via Wallet Connect', async () => {
     await browserPage.connectTo('https://app.aave.com/')
     const qrUri = await browserPage.getQrUri()
     await plusMenuPage.connectWallet(qrUri)
     await connectToSitePage.selectAccountAndconnect()
   })
 
-  it('should connect TraderJoe', async () => {
+  it('should connect TraderJoe via Wallet Connect', async () => {
     await browserPage.connectTo('https://traderjoexyz.com/avalanche')
     const qrUri = await browserPage.getQrUri()
     await plusMenuPage.connectWallet(qrUri)
     await connectToSitePage.selectAccountAndconnect()
   })
 
-  it('should connect OpenSea', async () => {
+  it('should connect OpenSea via Wallet Connect', async () => {
     await browserPage.connectTo('https://opensea.io/', true)
     const qrUri = await browserPage.getQrUri()
     await plusMenuPage.connectWallet(qrUri)
     await connectToSitePage.selectAccountAndconnect()
-    await connectToSitePage.approveSignMessage(Dapps.openSea)
-  })
-
-  it('should show connected dApps', async () => {
-    await securityAndPrivacyPage.goToConnectedSites()
-    for (const key in Dapps) {
-      const value = Dapps[key as keyof typeof Dapps]
-      await connectedSitesPage.verifyDapp(value)
-    }
+    await connectToSitePage.approveSignMessage(
+      'OpenSea, the largest NFT marketplace'
+    )
   })
 })
