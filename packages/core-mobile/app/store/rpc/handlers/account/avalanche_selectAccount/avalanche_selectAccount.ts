b1a3e4fc4ff7c40d31f643fc477a5a28fed7ef88
onghwan <onghwan@gmail.com>
2024-08-28 10:34:15 -0400

    auto switch account in web

--- packages/core-mobile/app/store/rpc/handlers/account/avalanche_selectAccount/avalanche_selectAccount.ts
+++ packages/core-mobile/app/store/rpc/handlers/account/avalanche_selectAccount/avalanche_selectAccount.ts
@@ -5,17 +5,10 @@ import {
   selectActiveAccount,
   setActiveAccountIndex
 } from 'store/account/slice'
-import * as Navigation from 'utils/Navigation'
-import AppNavigation from 'navigation/AppNavigation'
 import Logger from 'utils/Logger'
 import { RpcMethod, RpcRequest } from '../../../types'
-import {
-  ApproveResponse,
-  DEFERRED_RESULT,
-  HandleResponse,
-  RpcRequestHandler
-} from '../../types'
-import { parseApproveData, parseRequestParams } from './utils'
+import { HandleResponse, RpcRequestHandler } from '../../types'
+import { parseRequestParams } from './utils'
 
 export type AvalancheSelectAccountRequest =
   RpcRequest<RpcMethod.AVALANCHE_SELECT_ACCOUNT>
@@ -29,7 +22,7 @@ class AvalancheSelectAccountHandler
     request: AvalancheSelectAccountRequest,
     listenerApi: AppListenerEffectAPI
   ): HandleResponse => {
-    const { getState } = listenerApi
+    const { getState, dispatch } = listenerApi
     const { params } = request.data.params.request
 
     const result = parseRequestParams(params)
@@ -64,37 +57,7 @@ class AvalancheSelectAccountHandler
       }
     }
 
-    Navigation.navigate({
-      name: AppNavigation.Root.Wallet,
-      params: {
-        screen: AppNavigation.Modal.SelectAccountV2,
-        params: {
-          request,
-          account: requestedAccount
-        }
-      }
-    })
-
-    return { success: true, value: DEFERRED_RESULT }
-  }
-
-  approve = async (
-    payload: { request: AvalancheSelectAccountRequest; data?: unknown },
-    listenerApi: AppListenerEffectAPI
-  ): ApproveResponse => {
-    const { dispatch } = listenerApi
-    const result = parseApproveData(payload.data)
-
-    if (!result.success) {
-      return {
-        success: false,
-        error: rpcErrors.internal('Invalid approve data')
-      }
-    }
-
-    const accountIndex = result.data.account.index
-
-    dispatch(setActiveAccountIndex(accountIndex))
+    dispatch(setActiveAccountIndex(requestedAccount.index))
 
     return { success: true, value: [] }
   }
