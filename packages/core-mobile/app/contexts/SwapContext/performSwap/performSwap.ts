b4d4fcbe95fccd4998539741cca8afbff6639a9e
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2024-08-30 11:27:35 -0400

    CP-8935: upgrade paraswap to 6.9.1 (#1527)
    Signed-off-by: Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
    Co-authored-by: JungHwan Jang <onghwan@gmail.com>

--- packages/core-mobile/app/contexts/SwapContext/performSwap/performSwap.ts
+++ packages/core-mobile/app/contexts/SwapContext/performSwap/performSwap.ts
@@ -5,12 +5,11 @@ import { JsonRpcBatchInternal } from '@avalabs/core-wallets-sdk'
 import { TransactionParams } from '@avalabs/evm-module'
 import ERC20 from '@openzeppelin/contracts/build/contracts/ERC20.json'
 import Big from 'big.js'
-import { APIError, ETHER_ADDRESS, Transaction } from 'paraswap'
-import { OptimalRate } from 'paraswap-core'
+import { OptimalRate, TransactionParams as Transaction } from '@paraswap/sdk'
 import { promiseResolveWithBackoff, resolve } from '@avalabs/core-utils-sdk'
 import { bigIntToHex } from '@ethereumjs/util'
 import { rpcErrors } from '@metamask/rpc-errors'
-import { buildTx, getParaswapSpender } from './paraswapUtils'
+import SwapService, { ETHER_ADDRESS } from 'services/swap/SwapService'
 
 export type PerformSwapParams = {
   srcTokenAddress: string | undefined
@@ -52,20 +51,12 @@ export async function performSwap({
   assert(activeNetwork, 'Network Init Error: Wrong network')
   assert(!activeNetwork.isTestnet, 'Network Init Error: Wrong network')
 
-  const spenderAddress = await getParaswapSpender()
-
   const sourceTokenAddress = isSrcTokenNative ? ETHER_ADDRESS : srcTokenAddress
   const destinationTokenAddress = isDestTokenNative
     ? ETHER_ADDRESS
     : destTokenAddress
 
-  const buildOptions = undefined,
-    partnerAddress = undefined,
-    partner = 'Avalanche',
-    receiver = undefined,
-    permit = undefined,
-    deadline = undefined,
-    partnerFeeBps = undefined
+  const partner = 'Avalanche'
 
   let approveTxHash: string | undefined
 
@@ -85,6 +76,14 @@ export async function performSwap({
 
   // no need to approve native token
   if (!isSrcTokenNative) {
+    let spenderAddress: string
+
+    try {
+      spenderAddress = await SwapService.getParaswapSpender(activeNetwork)
+    } catch (error) {
+      throw new Error(`Spender Address Error: ${error}`)
+    }
+
     const contract = new Contract(sourceTokenAddress, ERC20.abi, provider)
 
     const [allowance, allowanceError] = await resolve<bigint>(
@@ -134,15 +133,20 @@ export async function performSwap({
         throw rpcErrors.internal('Invalid transaction hash')
       }
 
+      const receipt = hash && (await provider.waitForTransaction(hash))
+      if (!receipt || (receipt && receipt.status !== 1)) {
+        throw new Error('Swap token approval failed')
+      }
+
       approveTxHash = hash
     } else {
       approveTxHash = undefined
     }
   }
 
-  function checkForErrorsInResult(result: Transaction | APIError): boolean {
+  function checkForErrorsInResult(result: Transaction | Error): boolean {
     return (
-      (result as APIError).message === 'Server too busy' ||
+      (result as Error).message === 'Server too busy' ||
       // paraswap returns responses like this: {error: 'Not enough 0x4f60a160d8c2dddaafe16fcc57566db84d674…}
       // when they are too slow to detect the approval
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -153,8 +157,8 @@ export async function performSwap({
   const [txBuildData, txBuildDataError] = await resolve(
     promiseResolveWithBackoff(
       () =>
-        buildTx({
-          network: activeNetwork.chainId.toString(),
+        SwapService.buildTx({
+          network: activeNetwork,
           srcToken: sourceTokenAddress,
           destToken: destinationTokenAddress,
           srcAmount: sourceAmount,
@@ -162,14 +166,8 @@ export async function performSwap({
           priceRoute,
           userAddress,
           partner,
-          partnerAddress,
-          partnerFeeBps,
-          receiver,
-          options: buildOptions,
           srcDecimals: priceRoute.srcDecimals,
-          destDecimals: priceRoute.destDecimals,
-          permit,
-          deadline
+          destDecimals: priceRoute.destDecimals
         }),
       checkForErrorsInResult,
       0,
--- packages/core-mobile/app/swap/utils.ts
+++ packages/core-mobile/app/swap/utils.ts
@@ -1,6 +1,6 @@
-import { OptimalRate } from 'paraswap-core'
+import { OptimalRate } from '@paraswap/sdk'
 
-export const calculateRate = (optimalRate: OptimalRate) => {
+export const calculateRate = (optimalRate: OptimalRate): number => {
   const { destAmount, destDecimals, srcAmount, srcDecimals } = optimalRate
   const destAmountNumber = parseInt(destAmount) / Math.pow(10, destDecimals)
   const sourceAmountNumber = parseInt(srcAmount) / Math.pow(10, srcDecimals)
