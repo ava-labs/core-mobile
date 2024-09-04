6fa15055dcbdada3cf43a9791664a63728c0cb2c
Stephanie Roy <stephanie.roy@gmail.com>
2024-09-03 21:05:12 -0400

    Fully fix theme types

--- packages/k2-alpine/src/theme/ThemeProvider.tsx
+++ packages/k2-alpine/src/theme/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren, type FC } from 'react'
 import { DripsyProvider } from 'dripsy'
-import { darkTheme } from './theme'
+import { darkTheme, type K2AlpineTheme } from './theme'
 
 export const K2AlpineThemeProvider: FC<PropsWithChildren> = ({ children }) => (
-  <DripsyProvider theme={darkTheme}>{children}</DripsyProvider>
+  <DripsyProvider theme={darkTheme as K2AlpineTheme}>{children}</DripsyProvider>
 )
