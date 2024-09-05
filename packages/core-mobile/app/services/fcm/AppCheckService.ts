e4308667da340909a029cb02d7cdae8280a42065
Neven <piaf1986@gmail.com>
2024-09-05 14:19:57 +0200

    Move initialisation of AppCheck to AppCheckService

--- packages/core-mobile/app/services/fcm/AppCheckService.ts
+++ packages/core-mobile/app/services/fcm/AppCheckService.ts
@@ -5,6 +5,33 @@ import {
 import Logger from 'utils/Logger'
 
 class AppCheckService {
+  init = (): void => {
+    const rnfbProvider = firebase
+      .appCheck()
+      .newReactNativeFirebaseAppCheckProvider()
+    rnfbProvider.configure({
+      android: {
+        provider: __DEV__ ? 'debug' : 'playIntegrity',
+        debugToken:
+          'some token you have configured for your project firebase web console'
+      },
+      apple: {
+        provider: __DEV__ ? 'debug' : 'appAttestWithDeviceCheckFallback',
+        debugToken: process.env.APPCHECK_DEBUG_TOKEN_APPLE
+      }
+    })
+
+    firebase
+      .appCheck()
+      .initializeAppCheck({
+        provider: rnfbProvider,
+        isTokenAutoRefreshEnabled: true
+      })
+      .catch(reason => {
+        Logger.error(`initializeAppCheck failed: ${reason}`)
+      })
+  }
+
   verifyAppCheck = async (): Promise<void> => {
     try {
       const { token } = await firebase.appCheck().getToken(true)
