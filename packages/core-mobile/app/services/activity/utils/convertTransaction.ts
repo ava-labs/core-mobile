d07f0e004c57f792e5396387f8c758702e838595
onghwan <onghwan@gmail.com>
2024-09-09 15:14:29 -0400

    fix bug: show bridge transaction properly

--- packages/core-mobile/app/services/activity/utils/convertTransaction.ts
+++ packages/core-mobile/app/services/activity/utils/convertTransaction.ts
@@ -1,22 +1,44 @@
+import { CriticalConfig } from '@avalabs/core-bridge-sdk'
+import { Network } from '@avalabs/core-chains-sdk'
 import {
   Transaction as InternalTransaction,
   TransactionType,
   TokenType
 } from '@avalabs/vm-module-types'
-import { NULL_ADDRESS } from 'screens/bridge/utils/bridgeUtils'
+import {
+  isBridgeTransactionBTC,
+  isBridgeTransactionEthereum,
+  isBridgeTransactionERC20
+} from 'screens/bridge/utils/bridgeUtils'
 import UnifiedBridgeService from 'services/bridge/UnifiedBridgeService'
+import { isEthereumNetwork } from 'services/network/utils/isEthereumNetwork'
 import { Transaction } from 'store/transaction'
+import { isBitcoinNetwork } from 'utils/network/isBitcoinNetwork'
 
 export const convertTransaction = (
-  transaction: InternalTransaction
+  transaction: InternalTransaction,
+  network: Network,
+  criticalConfig: CriticalConfig | undefined
 ): Transaction => {
   const bridgeAddresses = UnifiedBridgeService.getBridgeAddresses()
-  const isBridge =
-    transaction.tokens[0]?.type === TokenType.ERC20 &&
-    [
-      transaction.tokens[0].to?.address.toLowerCase(),
-      transaction.tokens[0].from?.address.toLowerCase()
-    ].some(item => item && [...bridgeAddresses, NULL_ADDRESS].includes(item))
+  const isBridge = isEthereumNetwork(network)
+    ? isBridgeTransactionEthereum({
+        transaction,
+        network,
+        criticalConfig,
+        bridgeAddresses
+      })
+    : isBitcoinNetwork(network)
+    ? isBridgeTransactionBTC(
+        transaction,
+        criticalConfig?.criticalBitcoin?.walletAddresses
+      )
+    : transaction.tokens[0]?.type === TokenType.ERC20
+    ? isBridgeTransactionERC20({
+        token: transaction.tokens[0],
+        bridgeAddresses
+      })
+    : false
 
   return {
     ...transaction,
