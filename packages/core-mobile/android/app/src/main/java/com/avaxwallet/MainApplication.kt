60d139b9f93c0ba1afd055dc7892e6dbbc616caf
An Nguyen <an.nguyen@avalabs.org>
2024-09-12 09:45:42 -0500

    Use custom user agent on Android (#1707)

--- packages/core-mobile/android/app/src/main/java/com/avaxwallet/MainApplication.kt
+++ packages/core-mobile/android/app/src/main/java/com/avaxwallet/MainApplication.kt
@@ -1,20 +1,19 @@
 package com.avaxwallet;
 
 import android.app.Application
-import android.content.Context
 import android.database.CursorWindow
+import android.webkit.WebSettings
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.modules.network.OkHttpClientProvider
 import com.facebook.soloader.SoLoader
 import java.lang.reflect.Field
-import java.util.ArrayList
 
 class MainApplication : Application(), ReactApplication {
 
@@ -39,6 +38,22 @@ class MainApplication : Application(), ReactApplication {
     override fun onCreate() {
         super.onCreate()
 
+        increaseWindowCursorSize()
+
+        // Manually set user agent to our format. This helps avoid getting identified as a bot and rate limited by cloudflare
+        // the default format is usually okhttp/x.x.x
+        // while our format is
+        // avaxwallet/0.14.15.1532 Mozilla/5.0 (Linux; Android 13; Pixel 7 Build/TQ1A.221205.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/128.0.6613.127 Mobile Safari/537.36
+        OkHttpClientProvider.setOkHttpClientFactory(CoreOkHttpClientFactory(getUserAgent()))
+
+        SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            load()
+        }
+    }
+
+    private fun increaseWindowCursorSize() {
         // Temp workaround for data not getting persisted on Android
         // https://github.com/rt2zz/redux-persist/issues/199
         // https://github.com/rt2zz/redux-persist/issues/960
@@ -53,11 +68,18 @@ class MainApplication : Application(), ReactApplication {
         } catch (e: Exception) {
             e.printStackTrace()
         }
+    }
 
-        SoLoader.init(this, false)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
+    private fun getUserAgent(): String {
+        try {
+            val shortPackageName = packageName.substring(packageName.lastIndexOf(".") + 1)
+            val appVersion = BuildConfig.VERSION_NAME
+            val buildNumber = BuildConfig.VERSION_CODE
+            val systemUserAgent = WebSettings.getDefaultUserAgent(this.applicationContext)
+            return "$shortPackageName/$appVersion.$buildNumber $systemUserAgent"
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            return ""
         }
     }
 }
