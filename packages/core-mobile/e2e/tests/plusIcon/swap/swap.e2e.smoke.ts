71fc4af6a916c14663a295b5568ea8fbcb28f104
eunjisong <eunji.l.song@gmail.com>
2024-09-16 10:03:53 -0400

    fix tests

--- packages/core-mobile/e2e/tests/plusIcon/swap/swap.e2e.smoke.ts
+++ packages/core-mobile/e2e/tests/plusIcon/swap/swap.e2e.smoke.ts
@@ -4,18 +4,23 @@ import portfolioPage from '../../../pages/portfolio.page'
 import SendPage from '../../../pages/send.page'
 import SwapTabPage from '../../../pages/swapTab.page'
 
-describe('Swap AVAX', () => {
+describe('Swap', () => {
   beforeAll(async () => {
     await warmup(true)
   })
 
-  it('Should swap AVAX <> USDC', async () => {
+  it('Should swap AVAX to ERC20', async () => {
     await SwapTabPage.swap('AVAX', 'USDC')
     await SendPage.verifySuccessToast()
   })
 
-  it('Should swap USDC <> AVAX', async () => {
+  it('Should swap ERC20 to AVAX', async () => {
     await SwapTabPage.swap('USDC', 'AVAX', '0.001')
+    await SendPage.verifySuccessToast()
+  })
+
+  it('Should swap ERC20 to USDT', async () => {
+    await SwapTabPage.swap('USDC', 'USDT', '0.0001')
     await SendPage.verifySuccessToast()
   })
 
