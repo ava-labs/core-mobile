8900a2949ce6d0b472c6e3ca94c4558d9d808662
An Nguyen <an.nguyen@avalabs.org>
2024-09-30 09:24:29 -0500

    integrate with core mobile

--- packages/k2-alpine/src/theme/ThemeProvider.tsx
+++ packages/k2-alpine/src/theme/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren, type FC } from 'react'
 import { DripsyProvider } from 'dripsy'
-import { darkTheme, type K2AlpineTheme } from './theme'
+import { darkTheme } from './theme'
 
 export const K2AlpineThemeProvider: FC<PropsWithChildren> = ({ children }) => (
-  <DripsyProvider theme={darkTheme as K2AlpineTheme}>{children}</DripsyProvider>
+  <DripsyProvider theme={darkTheme}>{children}</DripsyProvider>
 )
