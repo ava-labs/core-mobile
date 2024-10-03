f663bb41eadafa9125489f9b399083d684141342
Neven <piaf1986@gmail.com>
2024-10-03 10:11:11 +0200

    Fix prettier

--- packages/core-mobile/app/services/earn/calculateCChainFee.test.ts
+++ packages/core-mobile/app/services/earn/calculateCChainFee.test.ts
@@ -1,6 +1,6 @@
 import { calculateCChainFee } from 'services/earn/calculateCrossChainFees'
 import { avaxSerial, UnsignedTx } from '@avalabs/avalanchejs'
-import {TokenUnit} from "@avalabs/core-utils-sdk"
+import { TokenUnit } from '@avalabs/core-utils-sdk'
 
 describe('earn/calculateCChainFee', () => {
   describe('calculateCChainFee', () => {
@@ -17,12 +17,18 @@ describe('earn/calculateCChainFee', () => {
     } as unknown as UnsignedTx
 
     it('should result 330030000000000n for single byte with base fee 30nAvax', async () => {
-      const result = calculateCChainFee(new TokenUnit(30*10**9, 18, "AVAX"), unsignedTxMock)
+      const result = calculateCChainFee(
+        new TokenUnit(30 * 10 ** 9, 18, 'AVAX'),
+        unsignedTxMock
+      )
       expect(result.toSubUnit()).toBe(330030000000000n)
     })
 
     it('should result 495045000000000n for single byte with base fee 45nAvax', async () => {
-      const result = calculateCChainFee(new TokenUnit(45*10**9, 18, "AVAX"), unsignedTxMock)
+      const result = calculateCChainFee(
+        new TokenUnit(45 * 10 ** 9, 18, 'AVAX'),
+        unsignedTxMock
+      )
       expect(result.toSubUnit()).toBe(495045000000000n)
     })
   })
