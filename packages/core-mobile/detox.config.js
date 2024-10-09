af053f1e9ef6922b97ce45c63de04661e918eca3
Eunji Song <32075932+eunjisong@users.noreply.github.com>
2024-10-09 13:01:46 -0400

    QA - add tsconfig for e2e to avoid import issues (#1902)
    Co-authored-by: Tyler Hackett <thackett86181@gmail.com>

--- packages/core-mobile/detox.config.js
+++ packages/core-mobile/detox.config.js
@@ -458,6 +458,33 @@ module.exports = {
         }
       }
     },
+    'android.external.release.regression.ci': {
+      device: 'emulator_ci',
+      app: 'android.external.release.ci',
+      artifacts: {
+        rootDir: './e2e/artifacts/android'
+      },
+      testRunner: {
+        $0: 'jest',
+        args: {
+          config: './e2e/configs/regressionConfig.json',
+          _: [process.env.TESTS_TO_RUN]
+        }
+      }
+    },
+    'android.external.release.regression.parameterized_tests.ci': {
+      device: 'emulator_ci',
+      app: 'android.external.release.ci',
+      artifacts: {
+        rootDir: './e2e/artifacts/android'
+      },
+      testRunner: {
+        $0: 'jest',
+        args: {
+          config: './e2e/configs/regressionParameterizedTestsConfig.json'
+        }
+      }
+    },
     'android.internal.e2e': {
       device: 'emulator',
       app: 'android.internal.e2e'
--- packages/core-mobile/scripts/bitrise/detox/androidE2ENoReuseAppState.sh
+++ packages/core-mobile/scripts/bitrise/detox/androidE2ENoReuseAppState.sh
@@ -1,40 +1,46 @@
 #!/usr/bin/env bash
 
-# make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
+# Make pipelines' return status equal to the last command to exit with a non-zero status, or zero if all commands exit successfully
 set -o pipefail
 
 yarn start &
 
 npm rebuild detox
 
-adb install -r $BITRISE_APK_PATH
-adb install -r $BITRISE_TEST_APK_PATH
+adb install -r "$BITRISE_APK_PATH"
+adb install -r "$BITRISE_TEST_APK_PATH"
 
 echo "IS_REGRESSION_RUN should be true: $IS_REGRESSION_RUN"
 echo "Got test list: $TESTS_TO_RUN"
 
 if [ "$IS_REGRESSION_RUN" = true ]; then
-  if ["$IS_INTERNAL_BUILD" = true ]; then
+  if [ "$IS_INTERNAL_BUILD" = true ]; then
     if [ "$PARAMETERIZED_TESTS" = true ]; then
-      echo "Running regression run on Pixel 6 for parameterized tests"
+      echo "Regression, Internal, Parameterized Test"
       QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test --configuration android.internal.release.regression.parameterized_tests.ci --headless
       test_result=$?
     else
-      echo "Running regression run..."
+      echo "Regression, Internal, NON Parameterized Test"
       QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test --configuration android.internal.release.regression.ci --headless
       test_result=$?
     fi
   else
-    echo "Running regression on external build..."
-    QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test --configuration android.external.release.regression.ci --headless
+    if [ "$PARAMETERIZED_TESTS" = true ]; then
+      echo "Regression, External, Parameterized Test"
+      QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test --configuration android.external.release.regression.parameterized_tests.ci --headless
+      test_result=$?
+    else
+      echo "Regression, External, NON Parameterized Test"
+      QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test --configuration android.external.release.regression.ci --headless
+      test_result=$?
+    fi
   fi
-
 else
   if [ "$PARAMETERIZED_TESTS" = true ]; then
-    exit 0  # we don't run parameterized tests on smoke run
+    echo "Smoke, Internal, Parameterized Test"
+    exit 0  # We don't run parameterized tests on smoke run
   else
-    echo "The test list above will be reduced by the android smoke config ignoreTestList"
-    echo "Running smoke run..."
+    echo "Smoke, Internal, NON Parameterized Test"
     QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test --configuration android.internal.release.smoke.ci --headless
     test_result=$?
   fi
