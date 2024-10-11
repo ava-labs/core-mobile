0c3f22601939931a90d3e79862580057d1ada5c7
onghwan <onghwan@gmail.com>
2024-10-11 11:10:00 -0400

    expo router

--- packages/expo-router-demo/app/signedOut/index.tsx
+++ packages/expo-router-demo/app/signedOut/index.tsx
@@ -1,5 +1,8 @@
+import { Link } from "expo-router"
+import { View } from "react-native"
+
 const SignUpScreen = (): JSX.Element => {
-  return <></>
+  return <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}><Link href="/signedIn/portfolio/">Sign up</Link></View>
 }
 
 export default SignUpScreen
