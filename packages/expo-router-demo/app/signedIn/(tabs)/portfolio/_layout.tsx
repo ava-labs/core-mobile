63afcc0f11d809565174e546d5a965df4dd5aa55
onghwan <onghwan@gmail.com>
2024-10-14 12:12:06 -0400

    use the same transition in android

--- packages/expo-router-demo/app/signedIn/(tabs)/portfolio/_layout.tsx
+++ packages/expo-router-demo/app/signedIn/(tabs)/portfolio/_layout.tsx
@@ -1,8 +1,9 @@
 import { Stack } from "@/layouts/Stack";
+import { stackNavigatorScreenOptions } from "@/utils/screenOptions";
 
 export default function PortfolioLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={stackNavigatorScreenOptions}>
       <Stack.Screen name="index" />
       <Stack.Screen name="assets" />
     </Stack>
