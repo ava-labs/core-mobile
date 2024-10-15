8d075c58924c96ba7834675615c85deef6c4a9f7
onghwan <onghwan@gmail.com>
2024-10-15 12:11:26 -0400

    refactoring

--- packages/core-mobile/expo-router/app/(signedIn)/(modals)/settings/account.tsx
+++ packages/core-mobile/expo-router/app/(signedIn)/(modals)/settings/account.tsx
@@ -1,7 +1,13 @@
+import { Link } from 'expo-router'
 import React from 'react'
+import { View } from '@avalabs/k2-alpine'
 
 const AccountScreen = (): JSX.Element => {
-  return <></>
+  return (
+    <View sx={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Link href="">Sign out</Link>
+    </View>
+  )
 }
 
 export default AccountScreen
