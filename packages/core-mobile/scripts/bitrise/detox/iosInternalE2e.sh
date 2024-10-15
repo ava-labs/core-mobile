eab0894f66f0487f034661120be030fc6062b8c5
Tyler Hackett <thackett86181@gmail.com>
2024-10-15 15:29:58 -0700

    update dashboard first

--- packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
@@ -3,17 +3,13 @@
 # make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
 set -o pipefail
 
-echo "Here is the public IP address of the machine running the tests"
-
-curl -s http://ipecho.net/plain; echo
-
 npm rebuild detox
 
 ./node_modules/.bin/detox test --configuration ios.internal.release.smoke.ci --retries 1 --max-workers 3; test_result=$?
 
-npx ts-node ./e2e/attachLogsSendResultsToTestrail.ts && sleep 5
+./scripts/datadog/updateIosDashboard.sh && sleep 5
 
-./scripts/datadog/updateIosDashboard.sh  & sleep 5
+npx ts-node ./e2e/attachLogsSendResultsToTestrail.ts && sleep 5
 
 if ((test_result != 0)); then
   exit 1
