5978c4c9a6eb74ec0f2cb73ef46349e1bed4826a
Tyler Hackett <thackett86181@gmail.com>
2024-10-22 14:02:22 -0700

    remove echo ip

--- packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
@@ -3,6 +3,8 @@
 # make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
 set -o pipefail
 
+npm rebuild detox
+
 ./node_modules/.bin/detox test --configuration ios.internal.release.smoke.ci --retries 1 --max-workers 3; test_result=$?
 
 npx ts-node ./e2e/attachLogsSendResultsToTestrail.ts && sleep 5
