57ba417211816d6f62d2a3fae1445704860115af
Tyler Hackett <thackett86181@gmail.com>
2024-10-22 14:01:53 -0700

    remove echo ip

--- packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
@@ -3,8 +3,6 @@
 # make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
 set -o pipefail
 
-echo "Here is the public IP address of the machine running the tests"
-
 ./node_modules/.bin/detox test --configuration ios.internal.release.smoke.ci --retries 1 --max-workers 3; test_result=$?
 
 npx ts-node ./e2e/attachLogsSendResultsToTestrail.ts && sleep 5
