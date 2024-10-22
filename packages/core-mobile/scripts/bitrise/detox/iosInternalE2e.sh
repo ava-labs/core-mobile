e8a3e0a6b60baefc963d4275fe1469af389ce5a5
Tyler Hackett <thackett86181@gmail.com>
2024-10-22 14:01:24 -0700

    remove echo ip

--- packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
@@ -5,10 +5,6 @@ set -o pipefail
 
 echo "Here is the public IP address of the machine running the tests"
 
-curl -s http://ipecho.net/plain; echo
-
-npm rebuild detox
-
 ./node_modules/.bin/detox test --configuration ios.internal.release.smoke.ci --retries 1 --max-workers 3; test_result=$?
 
 npx ts-node ./e2e/attachLogsSendResultsToTestrail.ts && sleep 5
