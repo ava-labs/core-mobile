66c36c598a084f95b44779a14ffec42f6a210414
onghwan <onghwan@gmail.com>
2024-10-25 13:09:34 -0400

    adjust background color

--- packages/core-mobile/app/new/components/navigation/BackBarButton.tsx
+++ packages/core-mobile/app/new/components/navigation/BackBarButton.tsx
@@ -4,7 +4,7 @@ import { View, Text } from '@avalabs/k2-alpine'
 const BackBarButton = (): JSX.Element => {
   return (
     // todo: use k2-alpine icon
-    <View style={{ paddingLeft: 18 }}>
+    <View style={{ paddingLeft: 18, backgroundColor: 'transparent' }}>
       <Text>{'<<'}</Text>
     </View>
   )
