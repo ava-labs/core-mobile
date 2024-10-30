084156b24dafbea8d4a539f3ce833b8901075356
Tyler Hackett <thackett86181@gmail.com>
2024-10-30 11:07:28 -0700

    Update iosInternalE2e.sh
    Signed-off-by: Tyler Hackett <thackett86181@gmail.com>

--- packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
@@ -3,10 +3,6 @@
 # make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
 set -o pipefail
 
-echo "Here is the public IP address of the machine running the tests"
-
-curl -s http://ipecho.net/plain; echo
-
 npm rebuild detox
 
 ./node_modules/.bin/detox test --configuration ios.internal.release.smoke.ci --retries 1 --max-workers 3; test_result=$?
