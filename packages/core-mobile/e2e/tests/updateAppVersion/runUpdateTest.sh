eed43bc67acf14effed64194df147d4edb409c75
Tyler Hackett <thackett86181@gmail.com>
2024-11-21 08:31:24 -0800

    move logic to one bash file

--- packages/core-mobile/e2e/tests/updateAppVersion/runUpdateTest.sh
+++ packages/core-mobile/e2e/tests/updateAppVersion/runUpdateTest.sh
@@ -3,7 +3,7 @@
 # make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
 set -o pipefail
 
-#yarn start &
+yarn start &
 
 npm rebuild detox
 
@@ -14,10 +14,24 @@ echo $PREVIOUS_VERSION_APK_PATH
 echo 'latest version apk path'
 echo $LATEST_VERSION_APK_PATH
 
-./e2e/tests/updateAppVersion/oldVersionApk/installOldApk.sh  && sleep 5
+# install previous apk version on test device
+echo "Installing old version apk on test device"
+PREVIOUS_VERSION_APK_PATH="./e2e/tests/updateAppVersion/oldVersionApk/app-external-e2e-bitrise-signed.apk"
+PREVIOUS_VERSION_TEST_APK_PATH="./e2e/tests/updateAppVersion/oldVersionApk/app-external-e2e-androidTest-bitrise-signed.apk"
+adb install -r $PREVIOUS_VERSION_APK_PATH && sleep 5
+echo "Installing old version test apk on device"
+adb install -r $PREVIOUS_VERSION_TEST_APK_PATH && sleep 5
 
+# Run a simple login test
 ./node_modules/.bin/detox test loginToAppForUpdate.e2e.ts -c android.external.old.e2e --reuse; test_result=$?
 
-./e2e/tests/updateAppVersion/latestVersionApk/installLatestApk.sh && sleep 5
+# install latest apk on test device
+echo "Installing latest version apk on test device"
+LATEST_VERSION_APK_PATH="./e2e/tests/updateAppVersion/latestVersionApk/app-external-e2e-bitrise-signed.apk"
+LATEST_VERSION_TEST_APK_PATH="./e2e/tests/updateAppVersion/latestVersionApk/app-external-e2e-androidTest-bitrise-signed.apk"
+adb install -r $LATEST_VERSION_APK_PATH && sleep 5
+echo "Installing latest version test apk on device"
+adb install -r $LATEST_VERSION_TEST_APK_PATH && sleep 5
 
+# Run same login test on latest version to test for a crash
 ./node_modules/.bin/detox test loginToAppForUpdate.e2e.ts -c android.external.latest.e2e --reuse; test_result=$?
