19247de7b898efda2ad1a3e26070c249c43e6549
onghwan <onghwan@gmail.com>
2024-11-21 17:30:14 -0500

    fix storybook

--- packages/k2-alpine/src/components/PageControl/PageControl.stories.tsx
+++ packages/k2-alpine/src/components/PageControl/PageControl.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { ScrollView, View } from '../Primitives'
+import { ScrollView, Text, View } from '../Primitives'
 import { Button, useTheme } from '../..'
 import { PageControl } from './PageControl'
 
@@ -10,17 +10,7 @@ export default {
 export const All = (): JSX.Element => {
   const { theme } = useTheme()
 
-  const NUMBER_OF_PAGE = 15
-
-  const [currentPage, setCurrentPage] = useState(0)
-
-  const handlePressPrevious = (): void => {
-    setCurrentPage(prev => Math.max(prev - 1, 0))
-  }
-
-  const handlePressNext = (): void => {
-    setCurrentPage(prev => Math.min(prev + 1, NUMBER_OF_PAGE - 1))
-  }
+  const NUMBER_OF_PAGES = [3, 5, 20]
 
   return (
     <ScrollView
@@ -29,13 +19,42 @@ export const All = (): JSX.Element => {
         backgroundColor: theme.colors.$surfacePrimary
       }}
       contentContainerStyle={{ padding: 16 }}>
+      <View style={{ marginTop: 32, gap: 100 }}>
+        {NUMBER_OF_PAGES.map((numberOfPage, index) => (
+          <View key={index}>
+            <PageControlStory numberOfPage={numberOfPage} />
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  )
+}
+
+const PageControlStory = ({
+  numberOfPage
+}: {
+  numberOfPage: number
+}): JSX.Element => {
+  const [currentPage, setCurrentPage] = useState(0)
+
+  const handlePressPrevious = (): void => {
+    setCurrentPage(prev => Math.max(prev - 1, 0))
+  }
+
+  const handlePressNext = (): void => {
+    setCurrentPage(prev => Math.min(prev + 1, numberOfPage - 1))
+  }
+
+  return (
+    <>
+      <Text sx={{ alignSelf: 'center' }}>Page Size: {numberOfPage}</Text>
       <PageControl
         style={{
-          marginTop: 100,
+          marginTop: 20,
           marginBottom: 20,
           alignSelf: 'center'
         }}
-        numberOfPage={NUMBER_OF_PAGE}
+        numberOfPage={numberOfPage}
         currentPage={currentPage}
       />
       <View sx={{ flexDirection: 'row', gap: 12, alignSelf: 'center' }}>
@@ -46,7 +65,6 @@ export const All = (): JSX.Element => {
           {'>'}
         </Button>
       </View>
-      <View style={{ height: 160 }} />
-    </ScrollView>
+    </>
   )
 }
