0382d4204e87d402da76fba29ca15b481c8b5097
JungHwan Jang <onghwan@gmail.com>
2024-11-22 14:32:17 -0500

    CP-9545: Fix error with insufficient balance error when hit max with native tokens (#2112)

--- packages/core-mobile/app/screens/bridge/hooks/useGetBridgeFees.ts
+++ packages/core-mobile/app/screens/bridge/hooks/useGetBridgeFees.ts
@@ -7,62 +7,65 @@ import { selectActiveAccount } from 'store/account'
 import { isBitcoinNetwork } from 'utils/network/isBitcoinNetwork'
 
 export const useGetBridgeFees = ({
-  amount,
   bridgeAsset,
   sourceNetwork,
   targetNetwork
 }: {
-  amount: bigint
   bridgeAsset: BridgeAsset | undefined
   sourceNetwork: Network | undefined
   targetNetwork: Network | undefined
 }): {
-  getBridgeFee: () => Promise<bigint | undefined>
-  getEstimatedGas: () => Promise<bigint | undefined>
+  getBridgeFee: (amount: bigint) => Promise<bigint | undefined>
+  getEstimatedGas: (amount: bigint) => Promise<bigint | undefined>
 } => {
   const activeAccount = useSelector(selectActiveAccount)
 
-  const getBridgeFee = useCallback(async () => {
-    if (!bridgeAsset || !targetNetwork || !sourceNetwork || amount === 0n) {
-      return undefined
-    }
+  const getBridgeFee = useCallback(
+    async (amount: bigint) => {
+      if (!bridgeAsset || !targetNetwork || !sourceNetwork || amount === 0n) {
+        return undefined
+      }
 
-    return await UnifiedBridgeService.getFee({
-      asset: bridgeAsset,
-      amount,
-      sourceNetwork,
-      targetNetwork
-    })
-  }, [amount, bridgeAsset, sourceNetwork, targetNetwork])
+      return await UnifiedBridgeService.getFee({
+        asset: bridgeAsset,
+        amount,
+        sourceNetwork,
+        targetNetwork
+      })
+    },
+    [bridgeAsset, sourceNetwork, targetNetwork]
+  )
 
-  const getEstimatedGas = useCallback(async () => {
-    if (!activeAccount || !bridgeAsset || !sourceNetwork || amount === 0n)
-      return
+  const getEstimatedGas = useCallback(
+    async (amount: bigint) => {
+      if (!activeAccount || !bridgeAsset || !sourceNetwork) return
 
-    if (!activeAccount) {
-      throw new Error('no active account found')
-    }
+      if (!activeAccount) {
+        throw new Error('no active account found')
+      }
 
-    if (!targetNetwork) {
-      throw new Error('no target network found')
-    }
+      if (!targetNetwork) {
+        throw new Error('no target network found')
+      }
 
-    const fromAddress = isBitcoinNetwork(sourceNetwork)
-      ? activeAccount.addressBTC
-      : activeAccount.addressC
+      const fromAddress = isBitcoinNetwork(sourceNetwork)
+        ? activeAccount.addressBTC
+        : activeAccount.addressC
 
-    const gasLimit = await UnifiedBridgeService.estimateGas({
-      asset: bridgeAsset,
-      fromAddress,
-      amount,
-      sourceNetwork,
-      targetNetwork
-    })
+      const gasLimit = await UnifiedBridgeService.estimateGas({
+        asset: bridgeAsset,
+        fromAddress,
+        amount,
+        sourceNetwork,
+        targetNetwork
+      })
 
-    if (gasLimit) {
-      return gasLimit
-    }
-  }, [activeAccount, amount, bridgeAsset, sourceNetwork, targetNetwork])
+      if (gasLimit) {
+        return gasLimit
+      }
+    },
+    [activeAccount, bridgeAsset, sourceNetwork, targetNetwork]
+  )
 
   return { getBridgeFee, getEstimatedGas }
 }
