83258d530173495d3b5fcf2ff6e68ef03eb4cb71
JungHwan Jang <onghwan@gmail.com>
2024-11-22 17:10:45 -0500

    CP-9550: fix Missing P and X Chain on Portfolio (#2117)

--- packages/core-mobile/app/services/activity/utils/convertTransaction.ts
+++ packages/core-mobile/app/services/activity/utils/convertTransaction.ts
@@ -4,30 +4,35 @@ import {
 } from '@avalabs/vm-module-types'
 import UnifiedBridgeService from 'services/bridge/UnifiedBridgeService'
 import { Transaction } from 'store/transaction'
+import { getCaip2ChainId } from 'utils/caip2ChainIds'
 
 export const convertTransaction = (
-  transaction: InternalTransaction
+  transaction: InternalTransaction,
+  shouldAnalyzeBridgeTxs: boolean
 ): Transaction => {
-  const bridgeAnalysis = UnifiedBridgeService.analyzeTx({
-    chainId: transaction.chainId,
-    from: transaction.from,
-    to: transaction.to,
-    tokenTransfers: transaction.tokens.map(token => {
-      return {
-        from: token.from?.address,
-        to: token.to?.address,
-        symbol: token.symbol
-      }
-    })
-  })
+  const bridgeAnalysis = shouldAnalyzeBridgeTxs
+    ? UnifiedBridgeService.analyzeTx({
+        chainId: getCaip2ChainId(Number(transaction.chainId)),
+        from: transaction.from,
+        to: transaction.to,
+        tokenTransfers: transaction.tokens.map(token => {
+          return {
+            from: token.from?.address,
+            to: token.to?.address,
+            symbol: token.symbol
+          }
+        })
+      })
+    : undefined
 
   return {
     ...transaction,
     bridgeAnalysis,
-    txType: bridgeAnalysis.isBridgeTx
-      ? TransactionType.BRIDGE
-      : transaction.txType
-      ? transaction.txType
-      : TransactionType.UNKNOWN
+    txType:
+      bridgeAnalysis?.isBridgeTx === true
+        ? TransactionType.BRIDGE
+        : transaction.txType
+        ? transaction.txType
+        : TransactionType.UNKNOWN
   }
 }
