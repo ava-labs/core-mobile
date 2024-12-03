a1fc9d88269a995e33194fdd8f81393b54d3b944
Tyler Hackett <thackett86181@gmail.com>
2024-12-03 12:41:19 -0800

    force fail first test

--- packages/core-mobile/e2e/tests/updateAppVersion/loginAfterVersionUpdate.e2e.ts
+++ packages/core-mobile/e2e/tests/updateAppVersion/loginAfterVersionUpdate.e2e.ts
@@ -1,9 +1,23 @@
+import { DeviceLaunchAppConfig, DevicePermissions } from 'detox/detox'
 import assertions from '../../helpers/assertions'
 import { handleJailbrokenWarning } from '../../helpers/warmup'
 import ExistingRecoveryPhrasePage from '../../pages/existingRecoveryPhrase.page'
 
+const permissions: DevicePermissions = { notifications: 'YES', camera: 'YES' }
+const initialArgs: DeviceLaunchAppConfig = {
+  permissions: permissions,
+  launchArgs: {
+    detoxURLBlacklistRegex: [
+      '.*cloudflare-ipfs.*',
+      '.*[ipfs.io/ipfs].*',
+      '.*[amazonaws.com].*'
+    ]
+  }
+}
+
 describe('Verify version update', () => {
   beforeAll(async () => {
+    await device.launchApp(initialArgs)
     await handleJailbrokenWarning()
   })
 
--- packages/core-mobile/e2e/tests/updateAppVersion/loginToAppForUpdate.e2e.ts
+++ packages/core-mobile/e2e/tests/updateAppVersion/loginToAppForUpdate.e2e.ts
@@ -1,13 +1,17 @@
 import { warmup } from '../../helpers/warmup'
 
-describe('Verify version update', () => {
+describe('Install older version of app and login', () => {
   beforeAll(async () => {
     await warmup()
     await device.disableSynchronization()
-    fail('not supposed to pass, dont worry about it...')
+    try {
+      fail('not supposed to pass, dont worry about it...')
+    } catch (e) {
+      console.log('Test is supposed to fail and this makes it pass!')
+    }
   })
 
-  it('Should verify Defi Items', async () => {
+  it('Should not reach this section', async () => {
     fail('not supposed to pass, dont worry about it...')
   })
 })
