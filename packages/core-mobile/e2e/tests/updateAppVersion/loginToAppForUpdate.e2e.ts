b8406c40006bae69eebd0b64e5e7f37a03a4614c
Tyler Hackett <thackett86181@gmail.com>
2024-12-03 09:54:12 -0800

    fail test in beforeAll

--- packages/core-mobile/e2e/tests/updateAppVersion/loginToAppForUpdate.e2e.ts
+++ packages/core-mobile/e2e/tests/updateAppVersion/loginToAppForUpdate.e2e.ts
@@ -4,6 +4,7 @@ describe('Verify version update', () => {
   beforeAll(async () => {
     await warmup()
     await device.disableSynchronization()
+    fail('not supposed to pass, dont worry about it...')
   })
 
   it('Should verify Defi Items', async () => {
