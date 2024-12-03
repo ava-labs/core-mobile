51fd99c9e80052756c9fbdcd718b713d83b8fbc4
Tyler Hackett <thackett86181@gmail.com>
2024-12-03 15:50:09 -0800

    add tests

--- packages/core-mobile/e2e/tests/updateAppVersion/loginToAppForUpdate.e2e.ts
+++ packages/core-mobile/e2e/tests/updateAppVersion/loginToAppForUpdate.e2e.ts
@@ -4,14 +4,11 @@ describe('Install older version of app and login', () => {
   beforeEach(async () => {
     await warmup()
     await device.disableSynchronization()
+
+    fail('not supposed to pass, dont worry about it...')
   })
 
   it('should fail', async () => {
-    console.log('Test is supposed to fail and this makes it pass!')
-    try {
-      fail('not supposed to pass, dont worry about it...')
-    } catch (e) {
-      console.log('Test failed as expected')
-    }
+    console.log('Test is supposed to fail!')
   })
 })
