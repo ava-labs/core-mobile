da9fd4df9ba7d87b77fca35799cefce8f694061f
Tyler Hackett <thackett86181@gmail.com>
2024-12-04 13:53:29 -0800

    log render functoin

--- packages/core-mobile/e2e/tests/updateAppVersion/loginToAppForUpdate.e2e.ts
+++ packages/core-mobile/e2e/tests/updateAppVersion/loginToAppForUpdate.e2e.ts
@@ -1,5 +1,6 @@
 import { handleJailbrokenWarning } from '../../helpers/warmup'
 import loginRecoverWallet from '../../helpers/loginRecoverWallet'
+import portfolioPage from '../../pages/portfolio.page'
 
 describe('Install older version of app and login', () => {
   beforeAll(async () => {
@@ -21,6 +22,24 @@ describe('Install older version of app and login', () => {
   })
 
   it('should fail', async () => {
+    failOnTimeout(async () => {
+      await portfolioPage.verifyPorfolioScreen()
+    })
     console.log('Test is supposed to pass and you should see this message!')
   })
 })
+
+interface FailOnTimeoutFunction {
+  (): void
+}
+
+async function failOnTimeout(func: FailOnTimeoutFunction): Promise<void> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const setTimeout = (f: () => void): void => {
+    throw new Error(`Executing timeout \n ${f.toString()} \n`)
+  }
+
+  // A small trick to change scope:
+  // eslint-disable-next-line no-eval
+  eval('(' + func.toString() + ')()')
+}
