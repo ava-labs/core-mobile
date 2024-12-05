8f0ae90cc307a91d19b68a9e4e72591c92311176
Tyler Hackett <thackett86181@gmail.com>
2024-12-05 15:58:16 -0800

    update signed apk path

--- packages/core-mobile/e2e/tests/updateAppVersion/androidExternalE2eUpdateApp.sh
+++ packages/core-mobile/e2e/tests/updateAppVersion/androidExternalE2eUpdateApp.sh
@@ -4,7 +4,7 @@ yarn start &
 
 npm rebuild detox
 
-adb install -r "$BITRISE_APK_PATH"
+adb install -r "$BITRISE_SIGNED_APK_PATH"
 adb install -r "$BITRISE_TEST_APK_PATH"
 
 QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test loginAfterVersionUpdate.e2e.ts --configuration android.external.release.smoke.ci --headless --reuse; test_result=$?
