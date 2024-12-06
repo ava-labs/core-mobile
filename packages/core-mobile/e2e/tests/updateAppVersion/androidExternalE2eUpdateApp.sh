63cf75b3ad9d723274542526a45128639729bda2
Tyler Hackett <thackett86181@gmail.com>
2024-12-06 09:16:21 -0800

    add sleep after detox test command

--- packages/core-mobile/e2e/tests/updateAppVersion/androidExternalE2eUpdateApp.sh
+++ packages/core-mobile/e2e/tests/updateAppVersion/androidExternalE2eUpdateApp.sh
@@ -7,7 +7,7 @@ npm rebuild detox
 adb install -r "e2e/tests/updateAppVersion/oldVersionApk/app-external-e2e-bitrise-signed.apk"
 adb install -r "e2e/tests/updateAppVersion/oldVersionApk/app-external-e2e-androidTest-bitrise-signed.apk"
 
-QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test loginAfterVersionUpdate.e2e.ts --configuration android.external.old.e2e --headless --reuse; test_result=$?
+QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test loginAfterVersionUpdate.e2e.ts --configuration android.external.old.e2e --headless --reuse; test_result=$? && sleep 5
 
 npx ts-node ./e2e/attachLogsSendResultsToTestrail.ts
 
