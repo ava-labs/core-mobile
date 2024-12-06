06b19448a25292d5d2ccb2251431a55f0f7d571e
ruijialin <ruijia.lin@avalabs.org>
2024-12-06 11:26:55 -0500

    upgrade wc to Reown

--- packages/core-mobile/polyfills/index.js
+++ packages/core-mobile/polyfills/index.js
@@ -1,7 +1,6 @@
 import './promise'
 import 'react-native-gesture-handler'
 import 'react-native-quick-crypto' // to override the global.crypto object
-import '@walletconnect/react-native-compat'
 import 'react-native-url-polyfill/auto'
 import '../shim'
 import './read_as_array_buffer_shim'
