60556db6f7baa829e2a18115a022489c4d777f2a
ruijialin <ruijia.lin@avalabs.org>
2024-12-10 16:03:23 -0500

    add back @walletConnect/react-native-compat

--- packages/core-mobile/polyfills/index.js
+++ packages/core-mobile/polyfills/index.js
@@ -1,6 +1,7 @@
 import './promise'
 import 'react-native-gesture-handler'
 import 'react-native-quick-crypto' // to override the global.crypto object
+import '@walletconnect/react-native-compat'
 import 'react-native-url-polyfill/auto'
 import '../shim'
 import './read_as_array_buffer_shim'
