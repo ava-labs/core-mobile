8486ac94255cefa5f69309b29511b1d849b12e2e
Tyler Hackett <thackett86181@gmail.com>
2024-12-12 14:30:01 -0800

    setup for regression runs

--- packages/core-mobile/e2e/tests/updateAppVersion/loginWithOlderVersion.sh
+++ packages/core-mobile/e2e/tests/updateAppVersion/loginWithOlderVersion.sh
@@ -1,14 +1,18 @@
 set -o pipefail
+if [ $IS_REGRESSION_RUN = true ]; then
 
-npm rebuild detox
+  npm rebuild detox
+  adb uninstall com.avaxwallet
+  npx ts-node ./e2e/tests/updateAppVersion/getOldAndroidVersion.js
 
-npx ts-node ./e2e/tests/updateAppVersion/getOldAndroidVersion.js
+  adb install -r "./e2e/tests/updateAppVersion/oldVersionApk/app-external-e2e-bitrise-signed.apk"
+  adb install -r "./e2e/tests/updateAppVersion/oldVersionApk/app-external-e2e-androidTest-bitrise-signed.apk"
 
-adb install -r "./e2e/tests/updateAppVersion/oldVersionApk/app-external-e2e-bitrise-signed.apk"
-adb install -r "./e2e/tests/updateAppVersion/oldVersionApk/app-external-e2e-androidTest-bitrise-signed.apk"
+  QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test loginWithOlderVersion.e2e.smoke.ts --configuration android.external.e2e.old_version; test_result=$? && sleep 5
 
-QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test loginWithOlderVersion.e2e.smoke.ts --configuration android.external.e2e.old_version; test_result=$? && sleep 5
-
-if ((test_result != 0)); then
-  exit 1
+  if ((test_result != 0)); then
+    exit 1
+  fi
+else
+  echo "This is not a regression run so not running the updateAppVersion tests"
 fi
--- packages/core-mobile/e2e/tests/updateAppVersion/updateToLatestVersion.sh
+++ packages/core-mobile/e2e/tests/updateAppVersion/updateToLatestVersion.sh
@@ -1,14 +1,17 @@
 set -o pipefail
+if [$IS_REGRESSION_RUN = true]; then
+  npm rebuild detox
 
-npm rebuild detox
+  adb install -r $BITRISE_APK_PATH
+  adb install -r $BITRISE_TEST_APK_PATH
 
-adb install -r $BITRISE_APK_PATH
-adb install -r $BITRISE_TEST_APK_PATH
+  QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test loginWithLatestVersion.e2e.smoke.ts --configuration android.external.release.ci --reuse; test_result=$? && sleep 5
 
-QT_QPA_PLATFORM=xcb ./node_modules/.bin/detox test loginWithLatestVersion.e2e.smoke.ts --configuration android.external.release.ci --reuse; test_result=$? && sleep 5
+  npx ts-node ./e2e/attachLogsSendResultsToTestrail.ts
 
-npx ts-node ./e2e/attachLogsSendResultsToTestrail.ts
-
-if ((test_result != 0)); then
-  exit 1
+  if ((test_result != 0)); then
+    exit 1
+  fi
+else
+  echo "This is not a regression run so not running the updateAppVersion tests"
 fi
