8385e990d3841c855477c39a0f772546a6547620
onghwan <onghwan@gmail.com>
2025-01-13 16:07:03 -0500

    fix wrong import

--- packages/core-mobile/app/screens/nft/components/NftGrid/GridItem.tsx
+++ packages/core-mobile/app/screens/nft/components/NftGrid/GridItem.tsx
@@ -8,7 +8,7 @@ import { NFTItem } from 'store/nft'
 import { SvgXml } from 'react-native-svg'
 import OvalTagBg from 'components/OvalTagBg'
 import { isErc1155 } from 'services/nft/utils'
-import { FastImage } from 'react-native-fast-image'
+import FastImage from 'react-native-fast-image'
 
 const SCREEN_WIDTH = Dimensions.get('window')?.width
 const GRID_ITEM_MARGIN = 8
