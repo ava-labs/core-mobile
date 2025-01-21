678cf45a2dd7b6d56dcd4e961f5427a8bf7f73df
ruijialin <ruijia.lin@avalabs.org>
2025-01-21 16:10:00 -0500

    test again

--- packages/core-mobile/app/services/passkey/consts.ts
+++ packages/core-mobile/app/services/passkey/consts.ts
@@ -1,4 +1,4 @@
 export const RP_NAME = 'Core'
-export const RP_ID = '8d36763b.core-identity.pages.dev/'
+export const RP_ID = 'identity.core.app'
 export const FIDO_CALLBACK_URL = 'core://fido/callback/'
 export const FIDO_TIMEOUT = 90000 // 90 seconds
