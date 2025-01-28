bbd9f8bc0a3b40cba14dd9e5a5d2142333a1d4df
onghwan <onghwan@gmail.com>
2025-01-28 15:50:26 -0500

    import expo-asset to fix image loading issue on iOS release build

--- packages/core-mobile/index.js
+++ packages/core-mobile/index.js
@@ -1,3 +1,6 @@
+// import expo-asset to fix the issue with the image loading in ios release build
+// https://github.com/expo/expo/issues/31377#issuecomment-2556120583
+import 'expo-asset'
 import {
   AppRegistry,
   Text,
