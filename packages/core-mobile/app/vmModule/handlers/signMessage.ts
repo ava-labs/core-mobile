d218eea1a385e3e799e5d5ae2c644c4b194a9993
onghwan <onghwan@gmail.com>
2025-01-28 14:03:22 -0500

    fix tsc error

--- packages/core-mobile/app/vmModule/handlers/signMessage.ts
+++ packages/core-mobile/app/vmModule/handlers/signMessage.ts
@@ -1,7 +1,7 @@
 import { Network } from '@avalabs/core-chains-sdk'
 import {
   ApprovalResponse,
-  RpcMethod as VmModuleRpcMethod,
+  RpcMethod,
   TypedData,
   MessageTypes,
   TypedDataV1
@@ -9,7 +9,6 @@ import {
 import WalletService from 'services/wallet/WalletService'
 import { rpcErrors } from '@metamask/rpc-errors'
 import { Account } from 'store/account/types'
-import { RpcMethod } from 'store/rpc'
 
 export const signMessage = async ({
   method,
@@ -18,7 +17,7 @@ export const signMessage = async ({
   network,
   resolve
 }: {
-  method: VmModuleRpcMethod
+  method: RpcMethod
   data: string | TypedData<MessageTypes> | TypedDataV1
   network: Network
   account: Account
@@ -26,7 +25,7 @@ export const signMessage = async ({
 }): Promise<void> => {
   try {
     const signedMessage = await WalletService.signMessage({
-      rpcMethod: method as unknown as RpcMethod,
+      rpcMethod: method,
       data,
       accountIndex: account.index,
       network
