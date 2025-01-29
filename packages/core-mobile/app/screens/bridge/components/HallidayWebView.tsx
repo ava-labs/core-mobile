919044e17e6881e467774eff1408b666716a86aa
ruijialin <ruijia.lin@avalabs.org>
2025-01-29 13:39:20 -0500

    remove reload logic

--- packages/core-mobile/app/screens/bridge/components/HallidayWebView.tsx
+++ packages/core-mobile/app/screens/bridge/components/HallidayWebView.tsx
@@ -2,25 +2,16 @@ import { WebView } from 'components/WebView'
 import { useInjectedJavascript } from 'hooks/browser/useInjectedJavascript'
 import React from 'react'
 
-const HALLIDAY_BRIDGE_URL = 'https://core.app/bridge?useHalliday=1&useEmbed=1'
+const HALLIDAY_BRIDGE_URL =
+  'http://localhost:3000/bridge?useHalliday=1&useEmbed=1'
 
 export const HallidayWebView = (): React.JSX.Element => {
   const { coreConnectInterceptor } = useInjectedJavascript()
 
-  // reload iframe with 5 seconds delay
-  const checkAndReloadIframeScript = `
-    const iframe = document.getElementById('halliday-iframe');
-    if (iframe) {      
-      setTimeout(() => {
-        iframe.src = iframe.src;
-      }, 5000); // Delay of 5 seconds
-    };
-  `
-
   return (
     <WebView
       url={HALLIDAY_BRIDGE_URL}
-      injectedJavaScript={checkAndReloadIframeScript + coreConnectInterceptor}
+      injectedJavaScript={coreConnectInterceptor}
       testID="halliday-bridge-webview"
     />
   )
