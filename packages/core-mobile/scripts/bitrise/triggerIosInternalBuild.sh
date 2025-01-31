f97b5f3a0900ff79c34cc46b5b728a77af88fea7
Tyler Hackett <thackett86181@gmail.com>
2025-01-31 09:22:19 -0800

    add triggered_at conditon

--- packages/core-mobile/scripts/bitrise/triggerIosInternalBuild.sh
+++ packages/core-mobile/scripts/bitrise/triggerIosInternalBuild.sh
@@ -18,33 +18,38 @@ BUILDS_IN_PROGRESS=$(
 }"
 )
 
+echo $BUILDS_IN_PROGRESS
+
 # Parses the first two builds in progress for their build numbers. 
 # If the builds are triggered in the same workflow then the build numbers will be the same and we don't need to trigger the ios build
 # If the build numbers are different then we can trigger the ios build
 build_number=$( jq -r '.data[0].build_number' <<< "$BUILDS_IN_PROGRESS" )
 build_number_two=$( jq -r '.data[1].build_number' <<< "$BUILDS_IN_PROGRESS" )
+build_triggered_at=$( jq -r '.data[0].triggered_at' <<< "$BUILDS_IN_PROGRESS" )
+build_triggered_at_two=$( jq -r '.data[1].triggered_at' <<< "$BUILDS_IN_PROGRESS" )
 
-if [[ "$build_number" != "$build_number_two" ]]; then
+array_of_build_numbers=$( jq -r '.data[].build_number' <<< "$BUILDS_IN_PROGRESS" )
+
+if [[ "$build_number" != "$build_number_two" ]] && [[ "$build_triggered_at" != "$build_triggered_at_two" ]]; then
   echo "start the ios build"
 
   PIPELINE=$(
   curl -X 'POST' \
-  "https://api.bitrise.io/v0.1/apps/$BITRISE_APP_SLUG/builds" \
-  -H 'accept: application/json' \
-  -H "Authorization: $BITRISE_ACCESS_TOKEN" \
-  -H 'Content-Type: application/json' \
-  -d "{
-  \"build_params\": {
-    \"branch\": \"$BITRISE_GIT_BRANCH\",
-    \"pipeline_id\": \"build-ios-apps-internal-triggered-e2e\",
-    \"commit_message\": \"No commit message\",
-  },
-  \"hook_info\": {
-    \"type\": \"bitrise\"
-  }
-}"
-  )
-
+    "https://api.bitrise.io/v0.1/apps/$BITRISE_APP_SLUG/builds" \
+    -H 'accept: application/json' \
+    -H "Authorization: $BITRISE_ACCESS_TOKEN" \
+    -H 'Content-Type: application/json' \
+    -d "{
+    \"build_params\": {
+      \"branch\": \"$BITRISE_GIT_BRANCH\",
+      \"pipeline_id\": \"build-ios-apps-internal-triggered-e2e\",
+      \"commit_message\": \"$GIT_CLONE_COMMIT_MESSAGE_SUBJECT\"
+    },
+    \"hook_info\": {
+      \"type\": \"bitrise\"
+    }
+  }"
+)
   echo "PIPELINE: $PIPELINE"
 else
   echo "Build already in progress"
