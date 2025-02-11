eb125760c04196905929f5e07dff9e40245566db
onghwan <onghwan@gmail.com>
2025-02-11 11:15:49 -0500

    design feedback

--- packages/core-mobile/app/new/routes/onboarding/seedless/(totp)/_layout.tsx
+++ packages/core-mobile/app/new/routes/onboarding/seedless/(totp)/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from 'common/components/Stack'
 
 export default function TotpLayout(): JSX.Element {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false
+      }}>
       <Stack.Screen name="authenticatorSetup" />
       <Stack.Screen name="scanQrCode" />
       <Stack.Screen name="copyCode" />
