ae24f3f3297dea30bf3215ea33fcb8cc846b993a
ruijialin <ruijia.lin@avalabs.org>
2025-02-11 13:22:55 -0500

    add assets to k2-alpine

--- packages/k2-alpine/src/theme/tokens/Logos.ts
+++ packages/k2-alpine/src/theme/tokens/Logos.ts
@@ -1,5 +1,33 @@
 import LogoCore from '../../assets/logos/core.svg'
+import CoreAppIconDark from '../../assets/logos/core-app-icon-dark.svg'
+import CoreAppIconLight from '../../assets/logos/core-app-icon-light.svg'
+import CoreAppIconDev from '../../assets/logos/core-app-icon-dev.svg'
+
+// partner logos
+import CoinbasePay from '../../assets/logos/coinbase-pay.svg'
+import Halliday from '../../assets/logos/halliday.svg'
+import Moonpay from '../../assets/logos/moonpay.svg'
+import Stripe from '../../assets/logos/stripe.svg'
+import PoweredByCircleDark from '../../assets/logos/powered-by-circle-dark.svg'
+import PoweredByCircleLight from '../../assets/logos/powered-by-circle-light.svg'
+import PoweredByParaswapDark from '../../assets/logos/powered-by-paraswap-dark.svg'
+import PoweredByParaswapLight from '../../assets/logos/powered-by-paraswap-light.svg'
 
 export const Logos = {
-  Core: LogoCore
+  AppIcons: {
+    Core: LogoCore,
+    CoreAppIconDark: CoreAppIconDark,
+    CoreAppIconLight: CoreAppIconLight,
+    CoreAppIconDev: CoreAppIconDev
+  },
+  PartnerLogos: {
+    CoinbasePay: CoinbasePay,
+    Halliday: Halliday,
+    Moonpay: Moonpay,
+    Stripe: Stripe,
+    PoweredByCircleDark: PoweredByCircleDark,
+    PoweredByCircleLight: PoweredByCircleLight,
+    PoweredByParaswapDark: PoweredByParaswapDark,
+    PoweredByParaswapLight: PoweredByParaswapLight
+  }
 }
