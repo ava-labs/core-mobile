250782559a8b14be8a2176121bf6e43aac528765
eunjisong <eunji.l.song@gmail.com>
2025-02-11 15:42:18 -0500

    add link token back for swap

--- packages/core-mobile/e2e/helpers/tokens.ts
+++ packages/core-mobile/e2e/helpers/tokens.ts
@@ -13,6 +13,7 @@ export const SwapTokens = [
   { symbol: 'WETH.e', name: 'Wrapped ETH', amount: '0.000001' },
   { symbol: 'USDT', name: 'Tether', amount: '0.00001' },
   { symbol: 'USDC', name: 'USD Coin', amount: '0.00001' },
+  { symbol: 'LINK', name: 'Chainlink Token', amount: '0.000001' },
   { symbol: 'DAI', name: 'Dai Stablecoin', amount: '0.000001' },
   { symbol: '1INCH', name: '1INCH Token', amount: '0.000001' },
   { symbol: 'AAVE', name: 'Aave Token', amount: '0.000001' },
