da550b0c24412e1ed5f8e6ef805c3b84e32daf23
ruijialin <ruijia.lin@avalabs.org>
2025-02-12 11:51:02 -0500

    update icons import

---
+++ packages/k2-alpine/src/theme/tokens/Icons.ts
@@ -0,0 +1,327 @@
+import IconCheck from '../../assets/icons/check.svg'
+import IconExpandMore from '../../assets/icons/expand_more.svg'
+import IconBackArrowCustom from '../../assets/icons/back_arrow_custom.svg'
+import IconFaceID from '../../assets/icons/face_id.svg'
+import IconTouchID from '../../assets/icons/touch_id.svg'
+import IconPin from '../../assets/icons/pin.svg'
+import IconInfo from '../../assets/icons/info.svg'
+import IconCheckCircleOutline from '../../assets/icons/check_circle_outline.svg'
+import IconErrorOutline from '../../assets/icons/error_outline.svg'
+import IconError from '../../assets/icons/error.svg'
+import IconRemoveModerator from '../../assets/icons/remove_moderator.svg'
+import IconGPPMaybe from '../../assets/icons/gpp_maybe.svg'
+import IconPasskey from '../../assets/icons/passkey.svg'
+import IconAuthenticator from '../../assets/icons/authenticator.svg'
+import IconYubikey from '../../assets/icons/yubikey.svg'
+import IconApple from '../../assets/icons/apple.svg'
+import IconGoogle from '../../assets/icons/google.svg'
+import IconAdd from '../../assets/icons/add.svg'
+import IconArrowForwardIOS from '../../assets/icons/arrow_forward_ios.svg'
+import IconClear from '../../assets/icons/clear.svg'
+import IconCopy from '../../assets/icons/copy.svg'
+import IconQrCode from '../../assets/icons/qr_code.svg'
+import IconGoogleAuthenticator from '../../assets/icons/google_authenticator.svg'
+import IconMicrosoftAuthenticator from '../../assets/icons/microsoft_authenticator.svg'
+import IconAuthenticatorApp from '../../assets/icons/authenticator_app.svg'
+import IconChevronRight from '../../assets/icons/chevron_right.svg'
+import IconSwitchRight from '../../assets/icons/switch_right.svg'
+import IconCompare from '../../assets/icons/compare.svg'
+import IconSend from '../../assets/icons/send.svg'
+import IconPsychiatry from '../../assets/icons/psychiatry.svg'
+import IconTrendingArrowUp from '../../assets/icons/trending_arrow_up.svg'
+import IconTrendingArrowDown from '../../assets/icons/trending_arrow_down.svg'
+import IconQRCode2 from '../../assets/icons/qr_code_2.svg'
+import IconNotifications from '../../assets/icons/notifications.svg'
+import IconAlertError from '../../assets/icons/alert_error.svg'
+import IconArrowDown from '../../assets/icons/arrow_down.svg'
+import IconCompass from '../../assets/icons/compass.svg'
+import IconWalletConnect from '../../assets/icons/wallet_connect.svg'
+import IconContactLess from '../../assets/icons/contactless.svg'
+import IconOutboundDark from '../../assets/icons/outbound_filled_dark.svg'
+import IconOutboundLight from '../../assets/icons/outbound_filled_light.svg'
+import IconKidStar from '../../assets/icons/kid_star.svg'
+import IconStarOutline from '../../assets/icons/star_outline.svg'
+import IconStarFilled from '../../assets/icons/star_filled.svg'
+import IconSignature from '../../assets/icons/signature.svg'
+import IconArrowDownHandleBar from '../../assets/icons/arrow_down_handlebar.svg'
+import IconSignPost from '../../assets/icons/signpost.svg'
+import IconSync from '../../assets/icons/sync.svg'
+import IconVisibilityOff from '../../assets/icons/visibility_off.svg'
+import IconShareIOS from '../../assets/icons/ios_share.svg'
+import IconSearchCustom from '../../assets/icons/search_custom.svg'
+import IconAlertCircleDark from '../../assets/icons/alert_circle_dark.svg'
+import IconAlertCircleLight from '../../assets/icons/alert_circle_light.svg'
+import IconAvalabsTrademark from '../../assets/icons/avalabs_trademark.svg'
+import IconChevronRightV2 from '../../assets/icons/chevron_right_v2.svg'
+
+// token logos
+import AAVE from '../../assets/tokenLogos/AAVE.svg'
+import ADA from '../../assets/tokenLogos/ADA.svg'
+import APT from '../../assets/tokenLogos/APT.svg'
+import ARB from '../../assets/tokenLogos/ARB.svg'
+import ATOM from '../../assets/tokenLogos/ATOM.svg'
+import AVAX1 from '../../assets/tokenLogos/AVAX-1.svg'
+import AVAX from '../../assets/tokenLogos/AVAX.svg'
+import BCH from '../../assets/tokenLogos/BCH.svg'
+import BNB from '../../assets/tokenLogos/BNB.svg'
+import BTC from '../../assets/tokenLogos/BTC.svg'
+import DAI from '../../assets/tokenLogos/DAI.svg'
+import DOGE from '../../assets/tokenLogos/DOGE.svg'
+import DOT from '../../assets/tokenLogos/DOT.svg'
+import ETH1 from '../../assets/tokenLogos/ETH-1.svg'
+import ETH from '../../assets/tokenLogos/ETH.svg'
+import FIL from '../../assets/tokenLogos/FIL.svg'
+import FLOW from '../../assets/tokenLogos/FLOW.svg'
+import GRT from '../../assets/tokenLogos/GRT.svg'
+import HBAR from '../../assets/tokenLogos/HBAR.svg'
+import ICP from '../../assets/tokenLogos/ICP.svg'
+import IMX from '../../assets/tokenLogos/IMX.svg'
+import LDO from '../../assets/tokenLogos/LDO.svg'
+import LEO from '../../assets/tokenLogos/LEO.svg'
+import LINK from '../../assets/tokenLogos/LINK.svg'
+import MATIC from '../../assets/tokenLogos/MATIC.svg'
+import NEAR from '../../assets/tokenLogos/NEAR.svg'
+import OKB from '../../assets/tokenLogos/OKB.svg'
+import ONDO from '../../assets/tokenLogos/ONDO.svg'
+import QNT from '../../assets/tokenLogos/QNT.svg'
+import sAVAX from '../../assets/tokenLogos/sAVAX.svg'
+import SHIB from '../../assets/tokenLogos/SHIB.svg'
+import SOL from '../../assets/tokenLogos/SOL.svg'
+import STX from '../../assets/tokenLogos/STX.svg'
+import SUI from '../../assets/tokenLogos/SUI.svg'
+import TON from '../../assets/tokenLogos/TON.svg'
+import TRX from '../../assets/tokenLogos/TRX.svg'
+import UNI from '../../assets/tokenLogos/UNI.svg'
+import USDC from '../../assets/tokenLogos/USDC.svg'
+import USDT from '../../assets/tokenLogos/USDT.svg'
+import VET from '../../assets/tokenLogos/VET.svg'
+import XLM from '../../assets/tokenLogos/XLM.svg'
+import XRP from '../../assets/tokenLogos/XRP.svg'
+import IconAvaxP from '../../assets/tokenLogos/AVAX_P_LIGHT.svg'
+import IconAvaxX from '../../assets/tokenLogos/AVAX_X_LIGHT.svg'
+import IconAvaxPDark from '../../assets/tokenLogos/AVAX_P_DARK.svg'
+import IconAvaxXDark from '../../assets/tokenLogos/AVAX_X_DARK.svg'
+
+// Currency/Flag Icons
+import AED from '../../assets/currency-flagKit/currency-aed.svg'
+import ARS from '../../assets/currency-flagKit/currency-ars.svg'
+import AUD from '../../assets/currency-flagKit/currency-aud.svg'
+import bdt from '../../assets/currency-flagKit/currency-bdt.svg'
+import BGN from '../../assets/currency-flagKit/currency-bgn.svg'
+import BRL from '../../assets/currency-flagKit/currency-brl.svg'
+import CAD from '../../assets/currency-flagKit/currency-cad.svg'
+import CHF from '../../assets/currency-flagKit/currency-chf.svg'
+import CLP from '../../assets/currency-flagKit/currency-clp.svg'
+import CNY from '../../assets/currency-flagKit/currency-cny.svg'
+import COP from '../../assets/currency-flagKit/currency-cop.svg'
+import CZK from '../../assets/currency-flagKit/currency-czk.svg'
+import DKK from '../../assets/currency-flagKit/currency-dkk.svg'
+import EGP from '../../assets/currency-flagKit/currency-egp.svg'
+import EUR from '../../assets/currency-flagKit/currency-eur.svg'
+import GBP from '../../assets/currency-flagKit/currency-gbp.svg'
+import HKD from '../../assets/currency-flagKit/currency-hkd.svg'
+import HUF from '../../assets/currency-flagKit/currency-huf.svg'
+import IDR1 from '../../assets/currency-flagKit/currency-idr-1.svg'
+import IDR from '../../assets/currency-flagKit/currency-idr.svg'
+import ILS from '../../assets/currency-flagKit/currency-ils.svg'
+import INR from '../../assets/currency-flagKit/currency-inr.svg'
+import IRR from '../../assets/currency-flagKit/currency-irr.svg'
+import JPY from '../../assets/currency-flagKit/currency-jpy.svg'
+import KRW from '../../assets/currency-flagKit/currency-krw.svg'
+import MAD from '../../assets/currency-flagKit/currency-mad.svg'
+import MXN from '../../assets/currency-flagKit/currency-mxn.svg'
+import MYR from '../../assets/currency-flagKit/currency-myr.svg'
+import NGN from '../../assets/currency-flagKit/currency-ngn.svg'
+import NOK from '../../assets/currency-flagKit/currency-nok.svg'
+import NZD from '../../assets/currency-flagKit/currency-nzd.svg'
+import PHP from '../../assets/currency-flagKit/currency-php.svg'
+import PKR from '../../assets/currency-flagKit/currency-pkr.svg'
+import PLN from '../../assets/currency-flagKit/currency-pln.svg'
+import RON from '../../assets/currency-flagKit/currency-ron.svg'
+import RUB from '../../assets/currency-flagKit/currency-rub.svg'
+import SAR from '../../assets/currency-flagKit/currency-sar.svg'
+import SEK from '../../assets/currency-flagKit/currency-sek.svg'
+import SGD from '../../assets/currency-flagKit/currency-sgd.svg'
+import THB from '../../assets/currency-flagKit/currency-thb.svg'
+import TRY from '../../assets/currency-flagKit/currency-try.svg'
+import TWD from '../../assets/currency-flagKit/currency-twd.svg'
+import USD from '../../assets/currency-flagKit/currency-usd.svg'
+import VND from '../../assets/currency-flagKit/currency-vnd.svg'
+import ZAR from '../../assets/currency-flagKit/currency-zar.svg'
+
+export const Icons = {
+  Action: {
+    Info: IconInfo,
+    CheckCircleOutline: IconCheckCircleOutline,
+    Clear: IconClear,
+    VisibilityOff: IconVisibilityOff
+  },
+  Alert: {
+    ErrorOutline: IconErrorOutline,
+    Error: IconAlertError,
+    AlertCircleDark: IconAlertCircleDark,
+    AlertCircleLight: IconAlertCircleLight
+  },
+  Device: {
+    GPPMaybe: IconGPPMaybe
+  },
+  Navigation: {
+    ArrowForwardIOS: IconArrowForwardIOS,
+    ChevronRightV2: IconChevronRightV2,
+    ChevronRight: IconChevronRight,
+    Check: IconCheck,
+    ExpandMore: IconExpandMore
+  },
+  Notification: {
+    Sync: IconSync
+  },
+  Social: {
+    RemoveModerator: IconRemoveModerator,
+    Notifications: IconNotifications,
+    ShareIOS: IconShareIOS
+  },
+  Communication: {
+    QRCode2: IconQRCode2
+  },
+  Content: {
+    Add: IconAdd
+  },
+  Toggle: {
+    StarOutline: IconStarOutline,
+    StarFilled: IconStarFilled
+  },
+  Custom: {
+    BackArrowCustom: IconBackArrowCustom,
+    Compare: IconCompare,
+    FaceID: IconFaceID,
+    TouchID: IconTouchID,
+    Send: IconSend,
+    SwitchRight: IconSwitchRight,
+    Pin: IconPin,
+    Psychiatry: IconPsychiatry,
+    Error: IconError,
+    TrendingArrowUp: IconTrendingArrowUp,
+    TrendingArrowDown: IconTrendingArrowDown,
+    ArrowDown: IconArrowDown,
+    Connect: IconWalletConnect,
+    Compass: IconCompass,
+    Contactless: IconContactLess,
+    OutboundDark: IconOutboundDark,
+    OutboundLight: IconOutboundLight,
+    KidStar: IconKidStar,
+    Signature: IconSignature,
+    ArrowDownHandleBar: IconArrowDownHandleBar,
+    SearchCustom: IconSearchCustom,
+    AvalabsTrademark: IconAvalabsTrademark,
+    SignPost: IconSignPost
+  },
+  RecoveryMethod: {
+    Passkey: IconPasskey,
+    Authenticator: IconAuthenticator,
+    Yubikey: IconYubikey,
+    Copy: IconCopy,
+    QrCode: IconQrCode
+  },
+  Logos: {
+    Apple: IconApple,
+    Google: IconGoogle,
+    GoogleAuthenticator: IconGoogleAuthenticator,
+    MicrosoftAuthenticator: IconMicrosoftAuthenticator,
+    AuthenticatorApp: IconAuthenticatorApp
+  },
+  TokenLogos: {
+    AAVE: AAVE,
+    ADA: ADA,
+    APT: APT,
+    ARB: ARB,
+    ATOM: ATOM,
+    AVAX1: AVAX1,
+    AVAX: AVAX,
+    BCH: BCH,
+    BNB: BNB,
+    BTC: BTC,
+    DAI: DAI,
+    DOGE: DOGE,
+    DOT: DOT,
+    ETH1: ETH1,
+    ETH: ETH,
+    FIL: FIL,
+    FLOW: FLOW,
+    GRT: GRT,
+    HBAR: HBAR,
+    ICP: ICP,
+    IMX: IMX,
+    LDO: LDO,
+    LEO: LEO,
+    LINK: LINK,
+    MATIC: MATIC,
+    NEAR: NEAR,
+    OKB: OKB,
+    ONDO: ONDO,
+    QNT: QNT,
+    sAVAX: sAVAX,
+    SHIB: SHIB,
+    SOL: SOL,
+    STX: STX,
+    SUI: SUI,
+    TON: TON,
+    TRX: TRX,
+    UNI: UNI,
+    USDC: USDC,
+    USDT: USDT,
+    VET: VET,
+    XLM: XLM,
+    XRP: XRP,
+    AVAX_P_LIGHT: IconAvaxP,
+    AVAX_X_LIGHT: IconAvaxX,
+    AVAX_P_DARK: IconAvaxPDark,
+    AVAX_X_DARK: IconAvaxXDark
+  },
+  Currencies: {
+    AED: AED,
+    ARS: ARS,
+    AUD: AUD,
+    BDT: bdt,
+    BGN: BGN,
+    BRL: BRL,
+    CAD: CAD,
+    CHF: CHF,
+    CLP: CLP,
+    CNY: CNY,
+    COP: COP,
+    CZK: CZK,
+    DKK: DKK,
+    EGP: EGP,
+    EUR: EUR,
+    GBP: GBP,
+    HKD: HKD,
+    HUF: HUF,
+    IDR1: IDR1,
+    IDR: IDR,
+    ILS: ILS,
+    INR: INR,
+    IRR: IRR,
+    JPY: JPY,
+    KRW: KRW,
+    MAD: MAD,
+    MXN: MXN,
+    MYR: MYR,
+    NGN: NGN,
+    NOK: NOK,
+    NZD: NZD,
+    PHP: PHP,
+    PKR: PKR,
+    PLN: PLN,
+    RON: RON,
+    RUB: RUB,
+    SAR: SAR,
+    SEK: SEK,
+    SGD: SGD,
+    THB: THB,
+    TRY: TRY,
+    TWD: TWD,
+    USD: USD,
+    VND: VND,
+    ZAR: ZAR
+  }
+}
