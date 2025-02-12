9ca08ced4862631e836a1923599eb95932860cef
ruijialin <ruijia.lin@avalabs.org>
2025-02-12 13:01:04 -0500

    fix createPin keyboard issue on android

--- packages/core-mobile/app/new/features/onboarding/components/CreatePin.tsx
+++ packages/core-mobile/app/new/features/onboarding/components/CreatePin.tsx
@@ -12,7 +12,6 @@ import {
 import { useCreatePin } from 'features/onboarding/hooks/useCreatePin'
 import { Switch } from 'react-native'
 import ScreenHeader from 'common/components/ScreenHeader'
-import { KeyboardAvoidingView } from 'common/components/KeyboardAvoidingView'
 
 export const CreatePin = ({
   useBiometrics,
@@ -58,65 +57,63 @@ export const CreatePin = ({
 
   return (
     <BlurredBarsContentLayout>
-      <KeyboardAvoidingView>
-        <SafeAreaView sx={{ flex: 1 }}>
-          <ScrollView
-            sx={{ flex: 1 }}
-            contentContainerSx={{
-              padding: 16,
-              flex: 1
+      <SafeAreaView sx={{ flex: 1 }}>
+        <ScrollView
+          sx={{ flex: 1 }}
+          contentContainerSx={{
+            padding: 16,
+            flex: 1
+          }}>
+          <ScreenHeader
+            title={
+              chosenPinEntered
+                ? 'Confirm your PIN code'
+                : 'Secure your wallet with a PIN'
+            }
+            description={
+              chosenPinEntered
+                ? undefined
+                : 'For extra security, avoid choosing a PIN that contains repeating digits in a sequential order'
+            }
+          />
+          <View
+            sx={{
+              flex: 1,
+              justifyContent: 'center'
             }}>
-            <ScreenHeader
-              title={
-                chosenPinEntered
-                  ? 'Confirm your PIN code'
-                  : 'Secure your wallet with a PIN'
-              }
-              description={
-                chosenPinEntered
-                  ? undefined
-                  : 'For extra security, avoid choosing a PIN that contains repeating digits in a sequential order'
+            <PinInput
+              ref={ref}
+              length={6}
+              value={chosenPinEntered ? confirmedPin : chosenPin}
+              onChangePin={
+                chosenPinEntered ? onEnterConfirmedPin : onEnterChosenPin
               }
             />
-            <View
-              sx={{
-                flex: 1,
-                justifyContent: 'center'
-              }}>
-              <PinInput
-                ref={ref}
-                length={6}
-                value={chosenPinEntered ? confirmedPin : chosenPin}
-                onChangePin={
-                  chosenPinEntered ? onEnterConfirmedPin : onEnterChosenPin
+          </View>
+        </ScrollView>
+        {!chosenPinEntered && (
+          <View
+            sx={{
+              paddingHorizontal: 16,
+              paddingBottom: 16,
+              backgroundColor: '$surfacePrimary'
+            }}>
+            <GroupList
+              data={[
+                {
+                  title: 'Unlock with Face ID',
+                  accessory: (
+                    <Switch
+                      value={useBiometrics}
+                      onValueChange={setUseBiometrics}
+                    />
+                  )
                 }
-              />
-            </View>
-          </ScrollView>
-          {!chosenPinEntered && (
-            <View
-              sx={{
-                paddingHorizontal: 16,
-                paddingBottom: 16,
-                backgroundColor: '$surfacePrimary'
-              }}>
-              <GroupList
-                data={[
-                  {
-                    title: 'Unlock with Face ID',
-                    accessory: (
-                      <Switch
-                        value={useBiometrics}
-                        onValueChange={setUseBiometrics}
-                      />
-                    )
-                  }
-                ]}
-              />
-            </View>
-          )}
-        </SafeAreaView>
-      </KeyboardAvoidingView>
+              ]}
+            />
+          </View>
+        )}
+      </SafeAreaView>
     </BlurredBarsContentLayout>
   )
 }
--- packages/core-mobile/app/new/features/portfolio/components/ListFilterHeader.tsx
+++ packages/core-mobile/app/new/features/portfolio/components/ListFilterHeader.tsx
@@ -8,6 +8,7 @@ import {
 } from '@avalabs/k2-alpine'
 import React from 'react'
 import { Rect } from 'react-native-popover-view'
+import { Platform } from 'react-native'
 import { Selection } from './assets/useFilterAndSort'
 
 interface Props {
@@ -23,6 +24,22 @@ export const ListFilterHeader = ({
 }: Props): React.JSX.Element => {
   const { theme } = useTheme()
   const tintColor = getTintColor('secondary', theme, false)
+
+  const rect = sort.anchorRect
+    ? Platform.OS === 'ios'
+      ? new Rect(
+          sort.anchorRect.width + sort.anchorRect.x + 8,
+          sort.anchorRect.y,
+          sort.anchorRect.width,
+          sort.anchorRect.height
+        )
+      : new Rect(
+          sort.anchorRect.width + sort.anchorRect.x,
+          sort.anchorRect.y + sort.anchorRect.height,
+          sort.anchorRect.width,
+          sort.anchorRect.height
+        )
+    : undefined
 
   return (
     <View
@@ -67,16 +84,7 @@ export const ListFilterHeader = ({
             {sort.title}
           </Button>
           <SimpleDropdown
-            from={
-              sort.anchorRect
-                ? new Rect(
-                    sort.anchorRect.width + sort.anchorRect.x + 8,
-                    sort.anchorRect.y,
-                    sort.anchorRect.width,
-                    sort.anchorRect.height
-                  )
-                : undefined
-            }
+            from={rect}
             sections={sort.data}
             isVisible={sort.isPopoverVisible}
             onRequestClose={sort.onHidePopover}
