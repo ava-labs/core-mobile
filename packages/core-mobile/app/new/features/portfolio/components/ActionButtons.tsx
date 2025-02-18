f4c66a33fc77c275711567960fe3af49fbf6877f
ruijialin <ruijia.lin@avalabs.org>
2025-02-18 13:57:32 -0500

    fix header backgroundColor

--- packages/core-mobile/app/new/features/portfolio/components/ActionButtons.tsx
+++ packages/core-mobile/app/new/features/portfolio/components/ActionButtons.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import Animated, { LinearTransition } from 'react-native-reanimated'
-import {
-  SquareButton,
-  SquareButtonIconType,
-  useTheme
-} from '@avalabs/k2-alpine'
+import { SquareButton, SquareButtonIconType } from '@avalabs/k2-alpine'
 import { useOnPressAnimation } from './assets/useOnPressAnimation'
 import { ActionButtonTitle, getItemEnteringAnimation } from './assets/consts'
 
@@ -13,9 +9,6 @@ export const ActionButtons = ({
 }: {
   buttons: ActionButton[]
 }): JSX.Element => {
-  const {
-    theme: { colors }
-  } = useTheme()
   const { animatedStyle } = useOnPressAnimation()
 
   const renderActionItem = (item: ActionButton, index: number): JSX.Element => {
@@ -36,7 +29,7 @@ export const ActionButtons = ({
 
   return (
     <Animated.FlatList
-      style={{ overflow: 'visible', backgroundColor: colors.$surfacePrimary }}
+      style={{ overflow: 'visible' }}
       contentContainerStyle={{ gap: 10, paddingHorizontal: 16 }}
       horizontal
       scrollEventThrottle={16}
