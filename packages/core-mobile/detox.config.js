80b780adb044d04b88d71f313cf9daaf74a54eb0
Tyler Hackett <thackett86181@gmail.com>
2025-02-26 09:26:13 -0800

    reduce workers

--- packages/core-mobile/detox.config.js
+++ packages/core-mobile/detox.config.js
@@ -17,7 +17,7 @@ module.exports = {
     simulator: {
       type: 'ios.simulator',
       device: {
-        type: 'iPhone 16',
+        type: 'iPhone 15 Pro',
         os: 'iOS 17.5'
       }
     },
