240bf351681a867804f3fefedbc1eedae9b954cb
Tyler Hackett <thackett86181@gmail.com>
2025-02-26 09:54:35 -0800

    remove conflicts

--- packages/core-mobile/scripts/bitrise/triggerIosInternalBuild.sh
+++ packages/core-mobile/scripts/bitrise/triggerIosInternalBuild.sh
@@ -3,8 +3,6 @@
 BUILDS_IN_PROGRESS=$(
   curl -X 'GET' \
   "https://api.bitrise.io/v0.1/apps/$BITRISE_APP_SLUG/builds?status=0" \
-<<<<<<< Updated upstream
-=======
   -H 'accept: application/json' \
   -H "Authorization: $BITRISE_ACCESS_TOKEN" \
   -H 'Content-Type: application/json' \
@@ -20,40 +18,6 @@ BUILDS_IN_PROGRESS=$(
 }"
 )
 
-# Parses the first two builds in progress for their build numbers. 
-# If the builds are triggered in the same workflow then the build numbers will be the same and we don't need to trigger the ios build
-# If the build numbers are different then we can trigger the ios build
-build_number=$( jq -r '.data[0].build_number' <<< "$BUILDS_IN_PROGRESS" )
-build_number_two=$( jq -r '.data[1].build_number' <<< "$BUILDS_IN_PROGRESS" )
-
-if [[ "$build_number" != "$build_number_two" ]]; then
-  echo "start the ios build"
-
-  PIPELINE=$(
-  curl -X 'POST' \
-  "https://api.bitrise.io/v0.1/apps/$BITRISE_APP_SLUG/builds" \
->>>>>>> Stashed changes
-  -H 'accept: application/json' \
-  -H "Authorization: $BITRISE_ACCESS_TOKEN" \
-  -H 'Content-Type: application/json' \
-  -d "{
-  \"build_params\": {
-    \"branch\": \"$BITRISE_GIT_BRANCH\",
-    \"pipeline_id\": \"build-ios-apps-internal-triggered-e2e\",
-    \"commit_message\": \"No commit message\",
-  },
-  \"hook_info\": {
-    \"type\": \"bitrise\"
-  }
-}"
-else
-  echo "Build already in progress"
-fi
-
-
-
-
-<<<<<<< Updated upstream
 echo $BUILDS_IN_PROGRESS
 
 # Parses the first two builds in progress for their build numbers. 
@@ -90,9 +54,3 @@ if [[ "$build_number" != "$build_number_two" ]] && [[ "$build_triggered_at" != "
 else
   echo "Build already in progress"
 fi
-
-
-
-
-=======
->>>>>>> Stashed changes
