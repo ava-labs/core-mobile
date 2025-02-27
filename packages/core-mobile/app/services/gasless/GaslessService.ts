47515a44bcd14d2fce80c98d72f3c7f69618154c
Neven <piaf1986@gmail.com>
2025-02-27 21:46:21 +0100

    Refactor GaslessService to ensure graceful handling of uninitialized SDK

--- packages/core-mobile/app/services/gasless/GaslessService.ts
+++ packages/core-mobile/app/services/gasless/GaslessService.ts
@@ -1,10 +1,10 @@
-import AppCheckService from 'services/fcm/AppCheckService'
 import { FundResult, GaslessSdk } from '@avalabs/core-gasless-sdk'
 import { SigningData_EthSendTx } from '@avalabs/vm-module-types'
 import { Transaction } from 'ethers'
 import { TransactionLike } from 'ethers/src.ts/transaction/transaction'
 import Config from 'react-native-config'
 import Logger from 'utils/Logger'
+import AppCheckService from 'services/fcm/AppCheckService'
 
 if (!Config.GAS_STATION_URL) {
   Logger.warn(
@@ -13,31 +13,43 @@ if (!Config.GAS_STATION_URL) {
 }
 
 class GaslessService {
-  private sdk = new GaslessSdk({
-    gasStationUrl: Config.GAS_STATION_URL
-  })
+  private sdk: GaslessSdk | null = null
 
-  private getSdk: () => Promise<GaslessSdk> = async () => {
+  constructor() {
+    if (Config.GAS_STATION_URL) {
+      this.sdk = new GaslessSdk(Config.GAS_STATION_URL)
+    }
+  }
+
+  private getSdk = async (): Promise<GaslessSdk | null> => {
+    if (!this.sdk) {
+      return null
+    }
     const appCheckToken = (await AppCheckService.getToken()).token
     this.sdk.setAppCheckToken(appCheckToken)
     return this.sdk
   }
 
-  isEligibleForChain: (chainId: string) => Promise<boolean> = async (
-    chainId: string
-  ) => {
+  isEligibleForChain = async (chainId: string): Promise<boolean> => {
     const sdk = await this.getSdk()
+    if (!sdk) return false
     return await sdk.isEligibleForChain(chainId)
   }
 
-  fundTx: (
+  fundTx = async (
     signingData: SigningData_EthSendTx,
     addressFrom: string
-  ) => Promise<FundResult> = async (
-    signingData: SigningData_EthSendTx,
-    addressFrom: string
-  ) => {
+  ): Promise<FundResult> => {
     const sdk = await this.getSdk()
+    if (!sdk) {
+      return {
+        error: {
+          category: 'DO_NOT_RETRY',
+          message: 'INTERNAL_ERROR'
+        }
+      }
+    }
+
     const { difficulty, challengeHex } = await sdk.fetchChallenge()
     const { solutionHex } = await sdk.solveChallenge(challengeHex, difficulty)
     const txHex = Transaction.from({
