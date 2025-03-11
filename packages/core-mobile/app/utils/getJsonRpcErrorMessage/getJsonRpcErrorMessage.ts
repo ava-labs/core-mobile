d16ca97c77d8b75d02be0fc64a69db491c0c0d39
An Nguyen <an.nguyen@avalabs.org>
2025-03-11 16:47:29 -0500

    Improve json rpc error parsing (#2376)

---
+++ packages/core-mobile/app/utils/getJsonRpcErrorMessage/getJsonRpcErrorMessage.ts
@@ -0,0 +1,76 @@
+import { JsonRpcError } from '@metamask/rpc-errors'
+import { isError } from 'ethers'
+
+export const getJsonRpcErrorMessage = (error: unknown): string => {
+  if (error instanceof JsonRpcError) {
+    return parseJsonRpcError(error)
+  }
+
+  if (error instanceof Error) {
+    // some errors have a `details` property that contains a more detailed and user friendly error message
+    // (i.e. BridgeError object thrown by the @avalabs/bridge-unified)
+    if ('details' in error && typeof error.details === 'string') {
+      return error.details
+    }
+
+    return error.message
+  }
+
+  return 'Unexpected error'
+}
+
+/**
+ * Handles JSON-RPC error cases and returns a user-friendly error message.
+ * @param error - The error object to process.
+ * @returns A string containing the appropriate error message.
+ */
+export const parseJsonRpcError = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error: JsonRpcError<any>,
+  customMessage?: string
+): string => {
+  const mainMessage = customMessage || error.message
+  const cause = error.data?.cause
+
+  // Handle detailed error message based on cause
+  if (cause?.error?.message) {
+    if (cause.error.message.startsWith('transaction underpriced')) {
+      return "Transaction failed. The gas price or max priority fee is too low compared to the network's current minimum requirement. Please increase the gas and try again."
+    } else if (cause.error.message === 'already known') {
+      /**
+       * Occurs when a user resubmits the exact same transaction (same nonce, sender, and data)
+       * that’s already in the mempool or has been mined, causing the node to reject it as a duplicate.
+       */
+      return 'This transaction has already been submitted. Try resubmitting with a higher gas fee or wait for it to complete.'
+    }
+    return `${mainMessage}\nError: ${cause.error.message}`
+  }
+
+  /**
+   * Handles the REPLACEMENT_UNDERPRICED case.
+   * Occurs when a user attempts to replace a pending transaction (same nonce) with a new one,
+   * but the new transaction's gas fees (maxFeePerGas or maxPriorityFeePerGas) are too low—
+   * typically not at least 10% higher than the original.
+   */
+  if (isError(cause, 'REPLACEMENT_UNDERPRICED')) {
+    return 'Transaction failed due to an already pending transaction and network congestion. To replace the pending transaction, increase the gas and try again.'
+  }
+
+  // Handles INSUFFICIENT_FUNDS case
+  if (isError(cause, 'INSUFFICIENT_FUNDS')) {
+    return "You don't have enough funds for this transaction (including gas fees). Please add more funds or reduce the amount."
+  }
+
+  // Handles NONCE_EXPIRED case
+  if (isError(cause, 'NONCE_EXPIRED')) {
+    return 'Transaction failed because the nonce was already used. Please try submitting again.'
+  }
+
+  // Fallback for cause with a message only
+  if (cause?.message) {
+    return `${mainMessage}\nError: ${cause.message}`
+  }
+
+  // Fallback for error without cause
+  return mainMessage
+}
