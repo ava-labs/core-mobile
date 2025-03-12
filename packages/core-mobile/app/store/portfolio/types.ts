09504d48c34329fe587a46884338099e452f0e19
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-03-12 21:51:10 +0200

    CP 9708/CP-9709 (#2372)

--- packages/core-mobile/app/store/portfolio/types.ts
+++ packages/core-mobile/app/store/portfolio/types.ts
@@ -1,9 +1,12 @@
 export const initialState: PortfolioState = {
-  tokenVisibility: {}
+  tokenVisibility: {},
+  collectibleVisibility: {}
 }
 
 export type TokenVisibility = Record<string, boolean>
+export type CollectibleVisibility = Record<string, boolean>
 
 export type PortfolioState = {
   tokenVisibility: TokenVisibility
+  collectibleVisibility: CollectibleVisibility
 }
