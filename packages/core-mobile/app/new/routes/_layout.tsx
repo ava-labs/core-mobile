49c0d1eb449ec983f2a4f62b2ca329da7bb36468
JungHwan Jang <onghwan@gmail.com>
2025-03-17 10:02:08 -0400

    CP-9933: Share (#2397)

--- packages/core-mobile/app/new/routes/_layout.tsx
+++ packages/core-mobile/app/new/routes/_layout.tsx
@@ -10,7 +10,7 @@ import { selectIsReady, selectWalletState, WalletState } from 'store/app'
 import { useSelector } from 'react-redux'
 import { useBgDetect } from 'navigation/useBgDetect'
 import { useFocusEffect, useRootNavigationState, useRouter } from 'expo-router'
-import { Platform } from 'react-native'
+import { Platform, StatusBar, useColorScheme } from 'react-native'
 /**
  * Temporarily import "useNavigation" from @react-navigation/native.
  * This is a workaround due to a render bug in the expo-router version.
@@ -27,6 +27,7 @@ import {
   stackNavigatorScreenOptions
 } from 'common/consts/screenOptions'
 import { OnboardingProvider } from 'features/onboarding/contexts/OnboardingProvider'
+import { useLoadFonts } from 'common/hooks/useLoadFonts'
 
 export default function RootLayout(): JSX.Element | null {
   const router = useRouter()
@@ -39,10 +40,20 @@ export default function RootLayout(): JSX.Element | null {
   >(null)
   const [enabledPrivacyScreen, setEnabledPrivacyScreen] = useState(false)
   const navigationState = useRootNavigationState()
+  const colorScheme = useColorScheme()
 
   const canGoBackToWallet = navigationState?.routes.some(
     route => route.name === '(signedIn)'
   )
+
+  useEffect(() => {
+    StatusBar.setBarStyle(
+      colorScheme === 'dark' ? 'light-content' : 'dark-content',
+      true
+    )
+  }, [colorScheme])
+
+  useLoadFonts()
 
   useEffect(() => {
     if (walletState === WalletState.NONEXISTENT) {
