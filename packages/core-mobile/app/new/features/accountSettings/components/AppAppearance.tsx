6215ebd10521bdc287f349ca62cfbd224941828a
JungHwan Jang <onghwan@gmail.com>
2025-03-17 15:25:23 -0400

    use k2 grouplist (#2410)

--- packages/core-mobile/app/new/features/accountSettings/components/AppAppearance.tsx
+++ packages/core-mobile/app/new/features/accountSettings/components/AppAppearance.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
-import {
-  TouchableOpacity,
-  useTheme,
-  Text,
-  Icons,
-  View,
-  Separator
-} from '@avalabs/k2-alpine'
-import { Dimensions } from 'react-native'
+import { useTheme, Text, Icons, View, GroupList } from '@avalabs/k2-alpine'
 import { useSelector } from 'react-redux'
 import { selectSelectedCurrency } from 'store/settings/currency'
-
-const SCREEN_WIDTH = Dimensions.get('window').width
 
 export const AppAppearance = ({
   selectAppAppearance,
@@ -27,28 +17,11 @@ export const AppAppearance = ({
   } = useTheme()
   const currency = useSelector(selectSelectedCurrency)
 
-  return (
-    <View
-      sx={{
-        backgroundColor: colors.$surfaceSecondary,
-        borderRadius: 12,
-        width: SCREEN_WIDTH - 32
-      }}>
-      {/* Currency */}
-      <TouchableOpacity
-        sx={{
-          alignItems: 'center',
-          paddingHorizontal: 16,
-          paddingVertical: 13,
-          flexDirection: 'row',
-          justifyContent: 'space-between'
-        }}
-        onPress={selectCurrency}>
-        <Text
-          variant="body2"
-          sx={{ color: colors.$textPrimary, fontSize: 16, lineHeight: 22 }}>
-          Currency
-        </Text>
+  const data = [
+    {
+      title: 'Currency',
+      onPress: selectCurrency,
+      value: (
         <View sx={{ flexDirection: 'row', alignItems: 'center' }}>
           <View
             sx={{
@@ -65,66 +38,31 @@ export const AppAppearance = ({
               color: colors.$textSecondary,
               fontSize: 16,
               lineHeight: 22,
-              marginLeft: 9,
-              marginRight: 16
+              marginLeft: 9
             }}>
             {currency.toUpperCase()}
           </Text>
-          <Icons.Navigation.ChevronRightV2 />
         </View>
-      </TouchableOpacity>
-      <Separator sx={{ marginHorizontal: 16 }} />
+      )
+    },
+    {
+      title: 'Appearance',
+      onPress: selectAppAppearance,
+      value: isDark ? 'Dark theme' : 'Light theme'
+    },
+    {
+      title: 'App icon',
+      onPress: selectAppIcon,
+      value: 'Default'
+    }
+  ]
 
-      {/* App Appearance */}
-      <TouchableOpacity
-        sx={{
-          alignItems: 'center',
-          paddingHorizontal: 16,
-          paddingVertical: 13,
-          flexDirection: 'row',
-          justifyContent: 'space-between'
-        }}
-        onPress={selectAppAppearance}>
-        <Text
-          variant="body2"
-          sx={{ color: colors.$textPrimary, fontSize: 16, lineHeight: 22 }}>
-          Appearance
-        </Text>
-        <View sx={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}>
-          <Text
-            variant="body2"
-            sx={{ color: colors.$textSecondary, fontSize: 16, lineHeight: 22 }}>
-            {isDark ? 'Dark theme' : 'Light theme'}
-          </Text>
-          <Icons.Navigation.ChevronRightV2 />
-        </View>
-      </TouchableOpacity>
-      <Separator sx={{ marginHorizontal: 16 }} />
-
-      {/* App Icon */}
-      <TouchableOpacity
-        sx={{
-          alignItems: 'center',
-          paddingHorizontal: 16,
-          paddingVertical: 13,
-          flexDirection: 'row',
-          justifyContent: 'space-between'
-        }}
-        onPress={selectAppIcon}>
-        <Text
-          variant="body2"
-          sx={{ color: colors.$textPrimary, fontSize: 16, lineHeight: 22 }}>
-          App icon
-        </Text>
-        <View sx={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}>
-          <Text
-            variant="body2"
-            sx={{ color: colors.$textSecondary, fontSize: 16, lineHeight: 22 }}>
-            Default
-          </Text>
-          <Icons.Navigation.ChevronRightV2 />
-        </View>
-      </TouchableOpacity>
-    </View>
+  return (
+    <GroupList
+      data={data}
+      titleSx={{ fontSize: 16, lineHeight: 22, fontFamily: 'Inter-Regular' }}
+      valueSx={{ fontSize: 16, lineHeight: 22 }}
+      separatorMarginRight={16}
+    />
   )
 }
