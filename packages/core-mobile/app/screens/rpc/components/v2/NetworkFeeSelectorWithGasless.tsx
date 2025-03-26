9f55073f15612113fcf7e23ec237e9d83101976c
An Nguyen <an.nguyen@avalabs.org>
2025-03-26 16:08:01 -0500

    adjust UI

--- packages/core-mobile/app/screens/rpc/components/v2/NetworkFeeSelectorWithGasless.tsx
+++ packages/core-mobile/app/screens/rpc/components/v2/NetworkFeeSelectorWithGasless.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Space } from 'components/Space'
 import { Row } from 'components/Row'
 import NetworkFeeSelector from 'components/NetworkFeeSelector'
-import { Text } from '@avalabs/k2-mobile'
+import { Text, useTheme, View } from '@avalabs/k2-mobile'
 import { Eip1559Fees } from 'utils/Utils'
 import { getChainIdFromCaip2 } from 'utils/caip2ChainIds'
 import Switch from 'components/Switch'
 import { Tooltip } from 'components/Tooltip'
 import InfoSVG from 'components/svg/InfoSVG'
+import Separator from 'components/Separator'
 
 type Props = {
   gaslessEnabled: boolean
@@ -26,6 +27,7 @@ const NetworkFeeSelectorWithGasless = ({
   caip2ChainId,
   handleFeesChange
 }: Props): JSX.Element => {
+  const { theme } = useTheme()
   const chainId = getChainIdFromCaip2(caip2ChainId)
 
   const renderGaslessSwitch = (): JSX.Element | null => {
@@ -37,7 +39,8 @@ const NetworkFeeSelectorWithGasless = ({
         style={{
           justifyContent: 'space-between',
           alignItems: 'center',
-          paddingVertical: 8
+          paddingTop: 8,
+          marginBottom: 24
         }}>
         <Row style={{ alignItems: 'center' }}>
           <Text variant="body2">Get Free Gas</Text>
@@ -46,7 +49,7 @@ const NetworkFeeSelectorWithGasless = ({
             content="When toggled Core will pay the network fee for this transaction."
             position="right"
             style={{ width: 200 }}
-            icon={<InfoSVG size={14} />}
+            icon={<InfoSVG size={14} color="white" />}
           />
         </Row>
         <Switch
@@ -57,6 +60,16 @@ const NetworkFeeSelectorWithGasless = ({
     )
   }
 
+  const renderSeparator = (): JSX.Element | null => {
+    if (!showGaslessSwitch || gaslessEnabled) return null
+
+    return (
+      <Separator
+        style={{ marginBottom: 10 }}
+        color={theme.colors.$neutral600}
+      />
+    )
+  }
   const renderNetworkFeeSelector = (): JSX.Element | null => {
     if (gaslessEnabled && showGaslessSwitch) return null
     if (!chainId || !gasLimit) return null
@@ -71,10 +84,21 @@ const NetworkFeeSelectorWithGasless = ({
   }
 
   return (
-    <>
-      {renderGaslessSwitch()}
+    <View
+      sx={{
+        backgroundColor: '$neutral800',
+        borderRadius: 8
+      }}>
+      <View
+        style={{
+          paddingHorizontal: 16,
+          paddingTop: 16
+        }}>
+        {renderGaslessSwitch()}
+        {renderSeparator()}
+      </View>
       {renderNetworkFeeSelector()}
-    </>
+    </View>
   )
 }
 
