2744c88cdb273c4544a35f39d95d39636ff253a6
Eunji Song <32075932+eunjisong@users.noreply.github.com>
2025-03-27 13:09:42 -0400

    QA - New settings tests (#2448)

---
+++ packages/core-mobile/e2e/tests/settings/coreAnalytics.e2e.ts
@@ -0,0 +1,29 @@
+import actions from '../../helpers/actions'
+import { warmup } from '../../helpers/warmup'
+import commonElsPage from '../../pages/commonEls.page'
+import settingsPage from '../../pages/settings.page'
+
+describe('Settings - Core Analytics', () => {
+  it('should have the Core Analystics ON by default after login', async () => {
+    await warmup()
+    await settingsPage.goSettings()
+    await actions.scrollToBottom(settingsPage.settingsScrollView)
+    await settingsPage.tapSecurityAndPrivacy()
+    await settingsPage.verifyAnalyticsSwitch()
+  })
+
+  it('should toggle on and off the Core Analystics', async () => {
+    // disable the switch and verify it is off
+    await settingsPage.tapAnalyticsSwitch()
+    await settingsPage.verifyAnalyticsSwitch(false)
+    await commonElsPage.tapBackButton()
+    await settingsPage.tapSecurityAndPrivacy()
+    await settingsPage.verifyAnalyticsSwitch(false)
+
+    // enable the switch and verify it's ON
+    await settingsPage.tapAnalyticsSwitch(false)
+    await commonElsPage.tapBackButton()
+    await settingsPage.tapSecurityAndPrivacy()
+    await settingsPage.verifyAnalyticsSwitch(true)
+  })
+})
