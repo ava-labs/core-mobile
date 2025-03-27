125f4ed974ffcd82b39f0156debcaf2ce9b63f0f
An Nguyen <an.nguyen@avalabs.org>
2025-03-27 12:08:34 -0500

    make react-native-quick-crypto work again and remove promise polyfill

---
+++ packages/core-mobile/polyfills/crypto.js
@@ -0,0 +1,5 @@
+import { install } from 'react-native-quick-crypto'
+
+// this will make both buffer (from @craftzdog/react-native-buffer)
+// and crypto (from react-native-quick-crypto) available globally
+install()
