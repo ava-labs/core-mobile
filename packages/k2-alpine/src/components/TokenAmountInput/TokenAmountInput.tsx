aca3832c1ea6ab5c7367e04d4e08ae7436ca9e7b
onghwan <onghwan@gmail.com>
2025-03-28 20:16:36 -0400

    token amount input

---
+++ packages/k2-alpine/src/components/TokenAmountInput/TokenAmountInput.tsx
@@ -0,0 +1,184 @@
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+  useState
+} from 'react'
+import { SxProp } from 'dripsy'
+import { TokenUnit } from '@avalabs/core-utils-sdk'
+import {
+  LayoutChangeEvent,
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback
+} from 'react-native'
+import { useTheme } from '../../hooks'
+import { Text, View } from '../Primitives'
+import { alpha } from '../../utils'
+import { getMaxDecimals, normalizeValue } from '../../utils/tokenUnitInput'
+
+export type TokenAmountInputHandle = {
+  setValue: (value: string) => void
+  focus: () => void
+  blur: () => void
+}
+
+type TokenAmountInputProps = {
+  amount?: TokenUnit
+  token: {
+    maxDecimals: number
+    symbol: string
+  }
+  onChange?(amount: TokenUnit): void
+  formatInCurrency?(amount: TokenUnit): string
+  sx?: SxProp
+}
+
+export const TokenAmountInput = forwardRef<
+  TokenAmountInputHandle,
+  TokenAmountInputProps
+>(({ amount, token, onChange, formatInCurrency, sx }, ref) => {
+  const {
+    theme: { colors }
+  } = useTheme()
+  const [value, setValue] = useState(amount?.toDisplay())
+  const [maxLength, setMaxLength] = useState<number>()
+  const textInputRef = useRef<TextInput>(null)
+  const [textInputMinimumLayout, setTextInputMinimumLayout] = useState<{
+    width: number
+    height: number
+  }>()
+
+  const inputAmount = useMemo(() => {
+    return new TokenUnit(
+      !value ? 0 : Number(value) * 10 ** token.maxDecimals,
+      token.maxDecimals,
+      token.symbol
+    )
+  }, [value, token])
+  const maxDecimalDigits = useMemo(() => {
+    return getMaxDecimals(inputAmount) ?? inputAmount.getMaxDecimals()
+  }, [inputAmount])
+
+  const handleValueChanged = (rawValue: string): void => {
+    if (!rawValue) {
+      setValue('')
+      return
+    }
+    const changedValue = rawValue.startsWith('.') ? '0.' : rawValue
+
+    /**
+     * Split the input and make sure the right side never exceeds
+     * the maxDecimals length
+     */
+    const [frontValue, endValue] = changedValue.split('.')
+
+    if (
+      frontValue !== undefined &&
+      !isNaN(Number(changedValue)) &&
+      (!endValue ||
+        endValue.length <= Math.min(maxDecimalDigits, token.maxDecimals))
+    ) {
+      const sanitizedFrontValue = frontValue.replace(/^0+(?!$)/, '')
+
+      //setting maxLength to TextInput prevents flickering, see https://reactnative.dev/docs/textinput#value
+      setMaxLength(
+        sanitizedFrontValue.length +
+          '.'.length +
+          Math.min(maxDecimalDigits, token.maxDecimals)
+      )
+
+      setValue(normalizeValue(changedValue))
+    } else {
+      setMaxLength(undefined)
+    }
+  }
+
+  const handlePress = (): void => {
+    textInputRef.current?.focus()
+  }
+
+  const handleTextInputLayout = (event: LayoutChangeEvent): void => {
+    setTextInputMinimumLayout(event.nativeEvent.layout)
+  }
+
+  useEffect(() => {
+    onChange?.(inputAmount)
+  }, [inputAmount, onChange])
+
+  useImperativeHandle(ref, () => ({
+    setValue: (newValue: string) => setValue(newValue),
+    focus: () => textInputRef.current?.focus(),
+    blur: () => textInputRef.current?.blur()
+  }))
+
+  return (
+    <View
+      sx={{
+        alignItems: 'center',
+        ...sx
+      }}>
+      <TouchableWithoutFeedback onPress={handlePress}>
+        <View
+          sx={{
+            flexDirection: 'row',
+            alignItems: 'flex-start',
+            gap: 5
+          }}>
+          <Text
+            onLayout={handleTextInputLayout}
+            style={[
+              styles.textInput,
+              { position: 'absolute', top: 0, opacity: 0 }
+            ]}>
+            0.0
+          </Text>
+          <TextInput
+            ref={textInputRef}
+            style={[
+              styles.textInput,
+              {
+                flexShrink: 1,
+                color: colors.$textPrimary,
+                textAlign: 'right',
+                minWidth: textInputMinimumLayout?.width
+              }
+            ]}
+            keyboardType="numeric"
+            placeholder="0.0"
+            autoFocus={true}
+            value={value}
+            onChangeText={handleValueChanged}
+            maxLength={maxLength}
+            selectionColor={colors.$textPrimary}
+          />
+          <Text
+            sx={{
+              fontFamily: 'Aeonik-Medium',
+              fontSize: 24,
+              lineHeight: 24,
+              marginTop: 14
+            }}>
+            {token.symbol}
+          </Text>
+        </View>
+      </TouchableWithoutFeedback>
+      {formatInCurrency && (
+        <Text
+          variant="subtitle2"
+          sx={{ marginTop: 0, color: alpha(colors.$textPrimary, 0.9) }}>
+          {formatInCurrency(inputAmount)}
+        </Text>
+      )}
+    </View>
+  )
+})
+
+const styles = StyleSheet.create({
+  textInput: {
+    fontFamily: 'Aeonik-Medium',
+    fontSize: 60
+  }
+})
