e6f89f625a5a91e3025b252b3470b0253bdcf929
onghwan <onghwan@gmail.com>
2025-03-28 20:16:36 -0400

    token amount input

---
+++ packages/k2-alpine/src/utils/tokenUnitInput.ts
@@ -0,0 +1,26 @@
+import { TokenUnit } from '@avalabs/core-utils-sdk'
+
+const MAX_DIGITS = 7
+
+export function getMaxDecimals(input: TokenUnit): number {
+  const stringAmount = input.toString().replaceAll(',', '')
+  const [whole] = stringAmount.split('.')
+  const maxDecimals = MAX_DIGITS - (whole?.length ?? 0)
+  if (maxDecimals < 0) return 0
+  return maxDecimals
+}
+
+export const normalizeValue = (value: string): string => {
+  // If the string consists solely of zeros (optionally ending with a dot),
+  // return "0" or "0." accordingly.
+  if (/^0+\.?$/.test(value)) {
+    return value.includes('.') ? '0.' : '0'
+  }
+  // Otherwise, remove all leading zeros.
+  let normalized = value.replace(/^0+/, '')
+  // If the result starts with a dot, prepend a "0"
+  if (normalized.startsWith('.')) {
+    normalized = '0' + normalized
+  }
+  return normalized
+}
