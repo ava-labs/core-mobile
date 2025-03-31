59fe0ab90f4722378c8057e305571276dc1be9d5
ruijialin <ruijia.lin@avalabs.org>
2025-03-31 19:19:32 -0400

    add polyfillCrypto

--- packages/core-mobile/app/new/routes/(signedIn)/(modals)/accountSettings/seedlessExportPhrase/_layout.tsx
+++ packages/core-mobile/app/new/routes/(signedIn)/(modals)/accountSettings/seedlessExportPhrase/_layout.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Stack } from 'common/components/Stack'
-import 'react-native-webview-crypto'
+import PolyfillCrypto from 'react-native-webview-crypto'
 
 export default function SeedlessExportPhraseLayout(): JSX.Element {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="notInitiated" />
-      <Stack.Screen name="pending" />
-      <Stack.Screen name="readyToExport" />
-      <Stack.Screen name="refreshSeedlessToken" />
-      <Stack.Screen name="verifyExportInitMfa" />
-      <Stack.Screen name="verifyExportCompleteMfa" />
-    </Stack>
+    <>
+      <PolyfillCrypto />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="notInitiated" />
+        <Stack.Screen name="pending" />
+        <Stack.Screen name="readyToExport" />
+        <Stack.Screen name="refreshSeedlessToken" />
+        <Stack.Screen name="verifyExportInitMfa" />
+        <Stack.Screen name="verifyExportCompleteMfa" />
+      </Stack>
+    </>
   )
 }
