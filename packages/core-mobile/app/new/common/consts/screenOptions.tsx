d21599d2a5b6e9d4759deb58fb165d83fdb44b20
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2025-04-01 15:56:40 -0400

    CP-10187: Add notification preferences (#2475)

--- packages/core-mobile/app/new/common/consts/screenOptions.tsx
+++ packages/core-mobile/app/new/common/consts/screenOptions.tsx
@@ -13,6 +13,7 @@ import { Link } from 'expo-router'
 import { ReceiveBarButton } from 'common/components/ReceiveBarButton'
 import { NotificationBarButton } from 'common/components/NotificationBarButton'
 import { AccountSettingBarButton } from 'common/components/AccountSettingBarButton'
+import { TestnetBanner } from 'common/components/TestnetBanner'
 
 const commonNavigatorScreenOptions: StackNavigationOptions = {
   title: '',
@@ -44,6 +45,8 @@ export const modalScreensOptions: StackNavigationOptions = {
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40
   },
+  cardOverlay: overlay => <TestnetBanner overlayStyle={overlay.style} />,
+  cardShadowEnabled: true,
   gestureEnabled: true,
   gestureDirection: 'vertical',
   cardStyleInterpolator: forModalPresentationIOS
@@ -96,7 +99,7 @@ function forModalPresentationIOS({
       overflow: 'hidden',
       transform: [{ translateY }]
     },
-    overlayStyle: { opacity: overlayOpacity }
+    overlayStyle: { opacity: overlayOpacity, transform: [{ translateY }] }
   }
 }
 
