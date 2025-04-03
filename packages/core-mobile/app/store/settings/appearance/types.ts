d02cf6acc700362fc61de48cb74e0210d6258e4c
ruijialin <ruijia.lin@avalabs.org>
2025-04-03 17:30:21 -0400

    add comment

--- packages/core-mobile/app/store/settings/appearance/types.ts
+++ packages/core-mobile/app/store/settings/appearance/types.ts
@@ -8,6 +8,12 @@ export type ColorSchemeName = 'light' | 'dark'
 
 export const DEFAULT_APPEARANCE = Appearance.System
 
+/*
+  selected: 'Dark theme' | 'Light theme' | 'System'
+  - this is the appearance that the user has selected in the settings
+  colorScheme: 'light' | 'dark'
+  - this is the color scheme that is currently being used by the app
+*/
 export const initialState: AppearanceState = {
   selected: DEFAULT_APPEARANCE,
   colorScheme: 'light'
