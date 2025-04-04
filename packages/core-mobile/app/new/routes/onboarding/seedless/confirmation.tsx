ff1d748de17fd63328ba8ec40eada40a60d1b65d
Neven <piaf1986@gmail.com>
2025-04-04 16:42:13 +0200

    Refactor pin creation to use structured arguments

--- packages/core-mobile/app/new/routes/onboarding/seedless/confirmation.tsx
+++ packages/core-mobile/app/new/routes/onboarding/seedless/confirmation.tsx
@@ -4,12 +4,13 @@ import { useWallet } from 'hooks/useWallet'
 import Logger from 'utils/Logger'
 import { WalletType } from 'services/wallet/types'
 import { SEEDLESS_MNEMONIC_STUB } from 'seedless/consts'
+import { uuid } from 'utils/uuid'
 
 export default function Confirmation(): JSX.Element {
   const { login } = useWallet()
 
   const handleNext = (): void => {
-    login(SEEDLESS_MNEMONIC_STUB, WalletType.SEEDLESS).catch(Logger.error)
+    login(uuid(), WalletType.SEEDLESS).catch(Logger.error)
   }
   return <Component onNext={handleNext} />
 }
