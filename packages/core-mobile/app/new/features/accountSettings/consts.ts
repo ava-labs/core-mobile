96006e2dcc30b1ad9a1edbd6ff8ffdbbb01093c9
ruijialin <ruijia.lin@avalabs.org>
2025-04-08 17:38:36 -0400

    update copy message

--- packages/core-mobile/app/new/features/accountSettings/consts.ts
+++ packages/core-mobile/app/new/features/accountSettings/consts.ts
@@ -1,7 +1,7 @@
 export const SHOW_RECOVERY_PHRASE = `Show recovery\nphrase`
 
 export enum AddressType {
-  CChain = 'Avalanche C-Chain',
+  CChain = 'Avalanche C-Chain/Ethereum',
   XP = 'Avalanche X/P-Chain',
   BTC = 'Bitcoin'
 }
