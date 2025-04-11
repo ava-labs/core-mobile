5411ff05391ce5e734b8a24b3163a66ab536c28e
ruijialin <ruijia.lin@avalabs.org>
2025-04-11 15:43:30 -0400

    mfa ui fix

--- packages/core-mobile/app/new/features/onboarding/components/FidoNameInput.tsx
+++ packages/core-mobile/app/new/features/onboarding/components/FidoNameInput.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Button, ScrollView, SafeAreaView } from '@avalabs/k2-alpine'
 import { FIDONameInputProps } from 'new/routes/onboarding/seedless/(fido)/fidoNameInput'
 import { SimpleTextInput } from 'common/components/SimpleTextInput'
 import ScreenHeader from 'common/components/ScreenHeader'
-import { KeyboardAvoidingView } from 'common/components/KeyboardAvoidingView'
+import { Platform, Keyboard, KeyboardEvent } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const FidoNameInput = ({
   title,
@@ -17,33 +18,53 @@ const FidoNameInput = ({
   setName: (value: string) => void
   handleSave: () => void
 }): JSX.Element => {
+  const [keyboardHeight, setKeyboardHeight] = useState(0)
+  const { bottom } = useSafeAreaInsets()
+
+  // Configure keyboard listeners
+  useEffect(() => {
+    if (Platform.OS !== 'ios') return
+    const keyboardDidShow = (e: KeyboardEvent): void => {
+      setKeyboardHeight(e.endCoordinates.height - bottom + 24)
+    }
+    const keyboardDidHide = (): void => {
+      setKeyboardHeight(0)
+    }
+
+    const showSub = Keyboard.addListener('keyboardDidShow', keyboardDidShow)
+    const hideSub = Keyboard.addListener('keyboardDidHide', keyboardDidHide)
+
+    return () => {
+      showSub.remove()
+      hideSub.remove()
+    }
+  }, [bottom])
+
   return (
-    <KeyboardAvoidingView>
-      <SafeAreaView sx={{ flex: 1 }}>
-        <ScrollView
-          sx={{ flex: 1 }}
-          contentContainerStyle={{
-            paddingHorizontal: 16,
-            gap: 27
-          }}>
-          <ScreenHeader title={title} description={description} />
-          <SimpleTextInput
-            value={name}
-            placeholder={textInputPlaceholder}
-            onChangeText={setName}
-          />
-        </ScrollView>
-        <View sx={{ padding: 16, backgroundColor: '$surfacePrimary' }}>
-          <Button
-            type="primary"
-            size="large"
-            disabled={name === ''}
-            onPress={handleSave}>
-            Next
-          </Button>
-        </View>
-      </SafeAreaView>
-    </KeyboardAvoidingView>
+    <SafeAreaView sx={{ flex: 1, marginBottom: keyboardHeight }}>
+      <ScrollView
+        sx={{ flex: 1 }}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          gap: 27
+        }}>
+        <ScreenHeader title={title} description={description} />
+        <SimpleTextInput
+          value={name}
+          placeholder={textInputPlaceholder}
+          onChangeText={setName}
+        />
+      </ScrollView>
+      <View sx={{ padding: 16, backgroundColor: '$surfacePrimary' }}>
+        <Button
+          type="primary"
+          size="large"
+          disabled={name === ''}
+          onPress={handleSave}>
+          Next
+        </Button>
+      </View>
+    </SafeAreaView>
   )
 }
 
