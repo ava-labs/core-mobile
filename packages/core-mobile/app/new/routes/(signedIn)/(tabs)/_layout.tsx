057bbd5ebdd111f0a7a9ae74524ca0dd7637b920
An Nguyen <an.nguyen@avalabs.org>
2025-04-13 01:50:50 -0500

    use correct tab icons and colors

--- packages/core-mobile/app/new/routes/(signedIn)/(tabs)/_layout.tsx
+++ packages/core-mobile/app/new/routes/(signedIn)/(tabs)/_layout.tsx
@@ -1,47 +1,59 @@
 import React from 'react'
+import { alpha, useTheme } from '@avalabs/k2-alpine'
 import { BottomTabs } from 'common/components/BottomTabs'
-import { Platform } from 'react-native'
 
 export default function TabLayout(): JSX.Element {
+  const {
+    theme: { colors, isDark }
+  } = useTheme()
+
   return (
     <BottomTabs
+      labeled
+      translucent
+      disablePageAnimations
+      tabBarActiveTintColor={isDark ? colors.$white : colors.$black}
+      tabBarInactiveTintColor={
+        isDark ? alpha(colors.$white, 0.6) : alpha('#1E1E24', 0.6)
+      }
       tabBarStyle={{
-        backgroundColor: Platform.OS === 'android' ? 'white' : 'transparent'
+        backgroundColor: isDark
+          ? alpha('#1E1E24', 0.9)
+          : alpha(colors.$white, 0.8)
+      }}
+      tabLabelStyle={{
+        fontFamily: 'Inter-Bold',
+        fontSize: 10
+      }}
+      screenOptions={{
+        freezeOnBlur: true
       }}>
       <BottomTabs.Screen
         name="portfolio"
         options={{
-          title: 'Home',
-          tabBarIcon: () => ({
-            uri: 'https://www.svgrepo.com/show/533813/hat-chef.svg'
-          })
+          title: 'Portfolio',
+          tabBarIcon: () => require('../../../assets/icons/tabs/layers.png')
         }}
       />
       <BottomTabs.Screen
         name="track"
         options={{
           title: 'Track',
-          tabBarIcon: () => ({
-            uri: 'https://www.svgrepo.com/show/533824/water-container.svg'
-          })
+          tabBarIcon: () =>
+            require('../../../assets/icons/tabs/search-custom.png')
         }}
       />
       <BottomTabs.Screen
         name="stake"
         options={{
           title: 'Stake',
-          tabBarIcon: () => ({
-            uri: 'https://www.svgrepo.com/show/533826/shop.svg'
-          })
+          tabBarIcon: () => require('../../../assets/icons/tabs/psychiatry.png')
         }}
       />
       <BottomTabs.Screen
         name="browser"
         options={{
-          title: 'Browser',
-          tabBarIcon: () => ({
-            uri: 'https://www.svgrepo.com/show/533828/cheese.svg'
-          })
+          tabBarIcon: () => require('../../../assets/icons/tabs/compass.png')
         }}
       />
     </BottomTabs>
