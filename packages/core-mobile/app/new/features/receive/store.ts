7a2974723c8f211dafba92f503923c4d04b8c7d6
onghwan <onghwan@gmail.com>
2025-04-18 02:12:33 -0400

    code review

--- packages/core-mobile/app/new/features/receive/store.ts
+++ packages/core-mobile/app/new/features/receive/store.ts
@@ -1,16 +1,7 @@
 import { Network } from '@avalabs/core-chains-sdk'
-import { createStore } from 'common/store/createStore'
+import { createZustandStore } from 'common/utils/createZustandStore'
 import { AVALANCHE_MAINNET_NETWORK } from 'services/network/consts'
 
-export const useReceiveSelectedNetworkStore = createStore<Network>(
+export const useReceiveSelectedNetwork = createZustandStore<Network>(
   AVALANCHE_MAINNET_NETWORK
 )
-
-export const useReceiveSelectedNetwork = (): [
-  Network,
-  (n: Network) => void
-] => {
-  const value = useReceiveSelectedNetworkStore(s => s.value)
-  const setValue = useReceiveSelectedNetworkStore(s => s.setValue)
-  return [value, setValue]
-}
