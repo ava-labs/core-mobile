9cfeb5e0f49f10068ff02b17e76bbfd53b982854
An Nguyen <an.nguyen@avalabs.org>
2025-04-21 18:49:18 -0500

    got sign message/transaction to work

--- packages/core-mobile/app/services/walletconnectv2/walletConnectCache/types.ts
+++ packages/core-mobile/app/services/walletconnectv2/walletConnectCache/types.ts
@@ -1,9 +1,32 @@
+import { CorePrimaryAccount } from '@avalabs/types'
+import { RpcRequest, DisplayData, SigningData } from '@avalabs/vm-module-types'
 import { ProposalTypes } from '@walletconnect/types'
 import { SiteScanResponse } from 'services/blockaid/types'
 import { WCSessionProposal } from 'store/walletConnectV2/types'
+import { Network } from '@avalabs/core-chains-sdk'
 
-export type SessionProposalV2Params = {
+export type SessionProposalParams = {
   request: WCSessionProposal
   namespaces: Record<string, ProposalTypes.RequiredNamespace>
   scanResponse?: SiteScanResponse
+}
+
+export type ApprovalParams = {
+  request: RpcRequest
+  displayData: DisplayData
+  signingData: SigningData
+  onApprove: ({
+    network,
+    account,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+    overrideData
+  }: {
+    network: Network
+    account: CorePrimaryAccount
+    maxFeePerGas?: bigint
+    maxPriorityFeePerGas?: bigint
+    overrideData?: string
+  }) => Promise<void>
+  onReject: (message?: string) => void
 }
