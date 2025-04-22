ad3de52fb1edb2d60eb741f4ecb759b1d70e16be
Eunji Song <32075932+eunjisong@users.noreply.github.com>
2025-04-22 12:54:30 -0400

    New Gen QA - new receive tests (#2577)

--- packages/core-mobile/e2e/locators/plusMenu.loc.ts
+++ packages/core-mobile/e2e/locators/plusMenu.loc.ts
@@ -5,7 +5,6 @@ export default {
   send: 'tab_navigator__send_button',
   swap: 'tab_navigator__swap_button',
   buy: 'tab_navigator__buy_button',
-  receive: 'tab_navigator__receive_button',
   walletConnectSVG: 'wallet_connect_svg',
   walletConnectButton: 'WalletConnect',
   inputTextField: 'input_text'
