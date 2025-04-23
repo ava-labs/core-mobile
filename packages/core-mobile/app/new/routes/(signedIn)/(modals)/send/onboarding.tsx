4709d6ec0f624a8e9584ecc8ffb93c3846cbaa63
ruijialin <ruijia.lin@avalabs.org>
2025-04-23 13:41:20 -0400

    use common onboarding screen

--- packages/core-mobile/app/new/routes/(signedIn)/(modals)/send/onboarding.tsx
+++ packages/core-mobile/app/new/routes/(signedIn)/(modals)/send/onboarding.tsx
@@ -1,3 +1,25 @@
-import { OnboardingScreen } from 'features/send/screens/OnboardingScreen'
+import React, { useCallback } from 'react'
+import { TransactionOnboarding } from 'common/components/TransactionOnboarding'
+import { Icons } from '@avalabs/k2-alpine'
+import { ViewOnceKey } from 'store/viewOnce'
+import { useRouter } from 'expo-router'
 
-export { OnboardingScreen as default }
+const SendOnboardingScreen = (): JSX.Element => {
+  const { replace } = useRouter()
+
+  const handlePressNext = useCallback(() => {
+    replace('send/recentContacts')
+  }, [replace])
+
+  return (
+    <TransactionOnboarding
+      icon={{ component: Icons.Custom.Bridge, size: 60 }}
+      title={`Send tokens to an address\nor contact`}
+      subtitle={`Send tokens to any address or contact\non a given network`}
+      viewOnceKey={ViewOnceKey.SEND_ONBOARDING}
+      onPressNext={handlePressNext}
+    />
+  )
+}
+
+export default SendOnboardingScreen
