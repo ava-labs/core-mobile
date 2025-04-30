21073f59b45381ab23315a8ca7d4f5e9e35cbf1b
An Nguyen <an.nguyen@avalabs.org>
2025-04-30 16:55:18 -0500

    Swap improvements (#2643)

--- packages/core-mobile/app/new/common/components/ScrollScreen.tsx
+++ packages/core-mobile/app/new/common/components/ScrollScreen.tsx
@@ -57,6 +57,8 @@ interface ScrollScreenProps extends KeyboardAwareScrollViewProps {
   renderFooter?: () => React.ReactNode
   /** Custom component to be rendered on the right side of the header */
   renderHeaderRight?: () => React.ReactNode
+  /** The distance between keyboard and focused TextInput when keyboard is shown. Default is 0. */
+  bottomOffset?: number
 }
 
 export const ScrollScreen = ({
@@ -70,6 +72,7 @@ export const ScrollScreen = ({
   renderHeader,
   renderFooter,
   renderHeaderRight,
+  bottomOffset,
   ...props
 }: ScrollScreenProps): JSX.Element => {
   const insets = useSafeAreaInsets()
@@ -148,6 +151,7 @@ export const ScrollScreen = ({
         keyboardDismissMode="interactive"
         keyboardShouldPersistTaps="handled"
         showsVerticalScrollIndicator={false}
+        bottomOffset={bottomOffset}
         extraKeyboardSpace={-keyboardHeight}
         {...props}
         contentContainerStyle={[
