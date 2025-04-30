47e0aff6b4ed7081b658f960693b4083ceee8451
ruijialin <ruijia.lin@avalabs.org>
2025-04-30 19:13:18 -0400

    enable notification prompt

--- packages/core-mobile/app/store/notifications/listeners/handlePromptNotifications.test.ts
+++ packages/core-mobile/app/store/notifications/listeners/handlePromptNotifications.test.ts
@@ -1,8 +1,6 @@
 import { AppListenerEffectAPI } from 'store/types'
 import { AnyAction } from '@reduxjs/toolkit'
-import { selectHasBeenViewedOnce, setViewOnce } from 'store/viewOnce/slice'
-import { ViewOnceKey } from 'store/viewOnce/types'
-import { selectNotificationSubscription } from '../slice'
+import { selectHasBeenViewedOnce } from 'store/viewOnce/slice'
 import { handlePromptNotifications } from './handlePromptNotifications'
 
 jest.mock('services/notifications/NotificationsService', () => ({
@@ -12,18 +10,6 @@ jest.mock('services/notifications/NotificationsService', () => ({
 
 jest.mock('../slice', () => ({
   selectNotificationSubscription: jest.fn(() => jest.fn)
-}))
-
-jest.mock('store/viewOnce/slice', () => ({
-  selectHasBeenViewedOnce: jest.fn(),
-  setViewOnce: jest.fn()
-}))
-
-jest.mock('store/viewOnce/types', () => ({
-  ViewOnceKey: {
-    CORE_INTRO: 'CORE_INTRO',
-    NOTIFICATIONS_PROMPT: 'NOTIFICATIONS_PROMPT'
-  }
 }))
 
 jest.mock('store/posthog/slice', () => {
@@ -54,35 +40,6 @@ describe('handlePromptNotifications', () => {
   it('should not prompt if user has already been prompted for notifications', async () => {
     ;(selectHasBeenViewedOnce as jest.Mock).mockReturnValue(() => true)
     await handlePromptNotifications(action, listenerApi)
-    // expect(Navigation.navigate).not.toHaveBeenCalled()
     expect(listenerApi.dispatch).not.toHaveBeenCalled()
-  })
-
-  it('should wait for intro screen to be dismissed before prompting', async () => {
-    ;(selectNotificationSubscription as jest.Mock).mockReturnValue(() => false)
-    ;(selectHasBeenViewedOnce as jest.Mock).mockReturnValue(() => false)
-    let resolveTake: (value?: unknown) => void
-    const takePromise = new Promise(resolve => {
-      resolveTake = resolve
-    })
-    ;(listenerApi.take as jest.Mock).mockReturnValue(takePromise)
-
-    const promise = handlePromptNotifications(action, listenerApi)
-
-    // verify that Navigation.navigate and listenerApi.dispatch have not been called yet
-    //expect(Navigation.navigate).not.toHaveBeenCalled()
-    expect(listenerApi.dispatch).not.toHaveBeenCalled()
-
-    // simulate the intro screen being dismissed
-    // @ts-ignore
-    resolveTake([{ payload: ViewOnceKey.CORE_INTRO }])
-
-    // wait for the promise to resolve
-    await promise
-
-    //expect(Navigation.navigate).toHaveBeenCalledWith(getNotificationsPrompt())
-    expect(listenerApi.dispatch).toHaveBeenCalledWith(
-      setViewOnce(ViewOnceKey.NOTIFICATIONS_PROMPT)
-    )
   })
 })
--- packages/core-mobile/app/store/notifications/listeners/handlePromptNotifications.ts
+++ packages/core-mobile/app/store/notifications/listeners/handlePromptNotifications.ts
@@ -1,68 +1,34 @@
 import { AppListenerEffectAPI } from 'store/types'
 import { AnyAction } from '@reduxjs/toolkit'
-import { selectHasBeenViewedOnce, setViewOnce } from 'store/viewOnce/slice'
-import { ViewOnceKey } from 'store/viewOnce/types'
 import NotificationsService from 'services/notifications/NotificationsService'
 import { AuthorizationStatus } from '@notifee/react-native'
 import { selectIsEnableNotificationPromptBlocked } from 'store/posthog'
 import { turnOnAllNotifications } from '../slice'
 
-let isWaitingForIntroMux = false
-
 export const handlePromptNotifications = async (
   _: AnyAction,
   listenerApi: AppListenerEffectAPI
 ): Promise<void> => {
-  const state = listenerApi.getState()
-  if (isWaitingForIntroMux) return //if already waiting ignore this handler
-  await waitIfIntroScreenIsYetNotDismissed(listenerApi)
-  isWaitingForIntroMux = false
+  const { getState, dispatch } = listenerApi
+  const state = getState()
   const isEnableNotificationPromptBlocked =
     selectIsEnableNotificationPromptBlocked(state)
-  const hasPromptedForNotifications = selectHasBeenViewedOnce(
-    ViewOnceKey.NOTIFICATIONS_PROMPT
-  )(state)
-
   const authorizationStatus =
     await NotificationsService.getNotificationSettings()
 
   // show prompt if any of the following is true
-  //   - if user has not seen the prompt
   //   - if user has denied/not determined permissions and ff is enabled
   if (
-    hasPromptedForNotifications &&
-    ((authorizationStatus !== AuthorizationStatus.DENIED &&
+    (authorizationStatus !== AuthorizationStatus.DENIED &&
       authorizationStatus !== AuthorizationStatus.NOT_DETERMINED) ||
-      isEnableNotificationPromptBlocked)
+    isEnableNotificationPromptBlocked
   )
     return
 
-  // if user has not seen the prompt and has granted permissions
-  // this means user is re-logging into wallet
-  // we will silently turn on all notifications
-  if (
-    authorizationStatus === AuthorizationStatus.AUTHORIZED ||
-    authorizationStatus === AuthorizationStatus.PROVISIONAL
-  ) {
-    listenerApi.dispatch(turnOnAllNotifications())
-    listenerApi.dispatch(setViewOnce(ViewOnceKey.NOTIFICATIONS_PROMPT))
-    return
-  }
+  const { permission } = await NotificationsService.getAllPermissions(false)
 
-  // TODO: fix enable notifications prompt
-  // Navigation.navigate(AppNavigation.Modal.EnableNotificationsPrompt)
-  listenerApi.dispatch(setViewOnce(ViewOnceKey.NOTIFICATIONS_PROMPT))
-}
-
-async function waitIfIntroScreenIsYetNotDismissed(
-  listenerApi: AppListenerEffectAPI
-): Promise<void> {
-  const { take } = listenerApi
-  const state = listenerApi.getState()
-  let hasSeenIntro = selectHasBeenViewedOnce(ViewOnceKey.CORE_INTRO)(state)
-  while (!hasSeenIntro) {
-    isWaitingForIntroMux = true
-    const [{ payload }] = await take(setViewOnce.match)
-    hasSeenIntro = payload === ViewOnceKey.CORE_INTRO
+  if (permission !== 'authorized') {
+    NotificationsService.openSystemSettings()
   }
+  dispatch(turnOnAllNotifications())
 }
