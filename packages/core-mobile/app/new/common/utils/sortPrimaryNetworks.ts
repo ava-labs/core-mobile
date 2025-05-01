a1fa8bffce519e9ab4b01315381d72d2123aca35
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-05-01 15:43:10 +0300

    add bitcoin and eth as alwaysEnabledNetworks (#2654)

--- packages/core-mobile/app/new/common/utils/sortPrimaryNetworks.ts
+++ packages/core-mobile/app/new/common/utils/sortPrimaryNetworks.ts
@@ -7,13 +7,13 @@ import { isBitcoinChainId } from 'utils/network/isBitcoinNetwork'
 export function sortPrimaryNetworks(a: Network, b: Network): number {
   if (isAvalancheCChainId(a.chainId)) return -1
   if (isAvalancheCChainId(b.chainId)) return 1
-  if (isPChain(a.chainId)) return -1
-  if (isPChain(b.chainId)) return 1
-  if (isXChain(a.chainId)) return -1
-  if (isXChain(b.chainId)) return 1
   if (isBitcoinChainId(a.chainId)) return -1
   if (isBitcoinChainId(b.chainId)) return 1
   if (isEthereumChainId(a.chainId)) return -1
   if (isEthereumChainId(b.chainId)) return 1
+  if (isPChain(a.chainId)) return -1
+  if (isPChain(b.chainId)) return 1
+  if (isXChain(a.chainId)) return -1
+  if (isXChain(b.chainId)) return 1
   return 0
 }
