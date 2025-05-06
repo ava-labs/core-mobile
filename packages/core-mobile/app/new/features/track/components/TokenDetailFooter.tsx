d01f8466048d57b9b1efb47f7f5b0665886c94fb
An Nguyen <an.nguyen@avalabs.org>
2025-05-06 09:45:06 -0500

    remove buy button for non-trending tokens

--- packages/core-mobile/app/new/features/track/components/TokenDetailFooter.tsx
+++ packages/core-mobile/app/new/features/track/components/TokenDetailFooter.tsx
@@ -10,10 +10,11 @@ import { selectTokenVisibility } from 'store/portfolio'
 import { selectIsEarnBlocked } from 'store/posthog'
 import { MarketType } from 'store/watchlist'
 
+// TODO: reenable buy after backend work is done
 export const TokenDetailFooter = ({
   tokenId,
   tokenInfo,
-  onBuy,
+  //onBuy,
   onStake,
   onSwap
 }: {
@@ -32,16 +33,16 @@ export const TokenDetailFooter = ({
   const { hasEnoughAvax } = useHasEnoughAvaxToStake()
   const earnBlocked = useSelector(selectIsEarnBlocked)
 
-  const buyButton = (
-    <Button
-      key="buy"
-      type={'primary'}
-      size={'large'}
-      onPress={onBuy}
-      style={{ flex: 1 }}>
-      Buy
-    </Button>
-  )
+  // const buyButton = (
+  //   <Button
+  //     key="buy"
+  //     type={'primary'}
+  //     size={'large'}
+  //     onPress={onBuy}
+  //     style={{ flex: 1 }}>
+  //     Buy
+  //   </Button>
+  // )
 
   const stakeButton = (
     <Button
@@ -69,18 +70,17 @@ export const TokenDetailFooter = ({
 
   if (isZeroBalance) {
     // only show buy button if the user has zero balance
-    actions.push(buyButton)
+    // actions.push(buyButton)
   } else if (tokenId === AVAX_COINGECKO_ID) {
     // for AVAX, show stake instead of buy button if user has enough AVAX
-    hasEnoughAvax
-      ? !earnBlocked && actions.push(stakeButton)
-      : actions.push(buyButton)
+    hasEnoughAvax && !earnBlocked && actions.push(stakeButton)
+    //  : actions.push(buyButton)
 
     // always show swap button for AVAX
     actions.push(generateSwapButton(USDC_TOKEN_ID))
   } else {
     // user has some balance, show both buy and swap button for all other tokens
-    actions.push(buyButton)
+    //actions.push(buyButton)
 
     // however, only show swap button if the token is a trending one
     // as we currently only support swapping on Avanlanche network
