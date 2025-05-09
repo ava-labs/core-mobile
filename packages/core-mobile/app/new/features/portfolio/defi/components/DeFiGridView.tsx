0dc0cc478414acc856c56d594650e1d9bcbe2f15
An Nguyen <an.nguyen@avalabs.org>
2025-05-09 09:50:51 -0500

    Update DeFiGridView.tsx

--- packages/core-mobile/app/new/features/portfolio/defi/components/DeFiGridView.tsx
+++ packages/core-mobile/app/new/features/portfolio/defi/components/DeFiGridView.tsx
@@ -59,8 +59,8 @@ export const DeFiGridView = ({
                 numberOfLines={1}>
                 {formattedPrice}
               </Text>
-              <View onTouchStart={e => e.stopPropagation()}>
-                <TouchableOpacity onPress={onPressArrow} hitSlop={10}>
+              <View onTouchStart={e => e.stopPropagation()} hitSlop={20}>
+                <TouchableOpacity onPress={onPressArrow} hitSlop={20}>
                   <Icons.Custom.Outbound color={theme.colors.$textPrimary} />
                 </TouchableOpacity>
               </View>
