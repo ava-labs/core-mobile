e3f4ff7f2db81c38fab0f38b1c39ca29a92a4043
ruijialin <ruijia.lin@avalabs.org>
2025-05-09 15:02:56 -0400

    use Redirect component

--- packages/core-mobile/app/new/routes/+not-found.tsx
+++ packages/core-mobile/app/new/routes/+not-found.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 // This component is used to redirect to the portfolio screen when the user tries to access a non-existing route.
 const NotFoundRedirect = (): null => {
   // @ts-ignore TODO: make routes typesafe
-  return <Redirect href="/portoflio" />
+  return <Redirect href="/portfolio" />
 }
 
 export default NotFoundRedirect
