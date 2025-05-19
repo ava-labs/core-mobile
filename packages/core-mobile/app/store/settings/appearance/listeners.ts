752a80faa5ef74c14fee389a481e9fa641f31b82
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-05-19 16:40:27 +0300

    CP-10573: Browser Navigation buttons are too buried (#2767)

--- packages/core-mobile/app/store/settings/appearance/listeners.ts
+++ packages/core-mobile/app/store/settings/appearance/listeners.ts
@@ -1,7 +1,8 @@
-import { AppListenerEffectAPI, AppStartListening } from 'store/types'
 import { AnyAction, isAnyOf } from '@reduxjs/toolkit'
+import { setThemePreference } from '@vonovak/react-native-theme-control'
 import { Appearance as RnAppearance } from 'react-native'
 import { onRehydrationComplete } from 'store/app'
+import { AppListenerEffectAPI, AppStartListening } from 'store/types'
 import { selectIsDeveloperMode, toggleDeveloperMode } from '../advanced'
 import {
   selectSelectedAppearance,
@@ -17,6 +18,7 @@ const handleAppearanceChange = (
 ): void => {
   const { getState, dispatch } = listenerApi
   const state = getState()
+
   const isDeveloperMode = selectIsDeveloperMode(state)
   const appearance = selectSelectedAppearance(state)
   const currentColorScheme = selectSelectedColorScheme(state)
@@ -27,8 +29,21 @@ const handleAppearanceChange = (
       ? 'light'
       : (RnAppearance.getColorScheme() as ColorSchemeName)
 
-  currentColorScheme !== colorScheme &&
+  switch (appearance) {
+    case Appearance.System:
+      setThemePreference('system')
+      break
+    case Appearance.Dark:
+      setThemePreference('dark')
+      break
+    case Appearance.Light:
+      setThemePreference('light')
+      break
+  }
+
+  if (currentColorScheme !== colorScheme) {
     dispatch(setSelectedColorScheme(colorScheme))
+  }
 }
 
 export const addAppearanceListeners = (
