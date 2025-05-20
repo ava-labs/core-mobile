6a80ff92836387baf72f9fb5b6d2a9adfba823f4
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2025-05-20 16:54:34 -0400

    CP-10681: fetch balance on account detail  (#2786)

--- packages/core-mobile/app/new/features/swap/screens/SwapScreen.tsx
+++ packages/core-mobile/app/new/features/swap/screens/SwapScreen.tsx
@@ -33,7 +33,7 @@ import { useSelector } from 'react-redux'
 import AnalyticsService from 'services/analytics/AnalyticsService'
 import {
   LocalTokenWithBalance,
-  selectTokensWithZeroBalance
+  selectTokensWithZeroBalanceByNetwork
 } from 'store/balance'
 import { getTokenAddress } from 'swap/getSwapRate'
 import { calculateRate } from 'swap/utils'
@@ -54,7 +54,6 @@ export const SwapScreen = (): JSX.Element => {
   const { formatCurrency } = useFormatCurrency()
 
   const swapList = useSwapList()
-  const tokensWithZeroBalance = useSelector(selectTokensWithZeroBalance)
 
   const {
     swap,
@@ -77,6 +76,10 @@ export const SwapScreen = (): JSX.Element => {
   const [toTokenValue, setToTokenValue] = useState<bigint>()
   const [localError, setLocalError] = useState<string>('')
   const cChainNetwork = useCChainNetwork()
+  const tokensWithZeroBalance = useSelector(
+    selectTokensWithZeroBalanceByNetwork(cChainNetwork?.chainId)
+  )
+
   const [isInputFocused, setIsInputFocused] = useState<boolean>(false)
   const swapButtonBackgroundColor = useMemo(
     () => getButtonBackgroundColor('secondary', theme, false),
