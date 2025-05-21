583419d50ab06ef896d8c3163ebd193e7abd77a9
ruijialin <ruijia.lin@avalabs.org>
2025-05-21 15:54:50 -0400

    migrade fav ids

---
+++ packages/core-mobile/app/new/features/track/market/hooks/useMigrateFavoriteIds.ts
@@ -0,0 +1,80 @@
+import { useWatchlist } from 'hooks/watchlist/useWatchlist'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  selectHasBeenViewedOnce,
+  setViewOnce,
+  ViewOnceKey
+} from 'store/viewOnce'
+import {
+  selectWatchlistFavoriteIds,
+  toggleWatchListFavorite
+} from 'store/watchlist'
+
+/**
+ * @description
+ * This hook is used to migrate favorite IDs from coingecko/contract address
+ * to internId
+ */
+export const useMigrateFavoriteIds = (): {
+  hasMigratedFavoriteIds: boolean
+} => {
+  const {
+    topTokens,
+    trendingTokens,
+    isLoadingTrendingTokens,
+    isLoadingTopTokens
+  } = useWatchlist()
+  const dispatch = useDispatch()
+  const favoriteIds = useSelector(selectWatchlistFavoriteIds)
+  const hasMigratedFavoriteIds = useSelector(
+    selectHasBeenViewedOnce(ViewOnceKey.MIGRATE_TOKEN_FAVORITE_IDS)
+  )
+
+  useEffect(() => {
+    if (
+      hasMigratedFavoriteIds ||
+      isLoadingTrendingTokens ||
+      isLoadingTopTokens ||
+      favoriteIds.length === 0
+    )
+      return
+
+    favoriteIds.forEach((favoriteId: string) => {
+      const topToken = topTokens.find(token => token.coingeckoId === favoriteId)
+      if (topToken) {
+        // if the token is in the top tokens, we need to remove it from the favorites
+        // and add it again with the new id
+        dispatch(toggleWatchListFavorite(favoriteId))
+        dispatch(toggleWatchListFavorite(topToken.id))
+        return
+      }
+      const trendingToken = trendingTokens.find(token =>
+        token.id.includes(favoriteId)
+      )
+      if (trendingToken) {
+        // if the token is in the trending tokens, we need to remove it from the favorites
+        // and add it again with the new id
+        dispatch(toggleWatchListFavorite(favoriteId))
+        dispatch(toggleWatchListFavorite(trendingToken.id))
+      }
+    })
+
+    // after the migration is done, we need to set the view once
+    // so we don't run the migration again
+    dispatch(setViewOnce(ViewOnceKey.MIGRATE_TOKEN_FAVORITE_IDS))
+  }, [
+    hasMigratedFavoriteIds,
+    favoriteIds.length,
+    isLoadingTrendingTokens,
+    isLoadingTopTokens,
+    dispatch,
+    favoriteIds,
+    topTokens,
+    trendingTokens
+  ])
+
+  return {
+    hasMigratedFavoriteIds
+  }
+}
