ad76baef11f7eb91c720b67c6f590390e63e9cbe
ruijialin <ruijia.lin@avalabs.org>
2025-05-22 17:08:46 -0400

    Revert "CP-10708: Update Receive Screen (#2813)"
    This reverts commit 5bf76bafd5aff9ab3be6eeeb70fe4fde196e914a.

--- packages/core-mobile/app/new/features/receive/screens/SelectReceiveNetworkScreen.tsx
+++ packages/core-mobile/app/new/features/receive/screens/SelectReceiveNetworkScreen.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import { SelectNetworkScreen } from 'common/screens/SelectNetworkScreen'
 import { usePrimaryNetworks } from 'common/hooks/usePrimaryNetworks'
-import { useL2Networks } from 'common/hooks/useL2Networks'
 import { useReceiveSelectedNetwork } from '../store'
 
 export const SelectReceiveNetworkScreen = (): JSX.Element => {
   const { networks } = usePrimaryNetworks()
-  const { networks: l2Networks } = useL2Networks()
   const [selectedNetwork, setSelectedNetwork] = useReceiveSelectedNetwork()
 
   return (
     <SelectNetworkScreen
-      networks={networks.concat(l2Networks)}
+      networks={networks}
       selected={selectedNetwork}
       onSelect={setSelectedNetwork}
     />
