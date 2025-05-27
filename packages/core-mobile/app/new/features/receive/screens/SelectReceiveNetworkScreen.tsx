33776674ba1426d995544b815c148abd31215ab9
ruijialin <ruijia.lin@avalabs.org>
2025-05-27 13:07:47 -0400

    show only enabled l2 networks in receive select network screen

--- packages/core-mobile/app/new/features/receive/screens/SelectReceiveNetworkScreen.tsx
+++ packages/core-mobile/app/new/features/receive/screens/SelectReceiveNetworkScreen.tsx
@@ -2,16 +2,23 @@ import React from 'react'
 import { SelectNetworkScreen } from 'common/screens/SelectNetworkScreen'
 import { usePrimaryNetworks } from 'common/hooks/usePrimaryNetworks'
 import { useL2Networks } from 'common/hooks/useL2Networks'
+import { useSelector } from 'react-redux'
+import { selectEnabledChainIds } from 'store/network'
 import { useReceiveSelectedNetwork } from '../store'
 
 export const SelectReceiveNetworkScreen = (): JSX.Element => {
   const { networks } = usePrimaryNetworks()
   const { networks: l2Networks } = useL2Networks()
   const [selectedNetwork, setSelectedNetwork] = useReceiveSelectedNetwork()
+  const enabledChainIds = useSelector(selectEnabledChainIds)
+
+  const enabledL2Networks = l2Networks.filter(network =>
+    enabledChainIds.includes(network.chainId)
+  )
 
   return (
     <SelectNetworkScreen
-      networks={networks.concat(l2Networks)}
+      networks={networks.concat(enabledL2Networks)}
       selected={selectedNetwork}
       onSelect={setSelectedNetwork}
     />
