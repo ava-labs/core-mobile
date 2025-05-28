8a338ea027bb6fa280391aef16e6d0a3390a2341
ruijialin <ruijia.lin@avalabs.org>
2025-05-28 11:36:21 -0400

    add back invalidateQueries logic for getNetwork

--- packages/core-mobile/app/hooks/networks/useNetworksListener.ts
+++ packages/core-mobile/app/hooks/networks/useNetworksListener.ts
@@ -1,11 +1,28 @@
+import { addListener, isAnyOf } from '@reduxjs/toolkit'
 import { QueryCacheNotifyEvent, QueryClient } from '@tanstack/react-query'
 import { ReactQueryKeys } from 'consts/reactQueryKeys'
 import { useCallback, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { onAppUnlocked } from 'store/app'
 import { onNetworksFetchedSuccess } from 'store/network'
+import { toggleDeveloperMode } from 'store/settings/advanced'
 
 export const useNetworksListener = (queryClient: QueryClient): void => {
   const dispatch = useDispatch()
+
+  // @ts-ignore
+  useEffect(() => {
+    return dispatch(
+      addListener({
+        matcher: isAnyOf(toggleDeveloperMode, onAppUnlocked),
+        effect: async () => {
+          await queryClient.invalidateQueries({
+            queryKey: [ReactQueryKeys.NETWORKS]
+          })
+        }
+      })
+    )
+  }, [dispatch, queryClient])
 
   const callback = useCallback(
     (event: QueryCacheNotifyEvent): void => {
