6f3084f73a93c1c16ed263638d6af0ee41ccdf72
Junghwan <junghwan.jang@avalabs.org>
2025-05-29 13:50:32 -0400

    add missing migration

--- packages/core-mobile/app/store/index.ts
+++ packages/core-mobile/app/store/index.ts
@@ -30,7 +30,7 @@ import { combinedReducer as browser } from './browser'
 import { snapshotsReducer as snapshots } from './snapshots/slice'
 import { reduxStorage } from './reduxStorage'
 
-const VERSION = 19
+const VERSION = 20
 
 // list of reducers that don't need to be persisted
 // for nested/partial blacklist, please use transform
