6e8ba33b3408fcf143e9c2a4ec7f545ed70b839f
ruijialin <ruijia.lin@avalabs.org>
2025-06-02 09:28:35 -0400

    sort USD to top

--- packages/core-mobile/app/new/features/buyOnramp/screens/SelectCurrencyScreen.tsx
+++ packages/core-mobile/app/new/features/buyOnramp/screens/SelectCurrencyScreen.tsx
@@ -1,13 +1,37 @@
 import { SelectCurrency } from 'features/accountSettings/components/SelectCurrency'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { LoadingState } from 'common/components/LoadingState'
 import { ErrorState } from 'common/components/ErrorState'
+import { CurrencySymbol } from 'store/settings/currency'
 import { useOnRampCurrencyCode } from '../store'
 import { useSearchFiatCurrencies } from '../hooks/useSearchFiatCurrencies'
 
 export const SelectCurrencyScreen = (): React.JSX.Element => {
   const [selectedCurrency, setSelectedCurrency] = useOnRampCurrencyCode()
   const { data: currencies, isLoading } = useSearchFiatCurrencies()
+
+  const sortedCurrencies = useMemo(() => {
+    if (currencies === undefined) {
+      return []
+    }
+    const usCurrency = currencies.filter(
+      currency => currency.currencyCode === CurrencySymbol.USD
+    )
+    const otherCurrencies = currencies.toSorted((a, b) => {
+      return a.name.localeCompare(b.name)
+    })
+    return [...usCurrency, ...otherCurrencies]
+  }, [currencies])
+
+  const transformedCurrencies = useMemo(() => {
+    return sortedCurrencies.map(curr => {
+      return {
+        name: curr.name,
+        symbol: curr.currencyCode.toUpperCase(),
+        logoUrl: curr.symbolImageUrl
+      }
+    })
+  }, [sortedCurrencies])
 
   if (isLoading) {
     return <LoadingState sx={{ flex: 1 }} />
@@ -19,13 +43,7 @@ export const SelectCurrencyScreen = (): React.JSX.Element => {
 
   return (
     <SelectCurrency
-      currencies={currencies.map(curr => {
-        return {
-          name: curr.name,
-          symbol: curr.currencyCode.toUpperCase(),
-          logoUrl: curr.symbolImageUrl
-        }
-      })}
+      currencies={transformedCurrencies}
       selectedCurrency={selectedCurrency}
       setSelectedCurrency={setSelectedCurrency}
     />
