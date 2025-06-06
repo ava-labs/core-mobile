47d39a38f7c578a92b867139b2ca5d3823aad1a3
ruijialin <ruijia.lin@avalabs.org>
2025-06-06 12:02:21 -0400

    listen to onLogin

--- packages/core-mobile/app/store/settings/currency/listeners.ts
+++ packages/core-mobile/app/store/settings/currency/listeners.ts
@@ -1,12 +1,7 @@
-import { AnyAction, isAnyOf } from '@reduxjs/toolkit'
-import { onAppUnlocked } from 'store/app'
+import { AnyAction } from '@reduxjs/toolkit'
+import { onLogIn } from 'store/app'
 import { AppListenerEffectAPI, AppStartListening } from 'store/types'
 import { getLocales } from 'expo-localization'
-import {
-  selectHasBeenViewedOnce,
-  setViewOnce,
-  ViewOnceKey
-} from 'store/viewOnce'
 import { setSelectedCurrency } from './slice'
 import { currencies, CurrencySymbol } from './types'
 
@@ -17,27 +12,19 @@ const handleSetDefaultCurrency = (
   const locales = getLocales()
   const defaultCurrency = locales[0]?.currencyCode
 
-  const { getState, dispatch } = listenerApi
-  const state = getState()
+  const { dispatch } = listenerApi
 
-  const hasSetDefaultCurrency = selectHasBeenViewedOnce(
-    ViewOnceKey.SET_DEFAULT_CURRENCY
-  )(state)
-
-  if (hasSetDefaultCurrency === false) {
-    const supportedCurrency =
-      currencies.find(curr => curr.symbol === defaultCurrency)?.symbol ??
-      CurrencySymbol.USD
-    dispatch(setSelectedCurrency(supportedCurrency))
-    dispatch(setViewOnce(ViewOnceKey.SET_DEFAULT_CURRENCY))
-  }
+  const supportedCurrency =
+    currencies.find(curr => curr.symbol === defaultCurrency)?.symbol ??
+    CurrencySymbol.USD
+  dispatch(setSelectedCurrency(supportedCurrency))
 }
 
 export const addCurrencyListeners = (
   startListening: AppStartListening
 ): void => {
   startListening({
-    matcher: isAnyOf(onAppUnlocked),
+    actionCreator: onLogIn,
     effect: handleSetDefaultCurrency
   })
 }
