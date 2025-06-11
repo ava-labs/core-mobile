d94331f604569fbaec2d2ef4346bc3cb4aff811f
Neven <piaf1986@gmail.com>
2025-06-11 19:12:02 +0200

    Update VerifyWithPinOrBiometry instance

--- packages/core-mobile/app/new/routes/(signedIn)/(modals)/accountSettings/verifyChangePin.tsx
+++ packages/core-mobile/app/new/routes/(signedIn)/(modals)/accountSettings/verifyChangePin.tsx
@@ -1,15 +1,25 @@
 import { VerifyWithPinOrBiometry } from 'common/components/VerifyWithPinOrBiometry'
+import { useActiveWalletId } from 'common/hooks/useActiveWallet'
 import { useRouter } from 'expo-router'
 import React from 'react'
+import BiometricsSDK from 'utils/BiometricsSDK'
 
 const VerifyChangePinScreen = (): React.JSX.Element => {
   const { replace } = useRouter()
+  const walletId = useActiveWalletId()
 
-  const handleLoginSuccess = (mnemonic: string): void => {
-    // @ts-ignore TODO: make routes typesafe
-    replace({ pathname: '/accountSettings/changePin', params: { mnemonic } })
+  const handleLoginSuccess = async (): Promise<void> => {
+    const walletSecret = await BiometricsSDK.loadWalletSecret(walletId)
+    if (!walletSecret) {
+      throw new Error('Failed to load wallet secret')
+    }
+    replace({
+      // @ts-ignore TODO: make routes typesafe
+      pathname: '/accountSettings/changePin',
+      params: { mnemonic: walletSecret }
+    })
   }
 
-  return <VerifyWithPinOrBiometry onLoginSuccess={handleLoginSuccess} />
+  return <VerifyWithPinOrBiometry onVerifySuccess={handleLoginSuccess} />
 }
 export default VerifyChangePinScreen
