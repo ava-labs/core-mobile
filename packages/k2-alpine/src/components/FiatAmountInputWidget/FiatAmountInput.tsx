82bc4244b5b6d610beac311d3d67a5d9482253ff
ruijialin <ruijia.lin@avalabs.org>
2025-06-13 15:01:36 -0400

    add comment

--- packages/k2-alpine/src/components/FiatAmountInputWidget/FiatAmountInput.tsx
+++ packages/k2-alpine/src/components/FiatAmountInputWidget/FiatAmountInput.tsx
@@ -114,7 +114,6 @@ export const FiatAmountInput = forwardRef<
           setMaxLength(sanitizedFrontValue.length + '.'.length + 5)
 
           const normalizedValue = normalizeValue(changedValue)
-          // const valueInCurrency = formatInCurrency(Number(normalizedValue))
           setValue(normalizedValue)
           onChange?.(normalizedValue)
         } else {
@@ -237,6 +236,8 @@ const styles = StyleSheet.create({
 
 const PLACEHOLDER = '0.00'
 
+// returns matching currency symbol for the amount with symbol
+// e.g '$' | '€' | '£' | '¥' | '₹'
 function getCurrencySymbol(amountWithSymbol: string): string {
   const ScRe =
     /[$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]/
