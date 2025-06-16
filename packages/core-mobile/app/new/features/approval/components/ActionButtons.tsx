ef46060868527127cf047266f9dbdc84930d4cee
Eunji Song <32075932+eunjisong@users.noreply.github.com>
2025-06-16 16:13:31 -0400

    QA - Send AVAX & fix onboarding tests (#2854)
    Signed-off-by: Eunji Song <32075932+eunjisong@users.noreply.github.com>

--- packages/core-mobile/app/new/features/approval/components/ActionButtons.tsx
+++ packages/core-mobile/app/new/features/approval/components/ActionButtons.tsx
@@ -22,11 +22,13 @@ export type ActionButtonsProps = {
     onPress: () => void
     disabled?: boolean
     isLoading?: boolean
+    testID?: string
   }
   cancel: {
     label: string
     onPress: () => void
     disabled?: boolean
+    testID?: string
   }
   alert?: Alert
 }
@@ -59,7 +61,10 @@ export const ActionButtons = ({
           size="large"
           type="primary"
           onPress={() => confirm.onPress()}
-          disabled={shouldDisableConfirm}>
+          disabled={shouldDisableConfirm}
+          testID={
+            shouldDisableConfirm ? 'approve_button_disabled' : 'approve_button'
+          }>
           {confirm.isLoading ? <ActivityIndicator /> : confirm.label}
         </Button>
         <Button
@@ -67,7 +72,8 @@ export const ActionButtons = ({
           type="tertiary"
           style={{ marginTop: 16 }}
           onPress={() => cancel.onPress()}
-          disabled={cancel.disabled}>
+          disabled={cancel.disabled}
+          testID="reject_button">
           {cancel.label}
         </Button>
       </View>
