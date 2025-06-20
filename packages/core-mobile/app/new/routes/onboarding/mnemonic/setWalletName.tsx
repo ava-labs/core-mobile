1e4018f39346eb0b3ac25ccb8b7dda12b6027aea
Neven <piaf1986@gmail.com>
2025-06-20 17:29:18 +0200

    Use useActiveWallet

--- packages/core-mobile/app/new/routes/onboarding/mnemonic/setWalletName.tsx
+++ packages/core-mobile/app/new/routes/onboarding/mnemonic/setWalletName.tsx
@@ -1,20 +1,18 @@
 import React, { useState } from 'react'
 import AnalyticsService from 'services/analytics/AnalyticsService'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useRouter } from 'expo-router'
 import { SetWalletName as Component } from 'features/onboarding/components/SetWalletName'
-import { selectActiveWallet, setWalletName } from 'store/wallet/slice'
+import { setWalletName } from 'store/wallet/slice'
+import { useActiveWallet } from 'common/hooks/useActiveWallet'
 
 export default function SetWalletName(): JSX.Element {
   const [name, setName] = useState<string>('Wallet 1')
   const dispatch = useDispatch()
   const { navigate } = useRouter()
-  const activeWallet = useSelector(selectActiveWallet)
+  const activeWallet = useActiveWallet()
 
   const handleNext = (): void => {
-    if (!activeWallet) {
-      return
-    }
     AnalyticsService.capture('Onboard:WalletNameSet')
     dispatch(setWalletName({ walletId: activeWallet.id, name }))
     navigate({
