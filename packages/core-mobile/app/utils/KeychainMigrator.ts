cd21f54c755f94e56d73e5898bc02f84a7ae5673
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-06-27 17:08:25 +0300

    move await after newPinKeyExists check

--- packages/core-mobile/app/utils/KeychainMigrator.ts
+++ packages/core-mobile/app/utils/KeychainMigrator.ts
@@ -24,13 +24,13 @@ class KeychainMigrator {
   > {
     // Check for legacy wallet data with both PIN and biometrics
     const newPinKeyExists = await BiometricsSDK.hasEncryptionKeyWithPin()
-    const newBioKeyExists = await BiometricsSDK.hasEncryptionKeyWithBiometry()
     //fully migrated
     if (newPinKeyExists) {
       return false
     }
 
     // no pin key, but bio key exists
+    const newBioKeyExists = await BiometricsSDK.hasEncryptionKeyWithBiometry()
     if (newBioKeyExists) {
       return 'completePartialMigration'
     }
