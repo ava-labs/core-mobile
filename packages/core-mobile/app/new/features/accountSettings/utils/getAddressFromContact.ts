a583e3646a32b066a16ea0eab2d0f4c91f4a0b67
James Boyer <james.boyer@avalabs.org>
2025-06-30 16:06:53 -0400

    CP-11072: Show contacts address for solana  (#2999)

--- packages/core-mobile/app/new/features/accountSettings/utils/getAddressFromContact.ts
+++ packages/core-mobile/app/new/features/accountSettings/utils/getAddressFromContact.ts
@@ -4,5 +4,6 @@ export const getAddressFromContact = (contact: Contact): string | undefined => {
   if (contact.address) return contact.address
   if (contact.addressXP) return contact.addressXP
   if (contact.addressBTC) return contact.addressBTC
+  if (contact.addressSVM) return contact.addressSVM
   return undefined
 }
