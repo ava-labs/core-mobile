def1aca0f50e07d478ccbed6a3d23cc51d552df7
ruijialin <ruijia.lin@avalabs.org>
2025-07-03 10:54:53 -0400

    fix change pin flow

--- packages/core-mobile/app/new/routes/(signedIn)/(modals)/accountSettings/changePin.tsx
+++ packages/core-mobile/app/new/routes/(signedIn)/(modals)/accountSettings/changePin.tsx
@@ -1,16 +1,33 @@
 import { useStoredBiometrics } from 'common/hooks/useStoredBiometrics'
+import { useRouter } from 'expo-router'
 import { CreatePin } from 'features/onboarding/components/CreatePin'
 import React, { useCallback } from 'react'
 import BiometricsSDK from 'utils/BiometricsSDK'
 import Logger from 'utils/Logger'
 
 const ChangePinScreen = (): React.JSX.Element => {
+  const { canGoBack, back } = useRouter()
   const { isBiometricAvailable, useBiometrics, setUseBiometrics } =
     useStoredBiometrics()
 
-  const handleEnteredValidPin = useCallback((pin: string): void => {
-    BiometricsSDK.changePin(pin).catch(Logger.error)
-  }, [])
+  const handleEnteredValidPin = useCallback(
+    (pin: string): void => {
+      if (useBiometrics) {
+        BiometricsSDK.enableBiometry()
+          .then(() => {
+            canGoBack() && back()
+          })
+          .catch(Logger.error)
+        return
+      }
+      BiometricsSDK.changePin(pin)
+        .then(() => {
+          canGoBack() && back()
+        })
+        .catch(Logger.error)
+    },
+    [back, canGoBack, useBiometrics]
+  )
 
   return (
     <CreatePin
