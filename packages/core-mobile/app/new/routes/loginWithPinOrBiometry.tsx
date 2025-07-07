9031880dd178ac3bfe2ae25a485a8f4b4fecf378
Junghwan <junghwan.jang@avalabs.org>
2025-07-08 02:01:29 +0900

    Revert "CP-11349: Show alert when biometric auth is triggered without permission (#3024)"
    This reverts commit abb2dc2b6ad4ae4480b4674bded59f860dbffcd9.

--- packages/core-mobile/app/new/routes/loginWithPinOrBiometry.tsx
+++ packages/core-mobile/app/new/routes/loginWithPinOrBiometry.tsx
@@ -7,7 +7,6 @@ import {
   Logos,
   PinInput,
   PinInputActions,
-  showAlert,
   Text,
   useTheme,
   View
@@ -19,7 +18,6 @@ import { usePreventScreenRemoval } from 'common/hooks/usePreventScreenRemoval'
 import { useStoredBiometrics } from 'common/hooks/useStoredBiometrics'
 import { useFocusEffect, useRouter } from 'expo-router'
 import { useWallet } from 'hooks/useWallet'
-import * as LocalAuth from 'expo-local-authentication'
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import {
   InteractionManager,
@@ -174,26 +172,9 @@ const LoginWithPinOrBiometry = (): JSX.Element => {
     }
   }
 
-  const handlePromptBioLogin = useCallback(async () => {
-    verifyBiometric().catch(err => {
-      if (bioType !== BiometricType.NONE) {
-        LocalAuth.isEnrolledAsync()
-          .then(isEnrolled => {
-            if (!isEnrolled) {
-              showAlert({
-                title: `Enable ${bioType} for Core`,
-                description: `${bioType} is currently disabled for Core. To use ${bioType} to log in, open the iOS Settings app and enable ${bioType} for Core.`,
-                buttons: [{ text: 'OK' }]
-              })
-            }
-            Logger.error(err)
-          })
-          .catch(Logger.error)
-      } else {
-        Logger.error(err)
-      }
-    })
-  }, [verifyBiometric, bioType])
+  const handlePromptBioLogin = useCallback(() => {
+    verifyBiometric().catch(Logger.error)
+  }, [verifyBiometric])
 
   const handlePressBackground = (): void => {
     if (pinInputRef.current?.isFocused()) {
@@ -223,14 +204,11 @@ const LoginWithPinOrBiometry = (): JSX.Element => {
   useFocusEffect(
     useCallback(() => {
       InteractionManager.runAfterInteractions(() => {
-        if (!isBiometricAvailable || !useBiometrics) {
-          focusPinInput()
-          return
-        }
-
         const accessType = BiometricsSDK.getAccessType()
         if (accessType === 'BIO') {
           handlePromptBioLogin()
+        } else if (!isBiometricAvailable || !useBiometrics) {
+          focusPinInput()
         }
       })
 
