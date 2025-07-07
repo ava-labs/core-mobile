568c180810a6024b43e85582d7202a0bdbeefed4
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-07-07 13:53:44 +0300

    added errors

--- packages/core-mobile/app/store/balance/types.ts
+++ packages/core-mobile/app/store/balance/types.ts
@@ -14,6 +14,7 @@ export type Balance = {
   accountId: string | undefined
   chainId: number
   tokens: LocalTokenWithBalance[]
+  error: Error | null
 }
 
 export type Balances = { [chainId_address: string]: Balance }
