92008d13227548975d75e5f497f9c0e74d74f110
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-07-18 23:39:19 +0300

    working ios version

--- packages/core-mobile/ios/AppDelegate.swift
+++ packages/core-mobile/ios/AppDelegate.swift
@@ -1,10 +1,13 @@
+import FirebaseCore
 import Expo
 import React
 import ReactAppDependencyProvider
- 
+
+
 @UIApplicationMain
 public class AppDelegate: ExpoAppDelegate {
   var window: UIWindow?
+  var dependencyProvider: RCTAppDependencyProvider?
  
   var reactNativeDelegate: ExpoReactNativeFactoryDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
@@ -13,18 +16,25 @@ public class AppDelegate: ExpoAppDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    // Firebase configuration
+    RNFBAppCheckModule.sharedInstance() // <-- new for AppCheck to work
+    FirebaseApp.configure()   
+    
+    self.dependencyProvider = RCTAppDependencyProvider()
+    
     let delegate = ReactNativeDelegate()
     let factory = ExpoReactNativeFactory(delegate: delegate)
-    delegate.dependencyProvider = RCTAppDependencyProvider()
  
     reactNativeDelegate = delegate
     reactNativeFactory = factory
     bindReactNativeFactory(factory)
- 
+
+    loadRocketSimConnect()
+
 #if os(iOS) || os(tvOS)
     window = UIWindow(frame: UIScreen.main.bounds)
     factory.startReactNative(
-      withModuleName: "main",
+      withModuleName: "AvaxWallet",
       in: window,
       launchOptions: launchOptions)
 #endif
@@ -50,6 +60,34 @@ public class AppDelegate: ExpoAppDelegate {
     let result = RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
     return super.application(application, continue: userActivity, restorationHandler: restorationHandler) || result
   }
+  
+  private func loadRocketSimConnect() {
+#if DEBUG
+    let frameworkPath = "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework"
+    guard let frameworkBundle = Bundle(path: frameworkPath) else {
+      print("Failed to find RocketSim framework")
+      return
+    }
+    
+    do {
+      try frameworkBundle.loadAndReturnError()
+      print("RocketSim Connect successfully linked")
+    } catch {
+      print("Failed to load linker framework: \(error)")
+    }
+#endif
+  }
+  
+  public override func applicationDidBecomeActive(_ application: UIApplication) {
+    super.applicationDidBecomeActive(application)
+    
+    // this only reset the badgeCount in app group userDefaults,
+    // the actual badge count is reset in handleNotificationCleanup
+    // inside react native project
+    if let userDefaults = UserDefaults(suiteName: "group.org.avalabs.corewallet") {
+      userDefaults.set(0, forKey: "badgeCount")
+    }
+  }
 }
  
 class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
@@ -59,7 +97,7 @@ class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
     // needed to return the correct URL for expo-dev-client.
     bridge.bundleURL ?? bundleURL()
   }
- 
+
   override func bundleURL() -> URL? {
 #if DEBUG
     return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: ".expo/.virtual-metro-entry")
