ca0df382972ee2208081bf38f6b0152e4b733998
An Nguyen <an.nguyen@avalabs.org>
2025-07-19 18:20:09 -0500

    Update setWalletName.tsx

--- packages/core-mobile/app/new/routes/onboarding/seedless/setWalletName.tsx
+++ packages/core-mobile/app/new/routes/onboarding/seedless/setWalletName.tsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 import AnalyticsService from 'services/analytics/AnalyticsService'
-import { useDispatch } from 'react-redux'
-import { setAccountTitle } from 'store/account'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectActiveAccount, setAccountTitle } from 'store/account'
 import { WalletType } from 'services/wallet/types'
 import { useRouter } from 'expo-router'
 import { SetWalletName as Component } from 'features/onboarding/components/SetWalletName'
-import { useActiveAccount } from 'common/hooks/useActiveAccount'
 
 export default function SetWalletName(): JSX.Element {
   const [name, setName] = useState<string>('Wallet 1')
   const dispatch = useDispatch()
   const { navigate } = useRouter()
-  const activeAccount = useActiveAccount()
+  const activeAccount = useSelector(selectActiveAccount)
 
   const handleNext = (): void => {
+    if (!activeAccount) {
+      return
+    }
+
     AnalyticsService.capture('Onboard:WalletNameSet')
     dispatch(
       setAccountTitle({
