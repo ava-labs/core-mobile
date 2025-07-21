f6e4224c64993a2b97bf934e8aaa5af032291a55
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2025-07-21 08:09:56 -0400

    CP-11599: use useDebounceCallback for onboarding buttons (#3073)
    Co-authored-by: An Nguyen <an.nguyen@avalabs.org>

--- packages/core-mobile/app/new/routes/onboarding/mnemonic/confirmation.tsx
+++ packages/core-mobile/app/new/routes/onboarding/mnemonic/confirmation.tsx
@@ -2,6 +2,7 @@ import { Confirmation as Component } from 'features/onboarding/components/Confir
 import { useWallet } from 'hooks/useWallet'
 import React from 'react'
 import { WalletType } from 'services/wallet/types'
+import { useDebouncedCallback } from 'use-debounce'
 import Logger from 'utils/Logger'
 
 export default function Confirmation(): JSX.Element {
@@ -11,5 +12,7 @@ export default function Confirmation(): JSX.Element {
     login(WalletType.MNEMONIC).catch(Logger.error)
   }
 
-  return <Component onNext={handleNext} />
+  const debouncedHandleNext = useDebouncedCallback(handleNext, 1000)
+
+  return <Component onNext={debouncedHandleNext} />
 }
