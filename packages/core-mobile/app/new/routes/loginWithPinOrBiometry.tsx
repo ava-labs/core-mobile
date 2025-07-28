d1908be45cee20a566ac8d6a1add928d9c3408ad
B0Y3R-AVA <james.boyer@avalabs.org>
2025-07-28 17:13:13 -0400

    fixes for skipping pin screen when biometrics is disabled

--- packages/core-mobile/app/new/routes/loginWithPinOrBiometry.tsx
+++ packages/core-mobile/app/new/routes/loginWithPinOrBiometry.tsx
@@ -37,6 +37,8 @@ import { selectSelectedAvatar } from 'store/settings/avatar'
 import { selectActiveWalletId } from 'store/wallet/slice'
 import BiometricsSDK, { BiometricType } from 'utils/BiometricsSDK'
 import Logger from 'utils/Logger'
+import { commonStorage } from 'utils/mmkv'
+import { StorageKey } from 'resources/Constants'
 
 const LoginWithPinOrBiometry = (): JSX.Element => {
   const walletState = useSelector(selectWalletState)
@@ -205,8 +207,27 @@ const LoginWithPinOrBiometry = (): JSX.Element => {
     useCallback(() => {
       InteractionManager.runAfterInteractions(() => {
         const accessType = BiometricsSDK.getAccessType()
-        if (accessType === 'BIO') {
+
+        /*
+         * Fix inconsistent state: if accessType is 'BIO' but biometrics are disabled or not available
+         * This is needed due to previous bug in BiometricsSDK.storeEncryptionKeyWithBiometry()
+         * which was called by KeychainMigrator during app updates/migrations.
+         * The bug set SECURE_ACCESS_SET to 'BIO' before checking if biometric storage succeeded,
+         * leaving users in an inconsistent state when biometrics weren't available.
+         * We automatically correct this by setting SECURE_ACCESS_SET back to 'PIN'
+         */
+        if (accessType === 'BIO' && (!useBiometrics || !isBiometricAvailable)) {
+          commonStorage.set(StorageKey.SECURE_ACCESS_SET, 'PIN')
+          // Re-fetch the corrected access type
+          const correctedAccessType = BiometricsSDK.getAccessType()
+
+          if (correctedAccessType === 'PIN') {
+            focusPinInput()
+          }
+        } else if (accessType === 'BIO') {
           handlePromptBioLogin()
+        } else if (accessType === 'PIN') {
+          focusPinInput()
         } else if (!isBiometricAvailable || !useBiometrics) {
           focusPinInput()
         }
