7cbf876342bc1c6978516871f234ffad04b9855a
eunjisong <eunji.l.song@gmail.com>
2025-08-08 14:54:59 -0400

    changes

--- packages/core-mobile/e2e/tests/transactions/swap/solana/swapSolana.e2e.smoke.ts
+++ packages/core-mobile/e2e/tests/transactions/swap/solana/swapSolana.e2e.smoke.ts
@@ -2,23 +2,23 @@ import { warmup } from '../../../../helpers/warmup'
 import commonElsPage from '../../../../pages/commonEls.page'
 import SwapTabPage from '../../../../pages/swapTab.page'
 
-describe('Swap', () => {
+describe('Swap on Solana', () => {
   beforeAll(async () => {
     await warmup()
   })
 
-  it('Should swap AVAX to ERC20', async () => {
-    await SwapTabPage.swap('AVAX', 'USDC', '0.00001')
+  it('Should swap SOL to SPL', async () => {
+    await SwapTabPage.swap('SOL', 'USDC', '0.00001')
     await commonElsPage.verifySuccessToast()
   })
 
-  it('Should swap ERC20 to AVAX', async () => {
-    await SwapTabPage.swap('USDC', 'AVAX', '0.0001')
+  it('Should swap SPL to SOL', async () => {
+    await SwapTabPage.swap('USDC', 'SOL', '0.0001')
     await commonElsPage.verifySuccessToast()
   })
 
-  it('Should swap ERC20 to ERC20', async () => {
-    await SwapTabPage.swap('USDC', 'USDT', '0.0001')
+  it('Should swap SPL to SPL', async () => {
+    await SwapTabPage.swap('USDC', 'JUP', '0.0001')
     await commonElsPage.verifySuccessToast()
   })
 })
