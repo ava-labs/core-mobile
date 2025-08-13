d374d3eaff908a7cc76f260999e5e8aa08ac4477
eunjisong <eunji.l.song@gmail.com>
2025-08-13 17:55:58 -0400

    cleanup

--- packages/core-mobile/app/new/features/accountSettings/components/AccountButtons.tsx
+++ packages/core-mobile/app/new/features/accountSettings/components/AccountButtons.tsx
@@ -135,7 +135,7 @@ export const AccountButtons = ({
       )}
       {!isRemoveEnabled && (
         <Text variant="caption" sx={{ color: theme.colors.$textSecondary }}>
-          Only the most recently added seed phrase  s may be removed
+          Only the most recently added seed phrase accounts may be removed
         </Text>
       )}
     </View>
