853568659d18e87e285cc9a5ecbc666d3184eae3
An Nguyen <an.nguyen@avalabs.org>
2025-08-14 09:10:29 -0500

    add copy button

--- packages/core-mobile/benchmark/utils.js
+++ packages/core-mobile/benchmark/utils.js
@@ -1,7 +1,17 @@
 import { Alert } from 'react-native'
+import { copyToClipboard } from '../app/new/common/utils/clipboard'
 
 // utility to await an alert dismissal
 export const showResult = (title, message) =>
   new Promise(resolve => {
-    Alert.alert(title, message, [{ text: 'OK', onPress: () => resolve() }])
+    Alert.alert(title, message, [
+      {
+        text: 'Copy Results',
+        onPress: () => {
+          copyToClipboard(message)
+          resolve()
+        }
+      },
+      { text: 'OK', onPress: () => resolve() }
+    ])
   })
