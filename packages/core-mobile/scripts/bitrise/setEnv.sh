685f2911022e254a2f6e90f0feaeaeeac459ab34
An Nguyen <an.nguyen@avalabs.org>
2025-08-18 10:43:06 -0500

    Update setEnv.sh

--- packages/core-mobile/scripts/bitrise/setEnv.sh
+++ packages/core-mobile/scripts/bitrise/setEnv.sh
@@ -23,9 +23,9 @@ fi
 if [ "$APP_TITLE" = "Core Mobile Internal" ]; then
     echo "using internal Google services files"
     if [ "$PLATFORM" = "Android" ]; then
-        cp google-services.json.internal android/app/google-services.json
+        cp './google-services-files/google-services.json.internal' android/app/google-services.json
     else
-        cp GoogleService-Info.plist.internal ios/GoogleService-Info.plist
+        cp './google-services-files/GoogleService-Info.plist.internal' ios/GoogleService-Info.plist
     fi
 else
     echo "using external Google services files"
