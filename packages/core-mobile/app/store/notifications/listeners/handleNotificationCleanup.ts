44c9af2d586f744bac034c07b91c5e71b407ea32
Junghwan <junghwan.jang@avalabs.org>
2025-08-22 16:31:39 -0400

    clean up notifications once on Android 16 to avoid crash caused by notifee

--- packages/core-mobile/app/store/notifications/listeners/handleNotificationCleanup.ts
+++ packages/core-mobile/app/store/notifications/listeners/handleNotificationCleanup.ts
@@ -2,6 +2,12 @@ import { AppListenerEffectAPI } from 'store/types'
 import NotificationsService from 'services/notifications/NotificationsService'
 import Logger from 'utils/Logger'
 import { selectIsEarnBlocked } from 'store/posthog'
+import { Platform } from 'react-native'
+import {
+  selectHasBeenViewedOnce,
+  setViewOnce,
+  ViewOnceKey
+} from 'store/viewOnce'
 import { isStakeCompleteNotificationDisabled } from './utils'
 
 export const handleNotificationCleanup = async (
@@ -23,15 +29,41 @@ export const handleStakeCompleteNotificationCleanup = async (
   const stakeCompleteNotificationDisabled =
     await isStakeCompleteNotificationDisabled(listenerApi)
 
-  if (isEarnBlocked || stakeCompleteNotificationDisabled) {
-    stakeCompleteNotificationDisabled &&
+  // There's a bug in Notifee on Android 16 that requires a one-time cleanup of notifications
+  // See: https://github.com/invertase/notifee/issues/515#issuecomment-1369637101
+  const hasNotificationsCleanedUpOnAndroid16 = selectHasBeenViewedOnce(
+    ViewOnceKey.NOTIFICATIONS_CLEANED_UP_ANDROID_16
+  )(state)
+  const isAndroid36OrHigher =
+    Platform.OS === 'android' && Platform.Version >= 36
+  const shouldCleanUpNotificationsOnceOnAndroid16 =
+    hasNotificationsCleanedUpOnAndroid16 === false && isAndroid36OrHigher
+
+  if (
+    isEarnBlocked ||
+    stakeCompleteNotificationDisabled ||
+    shouldCleanUpNotificationsOnceOnAndroid16
+  ) {
+    if (stakeCompleteNotificationDisabled) {
       Logger.info(
         'user has disabled either stake complete in-app notification or system-level notification, cancelling all displayed/pending stake complete notifications.'
       )
-    isEarnBlocked &&
+    }
+
+    if (isEarnBlocked) {
       Logger.info(
         'Earn feature is not enabled, cancelling all displayed/pending stake complete notifications.'
       )
+    }
+
+    if (shouldCleanUpNotificationsOnceOnAndroid16) {
+      Logger.info(
+        'Should clean up notifications once on Android 16 due to a Notifee bug, cancelling all displayed/pending stake complete notifications.'
+      )
+      listenerApi.dispatch(
+        setViewOnce(ViewOnceKey.NOTIFICATIONS_CLEANED_UP_ANDROID_16)
+      )
+    }
 
     // TODO only cancel stake complete notifications
     await NotificationsService.cancelAllNotifications()
