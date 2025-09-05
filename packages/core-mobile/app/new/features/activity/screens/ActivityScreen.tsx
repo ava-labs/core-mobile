34a6fda3e85b43cfbe97a6a3695551a7255283cd
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2025-09-05 11:44:01 -0400

    CP-12002: Fix unknown XP tx in activity tab (#3195)

--- packages/core-mobile/app/new/features/activity/screens/ActivityScreen.tsx
+++ packages/core-mobile/app/new/features/activity/screens/ActivityScreen.tsx
@@ -50,8 +50,13 @@ export const ActivityScreen = ({
     xpToken,
     network,
     networkFilterDropdown,
+    isXpChain,
     refresh
   } = useActivityFilterAndSearch({ searchText })
+
+  const isLoadingXpToken = useMemo(() => {
+    return isXpChain && !xpToken
+  }, [isXpChain, xpToken])
 
   const keyboardAvoidingStyle = useAnimatedStyle(() => {
     return {
@@ -88,7 +93,7 @@ export const ActivityScreen = ({
   }, [filter, network, networkFilterDropdown])
 
   const emptyComponent = useMemo(() => {
-    if (isRefreshing || isLoading) {
+    if (isRefreshing || isLoading || isLoadingXpToken) {
       return <LoadingState />
     }
 
@@ -114,7 +119,14 @@ export const ActivityScreen = ({
         description="Interact with this token onchain and see your activity here"
       />
     )
-  }, [isError, isLoading, isRefreshing, refresh, searchText.length])
+  }, [
+    isError,
+    isLoading,
+    isLoadingXpToken,
+    isRefreshing,
+    refresh,
+    searchText.length
+  ])
 
   const renderEmpty = useCallback(() => {
     return (
@@ -130,6 +142,10 @@ export const ActivityScreen = ({
     )
   }, [containerStyle.minHeight, emptyComponent, keyboardAvoidingStyle])
 
+  const activityListData = useMemo(() => {
+    return isLoadingXpToken ? [] : data
+  }, [data, isLoadingXpToken])
+
   return (
     <Animated.View
       entering={getListItemEnteringAnimation(5)}
@@ -138,7 +154,7 @@ export const ActivityScreen = ({
         flex: 1
       }}>
       <ActivityList
-        data={data}
+        data={activityListData}
         xpToken={xpToken}
         handlePendingBridge={handlePendingBridge}
         handleExplorerLink={handleExplorerLink}
