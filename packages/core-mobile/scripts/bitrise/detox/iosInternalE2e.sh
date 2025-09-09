5c3b01f02a48251f2ac5fecccd0d26d928a425b8
eunjisong <eunji.l.song@gmail.com>
2025-09-09 16:51:45 -0400

    testing

--- packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
@@ -1,29 +1,12 @@
 #!/usr/bin/env bash
 set -o pipefail
 
-if [[ "$(uname -m)" == "arm64" ]]; then
-  if ! /usr/bin/pgrep oahd >/dev/null 2>&1; then
-    echo "[E2E] Installing Rosetta..."
-    sudo softwareupdate --install-rosetta --agree-to-license || true
-  fi
-fi
-
 xcrun simctl boot "iPhone 15 Pro" || true
 xcrun simctl bootstatus "iPhone 15 Pro" -b || true
 open -a Simulator || true
 
+yarn start &
 npm rebuild detox
-
-if ! xcrun simctl get_app_container booted org.avalabs.avaxwallet.internal >/dev/null 2>&1; then
-  echo "[E2E] App not installed. Installing..."
-  if [[ -z "${BITRISE_APP_DIR_PATH:-}" || ! -d "$BITRISE_APP_DIR_PATH" ]]; then
-    echo "BITRISE_APP_DIR_PATH not found: ${BITRISE_APP_DIR_PATH:-<empty>}"
-    exit 1
-  fi
-  echo "Installing app from: $BITRISE_APP_DIR_PATH"
-  xcrun simctl install booted "$BITRISE_APP_DIR_PATH"
-  echo "App installed!"
-fi
 
 ./node_modules/.bin/detox test \
   --configuration ios.internal.release.smoke.ci \
