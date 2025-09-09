304c6014f3bce198dd41d316327dcfbabea116b3
eunjisong <eunji.l.song@gmail.com>
2025-09-09 11:51:38 -0400

    wait for 10 sec

--- packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/detox/iosInternalE2e.sh
@@ -3,9 +3,16 @@
 # make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
 set -o pipefail
 
+sleep 10
+
 npm rebuild detox
 
-./node_modules/.bin/detox test --configuration ios.internal.release.smoke.ci --record-logs all --retries 1 --headless --max-workers 1; test_result=$?
+./node_modules/.bin/detox test \
+  --configuration ios.internal.release.smoke.ci \
+  --record-logs all \
+  --retries 1 \
+  --headless \
+  --max-workers 1; test_result=$?
 
 npx ts-node ./e2e/attachLogsSendResultsToTestrail.ts && sleep 5
 
