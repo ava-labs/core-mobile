e297ee2b7b2531e9464958dc335ef119f0404be9
Junghwan <junghwan.jang@avalabs.org>
2025-09-10 16:49:37 -0400

    fix bug

--- packages/core-mobile/app/new/common/hooks/usePromptEnableNotificationsIfNeeded.ts
+++ packages/core-mobile/app/new/common/hooks/usePromptEnableNotificationsIfNeeded.ts
@@ -1,6 +1,6 @@
 import { showAlert } from '@avalabs/k2-alpine'
 import { AuthorizationStatus } from '@notifee/react-native'
-import { useCallback } from 'react'
+import { useCallback, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import NotificationsService from 'services/notifications/NotificationsService'
 import { turnOnAllNotifications } from 'store/notifications'
@@ -21,7 +21,12 @@ export const usePromptEnableNotificationsIfNeeded =
       selectHasBeenViewedOnce(ViewOnceKey.NOTIFICATIONS_PROMPT)
     )
 
+    // prevents double prompt in the same session
+    const showingAlertRef = useRef(false)
+
     return useCallback(async () => {
+      if (showingAlertRef.current) return
+
       const authorizationStatus =
         await NotificationsService.getNotificationSettings()
 
@@ -36,6 +41,7 @@ export const usePromptEnableNotificationsIfNeeded =
       )
         return
 
+      showingAlertRef.current = true
       dispatch(setViewOnce(ViewOnceKey.NOTIFICATIONS_PROMPT))
 
       // if user has not seen the prompt and has granted permissions
@@ -46,37 +52,43 @@ export const usePromptEnableNotificationsIfNeeded =
         authorizationStatus === AuthorizationStatus.PROVISIONAL
       ) {
         dispatch(turnOnAllNotifications())
+        showingAlertRef.current = false
         return
       }
 
-      await new Promise<void>(resolve => {
-        showAlert({
-          title: 'Enable push notifications',
-          description:
-            'Get notified about market updates, special offers, airdrops, balance changes and more',
-          buttons: [
-            {
-              text: 'Not now',
-              onPress: () => {
-                resolve()
-              }
-            },
-            {
-              text: 'Turn on',
-              onPress: async () => {
-                const { permission } =
-                  await NotificationsService.getAllPermissions(false)
-                if (permission !== 'authorized') {
-                  NotificationsService.openSystemSettings()
-                  return
+      try {
+        await new Promise<void>(resolve => {
+          showAlert({
+            title: 'Enable push notifications',
+            description:
+              'Get notified about market updates, special offers, airdrops, balance changes and more',
+            buttons: [
+              {
+                text: 'Not now',
+                onPress: () => {
+                  resolve()
                 }
-                dispatch(turnOnAllNotifications())
-                resolve()
+              },
+              {
+                text: 'Turn on',
+                onPress: async () => {
+                  const { permission } =
+                    await NotificationsService.getAllPermissions(false)
+                  if (permission !== 'authorized') {
+                    NotificationsService.openSystemSettings()
+                    resolve()
+                    return
+                  }
+                  dispatch(turnOnAllNotifications())
+                  resolve()
+                }
               }
-            }
-          ]
+            ]
+          })
         })
-      })
+      } finally {
+        showingAlertRef.current = false
+      }
     }, [
       dispatch,
       hasPromptedForNotifications,
