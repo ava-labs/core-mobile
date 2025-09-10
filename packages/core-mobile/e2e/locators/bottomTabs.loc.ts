bee31d5a9c0c8b27f7660c8e41b3cb4c2bfcef38
eunjisong <eunji.l.song@gmail.com>
2025-09-10 16:26:00 -0400

    testing

--- packages/core-mobile/e2e/locators/bottomTabs.loc.ts
+++ packages/core-mobile/e2e/locators/bottomTabs.loc.ts
@@ -1,11 +1,11 @@
 export default {
   watchlistIcon: 'watchlistSVG',
-  activityTab: 'Activity',
+  activityTab: 'Activity_tab',
   watchlistTab: 'navutils_normal_tab_button__watchlist',
   plusButton: 'add_svg',
-  portfolioTab: 'Portfolio',
-  trackTab: 'Track',
-  stakeTab: 'Stake',
-  browserTab: 'Browser',
+  portfolioTab: 'Portfolio_tab',
+  trackTab: 'Track_tab',
+  stakeTab: 'Stake_tab',
+  browserTab: 'Browser_tab',
   tabBar: 'Tab Bar'
 }
