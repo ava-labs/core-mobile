6c2da19cbd48eb3b4297d58a5131838174455e16
Eunji Song <32075932+eunjisong@users.noreply.github.com>
2025-09-19 00:31:12 -0400

    QA - should handle a new app update modal during the warmup  (#3228)

--- packages/core-mobile/e2e/helpers/warmup.ts
+++ packages/core-mobile/e2e/helpers/warmup.ts
@@ -6,20 +6,20 @@ import Action from './actions'
 import { Platform } from './constants'
 import loginRecoverWallet from './loginRecoverWallet'
 
-export const warmup = async (newInstance = false) => {
-  const initialArgs: DeviceLaunchAppConfig = {
-    permissions: { notifications: 'YES', camera: 'YES' },
-    launchArgs: {
-      detoxEnableSynchronization: false,
-      detoxURLBlacklistRegex: [
-        '.*cloudflare-ipfs.*',
-        '.*[ipfs.io/ipfs].*',
-        '.*[amazonaws.com].*',
-        '*facebook.react.*'
-      ]
-    }
+export const initialArgs: DeviceLaunchAppConfig = {
+  permissions: { notifications: 'YES', camera: 'YES' },
+  launchArgs: {
+    detoxEnableSynchronization: false,
+    detoxURLBlacklistRegex: [
+      '.*cloudflare-ipfs.*',
+      '.*[ipfs.io/ipfs].*',
+      '.*[amazonaws.com].*',
+      '*facebook.react.*'
+    ]
   }
+}
 
+export const warmup = async (newInstance = false) => {
   if (newInstance) {
     console.log('CI is true, setting newInstance to true')
     initialArgs.newInstance = true
