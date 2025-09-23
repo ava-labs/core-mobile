5e0e8a960eebaa594966ee1fa5f9f8903429fcec
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2025-09-23 14:50:11 -0400

    CP-12048: handle error when fetching quote failed (#3226)

--- packages/core-mobile/app/new/features/meld/hooks/useSelectAmount.tsx
+++ packages/core-mobile/app/new/features/meld/hooks/useSelectAmount.tsx
@@ -13,6 +13,7 @@ import { useNavigation } from '@react-navigation/native'
 import { ACTIONS } from 'contexts/DeeplinkContext/types'
 import { useDebouncedCallback } from 'use-debounce'
 import { useMarketTokenBySymbol } from 'common/hooks/useMarketTokenBySymbol'
+import Logger from 'utils/Logger'
 import {
   PaymentMethodNames,
   ServiceProviderCategories,
@@ -273,6 +274,34 @@ export const useSelectAmount = ({
     setServiceProvider
   ])
 
+  const minMaxErrorMessage = useMemo(
+    () =>
+      (cryptoQuotesError?.statusCode ===
+        CreateCryptoQuoteErrorCode.NO_RESOURCE_FOUND &&
+        cryptoQuotesError.message?.toLowerCase().includes('not found')) ||
+      (cryptoQuotesError?.statusCode ===
+        CreateCryptoQuoteErrorCode.INCOMPATIBLE_REQUEST &&
+        cryptoQuotesError?.message
+          ?.toLowerCase()
+          .includes('not within the payment method limits')) ||
+      cryptoQuotesError?.message?.toLowerCase().includes('crypto_min_limit') ||
+      cryptoQuotesError?.message?.toLowerCase().includes('crypto_max_limit'),
+    [cryptoQuotesError]
+  )
+
+  useEffect(() => {
+    if (cryptoQuotesError === undefined) return
+
+    if (minMaxErrorMessage) {
+      Logger.error(
+        `The selected amount is not within the minimum and maximum limits for ${category}:`,
+        cryptoQuotesError
+      )
+      return
+    }
+    Logger.error(`failed to fetch quotes for ${category}:`, cryptoQuotesError)
+  }, [minMaxErrorMessage, category, cryptoQuotesError])
+
   const errorMessage = useMemo(() => {
     if (
       category === ServiceProviderCategories.CRYPTO_OFFRAMP &&
@@ -299,24 +328,15 @@ export const useSelectAmount = ({
         : `The maximum withdrawal token amount is ${formattedMaximumLimit} ${selectedCurrency}`
     }
 
-    if (
-      (cryptoQuotesError?.statusCode === CreateCryptoQuoteErrorCode.NOT_FOUND &&
-        cryptoQuotesError.message.toLowerCase().includes('not found')) ||
-      (cryptoQuotesError?.statusCode ===
-        CreateCryptoQuoteErrorCode.INCOMPATIBLE_REQUEST &&
-        cryptoQuotesError.message
-          .toLowerCase()
-          .includes('does not match service providers'))
-    ) {
-      return `${token?.tokenWithBalance.name} cannot be ${
-        category === ServiceProviderCategories.CRYPTO_ONRAMP
-          ? 'purchased'
-          : 'withdrawn'
-      } at the moment, please adjust the amount or try again later.`
+    if (minMaxErrorMessage) {
+      return 'Transaction amount is invalid: it must be between the minimum and maximum allowed.'
     }
 
-    if (cryptoQuotesError?.message) {
-      return cryptoQuotesError.message
+    if (cryptoQuotesError?.statusCode) {
+      return (
+        cryptoQuotesError.message ??
+        'We are unable to fetch the quotes, please check your input. Adjust the country, currency, token, or amount and try again.'
+      )
     }
 
     return undefined
@@ -328,10 +348,9 @@ export const useSelectAmount = ({
     sourceAmount,
     isBelowMaximumLimit,
     maximumLimit,
-    cryptoQuotesError?.statusCode,
-    cryptoQuotesError?.message,
+    minMaxErrorMessage,
+    cryptoQuotesError,
     token?.tokenWithBalance.symbol,
-    token?.tokenWithBalance.name,
     formatCurrency,
     selectedCurrency
   ])
--- packages/core-mobile/app/new/features/meld/utils.ts
+++ packages/core-mobile/app/new/features/meld/utils.ts
@@ -8,7 +8,8 @@ import {
   CreateCryptoQuoteNotFoundError,
   CreateCryptoQuoteError,
   CryptoCurrency,
-  CryptoQuotesError
+  CryptoQuotesError,
+  CreateCryptoQuoteErrorCode
 } from './types'
 
 export const isSupportedNativeErc20Token = (
@@ -100,6 +101,13 @@ export const getErrorMessage = (
       return {
         statusCode: response.data.code,
         message: response.data.message
+      }
+    }
+    if ('status' in response) {
+      return {
+        statusCode: response.status as CreateCryptoQuoteErrorCode,
+        message:
+          'statusText' in response ? (response.statusText as string) : undefined
       }
     }
   }
