ead45603fa137d1f103c47d95eb8f9390629b8d3
JungHwan Jang <onghwan@gmail.com>
2025-09-23 11:35:36 -0400

    chore: remove k2-mobile (#3240)

--- packages/k2-mobile/lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-const { ESLint } = require('eslint')
-const eslint = new ESLint()
-
-const {
-  generateLintStagedConfig
-} = require('../../scripts/generate-lint-staged-config.js')
-
-module.exports = generateLintStagedConfig(eslint)
--- packages/k2-mobile/src/theme/K2ThemeProvider.tsx
+++
@@ -1,7 +0,0 @@
-import React, { PropsWithChildren } from 'react'
-import { DripsyProvider } from 'dripsy'
-import { theme } from './theme'
-
-export const K2ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  return <DripsyProvider theme={theme}>{children}</DripsyProvider>
-}
