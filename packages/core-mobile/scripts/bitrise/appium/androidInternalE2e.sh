1cce2c8fde67214516397c30e160ebb6b07fd644
eunjisong <eunji.l.song@gmail.com>
2025-09-30 11:51:35 -0400

    use node 20.19

--- packages/core-mobile/scripts/bitrise/appium/androidInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/appium/androidInternalE2e.sh
@@ -4,10 +4,6 @@ set -ex
 echo "Built app at: $BITRISE_APK_PATH"
 ls -la "$BITRISE_APK_PATH" || true
 
-asdf plugin add nodejs || true
-asdf install nodejs 20.19.0 || true
-asdf global nodejs 20.19.0
-asdf reshim
 which node
 node -v
 yarn -v
