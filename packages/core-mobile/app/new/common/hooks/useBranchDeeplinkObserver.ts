fdcb58a0c71da29fe0d40d5f8997939e94772c66
ruijialin <ruijia.lin@avalabs.org>
2025-10-01 09:29:02 -0400

    setRequestMetadata

--- packages/core-mobile/app/new/common/hooks/useBranchDeeplinkObserver.ts
+++ packages/core-mobile/app/new/common/hooks/useBranchDeeplinkObserver.ts
@@ -2,6 +2,8 @@ import { useEffect } from 'react'
 import branch, { BranchParams } from 'react-native-branch'
 import { useDeeplink } from 'contexts/DeeplinkContext/DeeplinkContext'
 import { DeepLink, DeepLinkOrigin } from 'contexts/DeeplinkContext/types'
+import { useSelector } from 'react-redux'
+import { selectDistinctID } from 'store/posthog'
 import Logger from 'utils/Logger'
 
 function handleBranchDeeplink(
@@ -18,16 +20,24 @@ function handleBranchDeeplink(
 
 export function useBranchDeeplinkObserver(): void {
   const { setPendingDeepLink } = useDeeplink()
+  const distinctID = useSelector(selectDistinctID)
 
   useEffect(() => {
+    branch.setRequestMetadata('$posthog_distinct_id', distinctID)
+
     branch
       .getLatestReferringParams()
       .then((params?: BranchParams) =>
         handleBranchDeeplink(setPendingDeepLink, params)
       )
       .catch(Logger.error)
-    branch.subscribe(({ params }) =>
+
+    const unsubscribe = branch.subscribe(({ params }) =>
       handleBranchDeeplink(setPendingDeepLink, params)
     )
-  }, [setPendingDeepLink])
+
+    return () => {
+      unsubscribe()
+    }
+  }, [distinctID, setPendingDeepLink])
 }
