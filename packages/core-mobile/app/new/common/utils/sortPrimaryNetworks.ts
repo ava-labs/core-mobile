e4914c8f38d22846aeca9d6ce7d339cb6e9d5c8a
James Boyer <james.boyer@avalabs.org>
2025-10-10 19:31:28 -0400

    CP-12247: Move X chain up on networks list and Default enable (#3276)

--- packages/core-mobile/app/new/common/utils/sortPrimaryNetworks.ts
+++ packages/core-mobile/app/new/common/utils/sortPrimaryNetworks.ts
@@ -10,13 +10,13 @@ export function sortPrimaryNetworks(a: Network, b: Network): number {
   if (isAvalancheCChainId(b.chainId)) return 1
   if (isPChain(a.chainId)) return -1
   if (isPChain(b.chainId)) return 1
+  if (isXChain(a.chainId)) return -1
+  if (isXChain(b.chainId)) return 1
   if (isBitcoinChainId(a.chainId)) return -1
   if (isBitcoinChainId(b.chainId)) return 1
   if (isEthereumChainId(a.chainId)) return -1
   if (isEthereumChainId(b.chainId)) return 1
   if (isSolanaChainId(a.chainId)) return -1
   if (isSolanaChainId(b.chainId)) return 1
-  if (isXChain(a.chainId)) return -1
-  if (isXChain(b.chainId)) return 1
   return 0
 }
