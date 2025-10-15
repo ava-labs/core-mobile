d685af42ace633265d764d979e28df8a825f4d49
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-10-16 00:08:48 +0300

    revert

--- packages/core-mobile/app/new/common/utils/dismissKeyboardIfNeeded.tsx
+++ packages/core-mobile/app/new/common/utils/dismissKeyboardIfNeeded.tsx
@@ -1,9 +1,9 @@
 import { Keyboard, Platform } from 'react-native'
 
 export const dismissKeyboardIfNeeded = (): void => {
-  // native screens need to dismiss the keyboard before navigating
+  // (Android) native screens need to dismiss the keyboard before navigating
   // the footer is outside of the scrollview that controls keyboardShouldPersistTaps
-  // so we need to dismiss it manually
+  // so on Android we need to dismiss it manually
   if (Platform.OS === 'android' && Keyboard.isVisible()) {
     Keyboard.dismiss()
   }
