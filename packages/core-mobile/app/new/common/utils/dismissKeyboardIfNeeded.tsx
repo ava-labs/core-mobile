0b49f25021a50a940ee706e51d75b25b96c9887c
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-10-16 00:52:56 +0300

    CP-12390 - iOS Swap - keyboard does not dismiss quickly  (#3296)

--- packages/core-mobile/app/new/common/utils/dismissKeyboardIfNeeded.tsx
+++ packages/core-mobile/app/new/common/utils/dismissKeyboardIfNeeded.tsx
@@ -1,10 +1,10 @@
-import { Keyboard, Platform } from 'react-native'
+import { Keyboard } from 'react-native'
 
 export const dismissKeyboardIfNeeded = (): void => {
-  // (Android) native screens need to dismiss the keyboard before navigating
+  // native screens need to dismiss the keyboard before navigating
   // the footer is outside of the scrollview that controls keyboardShouldPersistTaps
-  // so on Android we need to dismiss it manually
-  if (Platform.OS === 'android' && Keyboard.isVisible()) {
+  // we need to dismiss it manually
+  if (Keyboard.isVisible()) {
     Keyboard.dismiss()
   }
 }
