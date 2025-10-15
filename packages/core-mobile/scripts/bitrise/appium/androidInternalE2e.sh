6c1a1db3c0fd5eddc01525e0f1d45344e6b74a95
eunjisong <eunji.l.song@gmail.com>
2025-10-15 14:44:55 -0400

    testing

--- packages/core-mobile/scripts/bitrise/appium/androidInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/appium/androidInternalE2e.sh
@@ -4,6 +4,7 @@ set -ex
 echo "Built app at: $BITRISE_APK_PATH"
 ls -la "$BITRISE_APK_PATH" || true
 
+adb shell pm grant com.avaxwallet.internal android.permission.POST_NOTIFICATIONS
 
 echo "Setting animation scale to 0..."
 adb shell settings put global window_animation_scale 0
