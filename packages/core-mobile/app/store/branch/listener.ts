b173097dc704cce17edd9497da70d1cae76e5dfc
ruijialin <ruijia.lin@avalabs.org>
2025-11-19 17:04:39 -0500

    get latest referring params for custom event

--- packages/core-mobile/app/store/branch/listener.ts
+++ packages/core-mobile/app/store/branch/listener.ts
@@ -25,13 +25,47 @@ export const addBranchListeners = (startListening: AppStartListening): void => {
     createBranchOpenedEvent(distinctId)
   }
 
+  // eslint-disable-next-line sonarjs/cognitive-complexity
   const createBranchOpenedEvent = async (distinctId: string): Promise<void> => {
+    const latestReferringParams = await Branch.getLatestReferringParams()
+
+    console.log('------latestReferringParams', latestReferringParams)
     const branchUniversalObject = await Branch.createBranchUniversalObject(
       'app_opened',
       {}
     )
+
     const params = {
-      customData: { posthog_distinct_id: distinctId }
+      customData: {
+        posthog_distinct_id: distinctId,
+        utm_source: latestReferringParams['utm_source']?.toString() ?? '',
+        utm_campaign: latestReferringParams['utm_campaign']?.toString() ?? '',
+        utm_medium: latestReferringParams['utm_medium']?.toString() ?? '',
+        gclid: latestReferringParams['gclid']?.toString() ?? '',
+        channel: latestReferringParams['~channel']?.toString() ?? '',
+        feature: latestReferringParams['~feature']?.toString() ?? '',
+        tags: latestReferringParams['~tags']?.toString() ?? '',
+        campaign: latestReferringParams['~campaign']?.toString() ?? '',
+        stage: latestReferringParams['~stage']?.toString() ?? '',
+        creation_source:
+          latestReferringParams['~creation_source']?.toString() ?? '',
+        referring_link:
+          latestReferringParams['~referring_link']?.toString() ?? '',
+        id: latestReferringParams['~id']?.toString() ?? '',
+        match_guaranteed:
+          latestReferringParams['+match_guaranteed']?.toString() ?? '',
+        referrer: latestReferringParams['+referrer']?.toString() ?? '',
+        phone_number: latestReferringParams['+phone_number']?.toString() ?? '',
+        is_first_session:
+          latestReferringParams['+is_first_session']?.toString() ?? '',
+        clicked_branch_link:
+          latestReferringParams['+clicked_branch_link']?.toString() ?? '',
+        click_timestamp:
+          latestReferringParams['+click_timestamp']?.toString() ?? '',
+        url: latestReferringParams['+url']?.toString() ?? '',
+        rn_cached_initial_event:
+          latestReferringParams['+rn_cached_initial_event']?.toString() ?? ''
+      }
     }
 
     const event = new BranchEvent('app_opened', [branchUniversalObject], params)
