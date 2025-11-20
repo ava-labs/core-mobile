d93091ee9dd0ed6d68b72ca8eb4c2a720a736bd4
ruijialin <ruijia.lin@avalabs.org>
2025-11-20 08:54:26 -0500

    extract attribution params to custom event

--- packages/core-mobile/app/store/branch/listener.ts
+++ packages/core-mobile/app/store/branch/listener.ts
@@ -2,7 +2,7 @@ import { Action } from '@reduxjs/toolkit'
 import { onForeground, onLogOut, onRehydrationComplete } from 'store/app'
 import { selectDistinctID } from 'store/posthog'
 import { AppListenerEffectAPI, AppStartListening } from 'store/types'
-import Branch, { BranchEvent } from 'react-native-branch'
+import Branch, { BranchEvent, BranchParams } from 'react-native-branch'
 import Logger from 'utils/Logger'
 
 export const addBranchListeners = (startListening: AppStartListening): void => {
@@ -12,7 +12,6 @@ export const addBranchListeners = (startListening: AppStartListening): void => {
   ): Promise<void> => {
     const distinctId = selectDistinctID(listenerApi.getState())
     Branch.setIdentity(distinctId)
-    Branch.setRequestMetadata('$posthog_distinct_id', distinctId)
     createBranchOpenedEvent(distinctId)
   }
 
@@ -21,12 +20,12 @@ export const addBranchListeners = (startListening: AppStartListening): void => {
     listenerApi: AppListenerEffectAPI
   ): Promise<void> => {
     const distinctId = selectDistinctID(listenerApi.getState())
-    Branch.setRequestMetadata('$posthog_distinct_id', distinctId)
     createBranchOpenedEvent(distinctId)
   }
 
   // eslint-disable-next-line sonarjs/cognitive-complexity
   const createBranchOpenedEvent = async (distinctId: string): Promise<void> => {
+    Branch.setRequestMetadata('posthog_distinct_id', distinctId)
     const latestReferringParams = await Branch.getLatestReferringParams()
 
     const branchUniversalObject = await Branch.createBranchUniversalObject(
@@ -34,40 +33,14 @@ export const addBranchListeners = (startListening: AppStartListening): void => {
       {}
     )
 
-    const params = {
-      customData: {
-        posthog_distinct_id: distinctId,
-        utm_source: latestReferringParams['utm_source']?.toString() ?? '',
-        utm_campaign: latestReferringParams['utm_campaign']?.toString() ?? '',
-        utm_medium: latestReferringParams['utm_medium']?.toString() ?? '',
-        gclid: latestReferringParams['gclid']?.toString() ?? '',
-        channel: latestReferringParams['~channel']?.toString() ?? '',
-        feature: latestReferringParams['~feature']?.toString() ?? '',
-        tags: latestReferringParams['~tags']?.toString() ?? '',
-        campaign: latestReferringParams['~campaign']?.toString() ?? '',
-        stage: latestReferringParams['~stage']?.toString() ?? '',
-        creation_source:
-          latestReferringParams['~creation_source']?.toString() ?? '',
-        referring_link:
-          latestReferringParams['~referring_link']?.toString() ?? '',
-        id: latestReferringParams['~id']?.toString() ?? '',
-        match_guaranteed:
-          latestReferringParams['+match_guaranteed']?.toString() ?? '',
-        referrer: latestReferringParams['+referrer']?.toString() ?? '',
-        phone_number: latestReferringParams['+phone_number']?.toString() ?? '',
-        is_first_session:
-          latestReferringParams['+is_first_session']?.toString() ?? '',
-        clicked_branch_link:
-          latestReferringParams['+clicked_branch_link']?.toString() ?? '',
-        click_timestamp:
-          latestReferringParams['+click_timestamp']?.toString() ?? '',
-        url: latestReferringParams['+url']?.toString() ?? '',
-        rn_cached_initial_event:
-          latestReferringParams['+rn_cached_initial_event']?.toString() ?? ''
-      }
-    }
+    const customData = constructBranchCustomEventParams(
+      distinctId,
+      latestReferringParams
+    )
 
-    const event = new BranchEvent('app_opened', [branchUniversalObject], params)
+    const event = new BranchEvent('app_opened', [branchUniversalObject], {
+      customData
+    })
     event
       .logEvent()
       .then(() => {
@@ -76,6 +49,69 @@ export const addBranchListeners = (startListening: AppStartListening): void => {
       .catch(error => {
         Logger.error('branch custom event [app_opened] logging failed', error)
       })
+  }
+
+  const constructBranchCustomEventParams = (
+    distinctId: string,
+    params: BranchParams
+    // eslint-disable-next-line sonarjs/cognitive-complexity
+  ): Record<string, string> | undefined => {
+    if (params['+clicked_branch_link'] === true) {
+      return {
+        posthog_distinct_id: distinctId,
+        utm_source: params['utm_source']?.toString() ?? '',
+        utm_campaign: params['utm_campaign']?.toString() ?? '',
+        utm_medium: params['utm_medium']?.toString() ?? '',
+        gclid: params['gclid']?.toString() ?? '',
+        channel: params['~channel']?.toString() ?? '',
+        feature: params['~feature']?.toString() ?? '',
+        tags: params['~tags']?.toString() ?? '',
+        campaign: params['~campaign']?.toString() ?? '',
+        stage: params['~stage']?.toString() ?? '',
+        creation_source: params['~creation_source']?.toString() ?? '',
+        referring_link: params['~referring_link']?.toString() ?? '',
+        id: params['~id']?.toString() ?? '',
+        match_guaranteed: params['+match_guaranteed']?.toString() ?? '',
+        referrer: params['+referrer']?.toString() ?? '',
+        phone_number: params['+phone_number']?.toString() ?? '',
+        is_first_session: params['+is_first_session']?.toString() ?? '',
+        clicked_branch_link: params['+clicked_branch_link']?.toString() ?? '',
+        click_timestamp: params['+click_timestamp']?.toString() ?? '',
+        url: params['+url']?.toString() ?? '',
+        rn_cached_initial_event:
+          params['+rn_cached_initial_event']?.toString() ?? ''
+      }
+    }
+
+    const url = params['+non_branch_link']
+      ? new URL(params['+non_branch_link'] as string)
+      : undefined
+    if (url) {
+      return {
+        posthog_distinct_id: distinctId,
+        utm_source: url.searchParams.get('utm_source') ?? '',
+        utm_campaign: url.searchParams.get('utm_campaign') ?? '',
+        utm_medium: url.searchParams.get('utm_medium') ?? '',
+        gclid: url.searchParams.get('gclid') ?? '',
+        channel: url.searchParams.get('channel') ?? '',
+        feature: url.searchParams.get('feature') ?? '',
+        tags: url.searchParams.get('tags') ?? '',
+        campaign: url.searchParams.get('campaign') ?? '',
+        stage: url.searchParams.get('stage') ?? '',
+        creation_source: url.searchParams.get('creation_source') ?? '',
+        referring_link: url.searchParams.get('referring_link') ?? '',
+        id: url.searchParams.get('id') ?? '',
+        match_guaranteed: url.searchParams.get('match_guaranteed') ?? '',
+        referrer: url.searchParams.get('referrer') ?? '',
+        phone_number: url.searchParams.get('phone_number') ?? '',
+        is_first_session: url.searchParams.get('is_first_session') ?? '',
+        clicked_branch_link: url.searchParams.get('clicked_branch_link') ?? '',
+        click_timestamp: url.searchParams.get('click_timestamp') ?? '',
+        url: url.searchParams.get('url') ?? '',
+        rn_cached_initial_event:
+          url.searchParams.get('rn_cached_initial_event') ?? ''
+      }
+    }
   }
 
   const branchLogout = (_: Action, __: AppListenerEffectAPI): void => {
