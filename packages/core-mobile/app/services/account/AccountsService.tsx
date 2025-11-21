d97394bcd0f1e5eec633e3459f3ae0351bca6386
James Boyer <james.boyer@avalabs.org>
2025-11-20 21:07:12 -0500

    reverted un needed changes

--- packages/core-mobile/app/services/account/AccountsService.tsx
+++ packages/core-mobile/app/services/account/AccountsService.tsx
@@ -107,7 +107,6 @@ class AccountsService {
     walletId: string
     name: string
   }): Promise<Account> {
-
     if (walletType === WalletType.UNSET) throw new Error('invalid wallet type')
 
     if (walletType === WalletType.SEEDLESS) {
@@ -219,20 +218,17 @@ class AccountsService {
     accountIndex?: number
     isTestnet: boolean
   }): Promise<Record<NetworkVMType, string>> {
-
     // all vm modules need is just the isTestnet flag
     const network = {
       isTestnet
     } as Network
 
-    const addresses = await ModuleManager.deriveAddresses({
+    return await ModuleManager.deriveAddresses({
       walletId,
       walletType,
       accountIndex,
       network
     })
-
-    return addresses
   }
 
   async getAccountName({
