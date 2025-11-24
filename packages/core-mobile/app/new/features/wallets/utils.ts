34053691b15b8836791d92de0869d2b59c7c4032
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-11-24 19:30:22 +0100

    small refactor

--- packages/core-mobile/app/new/features/wallets/utils.ts
+++ packages/core-mobile/app/new/features/wallets/utils.ts
@@ -1,6 +1,4 @@
-import { CoreAccountType } from '@avalabs/types'
 import { Account } from 'store/account'
-import { IMPORTED_ACCOUNTS_VIRTUAL_WALLET_ID } from './consts'
 
 export function getIsActiveWallet(
   id: string,
@@ -9,9 +7,5 @@ export function getIsActiveWallet(
   if (!activeAccount) {
     return false
   }
-  return (
-    id === activeAccount.walletId ||
-    (id === IMPORTED_ACCOUNTS_VIRTUAL_WALLET_ID &&
-      activeAccount.type === CoreAccountType.IMPORTED)
-  )
+  return id === activeAccount.walletId
 }
