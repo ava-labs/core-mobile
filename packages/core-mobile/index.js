64ff068168b16232bb02cc9d1b5a3c25d901751c
An Nguyen <an.nguyen@avalabs.org>
2025-11-28 11:33:41 -0600

    add simple in-memory caching for debank requests

--- packages/core-mobile/index.js
+++ packages/core-mobile/index.js
@@ -14,6 +14,7 @@ import SentryService from 'services/sentry/SentryService'
 import NewApp from 'new/ContextApp'
 import { expo } from './app.json'
 import { server } from './tests/msw/native/server'
+import { setupDeBankCaching } from './app/utils/setupDeBankCaching'
 
 if (__DEV__) {
   require('./ReactotronConfig')
@@ -23,6 +24,8 @@ if (__DEV__) {
   // eslint-disable-next-line no-console
   console.reportErrorsAsExceptions = false
 }
+
+setupDeBankCaching()
 
 SentryService.init()
 
