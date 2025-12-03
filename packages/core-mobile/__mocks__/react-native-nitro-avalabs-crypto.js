11baa962cab250078a4f4998e932619a37d7cbd7
Ilia Gutu <ilia.gutu@gmail.com>
2025-12-03 13:07:15 +0100

    fix: Throw an error when using nitro module in mock

--- packages/core-mobile/__mocks__/react-native-nitro-avalabs-crypto.js
+++ packages/core-mobile/__mocks__/react-native-nitro-avalabs-crypto.js
@@ -1,8 +1,13 @@
+const getPublicKey = (d, _isCompressed = true) => {
+  throw new Error('Not implemented in mock implementation')
+}
+
 module.exports = {
   secp256k1: {
     sign: jest.fn(),
     verify: jest.fn()
   },
   sha256: jest.fn(),
-  randomBytes: jest.fn()
+  randomBytes: jest.fn(),
+  getPublicKey: getPublicKey
 }
