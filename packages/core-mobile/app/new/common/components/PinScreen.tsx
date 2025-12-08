5c3303a770f0e0890e0767ad8ab3854e91dcb79f
Alexandru Nicolae <alexandru.nicolae@avalabs.org>
2025-12-08 22:32:14 +0200

    remove nested requestAnimationFrate and setTimeout

--- packages/core-mobile/app/new/common/components/PinScreen.tsx
+++ packages/core-mobile/app/new/common/components/PinScreen.tsx
@@ -218,12 +218,10 @@ export const PinScreen = ({
     } else {
       InteractionManager.runAfterInteractions(() => {
         if (Platform.OS === 'android') {
+          // On Android, use requestAnimationFrame to ensure UI is fully rendered
+          // and keyboard system is ready before focusing
           requestAnimationFrame(() => {
-            requestAnimationFrame(() => {
-              setTimeout(() => {
-                focusPinInput()
-              }, 0)
-            })
+            focusPinInput()
           })
         } else {
           focusPinInput()
