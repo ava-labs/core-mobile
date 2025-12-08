dad268b21c1fa4f714fd84a6f110bfa4712d916e
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-12-09 00:51:48 +0200

    created ListViewItem

---
+++ packages/core-mobile/app/new/common/components/ListViewItem.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import {
+  AvatarType,
+  View,
+  TouchableOpacity,
+  useTheme,
+  Avatar,
+  Text,
+  Icons,
+  TextVariant
+} from '@avalabs/k2-alpine'
+import { TextProps } from 'react-native-svg'
+
+export const ListViewItem = ({
+  onPress,
+  image,
+  title,
+  subtitle,
+  isLast,
+  hideArrow,
+  titleProps,
+  subtitleProps
+}: {
+  onPress: () => void
+  image?: AvatarType['source']
+  title: string
+  subtitle?: string
+  isLast: boolean
+  hideArrow?: boolean
+  titleProps?: TextProps & { variant?: TextVariant }
+  subtitleProps?: TextProps & { variant?: TextVariant }
+}): JSX.Element => {
+  const {
+    theme: { colors }
+  } = useTheme()
+
+  return (
+    <TouchableOpacity
+      sx={{
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row'
+      }}
+      onPress={onPress}>
+      <View
+        sx={{
+          flex: 1,
+          height: 58,
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingLeft: 16
+        }}>
+        <View
+          sx={{
+            width: 40,
+            height: 40
+          }}>
+          <Avatar
+            backgroundColor="transparent"
+            size={40}
+            // @ts-ignore
+            source={image?.source}
+            hasLoading={false}
+          />
+        </View>
+        <View
+          sx={{
+            flex: 1,
+            height: 60,
+            marginLeft: 12,
+            paddingRight: 16,
+            flexDirection: 'row',
+            alignItems: 'center',
+            borderBottomWidth: 1,
+            borderColor: isLast ? 'transparent' : '$borderPrimary'
+          }}>
+          <View
+            sx={{
+              flex: 1,
+              justifyContent: 'center'
+            }}>
+            <Text
+              {...titleProps}
+              variant="buttonMedium"
+              numberOfLines={1}
+              sx={{
+                // this is needed for emojis to be displayed correctly
+                lineHeight: 20
+              }}>
+              {title}
+            </Text>
+            {subtitle && (
+              <Text
+                {...subtitleProps}
+                sx={{
+                  fontSize: 13,
+                  color: '$textSecondary'
+                }}
+                ellipsizeMode="tail"
+                numberOfLines={1}>
+                {subtitle}
+              </Text>
+            )}
+          </View>
+
+          {hideArrow ? null : (
+            <Icons.Navigation.ChevronRight
+              width={20}
+              height={20}
+              color={colors.$textSecondary}
+            />
+          )}
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
--- packages/core-mobile/app/new/features/send/components/RecentContacts.tsx
+++ packages/core-mobile/app/new/features/send/components/RecentContacts.tsx
@@ -1,27 +1,26 @@
 import { truncateAddress } from '@avalabs/core-utils-sdk'
 import {
-  Avatar,
   Icons,
   Image,
   SearchBar,
-  Separator,
   Text,
   TouchableOpacity,
   View,
   showAlert,
   useTheme
 } from '@avalabs/k2-alpine'
+import { NetworkVMType } from '@avalabs/vm-module-types'
 import { ErrorState } from 'common/components/ErrorState'
 import { ListScreen } from 'common/components/ListScreen'
+import { ListViewItem } from 'common/components/ListViewItem'
+import { TRUNCATE_ADDRESS_LENGTH } from 'common/consts/text'
 import { loadAvatar } from 'common/utils/loadAvatar'
+import { useGlobalSearchParams } from 'expo-router'
 import { isValidAddress } from 'features/accountSettings/utils/isValidAddress'
 import React, { useCallback, useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Contact } from 'store/addressBook'
 import { selectIsDeveloperMode } from 'store/settings/advanced'
-import { TRUNCATE_ADDRESS_LENGTH } from 'common/consts/text'
-import { useGlobalSearchParams } from 'expo-router'
-import { NetworkVMType } from '@avalabs/vm-module-types'
 import EMPTY_ADDRESS_BOOK_ICON from '../../../assets/icons/address_book_empty.png'
 import { getAddressByVmName } from '../utils/getAddressByVmName'
 
@@ -90,101 +89,33 @@ export const RecentContacts = ({
         vmName,
         isDeveloperMode
       })
-      const { name } = item
-      const isLastItem = index === searchResults.length - 1
+      const isLast = index === searchResults.length - 1
 
       const avatar = loadAvatar(item.avatar)
 
       return (
-        <View>
-          <TouchableOpacity
-            sx={{
-              marginTop: 12,
-              marginHorizontal: 16,
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              flexDirection: 'row'
-            }}
-            onPress={() => onSelectContact(item)}>
-            <View
-              sx={{
-                flex: 1,
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginBottom: 12
-              }}>
-              <View
-                sx={{
-                  width: 40,
-                  height: 40
-                }}>
-                <Avatar
-                  backgroundColor="transparent"
-                  size={40}
-                  source={avatar?.source}
-                  hasLoading={false}
-                />
-              </View>
-              <View
-                sx={{
-                  flexGrow: 1,
-                  marginHorizontal: 12,
-                  flexDirection: 'row'
-                }}>
-                <View
-                  sx={{
-                    justifyItems: 'center',
-                    justifyContent: 'center',
-                    width: '90%'
-                  }}>
-                  <Text
-                    testID={`recent_contacts__${name}`}
-                    accessibilityLabel={`recent_contacts__${name}`}
-                    variant="buttonMedium"
-                    numberOfLines={1}
-                    sx={{
-                      flex: 1,
-                      // this is needed for emojis to be displayed correctly
-                      lineHeight: 20
-                    }}>
-                    {name}
-                  </Text>
-                  {address && (
-                    <Text
-                      variant="mono"
-                      sx={{
-                        fontSize: 13,
-                        color: '$textSecondary'
-                      }}
-                      ellipsizeMode="tail"
-                      numberOfLines={1}>
-                      {truncateAddress(address, TRUNCATE_ADDRESS_LENGTH)}
-                    </Text>
-                  )}
-                </View>
-              </View>
-            </View>
-            <Icons.Navigation.ChevronRight
-              width={20}
-              height={20}
-              color={colors.$textSecondary}
-            />
-          </TouchableOpacity>
-          {!isLastItem && (
-            <View sx={{ marginLeft: 62 }}>
-              <Separator />
-            </View>
-          )}
-        </View>
+        <ListViewItem
+          image={avatar?.source}
+          title={item.name}
+          subtitle={
+            address
+              ? truncateAddress(address, TRUNCATE_ADDRESS_LENGTH)
+              : undefined
+          }
+          titleProps={{
+            testID: `recent_contacts__${item.name}`,
+            accessibilityLabel: `recent_contacts__${item.name}`
+          }}
+          subtitleProps={{
+            variant: 'mono'
+          }}
+          isLast={isLast}
+          onPress={() => onSelectContact(item)}
+          hideArrow
+        />
       )
     },
-    [
-      vmName,
-      isDeveloperMode,
-      searchResults.length,
-      colors.$textSecondary,
-      onSelectContact
-    ]
+    [vmName, isDeveloperMode, searchResults.length, onSelectContact]
   )
 
   const renderHeader = useCallback(() => {
