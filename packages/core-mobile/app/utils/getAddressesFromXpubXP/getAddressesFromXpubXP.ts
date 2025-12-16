c653365c4d7c7c7b60e44ee6759060c91f786eb2
Junghwan <junghwan.jang@avalabs.org>
2025-12-15 20:43:01 -0500

    fix

--- packages/core-mobile/app/utils/getAddressesFromXpubXP/getAddressesFromXpubXP.ts
+++ packages/core-mobile/app/utils/getAddressesFromXpubXP/getAddressesFromXpubXP.ts
@@ -196,7 +196,6 @@ const toAddressIndexArray = (
       address,
       index: dictionary[address]?.index ?? 0
     }))
-    .sort((a, b) => a.index - b.index)
 }
 
 export const getXpubXPIfAvailable = async ({
