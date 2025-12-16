bd2f3a6805a57bf6eb8bf292025493a1ad575430
ruijialin <ruijia.lin@avalabs.org>
2025-12-16 10:25:22 -0500

    update useHasXpAddresses hook

--- packages/core-mobile/app/new/common/hooks/useHasXpAddresses.ts
+++ packages/core-mobile/app/new/common/hooks/useHasXpAddresses.ts
@@ -3,5 +3,10 @@ import { selectActiveAccount } from 'store/account'
 
 export const useHasXpAddresses = (): boolean => {
   const activeAccount = useSelector(selectActiveAccount)
-  return !!activeAccount?.addressPVM && !!activeAccount?.addressAVM
+  return (
+    !!activeAccount?.addressPVM &&
+    activeAccount?.addressPVM.length > 0 &&
+    !!activeAccount?.addressAVM &&
+    activeAccount?.addressAVM.length > 0
+  )
 }
