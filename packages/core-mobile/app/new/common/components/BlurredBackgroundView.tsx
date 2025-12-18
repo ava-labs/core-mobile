128bcedffae4c444a6d666ae0b56e05c42046927
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2025-12-18 23:49:45 +0200

    fix bg

--- packages/core-mobile/app/new/common/components/BlurredBackgroundView.tsx
+++ packages/core-mobile/app/new/common/components/BlurredBackgroundView.tsx
@@ -13,7 +13,8 @@ const BlurredBackgroundView = ({
   hasGrabber = false,
   separator,
   shouldDelayBlurOniOS = false,
-  backgroundColor
+  backgroundColor,
+  hasAnimation = false
 }: {
   hasGrabber?: boolean
   separator?: {
@@ -22,12 +23,14 @@ const BlurredBackgroundView = ({
   }
   shouldDelayBlurOniOS?: boolean
   backgroundColor?: string
+  hasAnimation?: boolean
 }): JSX.Element => {
   const animatedBorderStyle = useAnimatedStyle(() => ({
     opacity: separator?.opacity.value
   }))
   const blurViewStyle = useAnimatedStyle(() => ({
-    opacity: Platform.OS === 'ios' ? separator?.opacity.value : 1
+    opacity:
+      Platform.OS === 'ios' && hasAnimation ? separator?.opacity.value : 1
   }))
   const insets = useSafeAreaInsets()
 
