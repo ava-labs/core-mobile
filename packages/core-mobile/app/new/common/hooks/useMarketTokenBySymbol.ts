c0f37ce01070303365cf225e8c950bb11d6559fa
JungHwan Jang <onghwan@gmail.com>
2025-12-19 12:23:16 -0500

    chore: Remove unnecessary logs (#3452)

--- packages/core-mobile/app/new/common/hooks/useMarketTokenBySymbol.ts
+++ packages/core-mobile/app/new/common/hooks/useMarketTokenBySymbol.ts
@@ -1,32 +1,17 @@
 import { useWatchlist } from 'hooks/watchlist/useWatchlist'
 import { useMemo } from 'react'
 import { MarketToken } from 'store/watchlist'
-import Logger from 'utils/Logger'
 
 export const useMarketTokenBySymbol = ({
-  symbol,
-  errorContext
+  symbol
 }: {
   symbol: string | undefined
-  errorContext?: string
 }): MarketToken | undefined => {
   const { getMarketTokenBySymbol } = useWatchlist()
 
   return useMemo(() => {
     if (!symbol) return undefined
 
-    const tokenBySymbol = getMarketTokenBySymbol(symbol)
-    if (errorContext && !tokenBySymbol) {
-      Logger.error(`[${errorContext}] Market token not found`, { symbol })
-    }
-    if (errorContext && tokenBySymbol?.priceChangePercentage24h === undefined) {
-      Logger.error(
-        `[${errorContext}] Market token priceChangePercentage24h is undefined`,
-        {
-          symbol
-        }
-      )
-    }
-    return tokenBySymbol
-  }, [symbol, getMarketTokenBySymbol, errorContext])
+    return getMarketTokenBySymbol(symbol)
+  }, [symbol, getMarketTokenBySymbol])
 }
