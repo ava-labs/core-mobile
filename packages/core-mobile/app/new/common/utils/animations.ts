9956e01774cf8848e5515ca9622406262ce92618
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2026-01-15 11:13:26 +0200

    small cleanup

--- packages/core-mobile/app/new/common/utils/animations.ts
+++ packages/core-mobile/app/new/common/utils/animations.ts
@@ -1,16 +1,4 @@
-import { SPRING_LINEAR_TRANSITION } from '@avalabs/k2-alpine'
-import React, { useLayoutEffect } from 'react'
-import { StyleProp, ViewStyle } from 'react-native'
-import Animated, {
-  Easing,
-  FadeInRight,
-  FadeInUp,
-  LinearTransition,
-  useAnimatedStyle,
-  useSharedValue,
-  withDelay,
-  withTiming
-} from 'react-native-reanimated'
+import { Easing, FadeInRight, FadeInUp } from 'react-native-reanimated'
 
 export const getItemEnteringAnimation = (index: number): FadeInRight =>
   FadeInRight.duration(100)
@@ -23,38 +11,3 @@ export const getListItemEnteringAnimation = (index: number): FadeInUp =>
     .delay(index * 50)
     .easing(Easing.bezierFn(0.25, 1, 0.5, 1))
     .springify()
-
-export const AnimatedFadeInUp = ({
-  index = 0,
-  style,
-  children,
-  layout,
-  ...props
-}: {
-  index?: number
-  style?: StyleProp<ViewStyle>
-  layout?: LinearTransition
-  children: React.ReactNode
-}): JSX.Element => {
-  const opacity = useSharedValue(0)
-  const translateY = useSharedValue(20)
-
-  useLayoutEffect(() => {
-    opacity.value = withDelay(index * 50, withTiming(1, { duration: 300 }))
-    translateY.value = withDelay(index * 50, withTiming(0, { duration: 300 }))
-  }, [index, opacity, translateY])
-
-  const animatedStyle = useAnimatedStyle(() => ({
-    opacity: opacity.value,
-    transform: [{ translateY: translateY.value }]
-  }))
-
-  return (
-    <Animated.View
-      style={[style, animatedStyle]}
-      layout={layout ?? SPRING_LINEAR_TRANSITION}
-      {...props}>
-      {children}
-    </Animated.View>
-  )
-}
