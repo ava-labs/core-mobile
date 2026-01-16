cb2b4eddb0906af217181de47bd134f6a9f085ec
eunjisong <eunji.l.song@gmail.com>
2026-01-16 14:35:41 -0500

    logging screen

--- packages/core-mobile/scripts/bitrise/appium/androidInternalE2e.sh
+++ packages/core-mobile/scripts/bitrise/appium/androidInternalE2e.sh
@@ -8,6 +8,11 @@ echo "Installing APK..."
 adb install -r -d "$BITRISE_APK_PATH"
 adb shell monkey -p com.avaxwallet.internal -c android.intent.category.LAUNCHER 1
 
+sleep 5
+
+adb shell uiautomator dump /sdcard/view.xml
+adb shell cat /sdcard/view.xml | head -50
+
 adb shell settings put global window_animation_scale 0
 adb shell settings put global transition_animation_scale 0
 adb shell settings put global animator_duration_scale 0
