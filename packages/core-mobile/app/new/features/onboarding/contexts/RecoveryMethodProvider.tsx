e787f10d1596f5e9d2a9441a1c18f0fc4626604d
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2026-02-03 15:46:56 +0100

    CP-13261 - Should not display the nest egg onboarding modal for non-new seedless wallet  (#3536)

--- packages/core-mobile/app/new/features/onboarding/contexts/RecoveryMethodProvider.tsx
+++ packages/core-mobile/app/new/features/onboarding/contexts/RecoveryMethodProvider.tsx
@@ -76,6 +76,7 @@ export const RecoveryMethodProvider = ({
     setTotpChallenge(undefined)
     setOidcAuth(undefined)
     setMfaMethods(undefined)
+    setIsNewSeedlessUser(false)
   }
 
   const state: RecoveryMethodContextState = {
