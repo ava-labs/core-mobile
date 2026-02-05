e8bc762a11c796a0a755eb54c6ac8736169c8086
Junghwan <junghwan.jang@avalabs.org>
2026-02-05 09:08:11 -0500

    refactor: inline enrichAaveMarketData into hook

--- packages/core-mobile/app/new/features/defiMarket/hooks/aave/useAaveAvailableMarkets.ts
+++ packages/core-mobile/app/new/features/defiMarket/hooks/aave/useAaveAvailableMarkets.ts
@@ -1,11 +1,11 @@
 import { QueryObserverResult, skipToken, useQuery } from '@tanstack/react-query'
-import { PublicClient } from 'viem'
+import { erc20Abi, Address, PublicClient } from 'viem'
 import { readContract } from 'viem/actions'
 import { useSelector } from 'react-redux'
 import { selectActiveAccount } from 'store/account'
 import { Network } from '@avalabs/core-chains-sdk'
 import { ReactQueryKeys } from 'consts/reactQueryKeys'
-import { type DefiMarket } from '../../types'
+import { type DefiMarket, MarketNames } from '../../types'
 import {
   AAVE_POOL_ADDRESSES_PROVIDER_C_CHAIN_ADDRESS,
   AAVE_UI_POOL_DATA_PROVIDER_C_CHAIN_ADDRESS
@@ -13,7 +13,19 @@ import {
 import { AAVE_POOL_DATA_PROVIDER } from '../../abis/aavePoolDataProvider'
 import { aaveInsertAvax } from '../../utils/aaveInsertAvax'
 import { getAaveFilteredMarketData } from '../../utils/getAaveFilteredMarketData'
-import { enrichAaveMarketData } from '../../utils/enrichAaveMarketData'
+import {
+  formatAaveInterest,
+  formatAaveSupplyApy,
+  formatAmount
+} from '../../utils/formatInterest'
+import { getAaveDepositedBalance } from '../../utils/getAaveDepositedBalance'
+import { getUniqueMarketId } from '../../utils/getUniqueMarketId'
+import { bigIntToBig } from '../../utils/bigInt'
+import {
+  fetchAaveApyHistory,
+  parseAaveApyHistory
+} from '../../utils/fetchAaveApyHistory'
+import { getMeritAprBonus } from '../../utils/getMeritAprBonus'
 import { useGetCChainToken } from '../useGetCChainToken'
 import { useMeritAprs } from './useMeritAprs'
 
@@ -55,16 +67,82 @@ export const useAaveAvailableMarkets = ({
 
             // Enrich each market with additional data
             const results = await Promise.all(
-              filteredMarkets.map(market =>
-                enrichAaveMarketData({
-                  market,
-                  network,
-                  networkClient,
-                  addressEVM: addressEVM as string,
-                  meritAprs,
-                  getCChainToken
+              filteredMarkets.map(async market => {
+                const decimals = bigIntToBig(market.decimals).toNumber()
+
+                // Calculate current APYs
+                const supplyApyPercent = formatAaveSupplyApy(
+                  formatAaveInterest(bigIntToBig(market.liquidityRate))
+                )
+                const borrowApyPercent = formatAaveSupplyApy(
+                  formatAaveInterest(bigIntToBig(market.variableBorrowRate))
+                )
+
+                // Fetch total supply and historical APY data in parallel
+                const [totalSupply, apyHistory] = await Promise.all([
+                  readContract(networkClient, {
+                    address: market.mintTokenAddress,
+                    abi: erc20Abi,
+                    functionName: 'totalSupply'
+                  }),
+                  fetchAaveApyHistory(network, market.underlyingAsset)
+                ])
+
+                const formattedTotalDeposits = formatAmount(
+                  bigIntToBig(totalSupply),
+                  decimals
+                )
+                const supplyCapReached = formattedTotalDeposits.gte(
+                  bigIntToBig(market.supplyCap)
+                )
+
+                const { historicalApyPercent, historicalBorrowApyPercent } =
+                  parseAaveApyHistory(apyHistory.supply, apyHistory.borrow)
+
+                const meritAprBonus = getMeritAprBonus(market.symbol, meritAprs)
+                const token = getCChainToken(
+                  market.symbol,
+                  market.underlyingAsset
+                )
+
+                const depositedBalanceResult = await getAaveDepositedBalance({
+                  cChainClient: networkClient,
+                  walletAddress: addressEVM as Address,
+                  underlyingTokenDecimals: decimals,
+                  underlyingAssetAddress: market.underlyingAsset
                 })
-              )
+
+                const marketData = {
+                  marketName: MarketNames.aave,
+                  network,
+                  type: 'lending' as const,
+                  supplyCapReached,
+                  totalDeposits: formatAmount(
+                    bigIntToBig(totalSupply),
+                    decimals
+                  ),
+                  asset: {
+                    mintTokenAddress: market.mintTokenAddress,
+                    assetName: market.name,
+                    decimals,
+                    iconUrl: token?.logoUri,
+                    symbol: market.symbol,
+                    contractAddress: market.underlyingAsset,
+                    mintTokenBalance: depositedBalanceResult
+                  },
+                  supplyApyPercent: supplyApyPercent + meritAprBonus,
+                  historicalApyPercent,
+                  borrowApyPercent,
+                  historicalBorrowApyPercent,
+                  borrowingEnabled: market.borrowingEnabled,
+                  canBeUsedAsCollateral: market.usageAsCollateralEnabled
+                }
+
+                return {
+                  ...marketData,
+                  uniqueMarketId: getUniqueMarketId(marketData)
+                }
+              })
             )
 
             return aaveInsertAvax(results)
