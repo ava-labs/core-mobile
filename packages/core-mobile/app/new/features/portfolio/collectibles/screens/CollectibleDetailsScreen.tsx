c42febf79498fa9e57f8ae6ab3e9963210d4c23f
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2026-02-05 20:53:05 +0200

    fix android navigation bar contrast

--- packages/core-mobile/app/new/features/portfolio/collectibles/screens/CollectibleDetailsScreen.tsx
+++ packages/core-mobile/app/new/features/portfolio/collectibles/screens/CollectibleDetailsScreen.tsx
@@ -364,7 +364,6 @@ export const CollectibleDetailsScreen = ({
               </Animated.View>
             </Animated.View>
           </Animated.View>
-
           <Animated.View
             style={[
               {
