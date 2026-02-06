50c5a23429d11cd7efa018c27d47579c3a4f7816
ruijialin <ruijia.lin@avalabs.org>
2026-02-06 16:08:58 -0500

    add sol minimum amount check

--- packages/core-mobile/app/new/features/send/components/SendSVM.tsx
+++ packages/core-mobile/app/new/features/send/components/SendSVM.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useSVMSend from 'common/hooks/send/useSVMSend'
 import { Network } from '@avalabs/core-chains-sdk'
-import { TokenWithBalanceSVM } from '@avalabs/vm-module-types'
+import { TokenType, TokenWithBalanceSVM } from '@avalabs/vm-module-types'
 import { Account } from 'store/account'
+import { Address, createSolanaRpc, address as toAddress } from '@solana/kit'
+import NetworkService from 'services/network/NetworkService'
+import { TokenUnit } from '@avalabs/core-utils-sdk/dist'
 import { useSendContext } from '../context/sendContext'
 import { useSendSelectedToken } from '../store'
 import { SendToken } from './SendToken'
+
+const RENT_EXEMPT_CACHE = new Map<bigint, bigint>()
+const ACCOUNT_SPACE_CACHE = new Map<Address, bigint>()
 
 export const SendSVM = ({
   nativeToken,
@@ -20,8 +26,9 @@ export const SendSVM = ({
   onSuccess: (txHash: string) => void
   onFailure: (txError: unknown) => void
 }): JSX.Element => {
-  const { maxFee } = useSendContext()
+  const { maxFee, recipient } = useSendContext()
   const [selectedToken] = useSendSelectedToken()
+  const [minimumSendAmount, setMinimumSendAmount] = useState<TokenUnit>()
 
   const { send } = useSVMSend({
     fromAddress: account.addressSVM,
@@ -44,5 +51,82 @@ export const SendSVM = ({
     }
   }
 
-  return <SendToken onSend={handleSend} />
+  useEffect(() => {
+    const fetchMinimumSendAmount = async (): Promise<void> => {
+      if (selectedToken?.type !== TokenType.NATIVE) {
+        return
+      }
+
+      const provider = NetworkService.getSolanaProvider(network)
+
+      if (!recipient?.addressSVM) {
+        return
+      }
+
+      const accountSpace = await getAccountOccupiedSpace(
+        toAddress(recipient.addressSVM),
+        provider
+      )
+
+      if (accountSpace !== 0n) return
+
+      const minimum = await getRentExemptMinimum(0n, provider)
+      setMinimumSendAmount(
+        new TokenUnit(minimum, nativeToken.decimals, nativeToken.symbol)
+      )
+    }
+    fetchMinimumSendAmount()
+  }, [network, recipient?.addressSVM, nativeToken, selectedToken?.type])
+
+  return <SendToken onSend={handleSend} minimumSendAmount={minimumSendAmount} />
+}
+
+const getAccountOccupiedSpace = async (
+  address: Address,
+  provider: ReturnType<typeof createSolanaRpc>
+): Promise<bigint> => {
+  if (ACCOUNT_SPACE_CACHE.has(address)) {
+    const cachedSpace = ACCOUNT_SPACE_CACHE.get(address)
+    if (cachedSpace !== undefined) {
+      return cachedSpace
+    }
+  }
+
+  try {
+    const accountInfo = await provider.getAccountInfo(address).send()
+    const space = accountInfo.value?.space ?? 0n
+    ACCOUNT_SPACE_CACHE.set(address, space)
+
+    return space
+  } catch (e) {
+    throw new Error(
+      `Failed to fetch account info for ${address.toString()}: ${e}`
+    )
+  }
+}
+
+const getRentExemptMinimum = async (
+  space: bigint,
+  provider: ReturnType<typeof createSolanaRpc>
+): Promise<bigint> => {
+  if (RENT_EXEMPT_CACHE.has(space)) {
+    const cachedRentExempt = RENT_EXEMPT_CACHE.get(space)
+    if (cachedRentExempt !== undefined) {
+      return cachedRentExempt
+    }
+  }
+
+  try {
+    const rentExemptMinimum = await provider
+      .getMinimumBalanceForRentExemption(0n)
+      .send()
+
+    RENT_EXEMPT_CACHE.set(0n, rentExemptMinimum)
+
+    return rentExemptMinimum
+  } catch (e) {
+    throw new Error(
+      `Failed to fetch rent-exempt minimum for space ${space}: ${e}`
+    )
+  }
 }
