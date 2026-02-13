4a019e73685681bb460bb895d29687f92f0bad5f
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2026-02-13 20:30:33 +0000

    revert

--- packages/core-mobile/android/app/src/main/java/com/avaxwallet/MainApplication.kt
+++ packages/core-mobile/android/app/src/main/java/com/avaxwallet/MainApplication.kt
@@ -28,7 +28,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              add(MainPackage())
+              // add(MainPackage())
             }
 
             override fun getJSMainModuleName(): String = "index"
