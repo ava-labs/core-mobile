0b5d2b0bfd02bb3416276448ccaa473704037ed8
An Nguyen <an.nguyen@avalabs.org>
2026-03-09 11:41:33 -0500

    add feature flags

--- packages/core-mobile/app/store/posthog/slice.ts
+++ packages/core-mobile/app/store/posthog/slice.ts
@@ -338,6 +338,32 @@ export const selectMarkrSwapGasBuffer = (state: RootState): number => {
   return parseFloat(featureFlags[FeatureVars.MARKR_SWAP_GAS_BUFFER] as string)
 }
 
+export const selectFusionFeeUnitsMarginBps = (state: RootState): number => {
+  const { featureFlags } = state.posthog
+  return parseInt(
+    featureFlags[FeatureVars.FUSION_FEE_UNITS_MARGIN_BPS] as string
+  )
+}
+
+export const selectFusionFeeSafetyBps = (state: RootState): number => {
+  const { featureFlags } = state.posthog
+  return parseInt(featureFlags[FeatureVars.FUSION_FEE_SAFETY_BPS] as string)
+}
+
+export const selectFusionBridgeFeeSafetyBps = (state: RootState): number => {
+  const { featureFlags } = state.posthog
+  return parseInt(
+    featureFlags[FeatureVars.FUSION_BRIDGE_FEE_SAFETY_BPS] as string
+  )
+}
+
+export const selectFusionTransferGasMarginBps = (state: RootState): number => {
+  const { featureFlags } = state.posthog
+  return parseInt(
+    featureFlags[FeatureVars.FUSION_TRANSFER_GAS_MARGIN_BPS] as string
+  )
+}
+
 export const selectMarkrSwapMaxRetries = (state: RootState): number => {
   const { featureFlags } = state.posthog
   return parseInt(featureFlags[FeatureVars.MARKR_SWAP_MAX_RETRIES] as string)
