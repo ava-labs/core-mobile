53e41abc1478477f298ed711c8a40dbceb68b31a
ruijialin <ruijia.lin@avalabs.org>
2026-03-20 13:06:27 -0400

    show swap onboarding on initial login

--- packages/core-mobile/app/new/features/swap/hooks/useNavigateToSwap.ts
+++ packages/core-mobile/app/new/features/swap/hooks/useNavigateToSwap.ts
@@ -3,6 +3,7 @@ import { caip2ChainIds } from 'consts/caip2ChainIds'
 import { tokenIds } from 'consts/tokenIds'
 import { useSelector } from 'react-redux'
 import { selectIsDeveloperMode } from 'store/settings/advanced'
+import { selectHasBeenViewedOnce, ViewOnceKey } from 'store/viewOnce'
 
 interface NavigateToSwapParams {
   fromTokenId?: string // internalId or raw contract address
@@ -15,6 +16,9 @@ export const useNavigateToSwap = (): {
   navigateToSwap: (params?: NavigateToSwapParams) => void
 } => {
   const isDeveloperMode = useSelector(selectIsDeveloperMode)
+  const shouldHideOnboarding = useSelector(
+    selectHasBeenViewedOnce(ViewOnceKey.SWAP_ONBOARDING)
+  )
   const { navigate } = useRouter()
 
   const navigateToSwap = ({
@@ -23,10 +27,11 @@ export const useNavigateToSwap = (): {
     fromCaip2Id,
     toCaip2Id
   }: NavigateToSwapParams = {}): void => {
-    if (fromTokenId === undefined && toTokenId === undefined) {
-      navigate({
-        pathname: '/swap/swap',
-        params: {
+    const isDefaultSwapPair =
+      fromTokenId === undefined && toTokenId === undefined
+
+    const params = isDefaultSwapPair
+      ? {
           initialTokenIdFrom: tokenIds.AVAX,
           initialTokenIdTo: tokenIds.USDC,
           initialFromCaip2Id: isDeveloperMode
@@ -36,23 +41,28 @@ export const useNavigateToSwap = (): {
             ? caip2ChainIds.FUJI
             : caip2ChainIds.C_CHAIN
         }
-      })
+      : {
+          initialTokenIdFrom: fromTokenId,
+          initialTokenIdTo: toTokenId,
+          initialFromCaip2Id:
+            fromCaip2Id ??
+            (isDeveloperMode ? caip2ChainIds.FUJI : caip2ChainIds.C_CHAIN),
+          initialToCaip2Id:
+            toCaip2Id ??
+            (isDeveloperMode ? caip2ChainIds.FUJI : caip2ChainIds.C_CHAIN)
+        }
 
+    if (!shouldHideOnboarding) {
+      navigate({
+        pathname: '/swap/onboarding',
+        params
+      })
       return
     }
 
     navigate({
       pathname: '/swap/swap',
-      params: {
-        initialTokenIdFrom: fromTokenId,
-        initialTokenIdTo: toTokenId,
-        initialFromCaip2Id:
-          fromCaip2Id ??
-          (isDeveloperMode ? caip2ChainIds.FUJI : caip2ChainIds.C_CHAIN),
-        initialToCaip2Id:
-          toCaip2Id ??
-          (isDeveloperMode ? caip2ChainIds.FUJI : caip2ChainIds.C_CHAIN)
-      }
+      params
     })
   }
 
