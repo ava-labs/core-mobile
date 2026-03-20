885387862ddaa4581937246c06fc50307d0bc29d
Ray Lin <137183702+ruijialin-avalabs@users.noreply.github.com>
2026-03-20 14:11:25 -0400

    CP-13679: Show swap onboarding on initial login (#3667)

--- packages/core-mobile/app/new/features/swap/screens/SwapOnboardingScreen.tsx
+++ packages/core-mobile/app/new/features/swap/screens/SwapOnboardingScreen.tsx
@@ -2,15 +2,39 @@ import React, { useCallback } from 'react'
 import { TransactionOnboarding } from 'common/components/TransactionOnboarding'
 import { Icons } from '@avalabs/k2-alpine'
 import { ViewOnceKey } from 'store/viewOnce'
-import { useRouter } from 'expo-router'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 
 export const SwapOnboardingScreen = (): JSX.Element => {
   const { navigate } = useRouter()
+  const {
+    initialTokenIdFrom,
+    initialTokenIdTo,
+    initialFromCaip2Id,
+    initialToCaip2Id
+  } = useLocalSearchParams<{
+    initialTokenIdFrom: string
+    initialTokenIdTo: string
+    initialFromCaip2Id: string
+    initialToCaip2Id: string
+  }>()
 
   const handlePressNext = useCallback(() => {
-    navigate('/swap/swap')
-  }, [navigate])
-
+    navigate({
+      pathname: '/swap/swap',
+      params: {
+        initialTokenIdFrom,
+        initialTokenIdTo,
+        initialFromCaip2Id,
+        initialToCaip2Id
+      }
+    })
+  }, [
+    initialFromCaip2Id,
+    initialToCaip2Id,
+    initialTokenIdFrom,
+    initialTokenIdTo,
+    navigate
+  ])
   return (
     <TransactionOnboarding
       icon={{ component: Icons.Custom.Compare, size: 75 }}
