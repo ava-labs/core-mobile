e606cec9e301c5ca2186be12d3375c78e837c2c1
ruijialin <ruijia.lin@avalabs.org>
2026-03-23 14:56:10 -0400

    pr comment

--- packages/core-mobile/app/new/features/swap/hooks/useNavigateToSwap.ts
+++ packages/core-mobile/app/new/features/swap/hooks/useNavigateToSwap.ts
@@ -1,8 +1,4 @@
 import { useRouter } from 'expo-router'
-import { caip2ChainIds } from 'consts/caip2ChainIds'
-import { tokenIds } from 'consts/tokenIds'
-import { useSelector } from 'react-redux'
-import { selectIsDeveloperMode } from 'store/settings/advanced'
 
 interface NavigateToSwapParams {
   fromTokenId?: string // internalId or raw contract address
@@ -14,7 +10,6 @@ interface NavigateToSwapParams {
 export const useNavigateToSwap = (): {
   navigateToSwap: (params?: NavigateToSwapParams) => void
 } => {
-  const isDeveloperMode = useSelector(selectIsDeveloperMode)
   const { navigate } = useRouter()
 
   const navigateToSwap = ({
@@ -23,33 +18,14 @@ export const useNavigateToSwap = (): {
     fromCaip2Id,
     toCaip2Id
   }: NavigateToSwapParams = {}): void => {
-    const isDefaultSwapPair =
-      fromTokenId === undefined && toTokenId === undefined
-
-    const params = isDefaultSwapPair
-      ? {
-          initialTokenIdFrom: tokenIds.AVAX,
-          initialTokenIdTo: tokenIds.USDC,
-          initialFromCaip2Id: isDeveloperMode
-            ? caip2ChainIds.FUJI
-            : caip2ChainIds.C_CHAIN,
-          initialToCaip2Id: isDeveloperMode
-            ? caip2ChainIds.FUJI
-            : caip2ChainIds.C_CHAIN
-        }
-      : {
-          initialTokenIdFrom: fromTokenId,
-          initialTokenIdTo: toTokenId,
-          initialFromCaip2Id:
-            fromCaip2Id ??
-            (isDeveloperMode ? caip2ChainIds.FUJI : caip2ChainIds.C_CHAIN),
-          initialToCaip2Id:
-            toCaip2Id ??
-            (isDeveloperMode ? caip2ChainIds.FUJI : caip2ChainIds.C_CHAIN)
-        }
-
+    const swapParams = {
+      initialTokenIdFrom: fromTokenId,
+      initialTokenIdTo: toTokenId,
+      initialFromCaip2Id: fromCaip2Id,
+      initialToCaip2Id: toCaip2Id
+    }
     // @ts-ignore navigate to modal root so _layout.tsx decides between onboarding/swap
-    navigate({ pathname: '/swap', params })
+    navigate({ pathname: '/swap', params: swapParams })
   }
 
   return { navigateToSwap }
