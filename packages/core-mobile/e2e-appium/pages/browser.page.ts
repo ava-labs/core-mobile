07ff5e0a4540c7803e0652ca077e4207acd681ae
eunjisong <eunji.l.song@gmail.com>
2026-04-01 16:09:49 -0400

    injected provider test

--- packages/core-mobile/e2e-appium/pages/browser.page.ts
+++ packages/core-mobile/e2e-appium/pages/browser.page.ts
@@ -2,23 +2,66 @@ import assert from 'assert'
 import { actions } from '../helpers/actions'
 import { selectors } from '../helpers/selectors'
 import browserLoc from '../locators/browser.loc'
+import txPage from './transactions.page'
 import commonElsPage from './commonEls.page'
 
 class BrowserPage {
-  get topBrowserBar() {
-    return selectors.getById(browserLoc.topBrowserBar)
-  }
-
-  get close() {
-    return selectors.getById(browserLoc.close)
+  get myWebview() {
+    return selectors.getById(browserLoc.myWebview)
   }
 
   get androidChromePager() {
     return selectors.getById(browserLoc.androidChromePager)
   }
 
-  get browserUrl() {
-    return selectors.getById(browserLoc.browserUrl)
+  get close() {
+    return selectors.getById(browserLoc.close)
+  }
+
+  get searchBar() {
+    return selectors.getById(browserLoc.searchBar)
+  }
+
+  get blackholeAmountInput() {
+    return selectors.getByXpath(browserLoc.blackholeAmountInput)
+  }
+
+  get blackholeSwapTitle() {
+    return selectors.getById(browserLoc.blackholeSwapTitle)
+  }
+
+  get blackholeSwapButton() {
+    return selectors.getById(browserLoc.blackholeSwapButton)
+  }
+
+  async goToUrl(url: string) {
+    await actions.waitFor(this.searchBar, 30000)
+    await actions.tap(this.searchBar)
+    await actions.type(this.searchBar, url)
+    await actions.tapEnterOnKeyboard('Go')
+    await actions.delay(4000)
+  }
+
+  async swapBlackhole() {
+    await actions.pasteText(this.blackholeAmountInput, '0.001')
+    await actions.swipe('up', 0.5, this.blackholeSwapTitle)
+    await actions.tap(this.blackholeSwapButton)
+    await actions.waitFor(txPage.approveTitle, 40000)
+    await txPage.tapApprove()
+    await actions.delay(4000)
+  }
+
+  async verifyUrl(expected: string) {
+    await actions.waitFor(this.searchBar)
+    const uiUrl = await actions.getText(this.searchBar)
+    assert.equal(uiUrl, expected, `"${uiUrl}" !== "${expected}"`)
+    console.log('URL text: ', uiUrl)
+  }
+
+  async verifyInjectedDapp() {
+    await actions.waitFor(this.myWebview, 30000)
+    await actions.waitFor(selectors.getBySomeText('0x'))
+    await actions.isNotVisible(selectors.getByText('Connect'))
   }
 
   async tapClose() {
@@ -28,13 +71,6 @@ class BrowserPage {
       await actions.waitFor(this.androidChromePager)
       await commonElsPage.goAndroidBack()
     }
-  }
-
-  async verifyUrl(url: string) {
-    await actions.waitFor(this.browserUrl)
-    const urlText = await actions.getText(this.browserUrl)
-    assert.equal(urlText, url, `${urlText} not equal to ${url}`)
-    console.log('URL text: ', urlText)
   }
 }
 
