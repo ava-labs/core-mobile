33e54d5e410cb80f55492c04d36629fe948e31c4
eunjisong <eunji.l.song@gmail.com>
2026-04-24 13:15:21 -0400

    testing

--- packages/core-mobile/e2e-appium/wdio.conf.ts
+++ packages/core-mobile/e2e-appium/wdio.conf.ts
@@ -170,7 +170,7 @@ export const config: WebdriverIO.Config = {
   logLevel: 'error',
   bail: 0,
   waitforTimeout: 20000,
-  specFileRetries: process.env.CI === 'true' ? 1 : 0,
+  specFileRetries: 0,
   connectionRetryTimeout: 120000,
   connectionRetryCount: 2,
   framework: 'mocha',
