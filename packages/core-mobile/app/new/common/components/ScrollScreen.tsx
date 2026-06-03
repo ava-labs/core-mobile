3195f3377de23fbae483c405f38c88cd43dc1b4b
Bogdan Dobritoiu <bogdan.dobri@gmail.com>
2026-06-03 12:27:38 +0300

    revert hideHeaderBackground

--- packages/core-mobile/app/new/common/components/ScrollScreen.tsx
+++ packages/core-mobile/app/new/common/components/ScrollScreen.tsx
@@ -105,7 +105,7 @@ export const ScrollScreen = ({
   shouldAvoidKeyboard,
   disableStickyFooter,
   showNavigationHeaderTitle = true,
-  hideHeaderBackground = true,
+  hideHeaderBackground,
   headerCenterOverlay,
   headerStyle,
   testID = 'bottom_sheet',
@@ -385,6 +385,8 @@ export const ScrollScreen = ({
   }, [insets.top, isModal])
 
   const renderHeaderBackground = useCallback(() => {
+    if (hideHeaderBackground) return null
+
     return (
       <View
         pointerEvents="none"
