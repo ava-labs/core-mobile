7e01358cb60c71fcd6faf2e1b27be5bc035a2ad1
Junghwan Jang <junghwan.jang@avalabs.org>
2026-06-17 16:32:00 -0400

    fix: migrate android react host to expo 56

--- packages/core-mobile/android/app/src/main/java/com/avaxwallet/MainApplication.kt
+++ packages/core-mobile/android/app/src/main/java/com/avaxwallet/MainApplication.kt
@@ -2,7 +2,7 @@ package com.avaxwallet;
 
 import android.content.res.Configuration
 import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
+import expo.modules.ExpoReactHostFactory
 import android.app.Application
 import android.database.CursorWindow
 import android.webkit.WebSettings
@@ -12,10 +12,6 @@ import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.modules.network.OkHttpClientProvider
 import com.gantix.JailMonkey.Rooted.RootedCheck
 import java.lang.reflect.Field
@@ -23,23 +19,15 @@ import io.branch.rnbranch.*
 
 class MainApplication : Application(), ReactApplication {
 
-    override val reactNativeHost: ReactNativeHost =
-        ReactNativeHostWrapper( this, object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
+    override val reactHost: ReactHost by lazy {
+        ExpoReactHostFactory.getDefaultReactHost(
+            context = applicationContext,
+            packageList = PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MainPackage())
             }
-
-            override fun getJSMainModuleName(): String = "index"
-
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        })
-
-    override val reactHost: ReactHost
-        get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
+        )
+    }
 
     override fun onCreate() {
         super.onCreate()
