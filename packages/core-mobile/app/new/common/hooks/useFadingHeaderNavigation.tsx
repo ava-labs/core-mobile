fd0a98d09975d9f110155b133cc3b5d6aa2738aa
Junghwan Jang <junghwan.jang@avalabs.org>
2026-06-23 00:01:59 -0400

    docs: remove stale navigation import comment

--- packages/core-mobile/app/new/common/hooks/useFadingHeaderNavigation.tsx
+++ packages/core-mobile/app/new/common/hooks/useFadingHeaderNavigation.tsx
@@ -15,12 +15,6 @@ import Animated, {
   useAnimatedStyle,
   useSharedValue
 } from 'react-native-reanimated'
-/**
- * Temporarily import "useNavigation" from @react-navigation/native.
- * This is a workaround due to a render bug in the expo-router version.
- * See: https://github.com/expo/expo/issues/35383
- * TODO: Adjust import back to expo-router once the bug is resolved.
- */
 import {
   NativeStackNavigationOptions,
   useFocusEffect,
