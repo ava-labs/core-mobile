fbe4c705e49b8825c7dcb2a304d546c60bbdd05e
JungHwan Jang <onghwan@gmail.com>
2026-07-10 10:03:06 -0400

    Remove text-encoding polyfill and bump react-native-qrcode-svg to 6.3.21 (#3969)

--- packages/core-mobile/polyfills/text_encoding.js
+++
@@ -1,6 +0,0 @@
-// inspired by https://github.com/expo/browser-polyfill
-// and https://github.com/acostalima/react-native-polyfill-globals
-import { TextDecoder, TextEncoder } from 'text-encoding'
-
-global.TextDecoder = global.TextDecoder || TextDecoder
-global.TextEncoder = global.TextEncoder || TextEncoder
